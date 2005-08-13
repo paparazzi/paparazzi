c2f2925f561216b7548c23bd1e6a9557d59ae6ac
Pascal Brisset <pascal.brisset@free.fr>
2005-08-13 13:46:27 +0000

    for JP !

---
+++ sw/ground_segment/tmtc/modem.mli
@@ -0,0 +1,54 @@
+(*
+ * $Id$
+ *
+ * Ground harware modem handling
+ *  
+ * Copyright (C) 2004 CENA/ENAC, Pascal Brisset, Antoine Drouin
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA. 
+ *
+ *)
+
+
+module Protocol :
+  sig
+    val stx : char
+    val etx : int
+    val index_start : string -> int
+    val payload_length : string -> int -> int
+    val length : string -> int -> int
+    val checksum : string -> bool
+  end
+
+val parse : string -> string option
+(* Returns None for modem specific messages (while updating status) *)
+
+type status = {
+  mutable last_message_date : float;
+  mutable valim : float;
+  mutable cd : int;
+  mutable error : int;
+  mutable debug : int;
+  mutable nb_byte : int;
+  mutable nb_msg : int;
+  mutable nb_err : int;
+}
+
+val status : status
+
+
