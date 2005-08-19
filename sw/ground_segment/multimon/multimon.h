c5acca85d791b7011134e25e851d7f3a15859d43
Pascal Brisset <pascal.brisset@free.fr>
2005-08-19 21:13:25 +0000

    unused demodulators

--- sw/ground_segment/multimon/multimon.h
+++ sw/ground_segment/multimon/multimon.h
@@ -177,10 +177,11 @@ extern const struct demod_param demod_zvei;
 
 extern const struct demod_param demod_scope;
 
-#define ALL_DEMOD &demod_poc5, &demod_poc12, &demod_poc24, \
-&demod_afsk1200, &demod_afsk2400, &demod_afsk2400_2, &demod_afsk4800p, \
-&demod_afsk4800, &demod_hapn4800, &demod_fsk9600, &demod_dtmf, \
-&demod_zvei, &demod_scope
+/* #define ALL_DEMOD &demod_poc5, &demod_poc12, &demod_poc24, \ */
+/* &demod_afsk1200, &demod_afsk2400, &demod_afsk2400_2, &demod_afsk4800p, \ */
+/* &demod_afsk4800, &demod_hapn4800, &demod_fsk9600, &demod_dtmf, \ */
+/* &demod_zvei, &demod_scope */
+#define ALL_DEMOD &demod_afsk4800p,&demod_scope
 
 /* ---------------------------------------------------------------------- */
 
