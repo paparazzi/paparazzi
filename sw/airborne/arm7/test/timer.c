46332c4b43bcb9e6d3e23facbac2aafd23060da7
Martin Mueller <martin@pfump.org>
2005-10-14 19:55:26 +0000

    yippieh!

--- sw/airborne/arm7/test/timer.c
+++ sw/airborne/arm7/test/timer.c
@@ -4,22 +4,68 @@
 #include "dev_board.h"
 
 void init_timer ( void );
-void tc0_cmp( void );
+void tc0_cmp( unsigned int isr_number );
 
 void delay_timer( uint32_t len );
 
 volatile uint32_t timeval;
 
-#define VIC_Channel_Timer0  4
-#define IRQ_MASK 0x00000080
+
+/* this belongs to interrupt.c */
+
+void (*ISR_Table[VIC_NUM_INTS]) (unsigned int);
+
+void init_isr(void);
+int register_isr(unsigned int ISR_number, void (*ISR_function)(unsigned int));
+void irq_dispatcher(unsigned int isr_number);
+void empty_isr(unsigned int isr_number);
+
+
+void init_isr(void)
+{
+    unsigned int i;
+
+    for (i = 0; i < VIC_NUM_INTS; i++)
+    {
+        register_isr(i, &empty_isr);
+    }
+}
+
+int register_isr(unsigned int ISR_number, void (*ISR_function)(unsigned int))
+{
+    ISR_Table[ISR_number] = ISR_function;
+    return (0);	
+}
+
+void irq_dispatcher(unsigned int isr_number)
+{
+    (*ISR_Table[isr_number])(isr_number);
+}
+
+void empty_isr(unsigned int isr_number)
+{
+}
+
+
+
 
 /* Timer0 Compare-Match Interrupt Handler (ISR) */
-void tc0_cmp( void ) {
+void tc0_cmp( unsigned int isr_number ) {
+
+  int i;
+  
+  /* we don't care now, but... */
+  isr_number = isr_number;
+  
+  for (i=0;i<100000;i++)
+  {
+    DACR = (i & 0x3FF) << 6;
+  }  
+
   timeval++;
+  
   // Clear interrupt flag by writing 1 to Bit 0
-  T0IR_bit.MR0I = 1; 
-  // Acknowledge Interrupt (rough?)
-  VICVectAddr = 0;
+  T0IR_bit.MR0I |= 1; 
 }
 
 
@@ -34,59 +80,62 @@ void delay_timer( uint32_t len ) {
   while (timeval < end);
 }
 
-static inline unsigned asm_get_cpsr(void) {
-  unsigned long retval;
-  asm volatile (" mrs  %0, cpsr" : "=r" (retval) : /* no inputs */  );
-  return retval;
-}
-
-static inline void asm_set_cpsr(unsigned val) {
-  asm volatile (" msr  cpsr, %0" : /* no outputs */ : "r" (val)  );
-}
-
-unsigned enableIRQ(void) {
-  unsigned _cpsr;
-
-  _cpsr = asm_get_cpsr();
-  asm_set_cpsr(_cpsr & ~IRQ_MASK);
-  return _cpsr;
-}
-
 void init_timer (void) {
+
+  // register routine
+  register_isr((1 << VIC_TIMER0), tc0_cmp);
+
+  // Compare-hit at 1 Sec (-1 reset "tick") (PCLK = CLK / 4)
+  T0MR0 = ((FOSC*PLL_MUL)/(4))-1; 
+  
+  // Interrupt and Reset on MR0
+  T0MCR_bit.MR0R |= 1;
+  T0MCR_bit.MR0I |= 1;
+
+  // Enable Timer0 Interrupt
+  VICIntEnable |= (1 << VIC_TIMER0);
+  
+  // Timer0 Enable
+  T0TCR_bit.CE |= 1;                  
+
+#if 0
+  // set interrupt vector in 0
+  VICVectAddr0 = (uint32_t)tc0_cmp;
+  VICDefVectAddr = (uint32_t)tc0_cmp;
+
   // Compare-hit at 10mSec (-1 reset "tick")
   T0MR0 = ((FOSC*PLL_MUL)/(1000/10))-1; 
   // Interrupt and Reset on MR0
   T0MCR_bit.MR0I = 1;
-  T0MCR_bit.MR0R = 1;
- // Timer0 Enable
-  T0TCR_bit.CE = 1;                  
-  // set interrupt vector in 0
-  VICVectAddr0 = (uint32_t)tc0_cmp;
+  T0MCR_bit.MR0R |= 1;
 
   VICVectCntl0_bit.IRSIA = VIC_Channel_Timer0; // use it for Timer 0 Interrupt:
-  VICVectCntl0_bit.IRQslot_en = 1;
-
+  VICVectCntl0_bit.IRQslot_en |= 1;
+  
   VICIntEnable = (1<<VIC_Channel_Timer0);      // Enable Timer0 Interrupt
+  // Timer0 Enable
+  T0TCR_bit.CE = 1;                  
+#endif 
 }
 
 
 int main ( int argc, char** argv) {
+
+  /* turn on DAC pins */
+  PINSEL1 &= 1 << 19;
+  PINSEL1 |= ~(1 << 18);
+
   init_timer ();
-
-  MAMCR = 2;    // MAM functions fully enabled
-
+      
   LED_INIT();
   YELLOW_LED_OFF();
   GREEN_LED_OFF();
-
-  MEMMAP = 1;
-  enableIRQ();
-
-  while (1) {
+  
+  while(1) {
     YELLOW_LED_ON();
-    delay_loop();
+    delay_timer(2);
     YELLOW_LED_OFF();
-    delay_loop();
+    delay_timer(2);
   }
   return 0;
 }
