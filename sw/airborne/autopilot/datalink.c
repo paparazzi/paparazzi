c0ac0172c5bd1f373223454475ec739f0301603c
Pascal Brisset <pascal.brisset@free.fr>
2005-11-05 18:09:18 +0000

    Handling of ground or other A/C messages

---
+++ sw/airborne/autopilot/datalink.c
@@ -0,0 +1,59 @@
+/*
+ * $Id$
+ *  
+ * Copyright (C) 2005  Pascal Brisset, Antoine Drouin
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA. 
+ *
+ */
+/** \file datalink.c
+ *  \brief Hadling of messages coming from ground and other A/Cs
+ *
+ */
+#define DATALINK_C
+
+#include <inttypes.h>
+#include "dl_protocol.h"
+#include "traffic_info.h"
+#include "nav.h"
+#include "datalink.h"
+
+#define MOfCm(_x) (((float)_x)/100.)
+
+#define MSG_SIZE 128
+char dl_buffer[MSG_SIZE];
+
+
+static uint8_t msg_id;
+
+void dl_parse_msg(void) {
+  if (msg_id == DL_ACINFO_ID) {
+    uint8_t id = DL_ACINFO_ac_id(dl_buffer);
+    float ux = MOfCm(DL_ACINFO_utm_east(dl_buffer));
+    float uy = MOfCm(DL_ACINFO_utm_north(dl_buffer));
+    float a = MOfCm(DL_ACINFO_alt(dl_buffer));
+    float c = RadOfDeg(((float)DL_ACINFO_course(dl_buffer))/ 10.);
+    SetAcInfo(id, ux, uy, c, a);
+  } else if (msg_id == DL_MOVE_WP_ID) {
+    uint8_t wp_id = DL_MOVE_WP_wp_id(dl_buffer);
+    float ux = MOfCm(DL_MOVE_WP_utm_east(dl_buffer));
+    float uy = MOfCm(DL_MOVE_WP_utm_north(dl_buffer));
+    float a = MOfCm(DL_MOVE_WP_alt(dl_buffer));
+    MoveWaypoint(wp_id, ux, uy, a);
+  }
+}
---
+++ sw/airborne/autopilot/datalink.h
@@ -0,0 +1,41 @@
+/*
+ * $Id$
+ *  
+ * Copyright (C) 2005  Pascal Brisset, Antoine Drouin
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA. 
+ *
+ */
+/** \file datalink.h
+ *  \brief Handling of messages coming from ground and other A/Cs
+ *
+ */
+
+#ifndef DATALINK_H
+#define DATALINK_H
+#ifdef DATALINK_C
+#define EXTERN
+#else
+#define EXTERN extern
+#endif
+
+EXTERN bool_t dl_msg_available;
+
+EXTERN void dl_parse_msg(void);
+/** Should be called when dl_msg_available is set */
+#endif
