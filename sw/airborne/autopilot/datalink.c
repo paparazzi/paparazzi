784aa3f015ebd718293b0960c7921258e9263e23
Pascal Brisset <pascal.brisset@free.fr>
2005-11-08 11:11:45 +0000

    ready to test !

--- sw/airborne/autopilot/datalink.c
+++ sw/airborne/autopilot/datalink.c
@@ -33,33 +33,41 @@
 #include "nav.h"
 #include "datalink.h"
 #include "flight_plan.h"
+#include "autopilot.h"
 
 #define MOfCm(_x) (((float)_x)/100.)
 
 
-/* void dl_parse_msg(void) { */
-/*   uint8_t msg_id = dl_buffer[0]; */
-/*   if (msg_id == DL_ACINFO_ID) { */
-/*     uint8_t id = DL_ACINFO_ac_id(dl_buffer); */
-/*     float ux = MOfCm(DL_ACINFO_utm_east(dl_buffer)); */
-/*     float uy = MOfCm(DL_ACINFO_utm_north(dl_buffer)); */
-/*     float a = MOfCm(DL_ACINFO_alt(dl_buffer)); */
-/*     float c = RadOfDeg(((float)DL_ACINFO_course(dl_buffer))/ 10.); */
-/*     float s = MOfCm(DL_ACINFO_speed(dl_buffer)); */
-/*     SetAcInfo(id, ux, uy, c, a, s); */
-/*   } else if (msg_id == DL_MOVE_WP_ID) { */
-/*     uint8_t wp_id = DL_MOVE_WP_wp_id(dl_buffer); */
-/*     float ux = MOfCm(DL_MOVE_WP_utm_east(dl_buffer)); */
-/*     float uy = MOfCm(DL_MOVE_WP_utm_north(dl_buffer)); */
-/*     float a = MOfCm(DL_MOVE_WP_alt(dl_buffer)); */
-/*     MoveWaypoint(wp_id, ux, uy, a); */
-/*   } */
-/* } */
-
-#include "uart.h"
 void dl_parse_msg(void) {
-  dl_buffer[6] = '\0';
-  uart0_print_string(dl_buffer);
-  uart0_transmit('\n');
+  uint8_t msg_id = dl_buffer[0];
+  if (msg_id == DL_ACINFO_ID) {
+    uint8_t id = DL_ACINFO_ac_id(dl_buffer);
+    float ux = MOfCm(DL_ACINFO_utm_east(dl_buffer));
+    float uy = MOfCm(DL_ACINFO_utm_north(dl_buffer));
+    float a = MOfCm(DL_ACINFO_alt(dl_buffer));
+    float c = RadOfDeg(((float)DL_ACINFO_course(dl_buffer))/ 10.);
+    float s = MOfCm(DL_ACINFO_speed(dl_buffer));
+    SetAcInfo(id, ux, uy, c, a, s);
+  } else if (msg_id == DL_MOVE_WP_ID) {
+    uint8_t wp_id = DL_MOVE_WP_wp_id(dl_buffer);
+    float ux = MOfCm(DL_MOVE_WP_utm_east(dl_buffer));
+    float uy = MOfCm(DL_MOVE_WP_utm_north(dl_buffer));
+    float a = MOfCm(DL_MOVE_WP_alt(dl_buffer));
+    MoveWaypoint(wp_id, ux, uy, a);
+  } else if (msg_id == DL_EVENT_ID) {
+    uint8_t event = DL_EVENT_event(dl_buffer);
+    switch (event) {
+    case 1 : rc_event_1 = TRUE; break; // FIXME !
+    case 2 : rc_event_2 = TRUE; break;
+    default: ;
+    }
+  }
 }
+
+/*  #include "uart.h" */
+/*  void dl_parse_msg(void) { */
+/*    dl_buffer[6] = '\0'; */
+/*    uart0_print_string(dl_buffer); */
+/*    uart0_transmit('\n'); */
+/*  } */
 
