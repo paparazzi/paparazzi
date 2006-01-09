a39399c23b8775557adb1bef3cfccdf44b3bebb5
Antoine Drouin <poinix@gmail.com>
2006-01-09 16:06:41 +0000

    *** empty log message ***

--- sw/airborne/control_2.h
+++ sw/airborne/control_2.h
@@ -7,13 +7,37 @@
 
 extern pprz_t control_commands[];
 
-static inline void control_process_radio_control( void ) {
+// twinjet1
+#ifdef 0
+static inline void control_process_radio_control_manual( void ) {
   control_commands[COMMAND_THROTTLE] = rc_values[RADIO_THROTTLE];
   control_commands[COMMAND_ROLL] = rc_values[RADIO_ROLL];
   control_commands[COMMAND_PITCH] = rc_values[RADIO_PITCH];
-  //  control_commands[COMMAND_YAW] = rc_values[RADIO_YAW];
+}
+#endif
+
+// twinstar_test
+#ifdef 0
+static inline void control_process_radio_control_manual( void ) {
+  control_commands[COMMAND_THROTTLE] = rc_values[RADIO_THROTTLE];
+  control_commands[COMMAND_ROLL] = rc_values[RADIO_ROLL];
+  control_commands[COMMAND_PITCH] = rc_values[RADIO_PITCH];
+  control_commands[COMMAND_YAW] = rc_values[RADIO_YAW];
+}
+#endif
+
+// gorazoptere_test
+static inline void control_process_radio_control_manual( void ) {
+  control_commands[COMMAND_THROTTLE] = rc_values[RADIO_THROTTLE];
+  control_setpoint_rolldot = rc_values[RADIO_ROLL]*0.015;
+  control_setpoint_pitchdot = rc_values[RADIO_ROLL]*0.015;
+  control_setpoint_yawdot = rc_values[RADIO_ROLL]*0.015;
 }
 
+static inline void control_run_manual( void ) {
+  
 
+
+}
 
 #endif /* CONTROL_H */
