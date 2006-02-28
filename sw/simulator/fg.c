7416304ea858298b095614a20456840f71431552
Antoine Drouin <poinix@gmail.com>
2006-02-28 14:20:48 +0000

    *** empty log message ***

--- sw/simulator/fg.c
+++ sw/simulator/fg.c
@@ -5,25 +5,45 @@
 #include <caml/mlvalues.h>
 #include <caml/memory.h>
 
-struct fg {
-  float x;
-  float y;
-  float z;
-  float phi;
-};
+#include "flight_gear.h"
 
 value fg_msg(value x, value y, value z, value phi) {
   CAMLparam4(x, y, z, phi);
   CAMLlocal1(s);
 
-  struct fg msg;
-  msg.x = Double_val(x);
-  msg.y = Double_val(x);
-  msg.z = Double_val(x);
-  msg.phi = Double_val(x);
+  const double earth_radius = 6372795.; 
+  double lat =  0.656480 + asin(Double_val(x)/earth_radius);
+  double lon = -2.135537 + asin(Double_val(y)/earth_radius);
+  
+  struct FGNetGUI msg;
+  msg.version = 7;
 
-  s = alloc_string(sizeof(struct fg));
-  strncpy(String_val(s), (char*)&msg, sizeof(struct fg));
+  msg.longitude = lon;
+  msg.latitude = lat;
+  msg.altitude = Double_val(z);
+  msg.agl = 0.;
+  msg.phi = Double_val(phi);
+  msg.theta = 0.;
+  msg.psi = 0.;
+
+  msg.vcas = 0.;
+  msg.climb_rate = 0.;
+  
+  msg.num_tanks = 1;
+  msg.fuel_quantity[0] = 10.;
+  
+  msg.cur_time = 3213082700ul;
+  msg.warp = 0;
+  msg.ground_elev = 0.;
+
+  msg.tuned_freq = 123.45;
+  msg.nav_radial = 123.;
+  msg.in_range = 1;
+  msg.course_deviation_deg = 12.;
+  msg.gs_deviation_deg = 123.;
+
+  s = alloc_string(sizeof(msg));
+  strncpy(String_val(s), (char*)&msg, sizeof(msg));
 
   CAMLreturn (s);
 }
