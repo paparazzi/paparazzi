d18d21b7a710137a835e88bec6eb23b3666c9c72
Pascal Brisset <pascal.brisset@free.fr>
2006-04-03 20:44:10 +0000

    Hack for adressing in uplink (using the sender id as the A/C receiver)

--- sw/airborne/datalink.c
+++ sw/airborne/datalink.c
@@ -41,32 +41,37 @@
 #define MOfCm(_x) (((float)_x)/100.)
 
 
+#define IdOfMsg(x) (x[1])
+
 void dl_parse_msg(void) {
-  uint8_t msg_id = dl_buffer[0];
-  if (msg_id == DL_ACINFO_ID) {
-    uint8_t id = DL_ACINFO_ac_id(dl_buffer);
-    float ux = MOfCm(DL_ACINFO_utm_east(dl_buffer));
-    float uy = MOfCm(DL_ACINFO_utm_north(dl_buffer));
-    float a = MOfCm(DL_ACINFO_alt(dl_buffer));
-    float c = RadOfDeg(((float)DL_ACINFO_course(dl_buffer))/ 10.);
-    float s = MOfCm(DL_ACINFO_speed(dl_buffer));
-    SetAcInfo(id, ux, uy, c, a, s);
-  } else if (msg_id == DL_MOVE_WP_ID) {
-    uint8_t wp_id = DL_MOVE_WP_wp_id(dl_buffer);
-    float ux = MOfCm(DL_MOVE_WP_utm_east(dl_buffer));
-    float uy = MOfCm(DL_MOVE_WP_utm_north(dl_buffer));
-    float a = MOfCm(DL_MOVE_WP_alt(dl_buffer));
-    MoveWaypoint(wp_id, ux, uy, a);
-  } else if (msg_id == DL_EVENT_ID) {
-    uint8_t event = DL_EVENT_event(dl_buffer);
-    switch (event) {
-    case 1 : rc_event_1 = TRUE; break; // FIXME !
-    case 2 : rc_event_2 = TRUE; break;
-    default: ;
+  /** Hack: id of the sender is used in uplink to select the receiver */
+  if (IdOfMsg(dl_buffer) == AC_ID) {   
+    uint8_t msg_id = dl_buffer[0];
+    if (msg_id == DL_ACINFO_ID) {
+      uint8_t id = DL_ACINFO_ac_id(dl_buffer);
+      float ux = MOfCm(DL_ACINFO_utm_east(dl_buffer));
+      float uy = MOfCm(DL_ACINFO_utm_north(dl_buffer));
+      float a = MOfCm(DL_ACINFO_alt(dl_buffer));
+      float c = RadOfDeg(((float)DL_ACINFO_course(dl_buffer))/ 10.);
+      float s = MOfCm(DL_ACINFO_speed(dl_buffer));
+      SetAcInfo(id, ux, uy, c, a, s);
+    } else if (msg_id == DL_MOVE_WP_ID) {
+      uint8_t wp_id = DL_MOVE_WP_wp_id(dl_buffer);
+      float ux = MOfCm(DL_MOVE_WP_utm_east(dl_buffer));
+      float uy = MOfCm(DL_MOVE_WP_utm_north(dl_buffer));
+      float a = MOfCm(DL_MOVE_WP_alt(dl_buffer));
+      MoveWaypoint(wp_id, ux, uy, a);
+    } else if (msg_id == DL_EVENT_ID) {
+      uint8_t event = DL_EVENT_event(dl_buffer);
+      switch (event) {
+      case 1 : rc_event_1 = TRUE; break; // FIXME !
+      case 2 : rc_event_2 = TRUE; break;
+      default: ;
+      }
+    } else if (msg_id == DL_SETTING_ID) {
+      DlSetting(DL_SETTING_index(dl_buffer), DL_SETTING_value(dl_buffer));
+    } else if (msg_id == DL_BLOCK_ID) {
+      nav_goto_block(DL_BLOCK_block_id(dl_buffer));
     }
-  } else if (msg_id == DL_SETTING_ID) {
-    DlSetting(DL_SETTING_index(dl_buffer), DL_SETTING_value(dl_buffer));
-  } else if (msg_id == DL_BLOCK_ID) {
-    nav_goto_block(DL_BLOCK_block_id(dl_buffer));
   }
 }
--- sw/ground_segment/tmtc/maxstream.ml
+++ sw/ground_segment/tmtc/maxstream.ml
@@ -1,8 +1,6 @@
 (*
  * $Id$
  *
- * Multi aircrafts receiver, logger and broadcaster
- *  
  * Copyright (C) 2004 CENA/ENAC, Pascal Brisset, Antoine Drouin
  *
  * This file is part of paparazzi.
@@ -32,7 +30,9 @@ module Ground_Pprz = Pprz.Protocol(struct let name = "ground" end)
 module Dl_Pprz = Pprz.Protocol(struct let name = "datalink" end)
 module PprzTransport = Serial.Transport(Tm_Pprz)
 
-let ground_id = 0
+(** Hack: id of the sender is used in uplink to select the receiver *)
+let ground_ac_id = ref 4
+
 
 type aircraft = { fd : Unix.file_descr; id : int; addr : W.addr }
 
@@ -77,14 +77,13 @@ let use_tele_message = fun buf ->
 
 let buffer = ref ""
 
-let maxstream_parse = fun buf f ->
+let maxstream_parse = fun buf ->
   let b = !buffer ^ buf in 
   let nb_used = PprzTransport.parse use_tele_message b in
   buffer := String.sub b nb_used (String.length b - nb_used);
   String.length buf
 
-let maxstream_receive = fun f ->
-  Serial.input (fun b -> maxstream_parse b f)
+let maxstream_receive = Serial.input (fun b -> maxstream_parse b)
 
 
 
@@ -94,30 +93,9 @@ let send = fun ac s ->
 
 let send_dl_msg = fun ac a ->
   let (id, values) = Dl_Pprz.values_of_string a in
-  let s  = Dl_Pprz.payload_of_values id ground_id values in
+  let s  = Dl_Pprz.payload_of_values id !ground_ac_id values in
   send ac s
     
-let broadcast_msg = fun ac (com, data) ->
-  match com with
-    W.RECEIVED_FRAME ->
-      Ivy.send (sprintf "FROM_WAVECARD %s" data)
-  | W.RES_READ_REMOTE_RSSI ->
-      Tm_Pprz.message_send (string_of_int ac.id) "WC_RSSI" ["raw_level", Pprz.Int (Char.code data.[0])];
-      Debug.call 'm' (fun f -> fprintf f "mm remote RSSI %d\n" (Char.code data.[0]))
-  | W.RES_READ_RADIO_PARAM ->
-      Ivy.send (sprintf "WC_ADDR %s" data);
-      Debug.call 'm' (fun f -> fprintf f "mm local addr : " ; for i = 0 to String.length data - 1 do fprintf f "%x " (Char.code data.[i]) done; fprintf f "\n")
-  | _ -> 
-      Debug.call 'm' (fun f -> fprintf f "mm receiving: %02x " (W.code_of_cmd com); for i = 0 to String.length data - 1 do fprintf f "%x " (Char.code data.[i]) done; fprintf f "\n");
-      Ivy.send (sprintf "RAW_FROM_WAVECARD %2x %s" (W.code_of_cmd com) data)
-
-
-let rssi_period = 5000 (** ms *)
-let req_rssi = fun ac ->
-(*  Wavecard.send_addressed ac.fd (W.REQ_READ_REMOTE_RSSI, ac.addr,"") *)
-  Debug.call 'm' (fun f -> fprintf f "mm PERIODIC\n")
- 
-
 
 let cm_of_m = fun f -> Pprz.Int (truncate (100. *. f))
 
@@ -138,7 +116,7 @@ let get_fp = fun ac _sender vs ->
 	      "alt", cm_of_m alt;
 	      "speed", cm_of_m gspeed] in
     let msg_id, _ = Dl_Pprz.message_of_name "ACINFO" in
-    let s = Dl_Pprz.payload_of_values msg_id ground_id vs in
+    let s = Dl_Pprz.payload_of_values msg_id !ground_ac_id vs in
     send ac s
 
 (** Got a MOVE_WAYPOINT and send a MOVE_WP *)
@@ -158,7 +136,7 @@ let move_wp = fun ac _sender vs ->
 	      "utm_north", cm_of_m utm.utm_y;
 	      "alt", cm_of_m alt] in
     let msg_id, _ = Dl_Pprz.message_of_name "MOVE_WP" in
-    let s = Dl_Pprz.payload_of_values msg_id ground_id vs in
+    let s = Dl_Pprz.payload_of_values msg_id !ground_ac_id vs in
     send ac s
 
 (** Got a SEND_EVENT, and send an EVENT *)
@@ -168,7 +146,7 @@ let send_event = fun ac _sender vs ->
     let ev_id = Pprz.int_assoc "event_id" vs in
     let vs = ["event", Pprz.Int ev_id] in
     let msg_id, _ = Dl_Pprz.message_of_name "EVENT" in
-    let s = Dl_Pprz.payload_of_values msg_id ground_id vs in
+    let s = Dl_Pprz.payload_of_values msg_id !ground_ac_id vs in
     send ac s
     
 
@@ -179,7 +157,7 @@ let setting = fun ac _sender vs ->
     let idx = Pprz.int_assoc "index" vs in
     let vs = ["event", Pprz.Int idx; "value", List.assoc "value" vs] in
     let msg_id, _ = Dl_Pprz.message_of_name "SETTING" in
-    let s = Dl_Pprz.payload_of_values msg_id ground_id vs in
+    let s = Dl_Pprz.payload_of_values msg_id !ground_ac_id vs in
     send ac s
 
 (** Got a JUMP_TO_BLOCK, and send an BLOCK *)
@@ -190,7 +168,7 @@ let jump_block = fun ac _sender vs ->
     let block_id = Pprz.int_assoc "block_id" vs in
     let vs = ["block_id", Pprz.Int block_id] in
     let msg_id, _ = Dl_Pprz.message_of_name "BLOCK" in
-    let s = Dl_Pprz.payload_of_values msg_id ground_id vs in
+    let s = Dl_Pprz.payload_of_values msg_id !ground_ac_id vs in
     send ac s
 
 
@@ -198,11 +176,10 @@ let _ =
   let ivy_bus = ref "127.255.255.255:2010" in
   let port = ref "/dev/ttyS0" in
   let distant_addr = ref "0x011804c0012d" in
-  let id = ref 4 in
 
   let options =
     [ "-b", Arg.Set_string ivy_bus, (sprintf "Ivy bus (%s)" !ivy_bus);
-      "-id", Arg.Set_int id, (sprintf "A/C Id (%d)" !id);
+      "-id", Arg.Set_int ground_ac_id, (sprintf "A/C Id (%d)" !ground_ac_id);
       "-a", Arg.Set_string distant_addr, (sprintf "Distant address (%s)" !distant_addr);
       "-d", Arg.Set_string port, (sprintf "Port (%s)" !port)] in
   Arg.parse
@@ -215,12 +192,10 @@ let _ =
   
   try
     let fd = Serial.opendev !port Serial.B9600 in
-    let ac = { fd=fd; id= !id; addr= W.addr_of_string !distant_addr } in
+    let ac = { fd=fd; id= !ground_ac_id; addr= W.addr_of_string !distant_addr } in
     (* Listening on wavecard *)
-(*    let listener = Wavecard.receive ~ack:(send_ack 10 fd) (broadcast_msg ac) in *)
-    let listener = maxstream_receive (broadcast_msg ac) in
     let cb = fun _ ->
-      listener fd;
+      maxstream_receive fd;
       true in
 
     ignore (Glib.Io.add_watch [`IN] cb (GMain.Io.channel_of_descr fd));
@@ -238,12 +213,6 @@ let _ =
     ignore (Ivy.bind (fun _ a -> send_dl_msg ac a.(0)) "TO_WAVECARD +(.*)");
 
 
-    ignore (GMain.Timeout.add rssi_period (fun _ -> req_rssi ac; true));
-    (* request own address *)
-(*    let s = String.make 1 (char_of_int 5) in
-    Wavecard.send ac.fd (W.REQ_READ_RADIO_PARAM, s);
-*)
-
     (* Main Loop *)
     let loop = Glib.Main.create true in
     while Glib.Main.is_running loop do
@@ -251,7 +220,3 @@ let _ =
     done
   with
     exn -> fprintf stderr "%s\n" (Printexc.to_string exn)
-      
-
-
-
