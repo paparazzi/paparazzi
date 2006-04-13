c62a358dbb9a99a4f5ff05384d57e5f03ed8bab3
Antoine Drouin <poinix@gmail.com>
2006-04-13 10:29:12 +0000

    *** empty log message ***

---
+++ sw/airborne/actuators.h
@@ -0,0 +1,36 @@
+/*  $Id$
+ *
+ * (c) 2003-2005 Pascal Brisset, Antoine Drouin
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.  
+ *
+ */
+#ifndef ACTUATORS_H
+#define ACTUATORS_H
+
+#if defined ACTUATORS
+
+#include "paparazzi.h"
+extern const pprz_t failsafe_values[];
+extern void actuators_init( void );
+extern void actuators_set(const pprz_t values[]);
+
+#include ACTUATORS /* ACTUATORS */
+
+
+#endif ACTUATORS_H
