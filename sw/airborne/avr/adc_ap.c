e74d26e1de56469d0501ec31298527424da4be43
Antoine Drouin <poinix@gmail.com>
2006-04-13 10:55:22 +0000

    *** empty log message ***

--- sw/airborne/avr/adc_ap.c
+++ sw/airborne/avr/adc_ap.c
@@ -56,7 +56,7 @@ static struct adc_buf* buffers[NB_ADC];
 
 void adc_buf_channel(uint8_t adc_channel, struct adc_buf* s, uint8_t av_nb_sample) {
   buffers[adc_channel] = s;
-	s->av_nb_sample = av_nb_sample;
+  s->av_nb_sample = av_nb_sample;
 }
 
 void 
--- sw/airborne/avr/adc_fbw.c
+++ sw/airborne/avr/adc_fbw.c
@@ -53,8 +53,9 @@ uint16_t		adc_samples[ NB_ADC ];
 
 static struct adc_buf* buffers[NB_ADC];
 
-void adc_buf_channel(uint8_t adc_channel, struct adc_buf* s) {
+void adc_buf_channel(uint8_t adc_channel, struct adc_buf* s, uint8_t av_nb_sample) {
   buffers[adc_channel] = s;
+  s->av_nb_sample = av_nb_sample;
 }
 
 void 
