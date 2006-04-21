416e7f009df93c39636b88bbc8df6d90fc9bb8c6
Pascal Brisset <pascal.brisset@free.fr>
2006-04-21 19:23:37 +0000

    *** empty log message ***

--- sw/airborne/sim/ivy_transport.h
+++ sw/airborne/sim/ivy_transport.h
@@ -13,12 +13,13 @@ extern char* ivy_p;
 #define IvyTransportTrailer() { *ivy_p = '\0'; IvySendMsg(ivy_buf); }
 
 #define IvyTransportPutUint8(x) { ivy_p += sprintf(ivy_p, "%u ", x); }
+#define IvyTransportPutNamedUint8(_name, _x) { ivy_p += sprintf(ivy_p, "%s", _name); }
 
-#define IvyTransportPutUint8ByAddr(x) { ivy_p += sprintf(ivy_p, "%u ", *x); }
-#define IvyTransportPutUint16ByAddr(x) { ivy_p += sprintf(ivy_p, "%u ", *x); }
-#define IvyTransportPutUint32ByAddr(x) { ivy_p += sprintf(ivy_p, "%u ", *x);}
+#define IvyTransportPutUint8ByAddr(x) { ivy_p += sprintf(ivy_p, " %u", *x); }
+#define IvyTransportPutUint16ByAddr(x) { ivy_p += sprintf(ivy_p, " %u", *x); }
+#define IvyTransportPutUint32ByAddr(x) { ivy_p += sprintf(ivy_p, " %u", *x);}
 
-#define IvyTransportPutInt8ByAddr(x) { ivy_p += sprintf(ivy_p, "%d ", *x); }
-#define IvyTransportPutInt16ByAddr(x) { ivy_p += sprintf(ivy_p, "%d ", *x); }
-#define IvyTransportPutInt32ByAddr(x) { ivy_p += sprintf(ivy_p, "%d ", *x); }
-#define IvyTransportPutFloatByAddr(x) { ivy_p += sprintf(ivy_p, "%f ", *x); }
+#define IvyTransportPutInt8ByAddr(x) { ivy_p += sprintf(ivy_p, " %d", *x); }
+#define IvyTransportPutInt16ByAddr(x) { ivy_p += sprintf(ivy_p, " %d", *x); }
+#define IvyTransportPutInt32ByAddr(x) { ivy_p += sprintf(ivy_p, " %d", *x); }
+#define IvyTransportPutFloatByAddr(x) { ivy_p += sprintf(ivy_p, " %f", *x); }
