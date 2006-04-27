f617bb50b212802b1ea107aa5abbf3b2ea8c1342
Pascal Brisset <pascal.brisset@free.fr>
2006-04-27 12:31:11 +0000

    including modem handling

--- sw/ground_segment/tmtc/link.ml
+++ sw/ground_segment/tmtc/link.ml
@@ -38,6 +38,13 @@ type transport =
   | Wavecard
   | Maxstream
 
+let transport_of_string = function
+    "modem" -> Modem
+  | "pprz" -> Pprz
+  | "wavecard" -> Wavecard
+  | "maxstream" -> Maxstream
+  | x -> invalid_arg (sprintf "transport_of_string: %s" x)
+
 type modem = { fd : Unix.file_descr; transport : transport }
 
 (*let send_ack = fun delay fd () ->
@@ -53,9 +60,6 @@ let use_tele_message = fun payload ->
   let (msg_id, ac_id, values) = Tm_Pprz.values_of_payload payload in
   let msg = Tm_Pprz.message_of_id msg_id in
   Tm_Pprz.message_send (string_of_int ac_id) msg.Pprz.name values
-
-let parse = fun buf ->
-  PprzTransport.parse use_tele_message buf
 
 let send = fun ac payload ->
   match ac.transport with
@@ -147,6 +151,78 @@ let raw_datalink = fun ac _sender vs ->
   let msg_id, vs = Dl_Pprz.values_of_string m in
   let s = Dl_Pprz.payload_of_values msg_id ac_id vs in
   send ac s
+
+module PprzModem = struct
+  type status = {
+      mutable rx_byte : int;
+      mutable rx_msg : int;
+      mutable rx_err : int
+    }
+  let status = { rx_byte = 0; rx_msg = 0; rx_err = 0 }
+
+  let msg_period = 1000 (** ms *)
+
+(** Modem monitoring messages *)
+  let send_msg =
+    let rx_msg = ref 0 
+    and rx_byte = ref 0 
+    and start = Unix.gettimeofday () in
+    fun () ->
+      let dt = float msg_period /. 1000. in
+      let t = int_of_float (Unix.gettimeofday () -. start) in
+      let byte_rate = float (status.rx_byte - !rx_byte) /. dt
+      and msg_rate = float (status.rx_msg - !rx_msg) /. dt in
+      rx_msg := status.rx_msg;
+      rx_byte := status.rx_byte;
+      let vs = ["run_time", Pprz.Int t;
+		"rx_bytes_rate", Pprz.Float byte_rate; 
+		"rx_msgs_rate", Pprz.Float msg_rate;
+		"rx_err", Pprz.Int status.rx_err;
+		"rx_bytes", Pprz.Int status.rx_byte;
+		"rx_msgs", Pprz.Int status.rx_msg
+	      ] in
+      Tm_Pprz.message_send "modem" "DOWNLINK_STATUS" vs;
+      let vs = ["valim", Pprz.Float Modem.status.Modem.valim;
+		"detected", Pprz.Int Modem.status.Modem.detected;
+		"cd", Pprz.Int Modem.status.Modem.cd;
+		"nb_err", Pprz.Int Modem.status.Modem.nb_err;
+		"nb_byte", Pprz.Int Modem.status.Modem.nb_byte;
+		"nb_msg", Pprz.Int Modem.status.Modem.nb_msg
+	      ] in
+      Tm_Pprz.message_send "modem" "MODEM_STATUS" vs
+
+  let use_tele_message_modem = fun payload ->
+    let buf = Serial.string_of_payload payload in
+    status.rx_byte <- status.rx_byte + String.length buf;
+    Debug.call 'P' (fun f -> fprintf f "use_pprz: %s\n" (Debug.xprint buf));
+    use_tele_message payload
+
+
+  let use_message =
+    let buffer = ref "" in
+    fun payload ->
+      let msg = Serial.string_of_payload payload in
+      Debug.call 'M' (fun f -> fprintf f "use_modem: %s\n" (Debug.xprint msg));
+      match Modem.parse msg with
+	None -> () (* Only internal modem data *)
+      | Some data ->
+	  (** Accumulate in a buffer *)
+	  let b = !buffer ^ data in
+	  Debug.call 'M' (fun f -> fprintf f "Pprz buffer: %s\n" (Debug.xprint b));
+	  (** Parse as pprz message and ... *)
+	  let x = PprzTransport.parse use_tele_message_modem b in
+	  status.rx_err <- !PprzTransport.nb_err;
+	  (** ... remove from the buffer the chars which have been used *)
+	  buffer := String.sub b x (String.length b - x)
+end (* PprzModem module *)
+
+
+let parse_of_transport = function
+    Pprz -> PprzTransport.parse use_tele_message
+  | Modem -> 
+      let module ModemTransport = Serial.Transport(Modem.Protocol) in
+      ModemTransport.parse PprzModem.use_message
+  | _ -> failwith "parse_of_transport: not yet"
     
 
 let _ =
@@ -167,26 +243,38 @@ let _ =
 
   Ivy.init "Link" "READY" (fun _ _ -> ());
   Ivy.start !ivy_bus;
+
+  let transport = transport_of_string !transport in
   
   try
     let fd = Serial.opendev !port (Serial.speed_of_baudrate !baurate) in
-    assert(!transport="pprz");
-    let ac = { fd=fd; transport=Pprz} in
+    let ac = { fd=fd; transport=transport} in
 
     (* Listening *)
+    let parse = parse_of_transport transport in
+    
     let buffered_input = 
-      match Serial.input (fun buf -> parse buf) with
+      match Serial.input parse with
 	Serial.Closure f -> f in
     let cb = fun _ -> buffered_input fd; true in
     ignore (Glib.Io.add_watch [`IN] cb (GMain.Io.channel_of_descr fd));
 
-    (** Listening on Ivy *)
+    (** Listening on Ivy (FIXME: remove the ad hoc messages) *)
     ignore (Ground_Pprz.message_bind "FLIGHT_PARAM" (get_fp ac));
     ignore (Ground_Pprz.message_bind "MOVE_WAYPOINT" (move_wp ac));
     ignore (Ground_Pprz.message_bind "SEND_EVENT" (send_event ac));
     ignore (Ground_Pprz.message_bind "DL_SETTING" (setting ac));
     ignore (Ground_Pprz.message_bind "JUMP_TO_BLOCK" (jump_block ac));
     ignore (Ground_Pprz.message_bind "RAW_DATALINK" (raw_datalink ac));
+
+    (** Periodic tasks *)
+    begin
+      match transport with
+	Modem ->
+	  (** Sending periodically modem and downlink status messages *)
+	  ignore (Glib.Timeout.add PprzModem.msg_period (fun () -> PprzModem.send_msg (); true))
+      | _ -> ()
+    end;
 
 
     (* Main Loop *)
