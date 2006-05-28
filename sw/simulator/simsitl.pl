4cf01f007790b70022e1140e4fc619d37bd218c2
Antoine Drouin <poinix@gmail.com>
2006-05-28 02:18:48 +0000

    removed warning

--- sw/simulator/simsitl.pl
+++ sw/simulator/simsitl.pl
@@ -19,7 +19,7 @@ GetOptions (
 	   );
 my @args = ();
 push @args, "-b", $options->{ivy_bus};
-push @args, "-fg", $options->{fg} if exists $options->{fg};
+push @args, "-fg", $options->{fg} if defined $options->{fg};
 my $sim_binary = Paparazzi::Environment::paparazzi_home()."/var/".$options->{aircraft}."/sim/simsitl";
 die "$sim_binary not found. try make AIRCRAFT=$options->{aircraft} ac\n" unless -e $sim_binary;
 exec ($sim_binary, @args)
