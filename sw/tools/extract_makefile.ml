8ba8e00256665efb869a3a116036c99e7025308d
Pascal Brisset <pascal.brisset@free.fr>
2006-08-21 09:45:19 +0000

    multiple target makefile sections

--- sw/tools/extract_makefile.ml
+++ sw/tools/extract_makefile.ml
@@ -3,12 +3,16 @@ open Printf
 let _ =
   let f = Sys.argv.(1) in
   let xml = Xml.parse_file f in
-  printf "# This file has been generated from %s\n" f;
+  printf "# This file has been generated from %s by sw/tools/extract_makefile\n" f;
   printf "# Please DO NOT EDIT\n";
-  try
-    match Xml.children (ExtXml.child xml "makefile") with
-      [Xml.PCData s] -> 
-	printf "%s\n" s
-    | _ -> ()
-  with
-    _ -> ()
+  List.iter (fun x ->
+    if ExtXml.tag_is x "makefile" then
+      begin
+	try
+	  printf "\n# makefile target '%s'\n" (Xml.attrib x "target")
+	with _ -> ()
+      end;
+    match Xml.children x with
+      [Xml.PCData s] -> printf "%s\n" s
+    | _ -> fprintf stderr "Warning: wrong makefile section in '%s': %s\n" f (Xml.to_string_fmt x))
+    (Xml.children xml)
