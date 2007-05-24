2fa4abd640fee45868a5f9f3cac1edbd6d07d09e
Pascal Brisset <pascal.brisset@free.fr>
2007-05-24 14:37:07 +0000

    *** empty log message ***

--- sw/airborne/fw_h_ctl.h
+++ sw/airborne/fw_h_ctl.h
@@ -73,6 +73,8 @@ extern float h_ctl_hi_throttle_roll_rate_pgain;
 extern float h_ctl_lo_throttle_roll_rate_pgain;
 extern float h_ctl_roll_rate_igain;
 extern float h_ctl_roll_rate_dgain;
+
+#define fw_h_ctl_SetRollRatePGain(v) { h_ctl_hi_throttle_roll_rate_pgain = v; h_ctl_lo_throttle_roll_rate_pgain = v; }
 #endif
 
 extern void h_ctl_init( void );
