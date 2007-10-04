d7308d63579fd0ef6db80889081a8ae4b38a450f
Antoine Drouin <poinix@gmail.com>
2007-10-04 17:22:45 +0000

    *** empty log message ***

---
+++ sw/airborne/6dof.h
@@ -0,0 +1,13 @@
+#ifndef _6DOF_H
+#define _6DOF_H
+
+#define AXIS_X  0
+#define AXIS_Y  1
+#define AXIS_Z  2
+#define AXIS_NB 3
+
+#define AXIS_P 0
+#define AXIS_Q 1
+#define AXIS_R 2
+
+#endif /* _6DOF_H */
