6371090ba28777c378e586ea5de1df61ead67d6a
Pascal Brisset <pascal.brisset@free.fr>
2007-10-05 10:01:59 +0000

    sys_time_usleep added

--- sw/airborne/sim/sys_time_hw.h
+++ sw/airborne/sim/sys_time_hw.h
@@ -1,11 +1,14 @@
 #ifndef SYS_TIME_HW_H
 #define SYS_TIME_HW_H
 
+#include <unistd.h>
+
 #define SYS_TICS_OF_USEC(x) (x)
 #define SIGNED_SYS_TICS_OF_USEC(x) (x)
 
 static inline void sys_time_init( void ) {}
 
 #define sys_time_periodic() TRUE
+#define sys_time_usleep(x) usleep(x)
 
 #endif /* SYS_TIME_HW_H */
