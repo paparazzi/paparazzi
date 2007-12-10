a4e840495bf7428731fe8c8f28ef2333615374d4
Pascal Brisset <pascal.brisset@free.fr>
2007-12-10 13:18:05 +0000

    led added

--- sw/airborne/arm7/uart_tunnel.c
+++ sw/airborne/arm7/uart_tunnel.c
@@ -4,6 +4,7 @@
 #include "std.h"
 
 #include "init_hw.h"
+#include "led.h"
 
 #define TXD0_PIN 0
 #define RXD0_PIN 1
@@ -12,6 +13,8 @@
 
 int main (int argc, char** argv) {
   hw_init();
+  led_init();
+
   /* TXD0 and TXD1 output */
   SetBit(IO0DIR, TXD0_PIN);
   SetBit(IO0DIR, TXD1_PIN);
@@ -25,10 +28,13 @@ int main (int argc, char** argv) {
       SetBit(IO0SET, TXD1_PIN);
     else
       SetBit(IO0CLR, TXD1_PIN);
-    if (bit_is_set(IO0PIN, RXD1_PIN))
+    if (bit_is_set(IO0PIN, RXD1_PIN)) {
+      LED_OFF(2);
       SetBit(IO0SET, TXD0_PIN);
-    else
+    } else {
+      LED_ON(2);
       SetBit(IO0CLR, TXD0_PIN);
+    }
   }
   return 0;
 }
