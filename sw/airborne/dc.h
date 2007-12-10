83b3b5bef864d1530896dbaeb78d350da1372f43
Pascal Brisset <pascal.brisset@free.fr>
2007-12-10 14:20:59 +0000

    configuration of the digital camera commands with leds

--- sw/airborne/dc.h
+++ sw/airborne/dc.h
@@ -14,33 +14,30 @@ extern uint8_t dc_timer;
 
 #include "LPC21xx.h"
 #include "std.h"
+#include "led.h"
+#include "airframe.h"
 
 #define SHUTTER_DELAY 2  /* 4Hz */
-#define SHUTTER_BANK 0 /* Grey */
-#define SHUTTER_PIN 16
-
-#define ZOOM_BANK 1
-#define ZOOM_PIN 20
 
 static inline uint8_t dc_shutter() {
   dc_timer = SHUTTER_DELAY; 
-  IO0SET = _BV(SHUTTER_PIN);
+  LED_OFF(DC_SHUTTER_LED);
   return 0;
 }
 
 static inline uint8_t dc_zoom() {
   dc_timer = SHUTTER_DELAY; 
-  IO1SET = _BV(ZOOM_PIN);
+  LED_OFF(DC_ZOOM_LED);
   return 0;
 }
 
 #define dc_Shutter(_) { dc_shutter(); }
 #define dc_Zoom(_) { dc_zoom(); }
 
-#define dc_init() { IO0DIR |= _BV(SHUTTER_PIN); IO0CLR = _BV(SHUTTER_PIN); ClearBit(PINSEL2, 3); IO1DIR |= _BV(ZOOM_PIN); IO1CLR = _BV(ZOOM_PIN); } /* Output */
+#define dc_init() { /* initialized as leds */ } /* Output */
 
 /* 4Hz */
-#define dc_periodic() { if (dc_timer) { dc_timer--; } else { IO0CLR = _BV(SHUTTER_PIN); IO1CLR = _BV(ZOOM_PIN); } }
+#define dc_periodic() { if (dc_timer) { dc_timer--; } else { LED_ON(DC_SHUTTER_LED); LED_ON(DC_ZOOM_LED); } }
 
 #endif /* ! SITL */
 
