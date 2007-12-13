fa978d39d2b8cff6ed0ee43e56d2733c7d30949a
Pascal Brisset <pascal.brisset@free.fr>
2007-12-13 17:46:31 +0000

    ivy-tcp connection

---
+++ sw/ground_segment/tmtc/broadcaster.ml
@@ -0,0 +1,56 @@
+open Printf
+
+let () =
+  let ivy_bus = ref "127.255.255.255:2010" in
+  
+  let port = ref 4242
+  and ivy_from = ref "DL"
+  and ivy_to = ref "TM" in
+
+  let options = [
+    "-b", Arg.Set_string ivy_bus, (sprintf "<ivy bus> Default is %s" !ivy_bus);
+    "-f", Arg.Set_string ivy_from, (sprintf "<tag> Default is %s" !ivy_from);
+    "-t", Arg.Set_string ivy_to , (sprintf "<tag> Default is %s" !ivy_to);
+    "-p", Arg.Set_int port, (sprintf "<remote port> Default is %d" !port)
+  ] in
+  Arg.parse
+    options
+    (fun x -> fprintf stderr "Warning: Discarding '%s'" x)
+    "Usage: ";
+  
+  let handler = fun i o ->
+    Ivy.init "broadcaster" "READY" (fun _ _ -> ());
+    Ivy.start !ivy_bus;
+
+    (* Forward telemetry on Ivy *)    
+    let buffer_size = 256 in
+    let buffer = String.create buffer_size in
+    let get_tcp = fun _ ->
+      begin
+	try
+	  let n = input i buffer 0 buffer_size in
+	  let data = String.sub buffer 0 n in
+
+	  Ivy.send (sprintf "%s %s" !ivy_to (Base64.encode_string data))
+	with
+	  exc -> prerr_endline (Printexc.to_string exc)
+      end;
+    true in
+
+    let ginput = GMain.Io.channel_of_descr (Unix.descr_of_in_channel i) in
+    ignore (Glib.Io.add_watch [`IN] get_tcp ginput);
+    let hangup = fun _ -> prerr_endline "hangup"; exit 1 in
+    ignore (Glib.Io.add_watch [`HUP] hangup ginput);
+
+    (* Forward datalink on Tcp *)
+    let get_ivy = fun _ args ->
+      try fprintf o "%s%!" (Base64.decode_string args.(0)) with
+	exc -> prerr_endline (Printexc.to_string exc) in
+    ignore (Ivy.bind get_ivy (sprintf "^%s (.*)" !ivy_from));
+
+    (* Main Loop *)
+    GMain.main ()
+  in
+
+  let sockaddr = Unix.ADDR_INET (Unix.inet_addr_any, !port) in
+  Unix.establish_server handler sockaddr;
--- sw/ground_segment/tmtc/ivy_tcp_controller.ml
+++ sw/ground_segment/tmtc/ivy_tcp_controller.ml
@@ -1,71 +1,70 @@
-let my_id = 0
+open Printf
+
 module Tm_Pprz = Pprz.Messages(struct let name = "telemetry" end)
 module Dl_Pprz = Pprz.Messages(struct let name = "datalink" end)
 module PprzTransport = Serial.Transport(Pprz.Transport)
 
-open Printf
 let () =
-  let ivy_bus = ref "127.255.255.255:2010" in
-  let host = ref "10.31.1.89"
-  and port = ref 4242
-  and id = ref "6" in
+  let host = ref "10.31.1.98"
+  and ivy_bus = ref "127.255.255.255:2010" in
+  let port = ref 4243 in
 
   let options = [
     "-b", Arg.Set_string ivy_bus, (sprintf "<ivy bus> Default is %s" !ivy_bus);
     "-h", Arg.Set_string host, (sprintf "<remote host> Default is %s" !host);
-    "-id", Arg.Set_string id , (sprintf "<id> Default is %s" !host);
     "-p", Arg.Set_int port, (sprintf "<remote port> Default is %d" !port)
   ] in
   Arg.parse
     options
     (fun x -> fprintf stderr "Warning: Discarding '%s'" x)
     "Usage: ";
-  
-  Ivy.init "ivy_tcp" "READY" (fun _ _ -> ());
-  Ivy.start !ivy_bus;
 
+  Ivy.init "tcp_ivy" "READY" (fun _ _ -> ());
+  Ivy.start !ivy_bus;
+    
   let addr = Unix.inet_addr_of_string !host in
   let sockaddr = Unix.ADDR_INET (addr, !port) in
 
   let (i, o) = Unix.open_connection sockaddr in
 
-  let get_ivy_message = fun _ args ->
-    try
-      let (msg_id, vs) = Tm_Pprz.values_of_string args.(0) in
-      let payload = Tm_Pprz.payload_of_values msg_id (int_of_string !id) vs in
-      let buf = Pprz.Transport.packet payload in
-      fprintf o "%s%!" buf
-    with _ -> () in
-  let _b = Ivy.bind get_ivy_message (sprintf "^%s (.*)" !id) in
-
-  (* Forward a datalink command on the bus *)
   let buffer_size = 256 in
   let buffer = String.create buffer_size in
-  let get_datalink_message = fun _ ->
+  let get_message = fun _ ->
     begin
       try
 	let n = input i buffer 0 buffer_size in
 	let b = String.sub buffer 0 n in
 	Debug.trace 'x' (Debug.xprint b);
-
-	let use_dl_message = fun payload ->
-	  Debug.trace 'x' (Debug.xprint (Serial.string_of_payload payload));
-	  let (msg_id, ac_id, values) = Dl_Pprz.values_of_payload payload in
-	  let msg = Dl_Pprz.message_of_id msg_id in
-	  Dl_Pprz.message_send "ground_dl" msg.Pprz.name values in
 	
-	assert (PprzTransport.parse use_dl_message b = n)
+	let use_tele_message = fun payload ->
+	  Debug.trace 'x' (Debug.xprint (Serial.string_of_payload payload));
+	  let (msg_id, ac_id, values) = Tm_Pprz.values_of_payload payload in
+	  let msg = Tm_Pprz.message_of_id msg_id in
+	  Tm_Pprz.message_send (string_of_int ac_id) msg.Pprz.name values in
+	
+	ignore (PprzTransport.parse use_tele_message b)
       with
 	exc ->
-	  prerr_endline (Printexc.to_string exc)
-    end;
+	    prerr_endline (Printexc.to_string exc)
+      end;
     true in
   
   let ginput = GMain.Io.channel_of_descr (Unix.descr_of_in_channel i) in
-  ignore (Glib.Io.add_watch [`IN] get_datalink_message ginput);
+  ignore (Glib.Io.add_watch [`IN] get_message ginput);
+    
+  (* Forward datalink messages *)
+  let get_ivy_message = fun _ args ->
+    try
+      let (msg_id, vs) = Dl_Pprz.values_of_string args.(0) in
+      let ac_id = Pprz.int_assoc "ac_id" vs in
+      let payload = Dl_Pprz.payload_of_values msg_id ac_id vs in
+      let buf = Pprz.Transport.packet payload in
+      fprintf o "%s%!" buf
+    with exc -> prerr_endline (Printexc.to_string exc) in
+  let _b = Ivy.bind get_ivy_message "^ground_dl (.*)" in
 
   let hangup = fun _ -> prerr_endline "hangup"; exit 1 in
   ignore (Glib.Io.add_watch [`HUP] hangup ginput);
-
+  
   (* Main Loop *)
-  GMain.main () 
+  GMain.main ()
