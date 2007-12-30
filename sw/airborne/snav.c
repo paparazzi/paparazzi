58427ef2a1f408b336150fc37e9e2e949fef2415
Pascal Brisset <pascal.brisset@free.fr>
2007-12-30 13:13:11 +0000

    Bug fixed for close poitns

--- sw/airborne/snav.c
+++ sw/airborne/snav.c
@@ -7,7 +7,7 @@
 #include "nav.h"
 #include "gps.h"
 
-#define Sign(_x) ((_x) > 0 ? -1 : 1)
+#define Sign(_x) ((_x) > 0 ? 1 : (-1))
 
 static struct point wp_cd, wp_td, wp_ca, wp_ta;
 static float d_radius, a_radius;
@@ -26,25 +26,36 @@ bool_t snav_init(uint8_t a, float desired_course_rad, float tow) {
   /* D_CD orthogonal to current course, CD on the side of A */
   float u_x = cos(M_PI_2 - estimator_hspeed_dir);
   float u_y = sin(M_PI_2 - estimator_hspeed_dir);
-  d_radius = Sign(u_x*da_y - u_y*da_x) * DEFAULT_CIRCLE_RADIUS;
+  d_radius = - Sign(u_x*da_y - u_y*da_x) * DEFAULT_CIRCLE_RADIUS;
   wp_cd.x = estimator_x + d_radius * u_y;
   wp_cd.y = estimator_y - d_radius * u_x;
   wp_cd.a = WaypointAlt(wp_a);
 
   /* A_CA orthogonal to desired course, CA on the side of D */
-  u_x = cos(M_PI_2 - desired_course_rad);
-  u_y = sin(M_PI_2 - desired_course_rad);
-  a_radius = - Sign(u_x*da_y - u_y*da_x) * DEFAULT_CIRCLE_RADIUS;
-  wp_ca.x = WaypointX(wp_a) + a_radius * u_y;
-  wp_ca.y = WaypointY(wp_a) - a_radius * u_x;
+  float desired_u_x = cos(M_PI_2 - desired_course_rad);
+  float desired_u_y = sin(M_PI_2 - desired_course_rad);
+  a_radius = Sign(desired_u_x*da_y - desired_u_y*da_x) * DEFAULT_CIRCLE_RADIUS;
+  wp_ca.x = WaypointX(wp_a) + a_radius * desired_u_y;
+  wp_ca.y = WaypointY(wp_a) - a_radius * desired_u_x;
   wp_ca.a = WaypointAlt(wp_a);
 
   /* Unit vector along CD-CA */
   u_x = wp_ca.x - wp_cd.x;
   u_y = wp_ca.y - wp_cd.y;
-  float cd_da = sqrt(u_x*u_x+u_y*u_y);
-  u_x /= cd_da;
-  u_y /= cd_da;
+  float cd_ca = sqrt(u_x*u_x+u_y*u_y);
+
+  /* If it is too close in reverse direction, set CA on the other side */
+  if (a_radius * d_radius < 0 && cd_ca < 2 * DEFAULT_CIRCLE_RADIUS) {
+    a_radius = -a_radius;
+    wp_ca.x = WaypointX(wp_a) + a_radius * desired_u_y;
+    wp_ca.y = WaypointY(wp_a) - a_radius * desired_u_x;
+    u_x = wp_ca.x - wp_cd.x;
+    u_y = wp_ca.y - wp_cd.y;
+    cd_ca = sqrt(u_x*u_x+u_y*u_y);
+  }
+
+  u_x /= cd_ca;
+  u_y /= cd_ca;
 
   if (a_radius * d_radius > 0) {
     /* Both arcs are in the same direction */
@@ -57,7 +68,7 @@ bool_t snav_init(uint8_t a, float desired_course_rad, float tow) {
     wp_ta.y = wp_ca.y + a_radius * u_x;
   } else {
     /* Arcs are in reverse directions: trigonemetric puzzle :-) */
-    float alpha = atan2(u_y, u_x) + acos(d_radius/(cd_da/2));
+    float alpha = atan2(u_y, u_x) + acos(d_radius/(cd_ca/2));
     wp_td.x = wp_cd.x + d_radius * cos(alpha);
     wp_td.y = wp_cd.y + d_radius * sin(alpha);
 
@@ -107,11 +118,11 @@ bool_t snav_on_time(void) {
   current_center.x = WaypointX(wp_a) + (wp_ca.x - WaypointX(wp_a)) * radius_factor;
   current_center.y = WaypointY(wp_a) + (wp_ca.y - WaypointY(wp_a)) * radius_factor;
   float current_qdr = M_PI_2 - atan2(estimator_y-current_center.y, estimator_x-current_center.x);
-  float remaining_angle = qdr_a - current_qdr;
+  float remaining_angle = Sign(a_radius)*(qdr_a - current_qdr);
   if (remaining_angle < 0.)
     remaining_angle += 2 * M_PI;
   float remaining_time = snav_desired_tow - gps_itow / 1000.;
-  
+
   radius_factor = (remaining_time * NOMINAL_AIRSPEED) / (remaining_angle * DEFAULT_CIRCLE_RADIUS);
 
   /* printf("ra=%.0f rt=%.0f\n", DegOfRad(remaining_angle), remaining_time); */
