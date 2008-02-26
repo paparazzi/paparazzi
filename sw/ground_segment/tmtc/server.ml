1aa4feb95593638dbca017d836f124b0cc840612
Pascal Brisset <pascal.brisset@free.fr>
2008-02-26 10:43:02 +0000

    connection on PPRZ_MODE required for replay

--- sw/ground_segment/tmtc/server.ml
+++ sw/ground_segment/tmtc/server.ml
@@ -746,7 +746,8 @@ let new_color = fun () ->
 (* Waits for new aircrafts *)
 let listen_acs = fun log ->
   (** Wait for any message (they all are identified with the A/C) *)
-  ignore (Ivy.bind (fun _ args -> ident_msg log args.(0)) "^(.*) ALIVE")
+  ignore (Ivy.bind (fun _ args -> ident_msg log args.(0)) "^(.*) ALIVE");
+  ignore (Ivy.bind (fun _ args -> ident_msg log args.(0)) "^(.*) PPRZ_MODE")
 
 
 let send_config = fun http _asker args ->
