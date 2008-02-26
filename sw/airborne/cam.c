29af45c117c4ca23a656b3071827427dde73f551
Pascal Brisset <pascal.brisset@free.fr>
2008-02-26 10:33:06 +0000

    rewriting

--- sw/airborne/cam.c
+++ sw/airborne/cam.c
@@ -28,15 +28,18 @@
 
 #include <math.h>
 #include "cam.h"
-#include "nav.h"
+#include "common_nav.h"
 #include "autopilot.h"
 #include "flight_plan.h"
 #include "estimator.h"
-#include "inter_mcu.h"
 #include "traffic_info.h"
 #ifdef POINT_CAM
 #include "point.h"
 #endif // POINT_CAM
+
+#ifdef TEST_CAM
+#include "test_cam.h"
+#endif // TEST_CAM
 
 #ifdef CAM_PAN_NEUTRAL
 #if (CAM_PAN_MAX == CAM_PAN_NEUTRAL)
@@ -60,165 +63,132 @@
 #define DELTA_ALPHA 0.2
 
 #ifdef CAM_PAN0
-float pan_c = RadOfDeg(CAM_PAN0);
+float cam_pan_c = RadOfDeg(CAM_PAN0);
 #else
-float pan_c;
+float cam_pan_c;
 #endif
 
 #ifdef CAM_TILT0
-float tilt_c = RadOfDeg(CAM_TILT0);
+float cam_tilt_c = RadOfDeg(CAM_TILT0);
 #else
-float tilt_c;
+float cam_tilt_c;
 #endif
 
-float phi_c;
-float theta_c;
+float cam_phi_c;
+float cam_theta_c;
 
-float target_x, target_y, target_alt;
+float cam_target_x, cam_target_y, cam_target_alt;
+uint8_t cam_target_wp;
 
-int use_hatch_cam = 0;
-int cam_mode_auto = 1;
+uint8_t cam_mode;
 
-#ifdef MOBILE_CAM
+int16_t cam_pan_command;
+int16_t cam_tilt_command;
 
-void cam_manual( void ) {
-  if (pprz_mode == PPRZ_MODE_AUTO2) {
-    static pprz_t cam_pan, cam_tilt;
-    int16_t yaw = fbw_state->channels[RADIO_YAW];
-    if (yaw > MIN_PPRZ_CAM || yaw < -MIN_PPRZ_CAM) { 
-      cam_pan += FLOAT_OF_PPRZ(yaw, 0, 300.);
-      cam_pan = TRIM_PPRZ(cam_pan);
-    }
-    int16_t pitch = fbw_state->channels[RADIO_PITCH];
-    if (pitch > MIN_PPRZ_CAM || pitch < -MIN_PPRZ_CAM) {
-      cam_tilt += FLOAT_OF_PPRZ(pitch, 0, 300.);
-      cam_tilt = TRIM_PPRZ(cam_tilt);
-    }
-#ifdef COMMAND_CAM_PAN
-    ap_state->commands[COMMAND_CAM_PAN] = cam_pan;
-#endif
-#ifdef COMMAND_CAM_TILT    
-    ap_state->commands[COMMAND_CAM_TILT] = cam_tilt;
-#endif    
+void cam_nadir(void);
+void cam_angles(void);
+void cam_target(void);
+void cam_waypoint_target(void);
+void cam_ac_target( uint8_t ac_id );
+
+void cam_init( void ) {
+  cam_mode = CAM_MODE_OFF;
+}
+
+void cam_periodic( void ) {
+  switch (cam_mode) {
+  case CAM_MODE_OFF:
+    break;
+  case CAM_MODE_ANGLES:
+    cam_angles();
+    break;
+  case CAM_MODE_NADIR:
+    cam_nadir();
+    break;
+  case CAM_MODE_XY_TARGET:
+    cam_target();
+    break;
+  case CAM_MODE_WP_TARGET:
+    cam_waypoint_target();
+    break;
   }
 }
 
-void cam_nadir( void ) {
-  pan_c = -estimator_phi;
-  tilt_c = -estimator_theta;
-}
-
-#if 0
-void cam_target_trigo( void ) {
-  /** Relative height of the target */
-  float h = estimator_z - target_alt;
-
-  /** Relative heading of the target (trigo) */
-  float dy = target_y-estimator_y;
-  float dx = target_x-estimator_x;
-  float alpha = atan2(dy, dx);
-
-  /** Projection in a horizontal plane of the distance ac-target */
-  float d = sqrt (dy*dy + dx*dx);
-
-  float d_h = d / h;
-  float pseudo_psi = M_PI/2 - estimator_hspeed_dir; /** Trigo */
-  float psi_alpha = - (pseudo_psi - M_PI/2 - alpha);
-  phi_c = atan(d_h * cos (psi_alpha)) + estimator_phi;
-  theta_c = atan(d_h * sin (psi_alpha)) - estimator_theta;
-}
-#endif
-
-void cam_target( void ) {
-
+/** Computes the servo values from cam_pan_c and cam_tilt_c */
+void cam_angles( void ) {
   float cam_pan = 0;
   float cam_tilt = 0;
 
-  if (cam_mode_auto != 0)
-  {
-    vPoint(estimator_x, estimator_y, estimator_z,
-           estimator_phi, estimator_theta, estimator_hspeed_dir,
-           target_x, target_y, target_alt,
-           &pan_c, &tilt_c);
-  }
-
-#ifdef CAM_PAN_NEUTRAL  
-  if (theta_c > RadOfDeg(CAM_PAN_NEUTRAL))
-    cam_pan = MAX_PPRZ * (theta_c / (RadOfDeg(CAM_PAN_MAX - CAM_PAN_NEUTRAL)) - 1.); 
+#ifdef CAM_PAN_NEUTRAL
+  float pan_diff = cam_pan_c - RadOfDeg(CAM_PAN_NEUTRAL);
+  if (pan_diff > 0)
+    cam_pan = MAX_PPRZ * (pan_diff / (RadOfDeg(CAM_PAN_MAX - CAM_PAN_NEUTRAL))); 
   else
-    cam_pan = MIN_PPRZ * (1. - (theta_c / (RadOfDeg(CAM_PAN_NEUTRAL - CAM_PAN_MIN)))); 
+    cam_pan = MIN_PPRZ * (pan_diff / (RadOfDeg(CAM_PAN_MIN - CAM_PAN_NEUTRAL))); 
 #endif
   
-#ifdef CAM_TILT_NEUTRAL  
-  if (theta_c > RadOfDeg(CAM_TILT_NEUTRAL))
-    cam_tilt = MAX_PPRZ * (theta_c / (RadOfDeg(CAM_TILT_MAX - CAM_TILT_NEUTRAL)) - 1.); 
+#ifdef CAM_TILT_NEUTRAL
+  float tilt_diff = cam_tilt_c - RadOfDeg(CAM_TILT_NEUTRAL);
+  if (tilt_diff > 0)
+    cam_tilt = MAX_PPRZ * (tilt_diff / (RadOfDeg(CAM_TILT_MAX - CAM_TILT_NEUTRAL))); 
   else
-    cam_tilt = MIN_PPRZ * (1. - (theta_c / (RadOfDeg(CAM_TILT_NEUTRAL - CAM_TILT_MIN)))); 
+    cam_tilt = MIN_PPRZ * (tilt_diff / (RadOfDeg(CAM_TILT_MIN - CAM_TILT_NEUTRAL))); 
 #endif
-
+  
   cam_pan = TRIM_PPRZ(cam_pan);
   cam_tilt = TRIM_PPRZ(cam_tilt);
-
-#ifdef COMMAND_HATCH
-//  if (0 != use_hatch_cam) 
-  ap_state->commands[COMMAND_HATCH] = cam_tilt;
-#endif
+  
 #ifdef COMMAND_CAM_PAN
-    ap_state->commands[COMMAND_CAM_PAN] = cam_pan;
+  ap_state->commands[COMMAND_CAM_PAN] = cam_pan;
 #endif
 #ifdef COMMAND_CAM_TILT
-    ap_state->commands[COMMAND_CAM_TILT] = cam_tilt;
+  ap_state->commands[COMMAND_CAM_TILT] = cam_tilt;
 #endif
-
-  /* not true in all camera mount cases */
-  phi_c = pan_c;
-  theta_c = tilt_c;
 }
 
-#define MAX_DIST_TARGET 500.
+/** Computes the right angles from target_x, target_y, target_alt */
+void cam_target( void ) {
+#ifdef TEST_CAM
+  vPoint(test_cam_estimator_x, test_cam_estimator_y, test_cam_estimator_z,
+	 test_cam_estimator_phi, test_cam_estimator_theta, test_cam_estimator_hspeed_dir,
+	 cam_target_x, cam_target_y, cam_target_alt,
+	 &cam_pan_c, &cam_tilt_c);
+#else
+  vPoint(estimator_x, estimator_y, estimator_z,
+	 estimator_phi, estimator_theta, estimator_hspeed_dir,
+	 cam_target_x, cam_target_y, cam_target_alt,
+	 &cam_pan_c, &cam_tilt_c);
+#endif
+  cam_angles();
+}
 
-void cam_manual_target( void ) {
-  int16_t yaw = fbw_state->channels[RADIO_YAW];
-  if (yaw > MIN_PPRZ_CAM || yaw < -MIN_PPRZ_CAM) {
-    target_x += FLOAT_OF_PPRZ(yaw, 0, -20.);
-    target_x = Min(target_x, MAX_DIST_TARGET + estimator_x);
-    target_x = Max(target_x, -MAX_DIST_TARGET + estimator_x);
-  }
-  int16_t pitch = fbw_state->channels[RADIO_PITCH];
-  if (pitch > MIN_PPRZ_CAM || pitch < -MIN_PPRZ_CAM) {
-    target_y += FLOAT_OF_PPRZ(pitch, 0, -20.);
-    target_y = Min(target_y, MAX_DIST_TARGET + estimator_y);
-    target_y = Max(target_y, -MAX_DIST_TARGET + estimator_y);
-  }
-  target_alt = GROUND_ALT;
+/** Point straight down */
+void cam_nadir( void ) {
+#ifdef TEST_CAM
+  cam_target_x = test_cam_estimator_x;
+  cam_target_y = test_cam_estimator_y;
+#else
+  cam_target_x = estimator_x;
+  cam_target_y = estimator_y;
+#endif
+  cam_target_alt = ground_alt;
   cam_target();
 }
 
-void cam_waypoint_target( uint8_t wp ) {
-  target_x = waypoints[wp].x;
-  target_y = waypoints[wp].y;
-  target_alt = GROUND_ALT;
+
+void cam_waypoint_target( void ) {
+  if (cam_target_wp < nb_waypoint) {
+    cam_target_x = waypoints[cam_target_wp].x;
+    cam_target_y = waypoints[cam_target_wp].y;
+  }
+  cam_target_alt = ground_alt;
   cam_target();
 }
 
 void cam_ac_target( uint8_t ac_id ) {
   struct ac_info_ * ac = get_ac_info(ac_id);
-  target_x = ac->east;
-  target_y = ac->north;
-  target_alt = ac->alt;
+  cam_target_x = ac->east;
+  cam_target_y = ac->north;
+  cam_target_alt = ac->alt;
   cam_target();
 }
-
-void cam_carrot( void ) {
-  target_x = carrot_x;
-  target_y = carrot_y;
-  cam_target();
-}
-
-void cam_init( void ) {
-}
-
-void cam_periodic( void ) {
-}
-
-#endif // MOBILE_CAM
--- sw/airborne/cam.h
+++ sw/airborne/cam.h
@@ -1,7 +1,7 @@
 /*
  * $Id$
  *  
- * Copyright (C) 2005  Pascal Brisset, Antoine Drouin
+ * Copyright (C) 2005-  Pascal Brisset, Antoine Drouin
  *
  * This file is part of paparazzi.
  *
@@ -30,26 +30,33 @@
 #define CAM_H
 
 #include <inttypes.h>
+#include "inter_mcu.h"
 
+#define CAM_MODE_OFF 0
+#define CAM_MODE_ANGLES 1
+#define CAM_MODE_NADIR 2
+#define CAM_MODE_XY_TARGET 3
+#define CAM_MODE_WP_TARGET 4
 
-extern float phi_c, theta_c;
-extern float pan_c, tilt_c;
-extern float target_x, target_y;
+extern uint8_t cam_mode;
 
-extern int use_hatch_cam;
-extern int cam_mode_auto;
+extern float cam_phi_c, cam_theta_c;
 
-#define CamNull() { phi_c = 0; theta_c = 0;  pan_c = 0; tilt_c = 0; }
-#define CamFix() {}
+extern float cam_pan_c, cam_tilt_c;
+/* pan (move left and right), tilt (move up and down) */
+/** Radians, for CAM_MODE_ANGLES mode */
 
-void cam_nadir(void);
-void cam_target(void);
-void cam_manual(void);
-void cam_manual_target(void);
-void cam_waypoint_target(uint8_t wp);
-void cam_carrot(void);
-void cam_ac_target( uint8_t ac_id );
+extern float cam_target_x, cam_target_y;
+/** For CAM_MODE_XY_TARGET mode */
+
+extern uint8_t cam_target_wp;
+/** For CAM_MODE_WP_TARGET mode */
 
 void cam_periodic( void );
 void cam_init( void );
+
+extern int16_t cam_pan_command;
+#define cam_SetPanCommand(x) { ap_state->commands[COMMAND_CAM_PAN] = cam_pan_command = x;}
+extern int16_t cam_tilt_command;
+#define cam_SetTiltCommand(x) { ap_state->commands[COMMAND_CAM_TILT] = cam_tilt_command = x;}
 #endif
