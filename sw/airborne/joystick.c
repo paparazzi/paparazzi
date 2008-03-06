885ec65f5ab01f6a189c9badfd314a321afeb310
Antoine Drouin <poinix@gmail.com>
2008-03-06 14:24:46 +0000

    *** empty log message ***

---
+++ sw/airborne/joystick.c
@@ -0,0 +1,9 @@
+#include "joystick.h"
+
+float joystick_x;
+float joystick_y;
+
+void joystick_init(void) {
+  joystick_x = 0.;
+  joystick_y = 0.;
+}
---
+++ sw/airborne/joystick.h
@@ -0,0 +1,9 @@
+#ifndef JOYSTICK_H
+#define JOYSTICK_H
+
+extern float joystick_x;
+extern float joystick_y;
+
+extern void joystick_init(void);
+
+#endif /* JOYSTICK_H */
