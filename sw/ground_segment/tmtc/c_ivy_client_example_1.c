ef708472eb54a381b23bd279411f18434dd879d7
Antoine Drouin <poinix@gmail.com>
2008-06-30 05:54:02 +0000

    *** empty log message ***

---
+++ sw/ground_segment/tmtc/c_ivy_client_example_1.c
@@ -0,0 +1,28 @@
+#include <glib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <Ivy/ivy.h>
+#include <Ivy/ivyglibloop.h>
+
+
+static void on_Attitude(IvyClientPtr app, void *user_data, int argc, char *argv[]){
+  guint ac_id = atoi(argv[0]);
+  float estimator_phi = argv[1];
+}
+
+
+int main ( int argc, char** argv) {
+
+  GMainLoop *ml =  g_main_loop_new(NULL, FALSE);
+
+  IvyInit ("Example1", "Example1 READY", NULL, NULL, NULL, NULL);
+  IvyBindMsg(on_Attitude, NULL, "^(\\S*) ATTITUDE (\\S*) (\\S*) (\\S*)");
+
+  IvyStart("127.255.255.255");
+  
+  g_main_loop_run(ml);
+
+  return 0;
+}
+
