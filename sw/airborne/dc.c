a6e0d58c41b086407a0e577cff71af6e74044ba9
Pascal Brisset <pascal.brisset@free.fr>
2008-07-20 21:59:19 +0000

    Periodic shot

--- sw/airborne/dc.c
+++ sw/airborne/dc.c
@@ -1,3 +1,5 @@
 #include "dc.h"
 
 uint8_t dc_timer;
+uint8_t dc_periodic_shutter;
+uint8_t dc_shutter_timer;
--- sw/airborne/dc.h
+++ sw/airborne/dc.h
@@ -1,31 +1,36 @@
 #ifndef DC_H
 #define DC_H
 
-#include <inttypes.h>
-
-extern uint8_t dc_timer;
-
-#ifdef SITL
-
-#define dc_Shutter(_) { }
-#define dc_Zoom(_) { }
-
-#else /* SITL */
-
-#include "LPC21xx.h"
 #include "std.h"
 #include "led.h"
 #include "airframe.h"
+#include "downlink.h"
+#include "estimator.h"
+#include "gps.h"
 
-#define SHUTTER_DELAY 2  /* 4Hz */
 
-static inline uint8_t dc_shutter() {
+extern uint8_t dc_timer;
+
+extern uint8_t dc_periodic_shutter;
+/* In s. If non zero, period of automatic calls to dc_shutter() */
+extern uint8_t dc_shutter_timer;
+/* In s. Related counter */
+
+
+#define SHUTTER_DELAY 2  /* 4Hz -> 0.5s */
+
+static inline uint8_t dc_shutter( void ) {
   dc_timer = SHUTTER_DELAY; 
   LED_OFF(DC_SHUTTER_LED);
+
+  int16_t phi = DegOfRad(estimator_phi);
+  int16_t theta = DegOfRad(estimator_theta);
+  DOWNLINK_SEND_DC_SHOT( &gps_utm_east, &gps_utm_north, &gps_utm_zone, &gps_course, &estimator_z, &phi, &theta);
+
   return 0;
 }
 
-static inline uint8_t dc_zoom() {
+static inline uint8_t dc_zoom( void ) {
   dc_timer = SHUTTER_DELAY; 
   LED_OFF(DC_ZOOM_LED);
   return 0;
@@ -33,12 +38,30 @@ static inline uint8_t dc_zoom() {
 
 #define dc_Shutter(_) { dc_shutter(); }
 #define dc_Zoom(_) { dc_zoom(); }
+#define dc_Periodic(s) { dc_periodic_shutter = s; dc_shutter_timer = s; }
 
-#define dc_init() { /* initialized as leds */ } /* Output */
+#define dc_init() { /* initialized as leds */ dc_periodic_shutter = 0; } /* Output */
 
 /* 4Hz */
-#define dc_periodic() { if (dc_timer) { dc_timer--; } else { LED_ON(DC_SHUTTER_LED); LED_ON(DC_ZOOM_LED); } }
+static inline void dc_periodic( void ) {
+  if (dc_timer) {
+    dc_timer--;
+  } else {
+    LED_ON(DC_SHUTTER_LED);
+    LED_ON(DC_ZOOM_LED);
+  }
 
-#endif /* ! SITL */
+  if (dc_periodic_shutter) {
+    RunOnceEvery(4, 
+    {
+      if (dc_shutter_timer) {
+	dc_shutter_timer--;
+      } else {
+	dc_shutter();
+	dc_shutter_timer = dc_periodic_shutter;
+      }
+    });
+  }
+}
 
-#endif
+#endif // DC_H
