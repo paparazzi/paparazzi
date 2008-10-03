5306869767be0ff31d62ed249c5d76e24f44bd22
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2008-10-03 12:25:42 +0000

    *** empty log message ***

--- sw/airborne/booz/booz_datalink.c
+++ sw/airborne/booz/booz_datalink.c
@@ -18,21 +18,27 @@ void dl_parse_msg(void) {
   switch (msg_id) {
   
   case  DL_PING:
-    DOWNLINK_SEND_PONG();
+    {
+      DOWNLINK_SEND_PONG();
+    }
     break;
     
   case DL_SETTING :
-    uint8_t i = DL_SETTING_index(dl_buffer);
-    float var = DL_SETTING_value(dl_buffer);
-    DlSetting(i, var);
-    DOWNLINK_SEND_DL_VALUE(&i, &var);
+    {
+      uint8_t i = DL_SETTING_index(dl_buffer);
+      float var = DL_SETTING_value(dl_buffer);
+      DlSetting(i, var);
+      DOWNLINK_SEND_DL_VALUE(&i, &var);
+    }
     break;
 
 #ifdef BOOZ2_STICK
   case DL_COMMANDS_RAW :
-    if (DL_COMMANDS_RAW_ac_id(dl_buffer) != AC_ID) break;
-    uint8_t l = DL_COMMANDS_RAW_commands_length(dl_buffer);
-    BOOZ2_PARSE_STICK_COMMAND(l,DL_COMMANDS_RAW_commands(dl_buffer));
+    {
+      if (DL_COMMANDS_RAW_ac_id(dl_buffer) != AC_ID) break;
+      uint8_t l = DL_COMMANDS_RAW_commands_length(dl_buffer);
+      BOOZ2_PARSE_STICK_COMMAND(l,DL_COMMANDS_RAW_commands(dl_buffer));
+    }
     break;
 #endif
 
