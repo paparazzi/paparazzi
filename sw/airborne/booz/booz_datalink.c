7c103e40c69dce29d508aeb7012561433dee30d6
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2008-10-03 08:55:57 +0000

    parse commands message

--- sw/airborne/booz/booz_datalink.c
+++ sw/airborne/booz/booz_datalink.c
@@ -7,24 +7,34 @@
 #include "dl_protocol.h"
 #include "uart.h"
 
+#ifdef BOOZ2_STICK
+#include "booz2_stick.h"
+#endif
+
 #define IdOfMsg(x) (x[1])
 
 void dl_parse_msg(void) {
   uint8_t msg_id = IdOfMsg(dl_buffer);
   switch (msg_id) {
   
-  case  DL_PING: {
+  case  DL_PING:
     DOWNLINK_SEND_PONG();
     break;
-  }
     
-  case DL_SETTING : {
+  case DL_SETTING :
     uint8_t i = DL_SETTING_index(dl_buffer);
     float var = DL_SETTING_value(dl_buffer);
     DlSetting(i, var);
     DOWNLINK_SEND_DL_VALUE(&i, &var);
     break;
-  }
+
+#ifdef BOOZ2_STICK
+  case DL_COMMANDS_RAW :
+    if (DL_COMMANDS_RAW_ac_id(dl_buffer) != AC_ID) break;
+    uint8_t l = DL_COMMANDS_RAW_commands_length(dl_buffer);
+    BOOZ2_PARSE_STICK_COMMAND(l,DL_COMMANDS_RAW_commands(dl_buffer));
+    break;
+#endif
 
   }
 }
