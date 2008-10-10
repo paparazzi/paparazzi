2edd64b98bfcd7a6367afc5ee64a0891c1f7fa14
Gustavo Oliveira Violato <gustavoviolato@gmail.com>
2008-10-10 12:51:19 +0000

    *** empty log message ***

--- sw/in_progress/wind_tunnel/main.c
+++ sw/in_progress/wind_tunnel/main.c
@@ -2,10 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
 #include <Ivy/ivy.h>
 #include <Ivy/ivyglibloop.h>
 
 #include "serial_port.h"
+
+#define BUF_SIZE 512
+#define AC_ID 2
 
 static struct SerialPort* sp;
 static GIOChannel* ioc;
@@ -26,48 +34,62 @@ static void configure_term(struct termios *termios, speed_t *speed) {
 }
 
 static void dump_buf ( int len, char* buf) {
+  
+  static gchar buf2[BUF_SIZE];
+  static int cur_len = 0;
   int i;
-  for (i=0; i<len; i++)
-    printf("%x", buf[i]);
-  printf("\n");
+
+  for (i=0; i<len; i++) {
+    if (buf[i] != '\r') {
+      if (buf[i] != 'T' && buf[i] != '.'){
+	buf2[cur_len] = buf[i];
+	cur_len++;
+      }
+    }
+    else {
+      int force;
+      buf2[cur_len] = 0;
+      g_message("buf2: %s",buf2);
+      sscanf(buf2,"%d",&force);
+      IvySendMsg("%d X_FORCE %d", AC_ID, force);
+      cur_len = 0;
+    }
+  }
+
+  //g_message("read %s and %s",buf,buf2);
 }
 
-#define BUF_SIZE 512
 static gboolean on_serial_data_received(GIOChannel *source,
                                         GIOCondition condition,
                                         gpointer data) {
 
-  //  g_message("hello world");
-
-
- static gchar buf[BUF_SIZE];
+  static gchar buf[BUF_SIZE];
   gsize len;
+  //int fd = open("data", O_RDWR | O_APPEND | O_CREAT);
+  
   g_io_channel_read_chars(source, buf, BUF_SIZE, &len, NULL);
-
-  //  g_message("read %d %d %d %d", len, buf[0], buf[1], buf[2]);
-  //dump_buf(len, buf);
-
-  g_message("read %s", buf);
-
+  dump_buf(len, buf);
+  
   return TRUE;
 }
 
 gboolean timeout_callback(gpointer data) {
 
-  //  g_message("hello world");
-
   const char* msg = "GT\n";
   gsize bw;
+  
   g_io_channel_write_chars(ioc, msg, 3, &bw, NULL); 
-  g_io_channel_flush(ioc, NULL);//  int foo = 42;
-  //  IvySendMsg("ME HELLO_WORLD 1234 5678 %d", foo);
+  g_io_channel_flush(ioc, NULL);
+  
   return TRUE;
+  
 }
 
 int main ( int argc, char** argv) {
 
   sp = serial_port_new();
-  serial_port_open(sp, "/dev/ttyUSB0", configure_term);
+  serial_port_open(sp, "/dev/ttyUSB1", configure_term);
+  
   ioc = g_io_channel_unix_new(sp->fd);
   g_io_channel_set_encoding(ioc, NULL, NULL);
   g_io_channel_set_flags (ioc,G_IO_FLAG_NONBLOCK, NULL );
@@ -76,7 +98,7 @@ int main ( int argc, char** argv) {
 
   GMainLoop *ml =  g_main_loop_new(NULL, FALSE);
 
-  IvyInit ("Example2", "Example2 READY", NULL, NULL, NULL, NULL);
+  IvyInit ("Wind_Tunnel", "Wind_Tunnel READY", NULL, NULL, NULL, NULL);
   IvyStart("127.255.255.255");
 
   g_timeout_add(500, timeout_callback, NULL);
