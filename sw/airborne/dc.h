fd181515ac1f711a095c0f38060a45381cdb3e4e
Pascal Brisset <pascal.brisset@free.fr>
2008-10-30 10:08:40 +0000

    comments added

--- sw/airborne/dc.h
+++ sw/airborne/dc.h
@@ -1,3 +1,52 @@
+/*
+ * Paparazzi $Id$
+ *  
+ * Copyright (C) 2003-2008  Pascal Brisset, Antoine Drouin
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA. 
+ *
+ */
+
+/** \file dc.h
+ *  \brief Digital Camera Control
+ * 
+ * Provides the control of the shutter and the zoom of a digital camera
+ * through standard binary IOs of the board.
+ *
+ * Configuration: 
+ *  Since the API of led.h is used, connected pins must be defined as led
+ *  numbers (usually in the airframe file):
+ *   <define name="DC_SHUTTER_LED" value="6"/>
+ *   <define name="DC_ZOOM_LED" value="7"/>
+ *  Related bank and pin must also be defined:
+ *   <define name="LED_6_BANK" value="0"/>
+ *   <define name="LED_6_PIN" value="2"/>
+ *  The required initialization (dc_init()) and periodic (4Hz) process
+ *  (dc_periodic()) are called if the DIGITAL_CAM flag is set:
+ *   ap.CFLAGS += -DDIGITAL_CAM
+ *
+ * Usage (from the flight plan, the settings or any airborne code):
+ *  - dc_Shutter(_) sets the DC_SHUTTER_LED pin output to 1 for 0.5s and sends
+ *   a DC_SHOT message
+ *  - dc_Zoom(_) sets the DC_ZOOM_LED pin output to 1 for 0.5s
+ *  - dc_Periodic(s) activates a periodic call to dc_Shutter() every s seconds
+ */
+
 #ifndef DC_H
 #define DC_H
 
@@ -7,9 +56,6 @@
 #include "ap_downlink.h"
 #include "estimator.h"
 #include "gps.h"
-
-
-
 
 extern uint8_t dc_timer;
 
