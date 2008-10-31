ade630c5f2849c9655953749512cf41a0df0a81a
Pascal Brisset <pascal.brisset@free.fr>
2008-10-31 16:10:37 +0000

    cleaning and comments

--- sw/tools/gen_messages.ml
+++ sw/tools/gen_messages.ml
@@ -3,7 +3,7 @@
  *
  * XML preprocessing of messages.xml for downlink protocol
  *  
- * Copyright (C) 2003-2005 Pascal Brisset, Antoine Drouin
+ * Copyright (C) 2003-2008 ENAC, Pascal Brisset, Antoine Drouin
  *
  * This file is part of paparazzi.
  *
@@ -24,28 +24,27 @@
  *
  *)
 
-(** FIXME: Should use the Pprz Module !!! *)
-
 open Printf
 
-
+type format = string
+      
+type _type = 
+    Basic of string
+  | Array of string * string
+	
+type field = _type  * string * format option
+      
+type fields = field list
+      
+type message = { 
+    name : string; 
+    id : int; 
+    period : float option;
+    fields : fields
+  }
+      
 module Syntax = struct
-  type format = string
-
-  type type_name = string
-
-  type _type = 
-      Basic of string
-    | Array of string * string
-
-  type field = _type  * string * format option
-
-  type fields = field list
-
-  type message = { name : string ; id : int; period : float option; fields : fields }
-
-  type messages = message list
-
+  (** Parse a type name and returns a _type value *)
   let parse_type = fun t varname ->
     let n = String.length t in
     if n >=2 && String.sub t (n-2) 2 = "[]" then
@@ -59,218 +58,201 @@ module Syntax = struct
     try
       List.assoc t Pprz.types
     with
-      Not_found -> fprintf stderr "Error: '%s' unknown type\n" t; exit 1
+      Not_found -> 
+	failwith (sprintf "Error: '%s' unknown type\n" t)
 
   let rec sizeof = function
       Basic t -> string_of_int (assoc_types t).Pprz.size
     | Array (t, varname) -> sprintf "1+%s*%s" (length_name varname) (sizeof (Basic t))
+
   let rec nameof = function
       Basic t -> String.capitalize t
-    | Array (t, varname) -> failwith "nameof"
+    | Array _ -> failwith "nameof"
 
-  let formatof = fun t -> (assoc_types t).Pprz.format
-
-  let print_format t = function
-      None -> printf "(%s)" (formatof t)
-    | Some f -> printf "(%s)" f
-
-  open Xml
-
-  let xml_error s = failwith ("Bad XML tag: "^s^ " expected")
-
-  let fprint_fields = fun f l ->
-    fprintf f "<";
-    List.iter (fun (a, b) -> fprintf f "%s=\"%s\" " a b) l;
-    fprintf f ">"
-      
-
-  let assoc_or_fail x l =
-    let x = String.uppercase x
-    and l = List.map (fun (a, v) -> String.uppercase a, v) l in
-    try
-      List.assoc x l
-    with
-      Not_found ->
-	fprintf stderr "Error: Field '%s' expected in <%a>" x fprint_fields l;
-	exit 1
-
-  let of_xml = function
-      Element ("message", fields, l) ->
-	let name = assoc_or_fail "name" fields
-	and id = int_of_string (assoc_or_fail "id" fields) in
-	{ id=id; name = name;
-	  period = (try Some (float_of_string (List.assoc "period" fields)) with Not_found -> None);
-	  fields=List.map (function
-	      Element ("field", fields, []) ->
-		let id = assoc_or_fail "name" fields
-		and type_name = assoc_or_fail "type" fields
-		and fmt = try Some (List.assoc "format" fields) with _ -> None in
-		let _type = parse_type type_name id in
-		
-		(_type, id, fmt)
-	    | _ -> xml_error "field")
-	    l}
-    | _ -> xml_error "message with id"
+  (** Translates a "message" XML element into a value of the 'message' type *)
+  let struct_of_xml = fun xml ->
+    let name = ExtXml.attrib xml "name"
+    and id = ExtXml.int_attrib xml "id"
+    and period = try Some (ExtXml.float_attrib xml "period") with _ -> None
+    and fields =
+      List.map
+	(fun field ->
+	  let id = ExtXml.attrib field "name"
+	  and type_name = ExtXml.attrib field "type"
+	  and fmt = try Some (Xml.attrib field "format") with _ -> None in
+	  let _type = parse_type type_name id in
+  	  (_type, id, fmt))
+	(Xml.children xml) in 
+    { id=id; name = name; period = period; fields = fields }
 	  
-
-  let read filename class_ = 
-    let xml =
-      try Xml.parse_file filename with
-	Xml.Error (msg, pos) -> fprintf stderr "%s:%d : %s\n" filename (Xml.line pos) (Xml.error_msg msg); exit 1
-    in
+  (** Translates one class of a XML message file into a list of messages *)
+  let read = fun filename class_ ->
+    let xml = Xml.parse_file filename in
     try
-      let xml_class =
-	List.find (fun x -> Xml.attrib x "name" = class_) (Xml.children xml) in
-      List.map of_xml (Xml.children xml_class)
+      let xml_class = ExtXml.child ~select:(fun x -> Xml.attrib x "name" = class_) xml "class" in
+      List.map struct_of_xml (Xml.children xml_class)
     with
       Not_found -> failwith (sprintf "No class '%s' found" class_)
-end
+end (* module Suntax *)
 
 
-let check_alignment = ref true
-
+(** Pretty printer of C macros for sending and parsing messages *)
 module Gen_onboard = struct
-  open Printf
-  open Syntax
-
-  let print_avr_field = fun avr_h (t, name, (_f:format option)) ->
+  let print_field = fun h (t, name, (_f: format option)) ->
     match t with 
       Basic _ ->
-	fprintf avr_h "\t  DownlinkPut%sByAddr((%s)); \\\n" (nameof t) name
+	fprintf h "\t  DownlinkPut%sByAddr((%s)); \\\n" (Syntax.nameof t) name
     | Array (t, varname) ->
-	let _s = sizeof (Basic t) in
-	fprintf avr_h "\t  DownlinkPut%sArray(%s, %s); \\\n" (nameof (Basic t)) (length_name varname) name
+	let _s = Syntax.sizeof (Basic t) in
+	fprintf h "\t  DownlinkPut%sArray(%s, %s); \\\n" (Syntax.nameof (Basic t)) (Syntax.length_name varname) name
 
-  let print_one avr_h = function
-      (Array _, s, _) -> fprintf avr_h "%s, %s" (length_name s) s
-    | (_, s, _) -> fprintf avr_h "%s" s
+  let print_parameter h = function
+      (Array _, s, _) -> fprintf h "%s, %s" (Syntax.length_name s) s
+    | (_, s, _) -> fprintf h "%s" s
   
-  let print_avr_macro_names avr_h = function
+  let print_macro_parameters h = function
       [] -> ()
     | f::fields ->
-	print_one avr_h f;
-	List.iter (fun f -> fprintf avr_h ", "; print_one avr_h f) fields
+	print_parameter h f;
+	List.iter (fun f -> fprintf h ", "; print_parameter h f) fields
 
   let rec size_fields = fun fields size ->
     match fields with
       [] -> size
-    | (t, _, _)::fields -> size_fields fields (size ^"+"^sizeof t)
+    | (t, _, _)::fields -> size_fields fields (size ^"+"^Syntax.sizeof t)
 
   let size_of_message = fun m -> size_fields m.fields "0"
       
-  let print_avr_macro = fun avr_h {name=s; fields = fields} ->
-    fprintf avr_h "#define DOWNLINK_SEND_%s(" s;
-    print_avr_macro_names avr_h fields;
-    fprintf avr_h "){ \\\n";
+  let print_downlink_macro = fun h {name=s; fields = fields} ->
+    fprintf h "#define DOWNLINK_SEND_%s(" s;
+    print_macro_parameters h fields;
+    fprintf h "){ \\\n";
     let size = (size_fields fields "0") in
-    fprintf avr_h "\tif (DownlinkCheckFreeSpace(DownlinkSizeOf(%s))) {\\\n" size;
-    fprintf avr_h "\t  DownlinkCountBytes(DownlinkSizeOf(%s)); \\\n" size;
-    fprintf avr_h "\t  DownlinkStartMessage(\"%s\", DL_%s, %s) \\\n" s s size;
-    List.iter (print_avr_field avr_h) fields;
-    fprintf avr_h "\t  DownlinkEndMessage() \\\n";
-    fprintf avr_h "\t} else \\\n";
-    fprintf avr_h "\t  DownlinkOverrun(); \\\n";
-    fprintf avr_h "}\n\n"
+    fprintf h "\tif (DownlinkCheckFreeSpace(DownlinkSizeOf(%s))) {\\\n" size;
+    fprintf h "\t  DownlinkCountBytes(DownlinkSizeOf(%s)); \\\n" size;
+    fprintf h "\t  DownlinkStartMessage(\"%s\", DL_%s, %s) \\\n" s s size;
+    List.iter (print_field h) fields;
+    fprintf h "\t  DownlinkEndMessage() \\\n";
+    fprintf h "\t} else \\\n";
+    fprintf h "\t  DownlinkOverrun(); \\\n";
+    fprintf h "}\n\n"
 
-  let print_null_avr_macro = fun avr_h {name=s; fields = fields} ->
-    fprintf avr_h "#define DOWNLINK_SEND_%s(" s;
-    print_avr_macro_names avr_h fields;
-    fprintf avr_h ") {}\n"
+  let print_null_downlink_macro = fun h {name=s; fields = fields} ->
+    fprintf h "#define DOWNLINK_SEND_%s(" s;
+    print_macro_parameters h fields;
+    fprintf h ") {}\n"
 
-  let print_enum = fun avr_h class_ messages ->
+  (** Prints the messages ids *)
+  let print_enum = fun h class_ messages ->
     List.iter (fun m ->
       if m.id < 0 || m.id > 255 then begin 
         fprintf stderr "Error: message %s has id %d but should be between 0 and 255\n" m.name m.id; exit 1;
       end
-      else fprintf avr_h "#define DL_%s %d\n" m.name m.id
+      else fprintf h "#define DL_%s %d\n" m.name m.id
       ) messages;
-    fprintf avr_h "#define DL_MSG_%s_NB %d\n\n" class_ (List.length messages)
+    fprintf h "#define DL_MSG_%s_NB %d\n\n" class_ (List.length messages)
 
-  let print_lengths_array = fun avr_h class_ messages ->
+  (** Prints the table of the messages lengths *)
+  let print_lengths_array = fun h class_ messages ->
     let sizes = List.map (fun m -> (m.id, size_of_message m)) messages in
     let max_id = List.fold_right (fun (id, _m) x -> max x id) sizes min_int in
     let n = max_id + 1 in
-    fprintf avr_h "#define MSG_%s_LENGTHS {" class_;
+    fprintf h "#define MSG_%s_LENGTHS {" class_;
     for i = 0 to n - 1 do
-      fprintf avr_h "%s," (try "(2+" ^ List.assoc i sizes^")" with Not_found -> "0")
+      fprintf h "%s," (try "(2+" ^ List.assoc i sizes^")" with Not_found -> "0")
     done;
-    fprintf avr_h "}\n\n"
+    fprintf h "}\n\n"
 
-  let print_avr_macros = fun filename avr_h class_ messages ->
-    print_enum avr_h class_ messages;
-    print_lengths_array avr_h class_ messages;
-    List.iter (print_avr_macro avr_h) messages
+  (** Prints the macros required to send a message *)
+  let print_downlink_macros = fun h class_ messages ->
+    print_enum h class_ messages;
+    print_lengths_array h class_ messages;
+    List.iter (print_downlink_macro h) messages
       
-  let print_null_avr_macros = fun avr_h messages ->
-    List.iter (print_null_avr_macro avr_h) messages
+  let print_null_downlink_macros = fun h messages ->
+    List.iter (print_null_downlink_macro h) messages
 
-  let print_get_macros = fun avr_h message ->
+  (** Prints the macro to get access to the fields of a received message *)
+  let print_get_macros = fun h check_alignment message ->
     let msg_name = message.name in
     let offset = ref Pprz.offset_fields in
-    
+   
+    (** Prints the macro for one field, using the global [offset] ref *)
     let parse_field = fun (_type, field_name, _format) ->
       if !offset < 0 then
-	failwith "FIXME: No field allowed after an array field (print_get_macros)x";
-      let typed = fun o t ->
-	let size = (assoc_types t).Pprz.size in
-	if !check_alignment && o mod size <> 0 then
+	failwith "FIXME: No field allowed after an array field (print_get_macros)";
+      (** Converts bytes into the required type *)
+      let typed = fun o pprz_type -> (* o for offset *)
+	let size = pprz_type.Pprz.size in
+	if check_alignment && o mod size <> 0 then
 	  failwith (sprintf "Wrong alignment of field '%s' in message '%s" field_name msg_name);
+	
 	match size with
-	  1 -> sprintf "(%s)(*((uint8_t*)_payload+%d))" (assoc_types t).Pprz.inttype o
-	| 2 -> sprintf "(%s)(*((uint8_t*)_payload+%d)|*((uint8_t*)_payload+%d+1)<<8)" (assoc_types t).Pprz.inttype o o
-	| 4 when (assoc_types t).Pprz.inttype = "float" -> 
+	  1 -> sprintf "(%s)(*((uint8_t*)_payload+%d))" pprz_type.Pprz.inttype o
+	| 2 -> sprintf "(%s)(*((uint8_t*)_payload+%d)|*((uint8_t*)_payload+%d+1)<<8)" pprz_type.Pprz.inttype o o
+	| 4 when pprz_type.Pprz.inttype = "float" -> 
 	    sprintf "({ union { uint32_t u; float f; } _f; _f.u = (uint32_t)(*((uint8_t*)_payload+%d)|*((uint8_t*)_payload+%d+1)<<8|((uint32_t)*((uint8_t*)_payload+%d+2))<<16|((uint32_t)*((uint8_t*)_payload+%d+3))<<24); _f.f; })" o o o o
 	| 4 -> 
-	    sprintf "(%s)(*((uint8_t*)_payload+%d)|*((uint8_t*)_payload+%d+1)<<8|((uint32_t)*((uint8_t*)_payload+%d+2))<<16|((uint32_t)*((uint8_t*)_payload+%d+3))<<24)" (assoc_types t).Pprz.inttype o o o o
+	    sprintf "(%s)(*((uint8_t*)_payload+%d)|*((uint8_t*)_payload+%d+1)<<8|((uint32_t)*((uint8_t*)_payload+%d+2))<<16|((uint32_t)*((uint8_t*)_payload+%d+3))<<24)" pprz_type.Pprz.inttype o o o o
 	| _ -> failwith "unexpected size in Gen_messages.print_get_macros" in
+
+      (** To be an array or not to be an array: *)
       match _type with 
 	Basic t ->
-	  fprintf avr_h "#define DL_%s_%s(_payload) (%s)\n" msg_name field_name (typed !offset t);
-	  offset := !offset + int_of_string (sizeof _type)
-      | Array (t, varname) ->
-	  fprintf avr_h "#define DL_%s_%s_length(_payload) (%s)\n" msg_name field_name (typed !offset "uint8");
+	  let pprz_type = Syntax.assoc_types t in
+	  fprintf h "#define DL_%s_%s(_payload) (%s)\n" msg_name field_name (typed !offset pprz_type);
+	  offset := !offset + pprz_type.Pprz.size
+
+      | Array (t, _varname) ->
+	  (** The macro to access to the length of the array *)
+	  fprintf h "#define DL_%s_%s_length(_payload) (%s)\n" msg_name field_name (typed !offset (Syntax.assoc_types "uint8"));
 	  incr offset;
-	  fprintf avr_h "#define DL_%s_%s(_payload) ((%s*)_payload+%d)\n" msg_name field_name (assoc_types t).Pprz.inttype !offset;
+
+	  (** The macro to access to the array itself *)
+	  fprintf h "#define DL_%s_%s(_payload) ((%s*)_payload+%d)\n" msg_name field_name (Syntax.assoc_types t).Pprz.inttype !offset;
 	  offset := -1 (** Mark for no more fields *)
     in
     
-    fprintf avr_h "\n";
+    fprintf h "\n";
+    (** Do it for all the fields of the message *)
     List.iter parse_field message.fields
 
-end
+end (* module Gen_onboard *)
 
-let _ =
+
+(********************* Main **************************************************)
+let () =
   if Array.length Sys.argv <> 3 then begin
     failwith (sprintf "Usage: %s <.xml file> <class_name>" Sys.argv.(0)) 
   end;
-  let filename = Sys.argv.(1) in
-  let class_name = Sys.argv.(2) in
 
-  let messages = Syntax.read filename class_name in
+  let filename = Sys.argv.(1)
+  and class_name = Sys.argv.(2) in
 
-  let avr_h = stdout in
+  try
+    let messages = Syntax.read filename class_name in
+    
+    let h = stdout in
+    
+    Printf.fprintf h "/* Automatically generated from %s */\n" filename;
+    Printf.fprintf h "/* Please DO NOT EDIT */\n";
+    
+    Printf.fprintf h "/* Macros to send and receive messages of class %s */\n" class_name;
+    
+    (** Macros for airborne downlink (sending) *)
+    if class_name = "telemetry" then begin (** FIXME *)
+      Printf.fprintf h "#ifdef DOWNLINK\n"
+    end;
+    Gen_onboard.print_downlink_macros h class_name messages;
+    if class_name = "telemetry" then begin
+      Printf.fprintf h "#else // DOWNLINK\n";
+      Gen_onboard.print_null_downlink_macros h messages;
+      Printf.fprintf h "#endif // DOWNLINK\n"
+    end;
+    
+    (** Macros for airborne datalink (receiving) *)
+    let check_alignment = class_name <> "telemetry" in
+    List.iter (Gen_onboard.print_get_macros h check_alignment) messages
 
-(*  Printf.fprintf avr_h "#ifndef _MESSAGES_H\n";
-  Printf.fprintf avr_h "#define _MESSAGES_H\n"; *)
-  Printf.fprintf avr_h "/* Automatically generated from %s */\n" filename;
-  Printf.fprintf avr_h "/* Please DO NOT EDIT */\n";
-
-  Printf.fprintf avr_h "/* Macros to send and receive messages of class %s */\n" class_name;
-
-  (** Macros for airborne downlink (sending) *)
-  if class_name = "telemetry" then begin (** FIXME *)
-    Printf.fprintf avr_h "#ifdef DOWNLINK\n";
-    check_alignment := false
-  end;
-  Gen_onboard.print_avr_macros filename avr_h class_name messages;
-  if class_name = "telemetry" then begin
-    Printf.fprintf avr_h "#else // DOWNLINK\n";
-    Gen_onboard.print_null_avr_macros avr_h messages;
-    Printf.fprintf avr_h "#endif // DOWNLINK\n"
-  end;
-
-  (** Macros for airborne datalink (receiving) *)
-  List.iter (Gen_onboard.print_get_macros avr_h) messages;
-
-(*  Printf.fprintf avr_h "#endif /* _MESSAGES_H*/\n" *)
-
+  with
+    Xml.Error (msg, pos) -> failwith (sprintf "%s:%d : %s\n" filename (Xml.line pos) (Xml.error_msg msg))
