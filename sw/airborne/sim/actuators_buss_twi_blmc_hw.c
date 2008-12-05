61c09c817875b4b2177aeb3f3c17e87c07d53919
Antoine Drouin <poinix@gmail.com>
2008-12-05 17:58:33 +0000

    transfered to booz directory in preparation for svn migrationZ

--- sw/airborne/sim/actuators_buss_twi_blmc_hw.c
+++
@@ -1,8 +0,0 @@
-#include "actuators.h"
-
-volatile bool_t  buss_twi_blmc_status;
-volatile uint8_t buss_twi_blmc_nb_err;
-
-uint8_t buss_twi_blmc_motor_power[BUSS_TWI_BLMC_NB];
-
-void actuators_init ( void ) {}
