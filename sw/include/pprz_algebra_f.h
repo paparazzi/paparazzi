4130ff4d229e0055f70930f333a109795750f875
Antoine Drouin <poinix@gmail.com>
2009-01-29 15:31:57 +0000

    *** empty log message ***

---
+++ sw/include/pprz_algebra_f.h
@@ -0,0 +1,21 @@
+#ifndef PPRZ_ALGEBRA_F_H
+#define PPRZ_ALGEBRA_F_H
+
+
+
+struct Pprz_double_utm {
+  double east;
+  double north;
+  double alt;
+  int zone;
+};
+
+struct Pprz_double_lla {
+  double lat;
+  double lon;
+  double alt;
+};
+
+
+
+#endif /* PPRZ_ALGEBRA_F_H */
