391b2c798dc7e08c5523cf297addb6e22d80022a
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2009-02-03 17:19:28 +0000

    invert x and y

--- sw/ground_segment/joystick/main_stick.c
+++ sw/ground_segment/joystick/main_stick.c
@@ -41,6 +41,8 @@
 /* Global vars */
 int fp_received_once  = 0;
 long int lon_sp,lat_sp,alt_sp,psi_sp;
+int last_button = 0;
+int8_t last_up = 0, last_down = 0;
 
 /* Options */
 char * device_name    = NULL;
@@ -71,12 +73,15 @@ l_help:
 }
 
 
-#define STICK_DEADBAND 5
+#define STICK_DEADBAND 10
 #define STICK_APPLY_DEADBAND(_v) (abs(_v) >= STICK_DEADBAND ? _v : 0)
 
 #define ATTITUDE_COEF 1430  // RadOfDeg(10 / 128) << 20
 #define CLIMB_COEF_SHIFT 6  // around 1 m/s
-#define YAW_RATE_COEF 143   // 10 deg/s = ATTITUDE_COEF / 10 (update frequncy)
+#define CLIMB_INCR (10*127) // 1 m step
+#define YAW_RATE_COEF 50   // 4 deg/s = ATTITUDE_COEF / 10 (update frequncy)
+
+#define bit_is_set(x, b) ((x >> b) & 0x1)
 
 // ATTITUDE
 static gboolean joystick_attitude_periodic(gpointer data __attribute__ ((unused))) {
@@ -86,16 +91,26 @@ static gboolean joystick_attitude_periodic(gpointer data __attribute__ ((unused)
 	int8_t roll = STICK_APPLY_DEADBAND(stick_axis_values[0]);
 	int8_t pitch = STICK_APPLY_DEADBAND(stick_axis_values[1]);
 	int8_t yaw_rate = STICK_APPLY_DEADBAND(stick_axis_values[2]);
-  int8_t climb = STICK_APPLY_DEADBAND(stick_axis_values[3]);
+  int climb = STICK_APPLY_DEADBAND(stick_axis_values[3]);
+
+  int8_t last_up = bit_is_set(last_button,3);
+  int8_t up = bit_is_set(stick_button_values,3);
+  int8_t last_down = bit_is_set(last_button,1);
+  int8_t down = bit_is_set(stick_button_values,1);
+  if (up != last_up && up > 0) climb = - CLIMB_INCR;
+  if (down != last_down && down > 0) climb = CLIMB_INCR;
+  last_button = stick_button_values;
 
   if (! fp_received_once) return 1;
 
+  alt_sp += (climb >> CLIMB_COEF_SHIFT);
+  psi_sp += YAW_RATE_COEF * yaw_rate;
   IvySendMsg("dl BOOZ2_FMS_COMMAND %d %d %d %d %d %d %d",
       2, 2,
-      alt_sp + (climb >> CLIMB_COEF_SHIFT),
+      alt_sp,
       ATTITUDE_COEF * roll,
       ATTITUDE_COEF * pitch,
-      psi_sp + YAW_RATE_COEF * yaw_rate,
+      psi_sp,
       aircraft_id);
 
 	return 1;
@@ -111,16 +126,30 @@ static gboolean joystick_hover_periodic(gpointer data __attribute__ ((unused))) 
 	int8_t vx = STICK_APPLY_DEADBAND(stick_axis_values[0]);
 	int8_t vy = STICK_APPLY_DEADBAND(stick_axis_values[1]);
 	int8_t yaw_rate = STICK_APPLY_DEADBAND(stick_axis_values[2]);
-  int8_t climb = STICK_APPLY_DEADBAND(stick_axis_values[3]);
+  int climb = STICK_APPLY_DEADBAND(stick_axis_values[3]);
+
+  int8_t last_up = bit_is_set(last_button,3);
+  int8_t up = bit_is_set(stick_button_values,3);
+  int8_t last_down = bit_is_set(last_button,1);
+  int8_t down = bit_is_set(stick_button_values,1);
+  if (up != last_up && up > 0) climb = - CLIMB_INCR;
+  if (down != last_down && down > 0) climb = CLIMB_INCR;
+  last_button = stick_button_values;
+  //printf("ok\n");
 
   if (! fp_received_once) return 1;
 
+  alt_sp += (climb >> CLIMB_COEF_SHIFT);
+  lon_sp += (vy >> SPEED_COEF_SHIFT);
+  lat_sp += (vx >> SPEED_COEF_SHIFT);
+  psi_sp += YAW_RATE_COEF * yaw_rate;
+  //printf("%ld %ld %ld %ld\n",alt_sp,lon_sp,lat_sp,psi_sp);
   IvySendMsg("dl BOOZ2_FMS_COMMAND %d %d %d %d %d %d %d",
       3, 2,
-      alt_sp + (climb >> CLIMB_COEF_SHIFT),
-      lon_sp + (vx >> SPEED_COEF_SHIFT),
-      lat_sp + (vy >> SPEED_COEF_SHIFT),
-      psi_sp + YAW_RATE_COEF * yaw_rate,
+      alt_sp,
+      lat_sp,
+      lon_sp,
+      psi_sp,
       aircraft_id);
 
 	return 1;
@@ -128,6 +157,7 @@ static gboolean joystick_hover_periodic(gpointer data __attribute__ ((unused))) 
 
 void readBOOZ2_FPIvyBus(IvyClientPtr app, void *data, int argc, char **argv) {
 
+  //printf("%s\n",argv[0]);
   if (argc > 0) {
     sscanf(argv[0],"%*d %*s %*d %*d %*d %*d %*d %*d %*d %*d %ld %ld %ld %ld",
         &lon_sp, &lat_sp, &alt_sp, &psi_sp);
