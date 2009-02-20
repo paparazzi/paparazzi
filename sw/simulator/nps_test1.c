7621584b85849e0f0f83b8d962f32b8acb489bdc
Gustavo Oliveira Violato <gustavoviolato@gmail.com>
2009-02-20 12:02:29 +0000

    *** empty log message ***

--- sw/simulator/nps_test1.c
+++ sw/simulator/nps_test1.c
@@ -1,5 +1,10 @@
 #include "nps_fdm.h"
+#include <FGFDMExec.h>
+#include <math/FGColumnVector3.h>
 
+string RootDir = "/home/violato/JSBSim/";
+string AircraftName = "ball";
+string ResetName = "reset00";
 
 static struct NpsFdmState fdm_state;
 
@@ -7,9 +12,9 @@ static struct NpsFdmState fdm_state;
 
 static void feed_inputs(JSBSim::FGFDMExec* fdmex) {
 
-  double hover_force = 0.;
+  //double hover_force = 0.;
 
-  fdmex->SetPropertyValue("/fdm/jsbsim/fcs/force_front", fdm_state.dummy.f_input);
+  fdmex->SetPropertyValue("/fdm/jsbsim/fcs/parachute_reef_pos_norm", fdm_state.dummy.f_input);
   
 
 }
@@ -18,9 +23,9 @@ static void fetch_state(JSBSim::FGFDMExec* fdmex) {
 
   //  double foo = fdmex->GetPropagate()->GetInertialVelocityMagnitude();
   //  double foo = fdmex->GetPropagate()->GetGeodLatitudeRad();
-  // cerr << "lat " << foo << endl ;
+  //  cerr << "lat " << foo << endl ;
   const JSBSim::FGColumnVector3 vel = fdmex->GetPropagate()->GetVel();
-  cerr << "vel " << vel << endl ;
+  //  cerr << "vel " << vel << endl ;
 
   //  eulers->ve[EULER_PHI] = vel
 
@@ -29,12 +34,50 @@ static void fetch_state(JSBSim::FGFDMExec* fdmex) {
 
 }
 
-
-
-
 int main(int argc, char** argv) {
 
+  bool result = false;
+
+  JSBSim::FGFDMExec* fdmex;
+  fdmex = new JSBSim::FGFDMExec();
+
+  fdmex->DisableOutput();
+  fdmex->SetDebugLevel(0);
+
+  if ( ! fdmex->LoadModel( RootDir + "aircraft",
+			   RootDir + "engine",
+			   RootDir + "systems",
+			   AircraftName,
+			   1)) {
+    cerr << "  JSBSim could not be started" << endl << endl;
+    delete fdmex;
+    exit(-1);
+  }
+
+  //fdmex->PrintPropertyCatalog();
+
+  JSBSim::FGInitialCondition* IC = fdmex->GetIC();
+  if ( ! IC->Load(ResetName)) {
+    delete fdmex;
+    cerr << "Initialization unsuccessful" << endl;
+    exit(-1);
+  }
+
+  result = fdmex->RunIC();
+
+  while (result) {
+    // run autopilot
+    feed_inputs(fdmex);
+    result = fdmex->Run();
+    fetch_state(fdmex);
+    //cerr << "time " << fdmex->GetSimTime() << " h " << fdmex->GetPropagate()->Geth() << endl ;
+    //cerr << "reef " << fdmex->GetPropertyValue("/fdm/jsbsim/fcs/parachute_reef_pos_norm") << endl ;
+    if (fdmex->GetPropagate()->Geth() < 10)
+      result = false;
+  }
   
+  delete fdmex;
+  return 0;
 
 
 }
