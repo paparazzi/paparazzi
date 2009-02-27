afe4129d2066f138440e912f90d253dd6202e9ba
Pascal Brisset <pascal.brisset@free.fr>
2009-02-27 16:28:49 +0000

    POSLLH message added (not working with large longitudes !!!!!!!!!)

--- sw/simulator/hitl.ml
+++ sw/simulator/hitl.ml
@@ -27,6 +27,7 @@
 open Printf
 open Stdlib
 module LL = Latlong
+open LL
 
 module TelePprz = Pprz.Messages(struct let name = "telemetry" end)
 module DatalinkPprz = Pprz.Messages(struct let name = "datalink" end)
@@ -65,8 +66,17 @@ module Make (A:Data.MISSION) (FM: FlightModel.SIG) = struct
 	
 
   let gps = fun gps ->
-    let utm = LL.utm_of LL.WGS84 gps.wgs84 in
+    let utm = LL.utm_of LL.WGS84 gps.wgs84
+    and lon = (Rad>>Deg)gps.wgs84.posn_long
+    and lat = (Rad>>Deg)gps.wgs84.posn_lat in
 
+    send_ubx_message "NAV" "POSLLH" ["ITOW", scale gps.time 1e3;
+				     "LON",scale lon 1e7; (* FIXME TOO LARGE *)
+				     "LAT", scale lat 1e7;
+				     "HEIGHT", scale gps.alt 1e3;
+				     "HMSL", scale gps.alt 1e3;
+				     "Hacc", scale 3. 1e3;
+				     "Vacc", scale 5. 1e3];
     send_ubx_message "NAV" "POSUTM" ["EAST", scale utm.LL.utm_x 1e2;
 				     "NORTH", scale utm.LL.utm_y 1e2;
 				     "ALT", scale gps.alt 1e2;
