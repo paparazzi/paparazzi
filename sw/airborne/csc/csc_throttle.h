36e1d365fbed025c49b44b74330fd28ded6dd7ad
Allen Ibara <uce.savannah@ibara.net>
2009-04-17 19:22:43 +0000

    Remove dead code from csc_throttle.h

--- sw/airborne/csc/csc_throttle.h
+++ sw/airborne/csc/csc_throttle.h
@@ -28,33 +28,10 @@
 #include "types.h"
 #include "std.h"
 
-extern uint16_t uart_throttle_faultflags;
-extern uint16_t uart_throttle_rpm;
-
-extern uint16_t uart_throttle_recv_cmd_tid;
-extern uint16_t uart_throttle_recv_cmd_cid;
-extern uint16_t uart_throttle_recv_cmd_arg1;
-extern uint16_t uart_throttle_recv_cmd_arg2;
-
-extern uint16_t uart_throttle_send_cmd_req;
-extern uint16_t uart_throttle_send_cmd_tid;
-extern uint16_t uart_throttle_send_cmd_cid;
-extern uint16_t uart_throttle_send_cmd_arg1;
-extern uint16_t uart_throttle_send_cmd_arg2;
-
 
 void uart_throttle_init( void );
 void uart_throttle_event_task( void );
 void throttle_send_command(uint8_t throttle_id, uint8_t cmd_id, uint16_t arg1, uint16_t arg2);
-
-#if 0
-#ifdef USE_UART_THROTTLE
-#define PERIODIC_SEND_MOTOR_CONTROL() DOWNLINK_SEND_MOTOR_CONTROL(uart_throttle_rpm, uart_throttle_rpm + 1, uart_throttle_faultflags, uart_throttle_faultflags + 1, \
-  &uart_throttle_recv_cmd_tid, &uart_throttle_recv_cmd_cid, &uart_throttle_recv_cmd_arg1, &uart_throttle_recv_cmd_arg2)
-#else
-#define PERIODIC_SEND_MOTOR_CONTROL() 
-#endif
-#endif
 
 
 #endif 
