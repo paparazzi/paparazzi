c71bb06fd72f3e03a4534582f7c4ce06b11a15e6
Martin Mueller <martin@pfump.org>
2009-05-10 20:44:41 +0000

    have usb ms driver

--- sw/airborne/arm7/usb_msc_hw.c
+++ sw/airborne/arm7/usb_msc_hw.c
@@ -1,7 +1,7 @@
 /*
 	LPCUSB, an USB device driver for LPC microcontrollers	
 	Copyright (C) 2006 Bertrik Sikken (bertrik@sikken.nl)
-    adapted to pprz    Martin Mueller (martinmm@pfump.org)
+	adapted to pprz    Martin Mueller (martinmm@pfump.org)
 
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions are met:
