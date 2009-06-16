b5de3c081fb449d3426f0f2ce5c4181e82dbdc61
Allen Ibara <uce.savannah@ibara.net>
2009-06-16 04:37:31 +0000

    Add BOOZ_TRIG_CONST define to allow configurable constness for booz int trig tables to control whether or not they go into RAM

--- sw/airborne/booz/booz_trig_int.h
+++ sw/airborne/booz/booz_trig_int.h
@@ -2,7 +2,15 @@
 #define BOOZ_TRIG_INT_H
 
 #include "std.h"
+#include "airframe.h"
 
-extern int16_t booz_trig_int[];
+/* Allow makefile to define BOOZ_TRIG_CONST in case we want
+ to make the trig tables const and store them in flash.
+ Otherwise use the empty string and keep the table in RAM. */
+#ifndef BOOZ_TRIG_CONST
+#define BOOZ_TRIG_CONST
+#endif
+
+extern BOOZ_TRIG_CONST int16_t booz_trig_int[];
 
 #endif /* BOOZ_TRIG_INT_H */
