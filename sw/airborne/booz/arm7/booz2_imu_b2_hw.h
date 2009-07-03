0a260cb739d587bbe3f420a2efd81e386b275e15
Antoine Drouin <poinix@gmail.com>
2009-07-03 06:57:37 +0000

    cleaned handling of multiple slave on SSP

--- sw/airborne/booz/arm7/booz2_imu_b2_hw.h
+++ sw/airborne/booz/arm7/booz2_imu_b2_hw.h
@@ -17,50 +17,14 @@
 #include "std.h"
 #include "LPC21xx.h"
 #include "interrupt_hw.h"  
-#include "spi_hw.h"
 
-//#include "booz2_debug.h"
+#define BOOZ2_IMU_SSP_STA_IDLE           0
+#define BOOZ2_IMU_SSP_STA_BUSY_MAX1168   1
+#define BOOZ2_IMU_SSP_STA_BUSY_MS2100    2
+extern volatile uint8_t booz2_imu_ssp_status;
 
 
 extern void booz2_imu_b2_hw_init(void);
-
-#define Booz2ImuSetSpi8bits() { \
-  SSPCR0 &= (~(0xF << 0)); \
-  SSPCR0 |= (0x07 << 0); /* data size : 8 bits */ \
-}
-
-#define Booz2ImuSetSpi16bits() { \
-  SSPCR0 &= (~(0xF << 0)); \
-  SSPCR0 |= (0x0F << 0); /* data size : 16 bits */ \
-}
-
-
-#ifdef USE_MICROMAG
-#define Booz2ImuSpiEvent(_handler1,_handler2) { \
-  if (do_booz2_max1168_read && booz2_imu_spi_selected == BOOZ2_SPI_NONE) { \
-    Booz2ImuSetSpi16bits(); \
-    booz2_imu_spi_selected = BOOZ2_SPI_SLAVE_MAX1168; \
-    do_booz2_max1168_read = false; \
-    _handler1(); \
-  } \
-  if (do_booz2_micromag_read && booz2_imu_spi_selected == BOOZ2_SPI_NONE) { \
-    Booz2ImuSetSpi8bits(); \
-    booz2_imu_spi_selected = BOOZ2_SPI_SLAVE_MM; \
-    do_booz2_micromag_read = false; \
-  } \
-  if (booz2_imu_spi_selected == BOOZ2_SPI_SLAVE_MM) _handler2();\
-}
-#else // NO MICROMAG
-#define Booz2ImuSpiEvent(_handler) { \
-  if (do_booz2_max1168_read && booz2_imu_spi_selected == BOOZ2_SPI_NONE) { \
-    Booz2ImuSetSpi16bits(); \
-    booz2_imu_spi_selected = BOOZ2_SPI_SLAVE_MAX1168; \
-    do_booz2_max1168_read = false; \
-    _handler(); \
-  } \
-}
-#endif
-
 
 
 #endif /* BOOZ2_IMU_B2_HW_H */
