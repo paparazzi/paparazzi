83f93aba9da7b96a1e9896e097db435b6ac17e90
Antoine Drouin <poinix@gmail.com>
2009-07-18 11:03:05 +0000

    trying to sort files

---
+++ sw/airborne/booz/impl/booz_imu_b2.h
@@ -0,0 +1,84 @@
+/*
+ * $Id$
+ *  
+ * Copyright (C) 2008-2009 Antoine Drouin <poinix@gmail.com>
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA. 
+ */
+
+#ifndef BOOZ_IMU_B2_H
+#define BOOZ_IMU_B2_H
+
+#include "booz_imu.h"
+
+#include "peripherals/booz_max1168.h"
+
+#define IMU_B2_MAG_NONE   0
+#define IMU_B2_MAG_MS2001 1
+#define IMU_B2_MAG_AMI601 2
+
+#if defined IMU_B2_MAG_TYPE && IMU_B2_MAG_TYPE == IMU_B2_MAG_MS2001
+#include "peripherals/booz_ms2001.h"
+#define BoozImuMagEvent(_mag_handler) {					\
+    if (micromag_status == MM_DATA_AVAILABLE) {				\
+      booz_imu.mag_unscaled.x = micromag_values[IMU_MAG_X_CHAN];	\
+      booz_imu.mag_unscaled.y = micromag_values[IMU_MAG_Y_CHAN];	\
+      booz_imu.mag_unscaled.z = micromag_values[IMU_MAG_Z_CHAN];	\
+      micromag_status = MM_IDLE;					\
+      _mag_handler();							\
+    }									\
+  }
+#elif defined IMU_B2_MAG_TYPE && IMU_B2_MAG_TYPE == IMU_B2_MAG_AMI601
+#include "peripherals/booz_ami601.h"
+#define BoozImuMagEvent(_mag_handler) {					\
+    if (booz_ami601_status == STA_AMI601_DATA_AVAILABLE) {		\
+      booz_imu.mag_unscaled.x = ami601_values[IMU_MAG_X_CHAN];	\
+      booz_imu.mag_unscaled.y = ami601_values[IMU_MAG_Y_CHAN];	\
+      booz_imu.mag_unscaled.z = ami601_values[IMU_MAG_Z_CHAN];	\
+      booz_ami601_status = STA_AMI601_IDLE;				\
+      _mag_handler();							\
+    }									\
+  }
+#else
+#define BoozImuMagEvent(_mag_handler) {}
+#endif
+
+
+#define BoozImuEvent(_gyro_accel_handler, _mag_handler) {		\
+    if (booz_max1168_status == STA_MAX1168_DATA_AVAILABLE) {		\
+      booz_imu.gyro_unscaled.p  = booz_max1168_values[IMU_GYRO_P_CHAN]; \
+      booz_imu.gyro_unscaled.q  = booz_max1168_values[IMU_GYRO_Q_CHAN]; \
+      booz_imu.gyro_unscaled.r  = booz_max1168_values[IMU_GYRO_R_CHAN]; \
+      booz_imu.accel_unscaled.x = booz_max1168_values[IMU_ACCEL_X_CHAN]; \
+      booz_imu.accel_unscaled.y = booz_max1168_values[IMU_ACCEL_Y_CHAN]; \
+      booz_imu.accel_unscaled.z = booz_max1168_values[IMU_ACCEL_Z_CHAN]; \
+      booz_max1168_status = STA_MAX1168_IDLE;				\
+      _gyro_accel_handler();						\
+    }									\
+    BoozImuMagEvent(_mag_handler);					\
+  }
+
+
+/* underlying architecture */
+#include "impl/booz_imu_b2_arch.h"
+/* must be implemented by underlying architecture */
+extern void booz_imu_b2_arch_init(void);
+
+
+#endif /* BOOZ_IMU_B2_H */
+
---
+++ sw/airborne/booz/impl/booz_imu_crista.h
@@ -0,0 +1,74 @@
+/*
+ * $Id$
+ *  
+ * Copyright (C) 2008-2009 Antoine Drouin <poinix@gmail.com>
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA. 
+ */
+
+#ifndef BOOZ_IMU_CRISTA_H
+#define BOOZ_IMU_CRISTA_H
+
+#include "booz_imu.h"
+
+
+extern void booz_imu_impl_init(void);
+extern void booz_imu_periodic(void);
+
+#define ADS8344_NB_CHANNELS 8
+extern uint16_t ADS8344_values[ADS8344_NB_CHANNELS];
+extern bool_t ADS8344_available;
+
+#define Booz2ImuEvent(_gyro_accel_handler, _mag_handler) {		\
+    if (ADS8344_available) {						\
+      ADS8344_available = FALSE;					\
+      booz_imu.gyro_unscaled.p = ADS8344_values[IMU_GYRO_P_CHAN];	\
+      booz_imu.gyro_unscaled.q = ADS8344_values[IMU_GYRO_Q_CHAN];	\
+      booz_imu.gyro_unscaled.r = ADS8344_values[IMU_GYRO_R_CHAN];	\
+      booz_imu.accel_unscaled.x = ADS8344_values[IMU_ACCEL_X_CHAN];	\
+      booz_imu.accel_unscaled.y = ADS8344_values[IMU_ACCEL_Y_CHAN];	\
+      booz_imu.accel_unscaled.z = ADS8344_values[IMU_ACCEL_Z_CHAN];	\
+      /* spare 3, temp 7 */						\
+      _gyro_accel_handler();						\
+    }									\
+    Booz2ImuMagEvent(_mag_handler);					\
+  }
+
+#ifdef USE_AMI601
+#include "peripherals/booz_ami601.h"
+#define foo_handler() {}
+#define Booz2ImuMagEvent(_mag_handler) {				\
+    AMI601Event(foo_handler);						\
+    if (ami601_status == AMI601_DATA_AVAILABLE) {			\
+      booz_imu.mag_unscaled.x = ami601_val[IMU_MAG_X_CHAN];		\
+      booz_imu.mag_unscaled.y = ami601_val[IMU_MAG_Y_CHAN];		\
+      booz_imu.mag_unscaled.z = ami601_val[IMU_MAG_Z_CHAN];		\
+      ami601_status = AMI601_IDLE;					\
+      _mag_handler();							\
+    }									\
+  }
+#else
+#define Booz2ImuMagEvent(_mag_handler) {}
+#endif
+
+/* underlying architecture */
+#include "impl/booz_imu_crista_arch.h"
+
+
+#endif /* BOOZ2_IMU_CRISTA_H */
+
