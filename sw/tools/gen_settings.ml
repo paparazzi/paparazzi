0ca0e7428e23abc813186aef795d30a87ac23b8d
Antoine Drouin <poinix@gmail.com>
2009-07-20 20:39:41 +0000

    iallowed use of full pathnames in "module" field

--- sw/tools/gen_settings.ml
+++ sw/tools/gen_settings.ml
@@ -81,9 +81,9 @@ let print_dl_settings = fun settings ->
 	try
 	  let h = ExtXml.attrib s "handler" and
 	      m =  ExtXml.attrib s "module" in
-	    lprintf "case %d: %s_%s( _value ); _value = %s; break;\\\n" !idx m h v
+	  lprintf "case %d: %s_%s( _value ); _value = %s; break;\\\n" !idx (Filename.basename m) h v
 	with  
-	    ExtXml.Error e -> lprintf "case %d: %s = _value; break;\\\n" !idx v
+	  ExtXml.Error e -> lprintf "case %d: %s = _value; break;\\\n" !idx v
 	end;
 	incr idx
     ) 
