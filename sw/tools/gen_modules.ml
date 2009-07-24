6d65a16dbc98b992a07ff7de3b9df01256658b52
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2009-07-24 12:26:21 +0000

    remove compilation warning

--- sw/tools/gen_modules.ml
+++ sw/tools/gen_modules.ml
@@ -229,7 +229,7 @@ let print_event_functions = fun modules ->
 
 let print_datalink_functions = fun modules ->
   lprintf out_h "\n#include \"messages.h\"\n";
-  lprintf out_h "static inline void modules_parse_datalink(uint8_t msg_id) {\n";
+  lprintf out_h "static inline void modules_parse_datalink(uint8_t msg_id __attribute__ ((unused))) {\n";
   right ();
   let else_ = ref "" in
   List.iter (fun m ->
