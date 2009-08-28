12c528e74f3625c92b611c506bdb5a633f997e21
Felix Ruess <felix.ruess@gmail.com>
2009-08-28 18:45:35 +0000

    sim: fix pointer to history for sensor latency

--- sw/simulator/nps/nps_sensor_gps.c
+++ sw/simulator/nps/nps_sensor_gps.c
@@ -1,5 +1,6 @@
 #include "nps_sensor_gps.h"
 
+
 #include "airframe.h"
 #include "nps_fdm.h"
 #include "nps_random.h"
@@ -11,15 +12,15 @@ void nps_sensor_gps_init(struct NpsSensorGps* gps, double time) {
   FLOAT_VECT3_ZERO(gps->ecef_vel);
   gps->pos_latency = NPS_GPS_POS_LATENCY;
   gps->speed_latency = NPS_GPS_SPEED_LATENCY;
-  VECT3_ASSIGN(gps->pos_noise_std_dev, 
+  VECT3_ASSIGN(gps->pos_noise_std_dev,
 			   NPS_GPS_POS_NOISE_STD_DEV, NPS_GPS_POS_NOISE_STD_DEV, NPS_GPS_POS_NOISE_STD_DEV);
-  VECT3_ASSIGN(gps->speed_noise_std_dev, 
+  VECT3_ASSIGN(gps->speed_noise_std_dev,
 			   NPS_GPS_SPEED_NOISE_STD_DEV, NPS_GPS_SPEED_NOISE_STD_DEV, NPS_GPS_SPEED_NOISE_STD_DEV);
-  VECT3_ASSIGN(gps->pos_bias_initial, 
+  VECT3_ASSIGN(gps->pos_bias_initial,
 			   NPS_GPS_POS_BIAS_INITIAL_X, NPS_GPS_POS_BIAS_INITIAL_Y, NPS_GPS_POS_BIAS_INITIAL_Z);
-  VECT3_ASSIGN(gps->pos_bias_random_walk_std_dev, 
-			   NPS_GPS_POS_BIAS_RANDOM_WALK_STD_DEV_X, 
-			   NPS_GPS_POS_BIAS_RANDOM_WALK_STD_DEV_Y, 
+  VECT3_ASSIGN(gps->pos_bias_random_walk_std_dev,
+			   NPS_GPS_POS_BIAS_RANDOM_WALK_STD_DEV_X,
+			   NPS_GPS_POS_BIAS_RANDOM_WALK_STD_DEV_Y,
 			   NPS_GPS_POS_BIAS_RANDOM_WALK_STD_DEV_Z);
   FLOAT_VECT3_ZERO(gps->pos_bias_random_walk_value);
   gps->next_update = time;
@@ -48,9 +49,9 @@ void nps_sensor_gps_run_step(struct NpsSensorGps* gps, double time) {
   double_vect3_add_gaussian_noise(&cur_speed_reading, &gps->speed_noise_std_dev);
 
   /* store that for later and retrieve a previously stored data */
-  UpdateSensorLatency(time, &cur_speed_reading, gps->speed_history, gps->speed_latency, &gps->ecef_vel);
+  UpdateSensorLatency(time, &cur_speed_reading, &gps->speed_history, gps->speed_latency, &gps->ecef_vel);
 
-  
+
   /*
    * simulate position sensor
    */
@@ -62,16 +63,16 @@ void nps_sensor_gps_run_step(struct NpsSensorGps* gps, double time) {
   /* update random walk bias and add it to error*/
   double_vect3_update_random_walk(&gps->pos_bias_random_walk_value, &gps->pos_bias_random_walk_std_dev, NPS_GPS_DT, 5.);
   VECT3_ADD(pos_error, gps->pos_bias_random_walk_value);
-  
+
   /* add error to current pos reading */
   struct DoubleVect3 cur_pos_reading;
   VECT3_COPY(cur_pos_reading, fdm.ecef_pos);
   VECT3_ADD(cur_pos_reading, pos_error);
 
   /* store that for later and retrieve a previously stored data */
-  UpdateSensorLatency(time, &cur_pos_reading, gps->pos_history, gps->pos_latency, &gps->ecef_pos);
+  UpdateSensorLatency(time, &cur_pos_reading, &gps->pos_history, gps->pos_latency, &gps->ecef_pos);
 
-  
+
   gps->next_update += NPS_GPS_DT;
   gps->data_available = TRUE;
 
--- sw/simulator/nps/nps_sensors_utils.c
+++ sw/simulator/nps/nps_sensors_utils.c
@@ -4,21 +4,20 @@
 #include "6dof.h"
 #include "math/pprz_algebra.h"
 
-void UpdateSensorLatency(double time, gpointer cur_reading, GSList* history, 
-						 double latency, gpointer sensor_reading) {
+void UpdateSensorLatency(double time, gpointer cur_reading, GSList **history, double latency, gpointer sensor_reading) {
   /* add new reading */
   struct BoozDatedSensor* cur_read = g_new(struct BoozDatedSensor, 1);
   cur_read->time = time;
   cur_read->value = (struct DoubleVect3*) g_memdup(cur_reading, sizeof(struct DoubleVect3));
-  history = g_slist_prepend(history, cur_read);
+  *history = g_slist_prepend(*history, cur_read);
   /* remove old readings */
-  GSList* last =  g_slist_last(history);
+  GSList* last =  g_slist_last(*history);
   while (last && ((struct BoozDatedSensor*)last->data)->time < time - latency) {
-    history = g_slist_remove_link(history, last);
+    *history = g_slist_remove_link(*history, last);
 	g_free(((struct BoozDatedSensor*)last->data)->value);
-    g_free((struct BoozDatedSensor*)last->data);
+	g_free((struct BoozDatedSensor*)last->data);
     g_slist_free(last);
-    last = g_slist_last(history);
+    last = g_slist_last(*history);
   }
   /* update sensor        */
   //g_memmove((gpointer)sensor_reading, (gpointer)((struct BoozDatedSensor*)last->data)->value, sizeof(struct DoubleVect3));
