dab2ef4c8c93acc3f23ba1f664184857a01bfe77
Felix Ruess <felix.ruess@gmail.com>
2009-08-28 14:27:34 +0000

    added sim sensor utils needed for GPS latency

---
+++ sw/simulator/nps/nps_sensors_utils.h
@@ -0,0 +1,16 @@
+#ifndef NPS_SENSORS_UTILS_H
+#define NPS_SENSORS_UTILS_H
+
+#include <glib.h>
+#include "math/pprz_algebra_double.h"
+
+struct BoozDatedSensor {
+  struct DoubleVect3* value;
+  double time;
+};
+
+/* cur_reading and sensor_reading must be of a type that can be cast to DoubleVect3* */
+extern void UpdateSensorLatency(double time, gpointer cur_reading, GSList* history, 
+								double latency, gpointer sensor_reading);
+
+#endif /* NPS_SENSORS_UTILS_H */
