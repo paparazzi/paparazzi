4bd2cdbe2fcdb4ee176a5711c67f83333debef0e
Antoine Drouin <poinix@gmail.com>
2009-12-08 07:02:35 +0000

    added license header

--- sw/airborne/booz/stabilization/booz_stabilization_attitude_ref_float.h
+++ sw/airborne/booz/stabilization/booz_stabilization_attitude_ref_float.h
@@ -1,6 +1,27 @@
+/*
+ * $Id$
+ *  
+ * Copyright (C) 2008-2009 Antoine Drouin <poinix@gmail.com>
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA. 
+ */
 #ifndef BOOZ_STABILISATION_ATTITUDE_REF_FLOAT_H
 #define BOOZ_STABILISATION_ATTITUDE_REF_FLOAT_H
-
 
 extern struct FloatEulers booz_stab_att_sp_euler;
 extern struct FloatQuat   booz_stab_att_sp_quat;
@@ -8,7 +29,6 @@ extern struct FloatEulers booz_stab_att_ref_euler;
 extern struct FloatQuat   booz_stab_att_ref_quat;
 extern struct FloatRates  booz_stab_att_ref_rate;
 extern struct FloatRates  booz_stab_att_ref_accel;
-
 
 #endif /* BOOZ_STABILISATION_ATTITUDE_REF_FLOAT_H */
 
