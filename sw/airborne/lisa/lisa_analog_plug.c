7cafe42132d1594fc263f37c23c6484d2b790248
Antoine Drouin <poinix@gmail.com>
2010-04-24 03:03:01 +0000

    added missing declaration

--- sw/airborne/lisa/lisa_analog_plug.c
+++ sw/airborne/lisa/lisa_analog_plug.c
@@ -8,6 +8,10 @@ uint16_t booz2_analog_baro_status;
 
 uint8_t booz2_battery_voltage;
 
+void booz2_analog_init(void);
+void booz2_analog_baro_init(void);
+void booz2_battery_init(void);
+
 void booz2_analog_init(void) {}
 
 void booz2_analog_baro_init(void) {}
