23f768a9bf386bf30fcf6c4c98b26e00dcc24ce9
Antoine Drouin <poinix@gmail.com>
2010-06-14 17:04:28 +0000

    used enum to select network mode

--- sw/airborne/fms/fms_network.h
+++ sw/airborne/fms/fms_network.h
@@ -4,6 +4,11 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+enum FmsNetworkMode {
+  FMS_NETWORK_UNICAST,
+  FMS_NETWORK_BROADCAST
+};
+
 #define FMS_UNICAST 0
 #define FMS_BROADCAST 1
 
@@ -15,7 +20,8 @@ struct FmsNetwork {
 };
 
 
-extern struct FmsNetwork* network_new(const char* str_ip_out, const int port_out, const int port_in, const int broadcast);
+extern struct FmsNetwork* network_new(const char* str_ip_out, const int port_out, const int port_in, 
+				      enum FmsNetworkMode mode);
 extern int network_write(struct FmsNetwork* me, char* buf, int len);
 
 #endif /* FMS_NETWORK_H */
