3b132313d38897586435134acae7b998f7692c7d
Antoine Drouin <poinix@gmail.com>
2010-07-26 21:22:01 +0000

    couple of define for that accelerometer

---
+++ sw/airborne/booz/peripherals/booz_adxl345.h
@@ -0,0 +1,17 @@
+#ifndef BOOZ_ADXL345_H
+#define BOOZ_ADXL345_H
+
+
+#define ADXL345_REG_BW_RATE     0x2C
+#define ADXL345_REG_POWER_CTL   0x2D
+#define ADXL345_REG_INT_ENABLE  0x2E
+#define ADXL345_REG_DATA_FORMAT 0x31
+#define ADXL345_REG_DATA_X0     0x32
+#define ADXL345_REG_DATA_X1     0x33
+#define ADXL345_REG_DATA_Y0     0x34
+#define ADXL345_REG_DATA_Y1     0x35
+#define ADXL345_REG_DATA_Z0     0x36
+#define ADXL345_REG_DATA_Z1     0x37
+
+
+#endif /* BOOZ_ADXL345_H */
