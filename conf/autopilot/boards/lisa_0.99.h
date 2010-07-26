410f5e3a80c130e46deff3a3c402039323fd0d10
Antoine Drouin <poinix@gmail.com>
2010-07-26 21:10:45 +0000

    moved IMU b2 default defintions to imu b2 header

--- conf/autopilot/boards/lisa_0.99.h
+++ conf/autopilot/boards/lisa_0.99.h
@@ -11,21 +11,6 @@
 #define LED_1_BANK 
 #define LED_STP08
 
-
-
-/* Default IMU b2 sensors connection */
-#ifndef IMU_OVERRIDE_CHANNELS
-#define IMU_GYRO_P_CHAN  1
-#define IMU_GYRO_Q_CHAN  0
-#define IMU_GYRO_R_CHAN  2
-#define IMU_ACCEL_X_CHAN 5
-#define IMU_ACCEL_Y_CHAN 3
-#define IMU_ACCEL_Z_CHAN 4
-#define IMU_MAG_X_CHAN   0
-#define IMU_MAG_Y_CHAN   1
-#define IMU_MAG_Z_CHAN   2
-#endif /* not IMU_OVERRIDE_CHANNELS */
-
 // FIXME, this is just to make it compile
 #define POWER_SWITCH_LED 5
 
