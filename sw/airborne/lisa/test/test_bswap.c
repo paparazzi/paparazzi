fa9b8c50d8c5a13d7f75e6542ce5e83bb77005b2
Antoine Drouin <poinix@gmail.com>
2010-08-06 08:06:27 +0000

    added a test for bswap

---
+++ sw/airborne/lisa/test/test_bswap.c
@@ -0,0 +1,36 @@
+
+#include <inttypes.h>
+//#include <byteswap.h>
+
+
+#define MyByteSwap16(n)				     \
+  ( ((((uint16_t) n) << 8) & 0xFF00) |		     \
+    ((((uint16_t) n) >> 8) & 0x00FF) )
+
+#define MyByteSwap32(n)				     \
+  ( ((((uint32_t) n) << 24) & 0xFF000000) |	     \
+    ((((uint32_t) n) <<  8) & 0x00FF0000) |	     \
+    ((((uint32_t) n) >>  8) & 0x0000FF00) |	     \
+    ((((uint32_t) n) >> 24) & 0x000000FF) )
+
+
+int main(void) {
+
+  //  uint32_t foo = 0;
+  //  uint32_t bar = __builtin_bswap32(*(uint32_t*)&foo);
+
+  //  uint16_t foo = 12345;
+  //  uint16_t bar = foo >> 8 | foo << 8;
+
+  //  uint16_t foo = 12345;
+  //  uint16_t bar = ByteSwap(foo);
+
+  uint16_t foo = 12345;
+  uint16_t bar = MyByteSwap16(foo);
+
+
+  uint32_t a = 23456;
+  uint32_t b = MyByteSwap32(a);
+
+  return 0;
+}
