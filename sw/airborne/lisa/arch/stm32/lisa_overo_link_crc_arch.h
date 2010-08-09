bfb014cf2658c806a4f2aaf89d11f5e741fbe7a3
Antoine Drouin <poinix@gmail.com>
2010-08-09 23:20:31 +0000

    it recovers

--- sw/airborne/lisa/arch/stm32/lisa_overo_link_crc_arch.h
+++ sw/airborne/lisa/arch/stm32/lisa_overo_link_crc_arch.h
@@ -16,22 +16,35 @@
 	LED_TOGGLE(OVERO_LINK_LED_OK);					\
 	LED_OFF(OVERO_LINK_LED_KO);					\
 	_data_received_handler();					\
-      	overo_link_arch_prepare_next_transfert(0);			\
-      	overo_link.status = IDLE;						\
       }									\
       else {								\
 	LED_OFF(OVERO_LINK_LED_OK);					\
 	LED_ON(OVERO_LINK_LED_KO);					\
 	_crc_failed_handler();						\
-        overo_link.status = CRC_ERROR;					\
+	/* wait until we're not selected - same thing, we would */	\
+	/* probably want a limit here                           */	\
+	DEBUG_S4_OFF();							\
+	while (!GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_4));		\
+	uint8_t foo __attribute__ ((unused)) = SPI_I2S_ReceiveData(SPI1); \
+	SPI_I2S_DeInit(SPI1);						\
+	SPI_Cmd(SPI1, DISABLE);						\
+	SPI_InitTypeDef SPI_InitStructure;				\
+	SPI_InitStructure.SPI_Direction         = SPI_Direction_2Lines_FullDuplex; \
+	SPI_InitStructure.SPI_Mode              = SPI_Mode_Slave;	\
+	SPI_InitStructure.SPI_DataSize          = SPI_DataSize_8b;	\
+	SPI_InitStructure.SPI_CPOL              = SPI_CPOL_Low;		\
+	SPI_InitStructure.SPI_CPHA              = SPI_CPHA_2Edge;	\
+	SPI_InitStructure.SPI_NSS               = SPI_NSS_Hard;		\
+	SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_2; \
+	SPI_InitStructure.SPI_FirstBit          = SPI_FirstBit_MSB;	\
+	SPI_InitStructure.SPI_CRCPolynomial     = 0x31; /* fuchsto: was 7 (reset) */ \
+	SPI_Init(SPI1, &SPI_InitStructure);				\
+	SPI_CalculateCRC(SPI1, ENABLE);					\
+	SPI_Cmd(SPI1, ENABLE);						\
+	DEBUG_S4_ON();							\
       }									\
-    }									\
-    else if (overo_link.status == CRC_ERROR) {				\
-	/* wait until we're not selected */				\
-	if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_4)) {			\
-	  uint8_t foo __attribute__ ((unused)) = SPI_I2S_ReceiveData(SPI1); \
-          overo_link.status = IDLE;	 				\
-	}								\
+      overo_link_arch_prepare_next_transfert(0);			\
+      overo_link.status = IDLE;						\
     }									\
   }
 
