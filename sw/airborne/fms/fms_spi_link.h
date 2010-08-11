32f47262ef56765bfe69d0e55ceb3356331484f1
Antoine Drouin <poinix@gmail.com>
2010-08-11 01:05:52 +0000

    spi link now has crc by default. moved crc computation to fms_spi_link.c

--- sw/airborne/fms/fms_spi_link.h
+++ sw/airborne/fms/fms_spi_link.h
@@ -11,11 +11,32 @@ struct SpiLink {
   uint8_t  bits;
   uint32_t speed;
   uint16_t delay;
+  /* number of message exchanged since initialization */
+  uint32_t msg_cnt;
+  /* number of crc errors on received messages        */
+  uint32_t crc_err_cnt;
 };
 
 struct SpiLink spi_link;
 
+/* 
+ * initialize peripheral 
+ */
 extern int spi_link_init(void);
-extern int spi_link_send(const void *buf_out, size_t count, void* buf_in);
+
+/* 
+ *  exchange a data buffer
+ *  the last byte of buf_out will be overrwiten with a crc
+ *  the last byte of buf_in  will contain the received crc
+ *  count is the size of buf_out and buf_in, that is
+ *  the count of data to exchange+1
+ */
+extern int spi_link_send(void *buf_out, size_t count, void* buf_in, uint8_t* crc_valid);
+
+/*
+ * just for debuging purposes
+ */
+extern uint8_t crc_calc_block_crc8(const uint8_t buf[], uint32_t len);
+
 
 #endif /* FMS_SPI_LINK_H */
