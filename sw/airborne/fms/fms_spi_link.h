94d5699ff83b0151eca90080a2f6894d685cfbed
Antoine Drouin <poinix@gmail.com>
2010-08-12 00:43:32 +0000

    fixed typo in comment

--- sw/airborne/fms/fms_spi_link.h
+++ sw/airborne/fms/fms_spi_link.h
@@ -1,3 +1,26 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) 2009-20010 Paparazzi Team
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 #ifndef FMS_SPI_LINK_H
 #define FMS_SPI_LINK_H
 
@@ -26,7 +49,7 @@ extern int spi_link_init(void);
 
 /* 
  *  exchange a data buffer
- *  the last byte of buf_out will be overrwiten with a crc
+ *  the last byte of buf_out will be overwiten with a crc
  *  the last byte of buf_in  will contain the received crc
  *  count is the size of buf_out and buf_in, that is
  *  the count of data to exchange+1
