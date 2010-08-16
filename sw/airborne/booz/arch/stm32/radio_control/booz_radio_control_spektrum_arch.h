3fc644466c2577cc0f2da447b1230f8f2be2d2cf
Antoine Drouin <poinix@gmail.com>
2010-08-16 09:38:18 +0000

    moved spektrum binding code from sw/airborne/stm32/bind.[ch] to sw/airborne/booz/arch/stm32/radio_control/booz_radio_control_spektrum.[ch]

---
+++ sw/airborne/booz/arch/stm32/radio_control/booz_radio_control_spektrum_arch.h
@@ -0,0 +1,31 @@
+/*
+ * $Id$
+ *  
+ * Copyright (C) 2009 Eric Parsonage <eric@eparsonage.com>
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA. 
+ */
+
+/*
+ *Function to bind Spektrum satellite receivers
+ */
+
+#ifndef BOOZ_RADIO_CONTROL_SPEKTRUM_ARCH_H
+#define BOOZ_RADIO_CONTROL_SPEKTRUM_ARCH_H
+extern void bind_init( void );
+#endif  /* BOOZ_RADIO_CONTROL_SPEKTRUM_ARCH_H */
