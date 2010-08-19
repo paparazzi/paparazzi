f690d2d19822e9b0113adc5b6d0a6c9cb62567c0
Eric Parsonage <eric@eparsonage.com>
2010-08-19 15:38:11 +0000

    refactored spektrum parser

---
+++ sw/airborne/booz/arch/lpc21/radio_control/booz_radio_control_spektrum_arch.c
@@ -0,0 +1,43 @@
+/*
+ * $Id$
+ *  
+ * Copyright (C) 2008-2009 Antoine Drouin <poinix@gmail.com>
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA. 
+ */
+
+#include "booz_radio_control.h"
+#include "booz_radio_control_spektrum_arch.h"
+#include "booz2_autopilot.h"
+
+bool_t   rc_spk_parser_status;
+uint8_t  rc_spk_parser_idx;
+uint8_t  rc_spk_parser_buf[RADIO_CONTROL_NB_CHANNEL*2];
+const int16_t rc_spk_throw[RADIO_CONTROL_NB_CHANNEL] = RC_SPK_THROWS;
+
+void radio_control_impl_init(void) {
+  rc_spk_parser_status = RC_SPK_STA_UNINIT;
+  rc_spk_parser_idx = 0;
+}
+
+void RadioControlEventImp(void) {
+  _RadioControlEvent(booz2_autopilot_on_rc_frame);
+}
+
+
+
