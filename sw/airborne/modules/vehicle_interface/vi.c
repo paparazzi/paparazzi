29298dec238dd2a1735ef8388fbbdfeff70c8134
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2010-08-24 16:19:11 +0000

    add a fms module (renamed VI) only working with booz for now

---
+++ sw/airborne/modules/vehicle_interface/vi.c
@@ -0,0 +1,74 @@
+/*
+ * $Id:  $
+ *  
+ * Copyright (C) 2008-2010 The Paparazzi Team
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA. 
+ */
+
+#include "vehicle_interface/vi.h"
+
+#include "booz/booz_imu.h"
+#include "booz/booz_gps.h"
+#include "booz/booz_ahrs.h"
+
+#include "airframe.h"
+
+struct VehicleInterface vi;
+
+#ifndef VI_TIMEOUT
+#define VI_TIMEOUT 100
+#endif
+
+void vi_init(void) {
+
+  vi.enabled = FALSE;
+  vi.timeouted = TRUE;
+  vi.last_msg = BOOZ_FMS_TIMEOUT;
+
+  vi.input.h_mode = BOOZ2_GUIDANCE_H_MODE_ATTITUDE;
+  INT_EULERS_ZERO(vi.input.h_sp.attitude);
+  vi.input.v_mode = BOOZ2_GUIDANCE_V_MODE_CLIMB;
+  vi.input.v_sp.climb = 0;
+  vi_impl_init();
+
+}
+
+void vi_periodic(void) {
+#if (VI_TIMEOUT != 0)
+  if (vi.last_msg < VI_TIMEOUT) 
+    vi.last_msg++;
+  else {
+    vi.timeouted = TRUE;
+    vi.input.h_mode = BOOZ2_GUIDANCE_H_MODE_ATTITUDE;
+    INT_EULERS_ZERO(vi.input.h_sp.attitude);
+    vi.input.v_mode = BOOZ2_GUIDANCE_V_MODE_CLIMB;
+    vi.input.v_sp.climb = 0;
+  }
+#endif
+  vi_impl_periodic();
+}
+
+void vi_set_enabled(bool_t enabled) {
+  vi_impl_set_enabled(enabled);
+
+}
+
+void vi_update_info(void) {
+
+}
---
+++ sw/airborne/modules/vehicle_interface/vi.h
@@ -0,0 +1,106 @@
+/*
+ * $Id:  $
+ *
+ * Copyright (C) 2008-2010 The Paparazzi Team
+ *
+ * This file is part of paparazzi.
+ *
+ * This is the "external interface" to the autopilot. It allows an external device to
+ * fetch the vehicle state and input commands at different levels. We should support
+ * different hardware peripherals like i2c, spi or uart.
+ * For now we only have an implementation using datalink messages. 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef VEHICLE_INTERFACE_H
+#define VEHICLE_INTERFACE_H
+
+#include "std.h"
+#include "math/pprz_algebra_int.h"
+#include "booz/booz2_autopilot.h"
+#include "booz/booz_guidance.h"
+#include "booz/booz2_navigation.h"
+
+struct Vi_imu_info {
+  struct Int16Vect3 gyro;
+  struct Int16Vect3 accel;
+  struct Int16Vect3 mag;
+};
+
+struct Vi_gps_info {
+  struct Int32Vect3 pos;
+  struct Int16Vect3 speed;
+  int32_t pacc;
+  uint8_t num_sv;
+  uint8_t fix;
+};
+
+struct Vi_ahrs_info {
+  struct Int16Eulers euler;
+  struct Int16Eulers  rate;
+};
+
+struct Vi_info {
+  struct Vi_imu_info  imu;
+  struct Vi_gps_info  gps;
+  struct Vi_ahrs_info ahrs;
+};
+
+struct Vi_command {
+  union {
+    struct Int32Vect3  rate;
+    struct Int32Eulers attitude;
+    struct Int32Vect3 speed; //FIXME Warning z is heading rate
+    struct Int32Vect3 pos; //FIXME Warning z is heading
+  } h_sp;
+  union {
+    int32_t direct;
+    int32_t climb;
+    int32_t height;
+  } v_sp;
+  uint8_t h_mode;
+  uint8_t v_mode;
+};
+
+struct VehicleInterface {
+  bool_t enabled;
+  bool_t timeouted;
+  uint8_t last_msg;
+  struct Vi_info info;
+  struct Vi_command input;
+};
+
+extern struct VehicleInterface vi;
+
+extern void vi_init(void);
+extern void vi_set_enabled(bool_t enabled);
+extern void vi_periodic(void);
+extern void vi_update_info(void);
+
+/* must be implemented by specific module */
+extern void vi_impl_init(void);
+extern void vi_impl_periodic(void);
+extern void vi_impl_set_enabled(bool_t enabled);
+
+
+#define vi_SetEnabled(_val) {		\
+    vi.enabled = _val;				\
+    vi_set_enabled(_val);			\
+  }
+
+#endif /* VI_H */
+
+
