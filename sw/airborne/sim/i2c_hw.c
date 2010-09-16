825a83bb7fcd1143bb26e7213c6fffcabfbc52b5
Antoine Drouin <poinix@gmail.com>
2010-09-16 21:44:24 +0000

    fixing unused parameter warnings

--- sw/airborne/sim/i2c_hw.c
+++ sw/airborne/sim/i2c_hw.c
@@ -2,4 +2,4 @@
 
 void i2c_hw_init ( void ) {}
 
-bool_t i2c_submit(struct i2c_periph* p, struct i2c_transaction* t) { return TRUE;}
+bool_t i2c_submit(struct i2c_periph* p __attribute__ ((unused)), struct i2c_transaction* t __attribute__ ((unused))) { return TRUE;}
