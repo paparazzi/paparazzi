7db3f0055189de9035e6217707b40a579523e45e
Allen Ibara <uce.savannah@ibara.net>
2010-10-06 05:10:52 +0000

    Add radio loss watch script

---
+++ sw/ground_segment/python/dashboard/radiowatch.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import wx
+import getopt
+import sys
+import radiowatchframe
+
+class RadioWatchApp(wx.App):
+    def OnInit(self):
+        self.main = radiowatchframe.RadioWatchFrame()
+        self.main.Show()
+        self.SetTopWindow(self.main)
+        return True
+
+def main():
+    application = RadioWatchApp(0)
+    application.MainLoop()
+
+if __name__ == '__main__':
+    main()
