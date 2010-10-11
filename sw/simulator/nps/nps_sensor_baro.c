1559eb41530512bc96f46a0b57a31fef095d4f26
Felix Ruess <felix.ruess@gmail.com>
2010-10-11 21:40:13 +0000

    comment out baro startup delay in nps sim

--- sw/simulator/nps/nps_sensor_baro.c
+++ sw/simulator/nps/nps_sensor_baro.c
@@ -17,20 +17,20 @@ void nps_sensor_baro_init(struct NpsSensorBaro* baro, double time) {
 
 
 void nps_sensor_baro_run_step(struct NpsSensorBaro* baro, double time) {
-  
+
   if (time < baro->next_update)
     return;
 
-  if (time < 10.)
+  /*if (time < 10.)
     baro->value = rint(time*90);
-  else {
+    else {*/
     double z = fdm.ltpprz_pos.z + get_gaussian_noise()*NPS_BARO_NOISE_STD_DEV;
     double baro_reading = NPS_BARO_QNH + z * NPS_BARO_SENSITIVITY;
     baro_reading = rint(baro_reading);
     baro->value = baro_reading;
     Bound(baro->value, 0, 1024);
-  }
-  
+    //}
+
   baro->next_update += NPS_BARO_DT;
   baro->data_available = TRUE;
 }
