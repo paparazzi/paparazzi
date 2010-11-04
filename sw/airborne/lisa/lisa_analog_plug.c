2d4e0034a24d4f94abbc26b08034189df222b61e
Antoine Drouin <poinix@gmail.com>
2010-11-04 15:46:13 +0000

    removed baro functions which are now implemented for real

--- sw/airborne/lisa/lisa_analog_plug.c
+++ sw/airborne/lisa/lisa_analog_plug.c
@@ -1,20 +1,10 @@
 #include "std.h"
 
-uint16_t booz2_analog_baro_offset;
-uint16_t booz2_analog_baro_value;
-uint16_t booz2_analog_baro_value_filtered;
-bool_t   booz2_analog_baro_data_available;
-uint16_t booz2_analog_baro_status;
-
 uint8_t battery_voltage;
 
 void booz2_analog_init(void);
-void booz2_analog_baro_init(void);
 void battery_init(void);
 
 void booz2_analog_init(void) {}
-
-void booz2_analog_baro_init(void) {}
-
 void battery_init(void) {}
 
