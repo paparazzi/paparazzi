296ba6afe0d2a02dd44ec6563b9004886d307065
Antoine Drouin <poinix@gmail.com>
2010-11-04 15:44:42 +0000

    making fixedwing build on lisa/M and lisa/L

--- sw/airborne/boards/lisa_l_1.0.h
+++ sw/airborne/boards/lisa_l_1.0.h
@@ -14,6 +14,14 @@
 // FIXME, this is just to make it compile
 #define POWER_SWITCH_LED 5
 
+/* configuration for aspirin - and more generaly IMUs */
+#define IMU_ACC_DRDY_RCC_GPIO         RCC_APB2Periph_GPIOD
+#define IMU_ACC_DRDY_GPIO             GPIOD
+#define IMU_ACC_DRDY_GPIO_PORTSOURCE  GPIO_PortSourceGPIOD
+
+
+#define ADC_CHANNEL_VSUPPLY 4
+#define DefaultVoltageOfAdc(adc) (0.01787109375*adc)
 
 
 #endif /* CONFIG_LISA_V1_0_H */
--- sw/airborne/boards/lisa_m_1.0.h
+++ sw/airborne/boards/lisa_m_1.0.h
@@ -15,5 +15,13 @@
 #define LED_2_GPIO_CLK RCC_APB2Periph_GPIOC
 #define LED_2_GPIO_PIN GPIO_Pin_13
 
+/* configuration for aspirin - and more generaly IMUs */
+#define IMU_ACC_DRDY_RCC_GPIO         RCC_APB2Periph_GPIOB
+#define IMU_ACC_DRDY_GPIO             GPIOB
+#define IMU_ACC_DRDY_GPIO_PORTSOURCE  GPIO_PortSourceGPIOB
+
+#define ADC_CHANNEL_VSUPPLY 4
+#define DefaultVoltageOfAdc(adc) (0.01787109375*adc)
+
 
 #endif /* CONFIG_LISA_M_1_0_H */
