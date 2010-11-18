93ba8eee06e31fd88130fea4638b6674344b08b5
Eric Parsonage <eric@eparsonage.com>
2010-11-18 13:36:08 +0000

    disable multimon when using OSX

--- sw/ground_segment/multimon/demodml.c
+++ sw/ground_segment/multimon/demodml.c
@@ -7,12 +7,19 @@
 
 value
 ml_demod_init(value dev) {
+#ifndef __APPLE__
   int fd = pprz_demod_init(String_val(dev));
   return Val_int(fd);
+#else
+  failwith("Not supported under OSX");  
+  return Val_int(0);
+#endif
+
 }
 
 value
 ml_demod_get_data(value unit) {
+#ifndef __APPLE__
   struct data *data = pprz_demod_read_data();
   if (data) {
     int i;
@@ -31,4 +38,11 @@ ml_demod_get_data(value unit) {
   } else {
     failwith("End of file");
   }
+#else
+  CAMLparam0();
+  CAMLlocal3 (result,l, r);
+  result = alloc(2, 0);
+  failwith("Not supported under OSX");
+  CAMLreturn (result);	
+#endif
 }
