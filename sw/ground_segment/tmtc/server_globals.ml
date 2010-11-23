9ce3c712cf623e1d460a82e71454be92eeffeab6
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2010-11-23 16:50:10 +0100

    add rc_rate to the rotorcraft for display in gcs

--- sw/ground_segment/tmtc/server_globals.ml
+++ sw/ground_segment/tmtc/server_globals.ml
@@ -8,6 +8,7 @@ let _AUTO2 = 2
 let gaz_modes = [|"MANUAL";"GAZ";"CLIMB";"ALT"|]
 let lat_modes = [|"MANUAL";"ROLL_RATE";"ROLL";"COURSE"|]
 let gps_modes = [|"NOFIX";"DRO";"2D";"3D";"GPSDRO"|]
+let _3D = 3
 let gps_hybrid_modes = [|"OFF";"ON"|]
 let horiz_modes = [|"WAYPOINT";"ROUTE";"CIRCLE";"ATTITUDE"|]
 
