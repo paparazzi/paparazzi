f3cd997e737e01bc4e82ee251a2316834666a8d3
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2010-11-30 15:54:41 +0100

    switch order of subsystem parsing

--- sw/tools/gen_aircraft.ml
+++ sw/tools/gen_aircraft.ml
@@ -280,8 +280,8 @@ let parse_firmware = fun makefile_ac firmware ->
     List.iter (print_firmware_define makefile_ac) t_defines;
     fprintf makefile_ac "include $(PAPARAZZI_SRC)/conf/boards/%s.makefile\n" (Xml.attrib target "board");
     fprintf makefile_ac "include $(PAPARAZZI_SRC)/conf/autopilot/%s.makefile\n" (Xml.attrib firmware "name");
-    List.iter (print_firmware_subsystem makefile_ac firmware) subsystems;
     List.iter (print_firmware_subsystem makefile_ac firmware) t_subsystems;
+    List.iter (print_firmware_subsystem makefile_ac firmware) subsystems;
     fprintf makefile_ac "endif\n\n"
   ) targets
 
