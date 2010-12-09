a3b15f21ded4670b15097116a9466ce885ddc83a
Felix Ruess <felix.ruess@gmail.com>
2010-12-09 21:23:51 +0100

    cleanup trailing whitespaces again

--- sw/airborne/subsystems/ahrs/dcm/dcm.h
+++ sw/airborne/subsystems/ahrs/dcm/dcm.h
@@ -1,4 +1,3 @@
-
 #include "math/pprz_algebra_float.h"
 
 // Inputs for DCM
@@ -23,7 +22,7 @@ extern struct FloatEulers euler;
 //#define Kp_ROLLPITCH 0.2
 #define Kp_ROLLPITCH 0.015
 #define Ki_ROLLPITCH 0.000010
-#define Kp_YAW 1.2      	//High yaw drift correction gain - use with caution!
+#define Kp_YAW 1.2          //High yaw drift correction gain - use with caution!
 #define Ki_YAW 0.00005
 
 #define GRAVITY 9.81
@@ -43,6 +42,3 @@ extern int renorm_sqrt_count;
 extern int renorm_blowup_count;
 extern float imu_health;
 #endif
-
-
-
