3f7baf34c3a091a0d9ef3c6bd2e8675a36cb8adc
Felix Ruess <felix.ruess@gmail.com>
2010-12-10 17:34:24 +0100

    update copyright info for ahrs_float_dcm

--- sw/airborne/subsystems/ahrs/ahrs_float_dcm.c
+++ sw/airborne/subsystems/ahrs/ahrs_float_dcm.c
@@ -1,22 +1,14 @@
 /*
- * Copyright (C) 2010 The Paparazzi Team
+ * Released under Creative Commons License
  *
- * This file is part of paparazzi.
+ * 2010 The Paparazzi Team
  *
- * paparazzi is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
  *
- * paparazzi is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Based on Code by Jordi Munoz and William Premerlani, Supported by Chris Anderson (Wired) and Nathan Sindle (SparkFun).
+ * Version 1.0 for flat board updated by Doug Weibel and Jose Julio
  *
- * You should have received a copy of the GNU General Public License
- * along with paparazzi; see the file COPYING.  If not, write to
- * the Free Software Foundation, 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * Modified at Hochschule Bremen, Germany
+ * 2010 Heinrich Warmers, Christoph Niemann, Oliver Riesener
  *
  */
 
