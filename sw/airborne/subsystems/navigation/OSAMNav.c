8189a25e5fa83dff08d25696016fe5ed0d198092
Christophe De Wagter <dewagter@gmail.com>
2010-12-11 14:55:21 +0100

    Remove compile warning and possible error on zero size

--- sw/airborne/subsystems/navigation/OSAMNav.c
+++ sw/airborne/subsystems/navigation/OSAMNav.c
@@ -364,6 +364,9 @@ bool_t InitializePolygonSurvey(uint8_t EntryWP, uint8_t Size, float sw, float Or
 	struct Point2D Corners[PolygonSize];
 
 	CSurveyStatus = Init;
+	
+	if (Size == 0)
+	  return TRUE;
 
 	//Don't initialize if Polygon is too big or if the orientation is not between 0 and 90
 	if(Size <= PolygonSize && Orientation >= -90 && Orientation <= 90)
