ea3db1db02d0e08ef9746ad24e09d736b31bc142
Felix Ruess <felix.ruess@gmail.com>
2011-01-18 18:08:06 +0100

    remove double defines

--- sw/airborne/modules/sensors/baro_scp.c
+++ sw/airborne/modules/sensors/baro_scp.c
@@ -3,9 +3,6 @@
 #include "led.h"
 #include "mcu.h"
 
-#ifndef DOWNLINK_DEVICE
-#define DOWNLINK_DEVICE DOWNLINK_AP_DEVICE
-#endif
 #include "mcu_periph/uart.h"
 #include "messages.h"
 #include "downlink.h"
