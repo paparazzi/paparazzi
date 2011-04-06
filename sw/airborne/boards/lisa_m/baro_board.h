570efe69f1487ea3ccfb809d018bdf3f71514477
Allen <aibara@jobyenergy.com>
2011-04-05 20:43:15 -0700

    Lisa/M baro: broken calibration calc

--- sw/airborne/boards/lisa_m/baro_board.h
+++ sw/airborne/boards/lisa_m/baro_board.h
@@ -30,6 +30,7 @@ struct BaroBoard {
 };
 
 struct bmp085_baro_calibration {
+  // These values come from EEPROM on sensor
   int16_t ac1;
   int16_t ac2;
   int16_t ac3;
@@ -41,6 +42,9 @@ struct bmp085_baro_calibration {
   int16_t mb;
   int16_t mc;
   int16_t md;
+  
+  // These values are calculated
+  int32_t b5;
 };
 
 extern struct BaroBoard baro_board;
@@ -50,10 +54,9 @@ extern struct bmp085_baro_calibration calibration;
 extern void baro_board_send_reset(void);
 extern void baro_board_send_config(void);
 
-static inline int32_t baro_apply_calibration(void)
+static inline int32_t baro_apply_calibration(int32_t raw)
 {
-  int32_t b5 = 0;
-  int32_t b6 = b5 - 4000;
+  int32_t b6 = calibration.b5 - 4000;
   int x1 = (calibration.b2 * (b6 * b6 >> 12)) >> 11;
   int x2 = calibration.ac2 * b6 >> 11;
   int32_t x3 = x1 + x2;
@@ -62,12 +65,12 @@ static inline int32_t baro_apply_calibration(void)
   x2 = (calibration.b1 * (b6 * b6 >> 12)) >> 16;
   x3 = ((x1 + x2) + 2) >> 2;
   uint32_t b4 = (calibration.ac4 * (uint32_t) (x3 + 32768)) >> 15;
-  //uint32_t b7 = ((uint32_t) bmp085_baro_pressure - b3) * (50000 >> BMP085_OSS);
-  //int32_t p = b7 < 0x80000000 ? (b7 * 2) / b4 : (b7 / b4) * 2;
-  //x1 = (p >> 8) * (p >> 8);
-  //x1 = (x1 * 3038) >> 16;
-  //x2 = (-7357 * p) >> 16;
-  //bmp085_baro_pressure_pa = (bmp085_baro_pressure_pa + (p + ((x1 + x2 + 3791) >> 4)))/2;
+  uint32_t b7 = (raw - b3) * (50000 >> BMP085_OSS);
+  int32_t p = b7 < 0x80000000 ? (b7 * 2) / b4 : (b7 / b4) * 2;
+  x1 = (p >> 8) * (p >> 8);
+  x1 = (x1 * 3038) >> 16;
+  x2 = (-7357 * p) >> 16;
+  return p + ((x1 + x2 + 3791) >> 4);
 }
 
 static inline void baro_event(void (*b_abs_handler)(void), void (*b_diff_handler)(void))
@@ -77,7 +80,9 @@ static inline void baro_event(void (*b_abs_handler)(void), void (*b_diff_handler
     baro_board.status = LBS_REQUEST_TEMP;
     if (baro_trans.status == I2CTransSuccess) {
       int32_t tmp = (baro_trans.buf[0]<<16) | (baro_trans.buf[1] << 8) | baro_trans.buf[2];
-      baro.absolute = tmp >> ( 8 - BMP085_OSS);
+      tmp = tmp >> ( 8 - BMP085_OSS);
+      //baro.absolute = baro_apply_calibration(tmp);
+      baro.absolute = tmp;
       b_abs_handler();
     }
   }
@@ -89,8 +94,8 @@ static inline void baro_event(void (*b_abs_handler)(void), void (*b_diff_handler
       int32_t tmp = (baro_trans.buf[0] << 8) | baro_trans.buf[1];
       int32_t x1 = ((tmp - calibration.ac6) * calibration.ac5) >> 15;
       int32_t x2 = (calibration.mc << 11) / (x1 + calibration.md);
-      int32_t b5 = x1 + x2;
-      baro.differential = (b5 + 8) >> 4;
+      calibration.b5 = x1 + x2;
+      baro.differential = (calibration.b5 + 8) >> 4;
       b_diff_handler();
     }
   }
