df76d5e80e8840abf6130439fbbf1c96f4e92845
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2011-05-25 10:38:10 +0200

    fix bugs in altitude filter

--- sw/airborne/modules/ins/alt_filter.c
+++ sw/airborne/modules/ins/alt_filter.c
@@ -36,6 +36,8 @@ TypeKalman alt_filter;
 /* Kalman parameters */
 float SigAltiGPS;
 float SigAltiAltimetre;
+float MarcheAleaBiaisAltimetre;
+float MarcheAleaAccelerometre;
 
 /* Function declaration */
 void kalmanInit(TypeKalman *k);
@@ -48,8 +50,10 @@ float last_gps_alt;
 float last_baro_alt;
 
 void alt_filter_init(void) {
-  SigAltiGPS = 20.;
-  SigAltiAltimetre = 0.5;
+  SigAltiGPS = 5.;
+  SigAltiAltimetre = 5.;
+  MarcheAleaBiaisAltimetre = 0.1;
+  MarcheAleaAccelerometre = 0.5;
   last_gps_alt = 0.;
   last_baro_alt = 0.;
 
@@ -62,13 +66,13 @@ void alt_filter_periodic(void) {
 
   // update on new data
   float ga = (float)gps_alt / 100.;
-  if (ga != last_gps_alt && GpsFixValid()) {
-    kalmanCorrectionGPS(&alt_filter, ga);
-    last_gps_alt = ga;
-  }
   if (baro_ets_altitude != last_baro_alt) {
     kalmanCorrectionAltimetre(&alt_filter, baro_ets_altitude);
     last_baro_alt = baro_ets_altitude;
+  }
+  if (ga != last_gps_alt && GpsFixValid()) {
+    kalmanCorrectionGPS(&alt_filter, ga);
+    last_gps_alt = ga;
   }
 
   RunOnceEvery(6,DOWNLINK_SEND_VFF(DefaultChannel, &baro_ets_altitude,
@@ -87,8 +91,8 @@ void alt_filter_periodic(void) {
 
 void kalmanInit(TypeKalman *k){
 
-  k->W[0][0] = 0.001; k->W[0][1] = 0;
-  k->W[1][0] = 0; k->W[1][1] = 0.001;
+  k->W[0][0] = MarcheAleaAccelerometre*MarcheAleaAccelerometre; k->W[0][1] = 0;
+  k->W[1][0] = 0; k->W[1][1] = MarcheAleaBiaisAltimetre*MarcheAleaBiaisAltimetre;
 
   k->X[0] = 0;
   k->X[1] = 0;
@@ -96,7 +100,7 @@ void kalmanInit(TypeKalman *k){
 
   k->P[0][0] = 1; k->P[0][1] = 0; k->P[0][2] = 0;
   k->P[1][0] = 0; k->P[1][1] = 1; k->P[1][2] = 0;
-  k->P[2][0] = 0; k->P[2][1] = 0; k->P[2][2] = 0;
+  k->P[2][0] = 0; k->P[2][1] = 0; k->P[2][2] = 0.0001;
 
   k->Te = (1./60.);
 
@@ -110,8 +114,8 @@ void kalmanInit(TypeKalman *k){
   k->Bd[1] = k->Te;
   k->Bd[2] = 0;
 
-  k->Md[0][0] = pow(k->Te, 1.5)/2; k->Md[0][1] = k->Te;
-  k->Md[1][0] = pow(k->Te, 0.5); k->Md[1][1] = 1;
+  k->Md[0][0] = pow(k->Te, 1.5)/2; k->Md[0][1] = 0;
+  k->Md[1][0] = pow(k->Te, 0.5); k->Md[1][1] = 0;
   k->Md[2][0] = 0; k->Md[2][1] = pow(k->Te, 0.5);
 }
 
@@ -131,12 +135,13 @@ void kalmanEstimation(TypeKalman *k, float accVert){
   float J[3][2];
 
   // Calcul de X
-  // X = A*X + B*U
-  k->X[0] = k->Ad[0][0]*k->X[0] + k->Ad[0][1]*k->X[1] + k->Bd[0]*accVert;
-  k->X[1] = k->Ad[1][1]*k->X[1] + k->Bd[1]*accVert;
-  k->X[2] = k->Ad[2][2]*k->X[2] + k->Bd[2]*accVert;
+  // X(k+1) = Ad*X(k) + Bd*U(k)
+  k->X[0] = k->Ad[0][0]*k->X[0] + k->Ad[0][1]*k->X[1] + k->Ad[0][2]*k->X[2] + k->Bd[0]*accVert;
+  k->X[1] = k->Ad[1][0]*k->X[0] + k->Ad[1][1]*k->X[1] + k->Ad[1][2]*k->X[2] + k->Bd[1]*accVert;
+  k->X[2] = k->Ad[2][0]*k->X[0] + k->Ad[2][1]*k->X[1] + k->Ad[2][2]*k->X[2] + k->Bd[2]*accVert;
 
   // Calcul de P
+  // P(k+1) = Ad*P(k)*Ad' + Md*W*Md'
   for(i=0;i<3;i++){
     for (j=0;j<3;j++){
       I[i][j] = k->Ad[i][0]*k->P[0][j] + k->Ad[i][1]*k->P[1][j] + k->Ad[i][2]*k->P[2][j];
@@ -172,6 +177,7 @@ void kalmanEstimation(TypeKalman *k, float accVert){
 void kalmanCorrectionGPS(TypeKalman *k, float altitude_gps){ // altitude_gps est l'altitude telle qu'elle est mesurée par le GPS
 
   int i, j, div;
+  float I[3][3]; // matrice temporaire
 
   float Kf[3] = { 0., 0., 0. };
 
@@ -181,24 +187,27 @@ void kalmanCorrectionGPS(TypeKalman *k, float altitude_gps){ // altitude_gps est
   div = k->P[0][0] + SigAltiGPS*SigAltiGPS;
 
   if (fabs(div) > 1e-5) {
-    // K = P*C'*inv(div)
+    // Kf = P*C'*inv(div)
     Kf[0] = k->P[0][0] / div;
     Kf[1] = k->P[1][0] / div;
     Kf[2] = k->P[2][0] / div;
 
     // calcul de X
-    // X = X + K*(meas - C*X)
+    // X = X + Kf*(meas - C*X)
     float constante = k->X[0];
     for(i=0;i<3;i++){
-      k->X[i] = k->X[i]+Kf[i]*(altitude_gps-constante);
+      k->X[i] = k->X[i]+Kf[i]*(altitude_gps - constante);
     }
 
     // calcul de P
-    // P = P - K*C*P
-    constante = Kf[0];
+    // P = P - Kf*C*P
+    I[0][0] = Kf[0]; I[0][1] = 0; I[0][2] = 0;
+    I[1][0] = Kf[1]; I[1][1] = 0; I[1][2] = 0;
+    I[2][0] = Kf[2]; I[2][1] = 0; I[2][2] = 0;
+	
     for(i=0;i<3;i++){
       for (j=0;j<3;j++){
-        k->P[i][j] = k->P[i][j] - constante*k->P[i][j];
+        k->P[i][j] = k->P[i][j] - I[i][0]*k->P[0][j] - I[i][1]*k->P[1][j] - I[i][2]*k->P[2][j];
       }
     }
   }
@@ -214,6 +223,7 @@ void kalmanCorrectionGPS(TypeKalman *k, float altitude_gps){ // altitude_gps est
 void kalmanCorrectionAltimetre(TypeKalman *k, float altitude_altimetre){
 
   int i, j, div;
+  float I[3][3]; // matrice temporaire
 
   float Kf[3] = { 0., 0., 0. };
 
@@ -221,25 +231,29 @@ void kalmanCorrectionAltimetre(TypeKalman *k, float altitude_altimetre){
   // C = [1 0 1]
   // div = C*P*C' + R
   div = k->P[0][0] + k->P[2][0] + k->P[0][2] + k->P[2][2] + SigAltiAltimetre*SigAltiAltimetre;
+  
   if (fabs(div) > 1e-5) {
-    // K = P*C'*inv(div)
+    // Kf = P*C'*inv(div)
     Kf[0] = (k->P[0][0] + k->P[0][2]) / div;
     Kf[1] = (k->P[1][0] + k->P[1][2]) / div;
     Kf[2] = (k->P[2][0] + k->P[2][2]) / div;
 
     // calcul de X
-    // X = X + K*(meas - C*X)
-    float constante = k->X[0] + k->X[2]; //C[0]*k->X[0]+C[1]*k->X[1]+C[2]*k->X[2];
+    // X = X + Kf*(meas - C*X)
+    float constante = k->X[0] + k->X[2];
     for(i=0;i<3;i++){
-      k->X[i] = k->X[i]+Kf[i]*(altitude_altimetre-constante);
+      k->X[i] = k->X[i]+Kf[i]*(altitude_altimetre - constante);
     }
 
     // calcul de P
-    // P = P - K*C*P
-    constante = Kf[0] + Kf[2]; //Kf[0]*C[0]+Kf[1]*C[1]+Kf[2]*C[2];
+    // P = P - Kf*C*P
+    I[0][0] = Kf[0]; I[0][1] = 0; I[0][2] = Kf[0];
+    I[1][0] = Kf[1]; I[1][1] = 0; I[1][2] = Kf[1];
+    I[2][0] = Kf[2]; I[2][1] = 0; I[2][2] = Kf[2];
+
     for(i=0;i<3;i++){
       for (j=0;j<3;j++){
-        k->P[i][j] = k->P[i][j] - constante*k->P[i][j];
+        k->P[i][j] = k->P[i][j] - I[i][0]*k->P[0][j] - I[i][1]*k->P[1][j] - I[i][2]*k->P[2][j];
       }
     }
   }
