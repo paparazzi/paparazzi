ef47d70c86cedadc38f7604251e7833b66bf3e33
Allen <aibara@jobyenergy.com>
2011-05-25 14:18:12 -0700

    More fixes for rotorcraft quaternion int attitude stabilization -- need to divide rate by 16 to match euler controler, remove dead code

--- sw/airborne/firmwares/rotorcraft/stabilization/stabilization_attitude_quat_int.c
+++ sw/airborne/firmwares/rotorcraft/stabilization/stabilization_attitude_quat_int.c
@@ -135,19 +135,16 @@ void stabilization_attitude_enter(void) {
   INT_EULERS_ZERO( stabilization_att_sum_err );
 }
 
+#define OFFSET_AND_ROUND(_a, _b) (((_a)+(1<<((_b)-1)))>>(_b))
+#define OFFSET_AND_ROUND2(_a, _b) (((_a)+(1<<((_b)-1))-((_a)<0?1:0))>>(_b))
 
 static void attitude_run_ff(int32_t ff_commands[], struct Int32AttitudeGains *gains, struct Int32Rates *ref_accel)
 {
   /* Compute feedforward based on reference acceleration */
 
-  ff_commands[COMMAND_ROLL]          = GAIN_PRESCALER_FF * gains->dd.x * RATE_FLOAT_OF_BFP(ref_accel->p);
-  ff_commands[COMMAND_PITCH]         = GAIN_PRESCALER_FF * gains->dd.y * RATE_FLOAT_OF_BFP(ref_accel->q);
-  ff_commands[COMMAND_YAW]           = GAIN_PRESCALER_FF * gains->dd.z * RATE_FLOAT_OF_BFP(ref_accel->r);
-  /*
-  ff_commands[COMMAND_ROLL_SURFACE]  = GAIN_PRESCALER_FF * gains->surface_dd.x * ref_accel->p;
-  ff_commands[COMMAND_PITCH_SURFACE] = GAIN_PRESCALER_FF * gains->surface_dd.y * ref_accel->q;
-  ff_commands[COMMAND_YAW_SURFACE]   = GAIN_PRESCALER_FF * gains->surface_dd.z * ref_accel->r;
-  */
+  ff_commands[COMMAND_ROLL]          = 0; GAIN_PRESCALER_FF * gains->dd.x * OFFSET_AND_ROUND(stabilization_gains.dd.x * ref_accel->p, 5);
+  ff_commands[COMMAND_PITCH]         = 0; GAIN_PRESCALER_FF * gains->dd.x * OFFSET_AND_ROUND(stabilization_gains.dd.y * ref_accel->q, 5);
+  ff_commands[COMMAND_YAW]           = 0; GAIN_PRESCALER_FF * gains->dd.x * OFFSET_AND_ROUND(stabilization_gains.dd.z * ref_accel->r, 5);
 }
 
 static void attitude_run_fb(int32_t fb_commands[], struct Int32AttitudeGains *gains, struct Int32Quat *att_err,
@@ -156,38 +153,21 @@ static void attitude_run_fb(int32_t fb_commands[], struct Int32AttitudeGains *ga
   /*  PID feedback */
   fb_commands[COMMAND_ROLL] =
     GAIN_PRESCALER_P * -gains->p.x  * QUAT1_FLOAT_OF_BFP(att_err->qx)  / 2+
-    GAIN_PRESCALER_D * gains->d.x  * RATE_FLOAT_OF_BFP(rate_err->p) +
+    GAIN_PRESCALER_D * gains->d.x  * RATE_FLOAT_OF_BFP(rate_err->p) / 16 +
     //GAIN_PRESCALER_D * gains->rates_d.x  * rate_err_d->p +
     GAIN_PRESCALER_I * gains->i.x  * QUAT1_FLOAT_OF_BFP(sum_err->qx) / 2;
 
   fb_commands[COMMAND_PITCH] =
     GAIN_PRESCALER_P * -gains->p.y  * QUAT1_FLOAT_OF_BFP(att_err->qy)  / 2+
-    GAIN_PRESCALER_D * gains->d.y  * RATE_FLOAT_OF_BFP(rate_err->q) +
+    GAIN_PRESCALER_D * gains->d.y  * RATE_FLOAT_OF_BFP(rate_err->q)  / 16+
     //GAIN_PRESCALER_D * gains->rates_d.y  * rate_err_d->q +
     GAIN_PRESCALER_I * gains->i.y  * QUAT1_FLOAT_OF_BFP(sum_err->qy) / 2;
 
   fb_commands[COMMAND_YAW] =
     GAIN_PRESCALER_P * -gains->p.z  * QUAT1_FLOAT_OF_BFP(att_err->qz)  / 2+
-    GAIN_PRESCALER_D * gains->d.z  * RATE_FLOAT_OF_BFP(rate_err->r) +
+    GAIN_PRESCALER_D * gains->d.z  * RATE_FLOAT_OF_BFP(rate_err->r)  / 16+
     //GAIN_PRESCALER_D * gains->rates_d.z  * rate_err_d->r +
     GAIN_PRESCALER_I * gains->i.z  * QUAT1_FLOAT_OF_BFP(sum_err->qz) / 2;
-
-  /*
-  fb_commands[COMMAND_ROLL_SURFACE] =
-    GAIN_PRESCALER_P * -gains->surface_p.x  * att_err->qx +
-    GAIN_PRESCALER_D * gains->surface_d.x  * rate_err->p +
-    GAIN_PRESCALER_I * gains->surface_i.x  * sum_err->qx;
-
-  fb_commands[COMMAND_PITCH_SURFACE] =
-    GAIN_PRESCALER_P * -gains->surface_p.y  * att_err->qy +
-    GAIN_PRESCALER_D * gains->surface_d.y  * rate_err->q +
-    GAIN_PRESCALER_I * gains->surface_i.y  * sum_err->qy;
-
-  fb_commands[COMMAND_YAW_SURFACE] =
-    GAIN_PRESCALER_P * -gains->surface_p.z  * att_err->qz +
-    GAIN_PRESCALER_D * gains->surface_d.z  * rate_err->r +
-    GAIN_PRESCALER_I * gains->surface_i.z  * sum_err->qz;
-    */
 
 }
 
@@ -207,6 +187,7 @@ void stabilization_attitude_run(bool_t enable_integrator) {
   INT32_QUAT_INV_COMP(att_err, ahrs.ltp_to_body_quat, stab_att_ref_quat);
   /* wrap it in the shortest direction       */
   INT32_QUAT_WRAP_SHORTEST(att_err);
+  INT32_QUAT_NORMALIZE(att_err);
 
   /*  rate error                */
   struct Int32Rates rate_err;
