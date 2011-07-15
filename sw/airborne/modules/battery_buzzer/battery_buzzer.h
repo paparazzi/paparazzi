e92f8a2828486ebcfa5ec9b2b61bd7c9e19a4665
Piotr Esden-Tempski <piotr@esden.net>
2011-07-14 19:57:06 -0700

    Added low battery voltage indicator buzzer module.

---
+++ sw/airborne/modules/battery_buzzer/battery_buzzer.h
@@ -0,0 +1,35 @@
+
+/*
+ * $Id: $
+ *
+ * Copyright (C) 2010 Eric Parsonage
+ * Copyright (C) 2011 Piotr Esden-Tempski <piotr@esden.net>
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+
+#ifndef BATTERY_BUZZER_H
+#define BATTERY_BUZZER_H
+
+#include "std.h"
+
+extern void battery_buzzer_init(void);
+extern void battery_buzzer_periodic(void);
+
+#endif  /* BATTERY_BUZZER_H */
