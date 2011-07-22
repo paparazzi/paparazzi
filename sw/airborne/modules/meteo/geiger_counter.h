813eee7a8e2c28d410eab0b87d9d269a2800bece
Martin Mueller <martin@pfump.org>
2011-07-22 17:31:19 +0200

    Add University of Reading Geiger-Mueller counter.

---
+++ sw/airborne/modules/meteo/geiger_counter.h
@@ -0,0 +1,11 @@
+#ifndef GEIGER_COUNTER_H
+#define GEIGER_COUNTER_H
+
+#include "std.h"
+
+void geiger_counter_init(void);
+void geiger_counter_periodic(void);
+void geiger_counter_event(void);
+
+#endif
+
