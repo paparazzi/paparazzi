6b6b32a715bf5847c267e71daf267185eaa27fce
Felix Ruess <felix.ruess@gmail.com>
2011-11-03 14:20:48 +0100

    rotorcraft guidance_h: set the same gain multipliers in hover as in nav. With this the hover (stay) behaviour should be the same in AP_HOVER and in nav (without using the ref). This means that if you tuned the p-gain and i-gain in AP_MODE_HOVER_x before, you will have to divide the old p-gain by 2 and the old i-gain by 4.

--- sw/airborne/firmwares/rotorcraft/guidance/guidance_h.c
+++ sw/airborne/firmwares/rotorcraft/guidance/guidance_h.c
@@ -240,12 +240,14 @@ __attribute__ ((always_inline)) static inline void  guidance_h_hover_run(void) {
 
   /* run PID */
   // cmd_earth < 15.17
-  guidance_h_command_earth.x = (guidance_h_pgain<<1)  * guidance_h_pos_err.x +
-                                     guidance_h_dgain * (guidance_h_speed_err.x>>9) +
-                                      guidance_h_igain * (guidance_h_pos_err_sum.x >> 12);
-  guidance_h_command_earth.y = (guidance_h_pgain<<1)  * guidance_h_pos_err.y +
-                                     guidance_h_dgain *( guidance_h_speed_err.y>>9) +
-		                      guidance_h_igain * (guidance_h_pos_err_sum.y >> 12);
+  guidance_h_command_earth.x =
+    guidance_h_pgain * (guidance_h_pos_err.x << (10 - INT32_POS_FRAC)) +
+    guidance_h_dgain * (guidance_h_speed_err.x >> (INT32_SPEED_FRAC - 10)) +
+    guidance_h_igain * (guidance_h_pos_err_sum.x >> (12 + INT32_POS_FRAC - 10));
+  guidance_h_command_earth.y =
+    guidance_h_pgain * (guidance_h_pos_err.y << (10 - INT32_POS_FRAC)) +
+    guidance_h_dgain * (guidance_h_speed_err.y >> (INT32_SPEED_FRAC - 10)) +
+    guidance_h_igain * (guidance_h_pos_err_sum.y >> (12 + INT32_POS_FRAC - 10));
 
   VECT2_STRIM(guidance_h_command_earth, -MAX_BANK, MAX_BANK);
 
