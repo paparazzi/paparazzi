08a81ce03432ad07d87790b8dbd355ba8187e672
Felix Ruess <felix.ruess@gmail.com>
2011-12-05 21:44:22 +0100

    added export to fw estimator

--- sw/airborne/subsystems/ahrs/ahrs_float_cmpl_rmat.h
+++ sw/airborne/subsystems/ahrs/ahrs_float_cmpl_rmat.h
@@ -42,4 +42,12 @@ struct AhrsFloatCmplRmat {
 extern struct AhrsFloatCmplRmat ahrs_impl;
 
 
+#ifdef AHRS_UPDATE_FW_ESTIMATOR
+// TODO copy ahrs to state instead of estimator
+void ahrs_update_fw_estimator(void);
+extern float ins_roll_neutral;
+extern float ins_pitch_neutral;
+#endif
+
+
 #endif /* AHRS_FLOAT_CMPL_RMAT */
