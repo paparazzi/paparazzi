81aea93baf1ffe9f3de91a2f55388e8e2d8b2cc3
Felix Ruess <felix.ruess@gmail.com>
2011-12-23 14:50:43 +0100

    scale_of_units: return 1.0 if units are the same

--- sw/lib/ocaml/pprz.ml
+++ sw/lib/ocaml/pprz.ml
@@ -162,22 +162,25 @@ exception Unit_conversion_error of string
 exception Unknown_conversion of string * string
 
 let scale_of_units = fun from_unit to_unit ->
-  try
-    let units_xml = Xml.parse_file units_file in
-    (* find the first occurence of matching units or raise Not_found *)
-    let _unit = List.find (fun u ->
-        (* will raise Xml.No_attribute if not a valid attribute *)
-        let f = Xml.attrib u "from"
-        and t = Xml.attrib u "to" in
-        if from_unit = f && to_unit = t then true else false
-      ) (Xml.children units_xml) in
-    (* return coef, raise Failure if coef is not a numerical value *)
-    float_of_string (Xml.attrib _unit "coef")
-  with Xml.File_not_found _ -> raise (Unit_conversion_error ("Parse error of conf/units.xml"))
-  | Xml.No_attribute _ | Xml.Not_element _ -> raise (Unit_conversion_error ("File conf/units.xml has errors"))
-  | Failure "float_of_string" -> raise (Unit_conversion_error ("Unit coef is not numerical value"))
-  | Not_found -> raise (Unknown_conversion (from_unit, to_unit))
-  | _ -> raise (Unknown_conversion (from_unit, to_unit))
+  if (from_unit = to_unit) then
+    1.0
+  else
+    try
+      let units_xml = Xml.parse_file units_file in
+      (* find the first occurence of matching units or raise Not_found *)
+      let _unit = List.find (fun u ->
+          (* will raise Xml.No_attribute if not a valid attribute *)
+          let f = Xml.attrib u "from"
+          and t = Xml.attrib u "to" in
+          if from_unit = f && to_unit = t then true else false
+        ) (Xml.children units_xml) in
+      (* return coef, raise Failure if coef is not a numerical value *)
+      float_of_string (Xml.attrib _unit "coef")
+    with Xml.File_not_found _ -> raise (Unit_conversion_error ("Parse error of conf/units.xml"))
+      | Xml.No_attribute _ | Xml.Not_element _ -> raise (Unit_conversion_error ("File conf/units.xml has errors"))
+      | Failure "float_of_string" -> raise (Unit_conversion_error ("Unit coef is not numerical value"))
+      | Not_found -> raise (Unknown_conversion (from_unit, to_unit))
+      | _ -> raise (Unknown_conversion (from_unit, to_unit))
 
 
 let alt_unit_coef_of_xml = function xml ->
