d94719bef88236de6f18f1d34887aec44b151f49
Felix Ruess <felix.ruess@gmail.com>
2012-01-07 22:04:40 +0100

    fix inlude of downlink for lisa/l baro board

--- sw/airborne/boards/lisa_l/baro_board.c
+++ sw/airborne/boards/lisa_l/baro_board.c
@@ -4,7 +4,7 @@
 // Downlink
 #include "mcu_periph/uart.h"
 #include "messages.h"
-#include "subsystems/datalink/downlink.h"
+#include "downlink.h"
 
 #ifndef DOWNLINK_DEVICE
 #define DOWNLINK_DEVICE DOWNLINK_AP_DEVICE
