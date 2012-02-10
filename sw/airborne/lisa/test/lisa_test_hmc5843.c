31ea8f14cd0b55909fe38d95a7ad1be38a03b7b0
Felix Ruess <felix.ruess@gmail.com>
2012-02-10 22:05:15 +0100

    fix some more lisa test progs

--- sw/airborne/lisa/test/lisa_test_hmc5843.c
+++ sw/airborne/lisa/test/lisa_test_hmc5843.c
@@ -66,7 +66,7 @@ int main(void) {
   main_init();
 
   while(1) {
-    if (sys_time_periodic())
+    if (sys_time_check_and_ack_timer(0))
       main_periodic_task();
     main_event_task();
   }
@@ -76,7 +76,7 @@ int main(void) {
 
 static inline void main_init( void ) {
   mcu_init();
-  sys_time_init();
+  sys_time_register_timer((1./PERIODIC_FREQUENCY), NULL);
   main_init_hw();
 }
 
--- sw/airborne/lisa/test_adc.c
+++ sw/airborne/lisa/test_adc.c
@@ -47,7 +47,7 @@ extern uint8_t adc_new_data_trigger;
 
 static inline void main_init( void ) {
     mcu_init();
-    sys_time_init();
+    sys_time_register_timer((1./PERIODIC_FREQUENCY), NULL);
     adc_init();
     adc_buf_channel(0, &adc0_buf, 8);
     adc_buf_channel(1, &adc1_buf, 3);
@@ -59,7 +59,7 @@ int main( void ) {
   main_init();
 
   while(1) {
-    if (sys_time_periodic()) {
+    if (sys_time_check_and_ack_timer(0)) {
       main_periodic_task();
     }
     main_event_task();
