1045759f72082aa92851691242bc046e1b45d318
Felix Ruess <felix.ruess@gmail.com>
2012-03-06 23:57:05 +0100

    xbee_api: use sys_time_usleep again, since that is fixed now

--- sw/airborne/subsystems/datalink/xbee.c
+++ sw/airborne/subsystems/datalink/xbee.c
@@ -50,17 +50,13 @@ void xbee_init( void ) {
 
 #ifndef NO_XBEE_API_INIT
   /** - busy wait 1.25s */
-  //sys_time_usleep(1250000);
-  SysTimeTimerStart(xbee_delay_time);
-  while (SysTimeTimer(xbee_delay_time) < 1250000);
+  sys_time_usleep(1250000);
 
   /** Switching to AT mode (FIXME: busy waiting) */
   XBeePrintString(XBEE_UART,AT_COMMAND_SEQUENCE);
 
   /** - busy wait 1.25s */
-  //sys_time_usleep(1250000);
-  SysTimeTimerStart(xbee_delay_time);
-  while (SysTimeTimer(xbee_delay_time) < 1250000);
+  sys_time_usleep(1250000);
 
   /** Setting my address */
   XBeePrintString(XBEE_UART,AT_SET_MY);
