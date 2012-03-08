c0b60b1c4d841bbe398e051413d25d58fd9051af
Christophe De Wagter <dewagter@gmail.com>
2012-03-08 22:11:09 +0100

    Precision Timing Catapult Launch System with Highspeed Module <-> Flightplan interaction

---
+++ sw/airborne/modules/nav/nav_catapult.h
@@ -0,0 +1,45 @@
+/*
+ *
+ * Copyright (C) 2012, Christophe De Wagter
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+/**
+ * @file module/nav/nav_catapult.h
+ * @brief catapult launch timing system
+ */
+
+#ifndef NAV_CATAPULT_H
+#define NAV_CATAPULT_H
+
+#include "std.h"
+#include "paparazzi.h"
+
+// Module Code
+void nav_catapult_highrate_module(void);
+
+// Flightplan Code
+extern bool_t nav_catapult_init(void);
+
+extern bool_t nav_catapult_arm(void);
+extern bool_t nav_catapult(uint8_t _climb);
+extern bool_t nav_catapult_disarm(void);
+
+#endif
