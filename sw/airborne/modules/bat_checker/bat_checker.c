885b1dbfe6b2d9c2fdd18091ce2f98439407ae89
Thomas Kolb <cfr34k-paparazzi@tkolb.de>
2012-04-05 11:47:03 +0200

    Added a battery checker module

---
+++ sw/airborne/modules/bat_checker/bat_checker.c
@@ -0,0 +1,65 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) 2012  Thomas Kolb
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include "bat_checker.h"
+#include "generated/airframe.h"
+#include "subsystems/electrical.h"
+#include "led.h"
+
+#ifndef CRITIC_BAT_LEVEL
+#error You must define CRITIC_BAT_LEVEL to use this module!
+#endif
+
+#define WARN_BAT_LEVEL (CRITIC_BAT_LEVEL*10)
+
+#pragma message "Battery checker included!"
+
+void init_bat_checker(void) {
+  // nothing to do here
+}
+
+void bat_checker_periodic(void) {
+  static uint8_t bat_low_counter;
+  if(electrical.vsupply < WARN_BAT_LEVEL) {
+    bat_low_counter++;
+  } else {
+    bat_low_counter = 0;
+  }
+
+  if(bat_low_counter >= BAT_CHECKER_DELAY) {
+    LED_TOGGLE(BAT_CHECKER_LED);
+    bat_low_counter = BAT_CHECKER_DELAY; // hold value to prevent overflow
+  } else {
+    LED_OFF(BAT_CHECKER_LED);
+  }
+}
+
+void start_bat_checker(void) {
+  LED_OFF(BAT_CHECKER_LED);
+}
+
+void stop_bat_checker(void) {
+  // nothing to do here
+}
+
