91809f64aca20ab1d4d14f55fea6f3a338444040
Pranay Sinha <psinha@psinha-laptop.(none)>
2012-04-25 12:53:41 -0700

    Allow motors to arm in any mode if they were switched off in a mode other than MODE_MANUAL.

--- sw/airborne/firmwares/rotorcraft/autopilot_arming_throttle.h
+++ sw/airborne/firmwares/rotorcraft/autopilot_arming_throttle.h
@@ -37,6 +37,7 @@ enum arming_throttle_state {
 
 enum arming_throttle_state autopilot_arming_state;
 uint8_t autopilot_arming_delay_counter;
+bool_t autopilot_unarming_mode_auto = FALSE;
 
 static inline void autopilot_arming_init(void) {
   autopilot_arming_state = STATE_UNINIT;
@@ -91,7 +92,7 @@ static inline void autopilot_arming_check_motors_on( void ) {
     autopilot_arming_delay_counter++;
     if (THROTTLE_STICK_DOWN() ||
         !rc_attitude_sticks_centered() ||
-        autopilot_mode != MODE_MANUAL) {
+        autopilot_mode != MODE_MANUAL || !autopilot_unarming_mode_auto) {
       autopilot_arming_state = STATE_MOTORS_OFF_READY;
     }
     else if (autopilot_arming_delay_counter >= AUTOPILOT_ARMING_DELAY)
@@ -110,6 +111,10 @@ static inline void autopilot_arming_check_motors_on( void ) {
       autopilot_arming_state = STATE_MOTORS_ON;
     else if (autopilot_arming_delay_counter == 0)
       autopilot_arming_state = STATE_MOTORS_OFF_READY;
+      if (autopilot_mode != MODE_MANUAL)
+         autopilot_unarming_mode_auto = TRUE;
+      else 
+         autopilot_unarming_mode_auto = FALSE; 
     break;
   default:
     break;
