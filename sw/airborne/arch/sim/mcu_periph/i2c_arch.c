264a8584d1185b2d7da94ea73414cdf811972856
Felix Ruess <felix.ruess@gmail.com>
2012-05-09 16:46:58 +0200

    [sim arch] added dummy functions for i2c er_isr to compile sim

--- sw/airborne/arch/sim/mcu_periph/i2c_arch.c
+++ sw/airborne/arch/sim/mcu_periph/i2c_arch.c
@@ -7,3 +7,5 @@ bool_t i2c_submit(struct i2c_periph* p __attribute__ ((unused)), struct i2c_tran
 void i2c_event(void) { }
 void i2c2_setbitrate(int bitrate __attribute__ ((unused))) { }
 
+void i2c1_er_isr(void) {}
+void i2c2_er_isr(void) {}
