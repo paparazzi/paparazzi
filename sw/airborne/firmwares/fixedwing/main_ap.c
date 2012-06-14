ac16d06b2247a9c0466b2d207d0b5acc9cf7e8c0
Tilman Baumann <tilman@baumann.name>
2012-06-14 23:14:46 +0200

    Mode switching via two two way switches.
    RADIO_MODE will switch between MANUAL and AUTO
          <define name="RADIO_MODE" value="RADIO_GEAR"/>
    And the RADIO_AUTO_MODE one will, in case the MODE switch is on AUTO select between the different AUTO MODES.
          <define name="RADIO_AUTO_MODE" value="RADIO_AUX1"/>
    
    closes #218

--- sw/airborne/firmwares/fixedwing/main_ap.c
+++ sw/airborne/firmwares/fixedwing/main_ap.c
@@ -282,7 +282,25 @@ static inline uint8_t pprz_mode_update( void ) {
       || TRUE
 #endif
       ) {
+#ifndef RADIO_AUTO_MODE
     return ModeUpdate(pprz_mode, PPRZ_MODE_OF_PULSE(fbw_state->channels[RADIO_MODE]));
+#else
+#pragma message "Using RADIO_AUTO_MODE to switch between AUTO1 and AUTO2."
+    /* If RADIO_AUTO_MODE is enabled mode swithing will be seperated between two switches/channels
+     * RADIO_MODE will switch between PPRZ_MODE_MANUAL and any PPRZ_MODE_AUTO mode selected by RADIO_AUTO_MODE.
+     *
+     * This is mainly a cludge for entry level radios with no three-way switch but two available two-way switches which can be used.
+     */
+    if(PPRZ_MODE_OF_PULSE(fbw_state->channels[RADIO_MODE]) == PPRZ_MODE_MANUAL) {
+      /* RADIO_MODE in MANUAL position */
+      return ModeUpdate(pprz_mode, PPRZ_MODE_MANUAL);
+    } else {
+      /* RADIO_MODE not in MANUAL position.
+       * Select AUTO mode bassed on RADIO_AUTO_MODE channel
+       */
+      return ModeUpdate(pprz_mode, (fbw_state->channels[RADIO_AUTO_MODE] > THRESHOLD2) ? PPRZ_MODE_AUTO2 : PPRZ_MODE_AUTO1);
+    }
+#endif // RADIO_AUTO_MODE
   } else
     return FALSE;
 }
