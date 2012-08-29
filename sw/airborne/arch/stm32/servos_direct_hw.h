0c0952f028c5eaeb056a5111ac93b71b18ba3c70
Felix Ruess <felix.ruess@gmail.com>
2012-08-29 23:59:14 +0200

    [stm32] fix ACTUATORS_PWM_NB for fixedwings with USE_SERVOS_7AND8

--- sw/airborne/arch/stm32/servos_direct_hw.h
+++ sw/airborne/arch/stm32/servos_direct_hw.h
@@ -4,10 +4,9 @@
 
 #include "std.h"
 
-#define ACTUATORS_PWM_NB 6
-extern int32_t actuators_pwm_values[ACTUATORS_PWM_NB];
-
 #include "actuators_pwm_arch.h"
+
+extern int32_t actuators_pwm_values[ACTUATORS_PWM_NB];
 
 #define SERVOS_TICS_OF_USEC(_v) (_v)
 #define Actuator(_x)  actuators_pwm_values[_x]
