4d52c1a6b70fe1b7a74315eea2133aa54625eaba
Felix Ruess <felix.ruess@gmail.com>
2012-09-14 13:45:29 +0200

    [baro] enable baro_board for sim as well, kluge for baro_board_module.c Seems Gautier forgot to commit the latter...

---
+++ sw/airborne/modules/sensors/baro_board_module.c
@@ -0,0 +1,17 @@
+
+//FIXME: this is just a kludge
+// Gautier plz commit the real one
+
+#include "baro_board_module.h"
+
+struct Baro baro;
+
+void baro_init(void) {
+  baro.status = BS_UNINITIALIZED;
+  baro.absolute     = 0;
+  baro.differential = 0;
+}
+
+void baro_periodic(void) {
+  baro.status = BS_RUNNING;
+}
