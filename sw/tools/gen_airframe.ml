02a53a5d5a1b4f9a4fd4e2b77fbb4e73e7aa0d33
MavLab <microuav@gmail.com>
2012-10-08 15:00:10 +0200

    Typo Bugfix. Apparently got lost on export.

--- sw/tools/gen_airframe.ml
+++ sw/tools/gen_airframe.ml
@@ -177,7 +177,7 @@ let parse_command_laws = fun command ->
        and rate_min = a "rate_min"
        and rate_max = a "rate_max" in
        let v = preprocess_value value "values" "COMMAND" in
-       printf "  static int16_t _var_%s = 0; _var_%s = Chop((%s) - (_var_%s), (%s), (%s));\\\n" var var v var rate_min rate_max
+       printf "  static int16_t _var_%s = 0; _var_%s += Chop((%s) - (_var_%s), (%s), (%s));\\\n" var var v var rate_min rate_max
    | "define" ->
        parse_element "" command
    | _ -> xml_error "set|let"
