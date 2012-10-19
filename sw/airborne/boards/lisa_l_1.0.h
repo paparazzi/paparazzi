4c3fb47be486cbe44ba0aef7a27a87dddce7843d
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2012-10-19 13:28:35 +0200

    [actuators] default actuators for lisa and classix
    - actuators_direct is now actuators_pwm (consistency with file names)

--- sw/airborne/boards/lisa_l_1.0.h
+++ sw/airborne/boards/lisa_l_1.0.h
@@ -21,6 +21,12 @@
 #define IMU_ACC_DRDY_GPIO             GPIOD
 #define IMU_ACC_DRDY_GPIO_PORTSOURCE  GPIO_PortSourceGPIOD
 
+/* Default actuators driver */
+#define DEFAULT_ACTUATORS "subsystems/actuators/actuators_pwm.h"
+#define ActuatorDefault(_x) ActuatorPwm(_x)
+#define ActuatorsDefaultInit() ActuatorsPwmInit()
+#define ActuatorsDefaultCommit() ActuatorsPwmCommit()
+
 
 /* PA0 - ADC0 */
 /* allow to define ADC_CHANNEL_VSUPPLY in the airframe file*/
