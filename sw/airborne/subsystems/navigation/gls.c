bdb9108a292ae5ec97e945701c405cbee3b7fbea
Felix Ruess <felix.ruess@gmail.com>
2012-10-24 22:06:01 +0200

    Revert "Merge pull request #309 from TobiasMue/v4.1"
    This reverts commit 0e00bf1e234b567d2cba4f39745a5cb6036aecfa, reversing
    changes made to 413e0326ed957f2f3e568cb44f6c821fe7e6508b.

--- sw/airborne/subsystems/navigation/gls.c
+++ sw/airborne/subsystems/navigation/gls.c
@@ -147,7 +147,7 @@ bool_t gls(uint8_t _af, uint8_t _tod, uint8_t _td) {
   float start_alt = WaypointAlt(_tod);
   float diff_alt = WaypointAlt(_td) - start_alt;
   float alt = start_alt + nav_final_progress * diff_alt;
-  Bound(alt, WaypointAlt(_td), start_alt +(pre_climb/(-v_ctl_altitude_pgain))) // to prevent climbing before intercept
+  Bound(alt, WaypointAlt(_td), start_alt +(pre_climb/(v_ctl_altitude_pgain))) // to prevent climbing before intercept
 
 
 
@@ -156,7 +156,7 @@ bool_t gls(uint8_t _af, uint8_t _tod, uint8_t _td) {
 
     NavVerticalAltitudeMode(WaypointAlt(_tod), 0);	// vertical mode (fly straigt and intercept glideslope)
 
-    NavVerticalAutoThrottleMode(0);			// throttle mode
+    NavVerticalAutoThrottleMode(0);		// throttle mode
 
     NavSegment(_af, _td);				// horizontal mode (stay on localiser)
   }
@@ -167,7 +167,7 @@ bool_t gls(uint8_t _af, uint8_t _tod, uint8_t _td) {
 
     NavVerticalAutoThrottleMode(0);		// throttle mode
 
-    NavSegment(_af, _td);			// horizontal mode (stay on localiser)
+    NavSegment(_af, _td);				// horizontal mode (stay on localiser)
   }
 
 
