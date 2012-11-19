b23cc28e9ede8ed2403ba3b961a20bc5e3cf868b
Edwin <edwin@edwin-XXX.(none)>
2012-11-19 12:25:15 +0100

    Added and OMAP sys_time_arch and a mcu_arch DUB

---
+++ sw/airborne/arch/omap/mcu_periph/sys_time_arch.c
@@ -0,0 +1,11 @@
+/**
+ * @file arch/omap/mcu_periph/sys_time_arch.c
+ * @brief OMAP timing functions.
+ *
+ */
+
+#include "mcu_periph/sys_time.h"
+
+void sys_time_arch_init(void){
+#pragma message "System time architecture DUB"
+}
---
+++ sw/airborne/arch/omap/mcu_periph/sys_time_arch.h
@@ -0,0 +1,12 @@
+/**
+ * @file arch/omap/mcu_periph/sys_time_arch.c
+ * @brief OMAP timing functions.
+ *
+ */
+
+#ifndef SYS_TIME_ARCH_H
+#define SYS_TIME_ARCH_H
+
+#include "mcu_periph/sys_time.h"
+
+#endif /* Sys_time_arch_h */
