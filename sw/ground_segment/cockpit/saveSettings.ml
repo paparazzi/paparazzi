82cc142159284eb22ba64b6647cb202afc86d098
Felix Ruess <felix.ruess@gmail.com>
2013-03-12 23:59:13 +0100

    [gcs][settings] nicer popup warning about parameters not in the airframe file

--- sw/ground_segment/cockpit/saveSettings.ml
+++ sw/ground_segment/cockpit/saveSettings.ml
@@ -198,7 +198,7 @@ let fill_data = fun (model:GTree.tree_store) settings airframe_xml ->
 
   (* Warning if needed *)
   if !not_in_airframe_file <> [] then begin
-    GToolbox.message_box ~title:"Warning" (Printf.sprintf "Parameter(s) '%s' not writable in the airframe file" (String.concat "," !not_in_airframe_file));
+    GToolbox.message_box ~title:"Warning" (Printf.sprintf "Some parameters not writable in the airframe file:\n\n%s" (String.concat "\n" !not_in_airframe_file));
   end
 
 
