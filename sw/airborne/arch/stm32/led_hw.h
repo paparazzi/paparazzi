79848bb0fb06e4cb181a300415d28c310e4dd6fd
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2013-03-14 14:03:48 +0100

    [led] update led for f4

--- sw/airborne/arch/stm32/led_hw.h
+++ sw/airborne/arch/stm32/led_hw.h
@@ -22,13 +22,17 @@
 #ifndef LED_HW_H
 #define LED_HW_H
 
+#if defined(STM32F1)
 #include <libopencm3/stm32/f1/gpio.h>
 #include <libopencm3/stm32/f1/rcc.h>
+#elif defined(STM32F4)
+#include <libopencm3/stm32/f4/gpio.h>
+#include <libopencm3/stm32/f4/rcc.h>
+#endif
 
 #include BOARD_CONFIG
 
 #include "std.h"
-
 
 /*
  *
@@ -52,19 +56,31 @@
 #define LED_AFIO_REMAP(i) _LED_AFIO_REMAP(LED_ ## i ## _AFIO_REMAP)
 
 /* set pin as output */
+#if defined(STM32F1) || defined(STM32F2)
 #define LED_INIT(i) {                               \
     rcc_peripheral_enable_clock(&RCC_APB2ENR,       \
-                                LED_GPIO_CLK(i));	\
+                                LED_GPIO_CLK(i));	  \
     gpio_set_mode(LED_GPIO(i),                      \
                   GPIO_MODE_OUTPUT_50_MHZ,          \
                   GPIO_CNF_OUTPUT_PUSHPULL,         \
                   LED_GPIO_PIN(i));                 \
     LED_AFIO_REMAP(i);                              \
   }
+#elif defined(STM32F4)
+#define LED_INIT(i) {                             \
+    rcc_peripheral_enable_clock(&RCC_AHB1ENR,     \
+                                LED_GPIO_CLK(i));	\
+    gpio_mode_setup(LED_GPIO(i),                  \
+                  GPIO_MODE_OUTPUT,			          \
+                  GPIO_PUPD_NONE,                 \
+                  LED_GPIO_PIN(i));               \
+    LED_AFIO_REMAP(i);                            \
+  }
+#endif
 
-#define LED_ON(i) { LED_GPIO_ON(i)(LED_GPIO(i)) = LED_GPIO_PIN(i);}
-#define LED_OFF(i) { LED_GPIO_OFF(i)(LED_GPIO(i)) = LED_GPIO_PIN(i);}
-#define LED_TOGGLE(i) {	GPIO_ODR(LED_GPIO(i)) ^= LED_GPIO_PIN(i);}
+#define LED_ON(i) { gpio_clear(LED_GPIO(i), LED_GPIO_PIN(i)); }
+#define LED_OFF(i) { gpio_set(LED_GPIO(i), LED_GPIO_PIN(i)); }
+#define LED_TOGGLE(i) {	gpio_toggle(LED_GPIO(i), LED_GPIO_PIN(i)); }
 
 #define LED_PERIODIC() {}
 
