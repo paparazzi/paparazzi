370100b83ea4d29a1c06b1964a56520d500511d4
Felix Ruess <felix.ruess@gmail.com>
2013-04-13 14:01:31 +0200

    [rotorcraft] use horizontal guidance ref by default
    Also use the horizontal guidance reference for HOVER mode.
    Using the reference can be temporarly disabled via settings.
    The reference is then still computed to stay consistent, but not used.

--- sw/airborne/firmwares/rotorcraft/guidance/guidance_h.h
+++ sw/airborne/firmwares/rotorcraft/guidance/guidance_h.h
@@ -31,6 +31,15 @@
 #include "math/pprz_algebra_int.h"
 
 #include "firmwares/rotorcraft/guidance/guidance_h_ref.h"
+#include "generated/airframe.h"
+#include "std.h"
+
+/** Use horizontal guidance reference trajectory.
+ * Default is TRUE, define to FALSE to always disable it.
+ */
+#ifndef GUIDANCE_H_USE_REF
+#define GUIDANCE_H_USE_REF TRUE
+#endif
 
 #define GUIDANCE_H_MODE_KILL        0
 #define GUIDANCE_H_MODE_RATE        1
@@ -42,6 +51,7 @@
 
 
 extern uint8_t guidance_h_mode;
+extern bool_t guidance_h_use_ref;
 
 /** horizontal position setpoint in NED.
  *  fixed point representation: Q23.8
@@ -70,13 +80,20 @@ extern int32_t guidance_h_again;
 
 extern void guidance_h_init(void);
 extern void guidance_h_mode_changed(uint8_t new_mode);
-extern void guidance_h_read_rc(bool_t  in_flight);
-extern void guidance_h_run(bool_t  in_flight);
+extern void guidance_h_read_rc(bool_t in_flight);
+extern void guidance_h_run(bool_t in_flight);
 
 
-#define guidance_h_SetKi(_val) {			\
-    guidance_h_igain = _val;			\
+#define guidance_h_SetKi(_val) {            \
+    guidance_h_igain = _val;                \
     INT_VECT2_ZERO(guidance_h_pos_err_sum);	\
+  }
+
+/* Make sure that ref can only be temporarily disabled for testing,
+ * but not enabled if GUIDANCE_H_USE_REF was defined to FALSE.
+ */
+#define guidance_h_SetUseRef(_val) {                    \
+    guidance_h_use_ref = _val && GUIDANCE_H_USE_REF;    \
   }
 
 #endif /* GUIDANCE_H_H */
