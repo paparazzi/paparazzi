82a73bcdb9e0220629a3fbb388317d134c845aaf
Dino Hensen <dino.hensen@gmail.com>
2013-05-10 12:51:58 +0200

    [ardrone] Changed the structure of the ardrone actuators a little bit

--- sw/airborne/boards/ardrone2_raw.h
+++ sw/airborne/boards/ardrone2_raw.h
@@ -3,5 +3,10 @@
 
 #define BOARD_ARDRONE2_RAW
 
+/* Default actuators driver */
+#define DEFAULT_ACTUATORS "arch/omap/subsystems/actuators/actuators_ardrone2_raw.h"
+#define ActuatorDefaultSet(_x,_y) ActuatorArdroneSet(_x,_y)
+#define ActuatorsDefaultInit() ActuatorsArdroneInit()
+#define ActuatorsDefaultCommit() ActuatorsArdroneCommit()
 
 #endif /* CONFIG_ARDRONE2_RAW */
