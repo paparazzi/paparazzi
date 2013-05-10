cf6b17976d93860ca59346e3a3a52146132c6626
Dino Hensen <dino.hensen@gmail.com>
2013-05-10 18:34:01 +0200

    [omap] All OMAP aditions add during ardrone development

---
+++ sw/airborne/arch/omap/subsystems/settings_arch.h
@@ -0,0 +1,35 @@
+/*
+ *
+ * Copyright (C) 2009-2013 The Paparazzi Team
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/**
+ * @file arch/omap/subsystems/settings_arch.h
+ * OMAP arch Persistent settings.
+ *
+ * Unimplemented.
+ */
+
+#ifndef OMAP_SUBSYSTEMS_SETTINGS_H
+#define OMAP_SUBSYSTEMS_SETTINGS_H
+
+
+
+#endif /* OMAP_SUBSYSTEMS_SETTINGS_H */
