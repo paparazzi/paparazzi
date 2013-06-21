aae9841d877d09814055835612f3a211cc2efe00
Felix Ruess <felix.ruess@gmail.com>
2013-06-21 09:53:02 +0200

    [peripherals] mpu60x0_i2c: only copy ext data if i2c_bypass is false

--- sw/airborne/peripherals/mpu60x0_i2c.c
+++ sw/airborne/peripherals/mpu60x0_i2c.c
@@ -95,8 +95,8 @@ void mpu60x0_i2c_event(struct Mpu60x0_I2c *mpu)
         mpu->data_rates.rates.q = Int16FromBuf(mpu->i2c_trans.buf, 11);
         mpu->data_rates.rates.r = Int16FromBuf(mpu->i2c_trans.buf, 13);
 
-        // if we are reading slaves, copy the ext_sens_data
-        if (mpu->config.nb_slaves > 0)
+        // if we are reading slaves through the mpu, copy the ext_sens_data
+        if ((mpu->config.i2c_bypass == FALSE) && (mpu->config.nb_slaves > 0))
           memcpy(mpu->data_ext, (void *) &(mpu->i2c_trans.buf[15]), mpu->config.nb_bytes - 15);
 
         mpu->data_available = TRUE;
