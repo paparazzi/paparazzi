30fb6211ebacc42c7dc7d0981d71e57f4b41f29c
Felix Ruess <felix.ruess@gmail.com>
2013-07-12 00:05:42 +0200

    [rotorcraft] fix MODE_STARTUP
    - init nav, guidance and stabilization from autopilot_init
    - after init of the above, set the autopilot_mode so it propagates to the guidance submodes.
    - also call autopilot_init after the ahrs_init for ahrs_is_aligned check...

--- sw/airborne/firmwares/rotorcraft/main.c
+++ sw/airborne/firmwares/rotorcraft/main.c
@@ -138,11 +138,6 @@ STATIC_INLINE void main_init( void ) {
 
   baro_init();
   imu_init();
-  autopilot_init();
-  nav_init();
-  guidance_h_init();
-  guidance_v_init();
-  stabilization_init();
 
   ahrs_aligner_init();
   ahrs_init();
@@ -152,6 +147,8 @@ STATIC_INLINE void main_init( void ) {
 #if USE_GPS
   gps_init();
 #endif
+
+  autopilot_init();
 
   modules_init();
 
