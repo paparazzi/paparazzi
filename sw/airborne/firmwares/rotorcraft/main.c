1af708f4220c7c8f189fe3d34cc2ee58da07f55d
Dino Hensen <dino.hensen@gmail.com>
2013-07-11 13:20:31 +0200

    Added check if autopilot_motors_on do determine if you want to switch to AP_MODE_FAILSAFE

--- sw/airborne/firmwares/rotorcraft/main.c
+++ sw/airborne/firmwares/rotorcraft/main.c
@@ -222,6 +222,7 @@ STATIC_INLINE void failsafe_check( void ) {
 
 #if USE_GPS
   if (autopilot_mode == AP_MODE_NAV &&
+      autopilot_motors_on &&
 #if NO_GPS_LOST_WITH_RC_VALID
       radio_control.status != RC_OK &&
 #endif
