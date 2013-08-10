dcdc7e80b630787123f0e9e719946f998141d5e8
Dirk Dokter <dirk@dirkdokter.nl>
2013-08-10 15:48:02 +0200

    [STM32] Very limited simple SPI1 slave driver

---
+++ sw/airborne/modules/gumstix_interface/qr_code_spi_link.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2005-2013 The Paparazzi Team
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef QR_CODE_SPI_LINK_H_
+#define QR_CODE_SPI_LINK_H_
+
+#include "std.h"
+
+extern void qr_code_spi_link_init(void);
+extern void qr_code_spi_link_periodic(void);
+
+#endif /* QR_CODE_SPI_LINK_H_ */
