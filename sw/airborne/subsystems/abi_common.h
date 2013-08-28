e867282eb0b7926c1689d022425be98a6c1076b1
Felix Ruess <felix.ruess@gmail.com>
2013-08-28 19:29:59 +0200

    [dox] doxygen headers for abi

--- sw/airborne/subsystems/abi_common.h
+++ sw/airborne/subsystems/abi_common.h
@@ -19,7 +19,11 @@
  * Boston, MA 02111-1307, USA.
  */
 
-/* Common tools for ABI middelware */
+/**
+ * @file subsystems/abi_common.h
+ *
+ * Common tools for ABI middelware.
+ */
 
 #ifndef ABI_COMMON_H
 #define ABI_COMMON_H
@@ -44,8 +48,8 @@
 typedef void (*abi_callback)(void);
 
 /** Broadcast address.
- * When passing broadcast address has a sender id,
- * messages from all sender are received
+ * When passing broadcast address as a sender id,
+ * messages from all senders are received
  */
 #define ABI_BROADCAST 0
 
