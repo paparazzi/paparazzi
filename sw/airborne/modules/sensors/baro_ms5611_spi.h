8cc90461ecabb3b42020c0f4c6505add13e3ed61
Felix Ruess <felix.ruess@gmail.com>
2013-08-30 10:17:17 +0200

    [modules] baro_ms5611_spi: fix update macro

--- sw/airborne/modules/sensors/baro_ms5611_spi.h
+++ sw/airborne/modules/sensors/baro_ms5611_spi.h
@@ -50,8 +50,8 @@ extern void baro_ms5611_read(void);
 extern void baro_ms5611_periodic_check(void);
 extern void baro_ms5611_event(void);
 
-#define BaroMs5611UpdatePressure(_b) { if (baro_ms5611.data_available) { _b = baro_ms5611.data.pressure; baro_ms5611.data_available = FALSE; } }
+#define BaroMs5611UpdatePressure(_b, _h) { if (baro_ms5611.data_available) { _b = baro_ms5611.data.pressure; _h(); baro_ms5611.data_available = FALSE; } }
 
-#define BaroMs5611UpdateAlt(_b) { if (baro_ms5611.data_available) { _b = baro_ms5611_alt; baro_ms5611.data_available = FALSE; } }
+#define BaroMs5611UpdateAlt(_b, _h) { if (baro_ms5611.data_available) { _b = baro_ms5611_alt; _h(); baro_ms5611.data_available = FALSE; } }
 
 #endif
