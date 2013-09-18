753eb3e57c2e1499f8ae21f5ceb2b2668015159f
Tobias Muench <muench.tm@googlemail.com>
2013-09-18 10:02:13 +0200

    module: gpio_timer on/off

---
+++ sw/airborne/modules/gpio_timer/gpio_timer.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2014 Tobias Muench
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/** @file modules/gpio_timer/gpio_timer.h
+ *  @brief GPIO timer
+ *
+ * Provides a timer for the GPIOS.
+ * 
+ *
+ */
+
+#ifndef GPIO_TIMER_H
+#define GPIO_TIMER_H
+
+extern void gpio_timer_init(void);
+
+/** 4Hz Periodic */
+extern void gpio_timer_periodic(void);
+
+#endif // GPIO_TIMER_H
