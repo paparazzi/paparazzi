3fe72e28e3dc419b02f7c2679d0c5af5fc831ed6
Felix Ruess <felix.ruess@gmail.com>
2013-10-24 00:07:22 +0200

    [test] update compare_ahrs

--- sw/airborne/test/ahrs/run_ahrs_on_synth.c
+++ sw/airborne/test/ahrs/run_ahrs_on_synth.c
@@ -37,10 +37,6 @@ static void report(void) {
   int output_sensors = FALSE;
   int output_pos = FALSE;
 
-#if AHRS_TYPE == AHRS_TYPE_ICE || AHRS_TYPE == AHRS_TYPE_ICQ
-  EULERS_FLOAT_OF_BFP(ahrs_float.ltp_to_imu_euler, ahrs.ltp_to_imu_euler);
-  RATES_FLOAT_OF_BFP(ahrs_float.imu_rate, ahrs.imu_rate);
-#endif
   printf("%f ", aos.time);
 
   printf("%f %f %f ", DegOfRad(aos.ltp_to_imu_euler.phi),
@@ -55,23 +51,37 @@ static void report(void) {
 	              DegOfRad(aos.gyro_bias.q),
 	              DegOfRad(aos.gyro_bias.r));
 
-
-  printf("%f %f %f ", DegOfRad(ahrs_float.ltp_to_imu_euler.phi),
-	              DegOfRad(ahrs_float.ltp_to_imu_euler.theta),
-                      DegOfRad(ahrs_float.ltp_to_imu_euler.psi));
-
-  printf("%f %f %f ", DegOfRad(ahrs_float.imu_rate.p),
-	              DegOfRad(ahrs_float.imu_rate.q),
-	              DegOfRad(ahrs_float.imu_rate.r));
-
 #if AHRS_TYPE == AHRS_TYPE_ICQ
+  struct Int32Eulers ltp_to_imu_euler_i;
+  INT32_EULERS_OF_QUAT(ltp_to_imu_euler_i, ahrs_impl.ltp_to_imu_quat);
+  struct FloatEulers ltp_to_imu_euler_f;
+  EULERS_FLOAT_OF_BFP(ltp_to_imu_euler_f, ltp_to_imu_euler_i);
+  printf("%f %f %f ", DegOfRad(ltp_to_imu_euler_f.phi),
+         DegOfRad(ltp_to_imu_euler_f.theta),
+         DegOfRad(ltp_to_imu_euler_f.psi));
+
+  struct FloatRates imu_rate_f;
+  RATES_FLOAT_OF_BFP(imu_rate_f, ahrs_impl.imu_rate);
+  printf("%f %f %f ", DegOfRad(imu_rate_f.p),
+         DegOfRad(imu_rate_f.q),
+         DegOfRad(imu_rate_f.r));
+
   struct FloatRates bias;
   RATES_FLOAT_OF_BFP(bias, ahrs_impl.gyro_bias);
   printf("%f %f %f ", DegOfRad(bias.p),
 	             DegOfRad(bias.q),
 	             DegOfRad(bias.r));
-#endif
-#if AHRS_TYPE == AHRS_TYPE_FLQ || AHRS_TYPE == AHRS_TYPE_FCR2 || AHRS_TYPE == AHRS_TYPE_FCQ || AHRS_TYPE == AHRS_TYPE_FCR
+
+#elif AHRS_TYPE == AHRS_TYPE_FCR2 || AHRS_TYPE == AHRS_TYPE_FCQ || AHRS_TYPE == AHRS_TYPE_FCR
+
+  printf("%f %f %f ", DegOfRad(ahrs_impl.ltp_to_imu_euler.phi),
+         DegOfRad(ahrs_impl.ltp_to_imu_euler.theta),
+         DegOfRad(ahrs_impl.ltp_to_imu_euler.psi));
+
+  printf("%f %f %f ", DegOfRad(ahrs_impl.imu_rate.p),
+         DegOfRad(ahrs_impl.imu_rate.q),
+         DegOfRad(ahrs_impl.imu_rate.r));
+
   printf("%f %f %f ", DegOfRad(ahrs_impl.gyro_bias.p),
 	             DegOfRad(ahrs_impl.gyro_bias.q),
 	             DegOfRad(ahrs_impl.gyro_bias.r));
