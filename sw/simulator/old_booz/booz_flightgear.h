c9fa0b46924bad8c7cfd4640bdc31ab62c0d0dda
Felix Ruess <felix.ruess@gmail.com>
2013-10-29 19:26:04 +0100

    [simulator] cleanup: remove old_booz sim

--- sw/simulator/old_booz/booz_flightgear.h
+++
@@ -1,9 +0,0 @@
-#ifndef BOOZ_FLIGHTGEAR_H
-#define BOOZ_FLIGHTGEAR_H
-
-#include "booz_flight_model.h"
-
-extern void booz_flightgear_init(const char* host,  unsigned int port);
-extern void booz_flightgear_send();
-
-#endif /* BOOZ_FLIGHTGEAR_H */
