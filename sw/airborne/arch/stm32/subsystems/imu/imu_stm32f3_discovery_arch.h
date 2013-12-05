b748fb317763f142397ee967822a7bebb76fdb1a
Federico Ruiz Ugalde <fruiz@eie.ucr.ac.cr>
2013-12-04 21:39:38 -0600

    Stm32f3discovery imu arch skeleton source files

---
+++ sw/airborne/arch/stm32/subsystems/imu/imu_stm32f3_discovery_arch.h
@@ -0,0 +1,8 @@
+#ifndef IMU_STM32F3_DISCOVERY_ARCH_H
+#define IMU_STM32F3_DISCOVERY_ARCH_H
+
+#include "subsystems/imu.h"
+
+void imu_stm32f3_discovery_arch_init(void);
+
+#endif /* IMU_STM32F3_DISCOVERY_ARCH_H */
