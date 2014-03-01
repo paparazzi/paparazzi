007692d1ab3dbf593a10898b9a334b43bf736b1c
Felix Ruess <felix.ruess@gmail.com>
2014-03-01 14:11:58 +0100

    [modules] rewrite cam_control to use gpio instead of led

---
+++ sw/airborne/modules/digital_cam/gpio_cam_ctrl.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2010-2014 The Paparazzi Team
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/** @file modules/digital_cam/gpio_cam_ctrl.h
+ *  @brief Digital Camera Control
+ *
+ * Provides the control of the shutter and the zoom of a digital camera
+ * through standard binary IOs of the board.
+ *
+ * The required initialization (dc_init()) and periodic (4Hz) process.
+ */
+
+#ifndef GPIO_CAM_CTRL_H
+#define GPIO_CAM_CTRL_H
+
+extern void gpio_cam_ctrl_init(void);
+
+/** 4Hz Periodic */
+extern void gpio_cam_ctrl_periodic(void);
+
+#endif // GPIO_CAM_CTRL_H
