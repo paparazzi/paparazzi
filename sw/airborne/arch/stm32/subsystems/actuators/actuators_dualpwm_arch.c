73bdf4fd070697b8bef12d8b95f4ab0603732c3f
karlito139 <karlito@martobre.fr>
2014-03-31 14:54:46 +0200

    Cleared the function calls for timmers that are never going to be used by the dualPWM servos.

--- sw/airborne/arch/stm32/subsystems/actuators/actuators_dualpwm_arch.c
+++ sw/airborne/arch/stm32/subsystems/actuators/actuators_dualpwm_arch.c
@@ -80,77 +80,11 @@ void actuators_dualpwm_arch_init(void) {
   AFIO_MAPR |= AFIO_MAPR_TIM3_REMAP_FULL_REMAP;
 #endif
 
-#ifdef DUAL_PWM_SERVO_0
-  set_servo_gpio(DUAL_PWM_SERVO_0_GPIO, DUAL_PWM_SERVO_0_PIN, DUAL_PWM_SERVO_0_AF, DUAL_PWM_SERVO_0_RCC);
-#endif
-#ifdef DUAL_PWM_SERVO_1
-  set_servo_gpio(DUAL_PWM_SERVO_1_GPIO, DUAL_PWM_SERVO_1_PIN, DUAL_PWM_SERVO_1_AF, DUAL_PWM_SERVO_1_RCC);
-#endif
-#ifdef DUAL_PWM_SERVO_2
-  set_servo_gpio(DUAL_PWM_SERVO_2_GPIO, DUAL_PWM_SERVO_2_PIN, DUAL_PWM_SERVO_2_AF, DUAL_PWM_SERVO_2_RCC);
-#endif
-#ifdef DUAL_PWM_SERVO_3
-  set_servo_gpio(DUAL_PWM_SERVO_3_GPIO, DUAL_PWM_SERVO_3_PIN, DUAL_PWM_SERVO_3_AF, DUAL_PWM_SERVO_3_RCC);
-#endif
-#ifdef DUAL_PWM_SERVO_4
-  set_servo_gpio(DUAL_PWM_SERVO_4_GPIO, DUAL_PWM_SERVO_4_PIN, DUAL_PWM_SERVO_4_AF, DUAL_PWM_SERVO_4_RCC);
-#endif
 #ifdef DUAL_PWM_SERVO_5
   set_servo_gpio(DUAL_PWM_SERVO_5_GPIO, DUAL_PWM_SERVO_5_PIN, DUAL_PWM_SERVO_5_AF, DUAL_PWM_SERVO_5_RCC);
 #endif
 #ifdef DUAL_PWM_SERVO_6
   set_servo_gpio(DUAL_PWM_SERVO_6_GPIO, DUAL_PWM_SERVO_6_PIN, DUAL_PWM_SERVO_6_AF, DUAL_PWM_SERVO_6_RCC);
-#endif
-#ifdef DUAL_PWM_SERVO_7
-  set_servo_gpio(DUAL_PWM_SERVO_7_GPIO, DUAL_PWM_SERVO_7_PIN, DUAL_PWM_SERVO_7_AF, DUAL_PWM_SERVO_7_RCC);
-#endif
-#ifdef DUAL_PWM_SERVO_8
-  set_servo_gpio(DUAL_PWM_SERVO_8_GPIO, DUAL_PWM_SERVO_8_PIN, DUAL_PWM_SERVO_8_AF, DUAL_PWM_SERVO_8_RCC);
-#endif
-#ifdef DUAL_PWM_SERVO_9
-  set_servo_gpio(DUAL_PWM_SERVO_9_GPIO, DUAL_PWM_SERVO_9_PIN, DUAL_PWM_SERVO_9_AF, DUAL_PWM_SERVO_9_RCC);
-#endif
-#ifdef DUAL_PWM_SERVO_10
-  set_servo_gpio(DUAL_PWM_SERVO_10_GPIO, DUAL_PWM_SERVO_10_PIN, DUAL_PWM_SERVO_10_AF, DUAL_PWM_SERVO_10_RCC);
-#endif
-#ifdef DUAL_PWM_SERVO_11
-  set_servo_gpio(DUAL_PWM_SERVO_11_GPIO, DUAL_PWM_SERVO_11_PIN, DUAL_PWM_SERVO_11_AF, DUAL_PWM_SERVO_11_RCC);
-#endif
-
-
-
-
-
-#if DUAL_PWM_USE_TIM1
-  set_servo_timer(TIM1, TIM1_SERVO_HZ, PWM_TIM1_CHAN_MASK);
-
-  nvic_set_priority(NVIC_TIM1_IRQ, 2);
-  nvic_enable_irq(NVIC_TIM1_IRQ);
-  timer_enable_irq(TIM1, TIM_DIER_CC1IE);
-#endif
-
-#if DUAL_PWM_USE_TIM2
-  set_servo_timer(TIM2, TIM2_SERVO_HZ, PWM_TIM2_CHAN_MASK);
-
-  nvic_set_priority(NVIC_TIM2_IRQ, 2);
-  nvic_enable_irq(NVIC_TIM2_IRQ);
-  timer_enable_irq(TIM2, TIM_DIER_CC1IE);
-#endif
-
-#if DUAL_PWM_USE_TIM3
-  set_servo_timer(TIM3, TIM3_SERVO_HZ, PWM_TIM3_CHAN_MASK);
-
-  nvic_set_priority(NVIC_TIM3_IRQ, 2);
-  nvic_enable_irq(NVIC_TIM3_IRQ);
-  timer_enable_irq(TIM3, TIM_DIER_CC1IE);
-#endif
-
-#if DUAL_PWM_USE_TIM4
-  set_servo_timer(TIM4, TIM4_SERVO_HZ, PWM_TIM4_CHAN_MASK);
-
-  nvic_set_priority(NVIC_TIM4_IRQ, 2);
-  nvic_enable_irq(NVIC_TIM4_IRQ);
-  timer_enable_irq(TIM4, TIM_DIER_CC1IE);
 #endif
 
 #if DUAL_PWM_USE_TIM5
@@ -159,30 +93,6 @@ void actuators_dualpwm_arch_init(void) {
   nvic_set_priority(NVIC_TIM5_IRQ, 2);
   nvic_enable_irq(NVIC_TIM5_IRQ);
   timer_enable_irq(TIM5, TIM_DIER_CC1IE);
-#endif
-
-#if DUAL_PWM_USE_TIM8
-  set_servo_timer(TIM8, TIM8_SERVO_HZ, PWM_TIM8_CHAN_MASK);
-
-  nvic_set_priority(NVIC_TIM8_IRQ, 2);
-  nvic_enable_irq(NVIC_TIM8_IRQ);
-  timer_enable_irq(TIM8, TIM_DIER_CC1IE);
-#endif
-
-#if DUAL_PWM_USE_TIM9
-  set_servo_timer(TIM9, TIM9_SERVO_HZ, PWM_TIM9_CHAN_MASK);
-
-  nvic_set_priority(NVIC_TIM9_IRQ, 2);
-  nvic_enable_irq(NVIC_TIM9_IRQ);
-  timer_enable_irq(TIM9, TIM_DIER_CC1IE);
-#endif
-
-#if DUAL_PWM_USE_TIM12
-  set_servo_timer(TIM12, TIM12_SERVO_HZ, PWM_TIM12_CHAN_MASK);
-
-  nvic_set_priority(NVIC_TIM12_IRQ, 2);
-  nvic_enable_irq(NVIC_TIM12_IRQ);
-  timer_enable_irq(TIM12, TIM_DIER_CC1IE);
 #endif
 
   //calculation the values to put into the timer registers to generate pulses every 4ms and 16ms.
@@ -194,34 +104,6 @@ void actuators_dualpwm_arch_init(void) {
 
 /** Interuption called at the end of the timer. In our case alternatively very 4ms and 16ms (twice every 20ms)
  */
-#if DUAL_PWM_USE_TIM1
-void tim1_isr(void){
-
-  dual_pwm_isr();
-}
-#endif
-
-#if DUAL_PWM_USE_TIM2
-void tim2_isr(void){
-
-  dual_pwm_isr();
-}
-#endif
-
-#if DUAL_PWM_USE_TIM3
-void tim3_isr(void){
-
-  dual_pwm_isr();
-}
-#endif
-
-#if DUAL_PWM_USE_TIM4
-void tim4_isr(void){
-
-  dual_pwm_isr();
-}
-#endif
-
 #if DUAL_PWM_USE_TIM5
 void tim5_isr(void){
 
@@ -230,137 +112,30 @@ void tim5_isr(void){
 #endif
 
 
-#if DUAL_PWM_USE_TIM8
-void tim8_isr(void){
-
-  dual_pwm_isr();
-}
-#endif
-
-#if DUAL_PWM_USE_TIM9
-void tim9_isr(void){
-
-  dual_pwm_isr();
-}
-#endif
-
-#if DUAL_PWM_USE_TIM12
-void tim12_isr(void){
-
-  dual_pwm_isr();
-}
-#endif
-
-
-
 
 
 /** Fonction that clears the flag of interuption in order to reactivate the interuption
  */
 void clear_timer_flag(void){
 
-#if DUAL_PWM_USE_TIM1
-  timer_clear_flag(TIM1, TIM_SR_CC1IF);
-#endif
-
-#if DUAL_PWM_USE_TIM2
-  timer_clear_flag(TIM2, TIM_SR_CC1IF);
-#endif
-
-#if DUAL_PWM_USE_TIM3
-  timer_clear_flag(TIM3, TIM_SR_CC1IF);
-#endif
-
-#if DUAL_PWM_USE_TIM4
-  timer_clear_flag(TIM4, TIM_SR_CC1IF);
-#endif
-
 #if DUAL_PWM_USE_TIM5
   timer_clear_flag(TIM5, TIM_SR_CC1IF);
-#endif
-
-#if DUAL_PWM_USE_TIM8
-  timer_clear_flag(TIM8, TIM_SR_CC1IF);
-#endif
-
-#if DUAL_PWM_USE_TIM9
-  timer_clear_flag(TIM9, TIM_SR_CC1IF);
-#endif
-
-#if DUAL_PWM_USE_TIM12
-  timer_clear_flag(TIM12, TIM_SR_CC1IF);
 #endif
 }
 
 
 void set_dual_pwm_timer_s_period(uint32_t period){
 
-#if DUAL_PWM_USE_TIM1
-  timer_set_period(TIM1, period);
-#endif
-
-#if DUAL_PWM_USE_TIM2
-  timer_set_period(TIM2, period);
-#endif
-
-#if DUAL_PWM_USE_TIM3
-  timer_set_period(TIM3, period);
-#endif
-
-#if DUAL_PWM_USE_TIM4
-  timer_set_period(TIM4, period);
-#endif
-
 #if DUAL_PWM_USE_TIM5
   timer_set_period(TIM5, period);
-#endif
-
-#if DUAL_PWM_USE_TIM8
-  timer_set_period(TIM8, period);
-#endif
-
-#if DUAL_PWM_USE_TIM9
-  timer_set_period(TIM9, period);
-#endif
-
-#if DUAL_PWM_USE_TIM12
-  timer_set_period(TIM12, period);
 #endif
 }
 
 
 void set_dual_pwm_timer_s_oc(uint32_t oc_value){
 
-#if DUAL_PWM_USE_TIM1
-  timer_set_oc_value(DUAL_PWM_SERVO_1_TIMER, DUAL_PWM_SERVO_1_OC, oc_value);
-#endif
-
-#if DUAL_PWM_USE_TIM2
-  timer_set_oc_value(DUAL_PWM_SERVO_2_TIMER, DUAL_PWM_SERVO_2_OC, oc_value);
-#endif
-
-#if DUAL_PWM_USE_TIM3
-  timer_set_oc_value(DUAL_PWM_SERVO_3_TIMER, DUAL_PWM_SERVO_3_OC, oc_value);
-#endif
-
-#if DUAL_PWM_USE_TIM4
-  timer_set_oc_value(DUAL_PWM_SERVO_4_TIMER, DUAL_PWM_SERVO_4_OC, oc_value);
-#endif
-
 #if DUAL_PWM_USE_TIM5
   timer_set_oc_value(DUAL_PWM_SERVO_5_TIMER, DUAL_PWM_SERVO_5_OC, oc_value);
-#endif
-
-#if DUAL_PWM_USE_TIM8
-  timer_set_oc_value(DUAL_PWM_SERVO_8_TIMER, DUAL_PWM_SERVO_8_OC, oc_value);
-#endif
-
-#if DUAL_PWM_USE_TIM9
-  timer_set_oc_value(DUAL_PWM_SERVO_9_TIMER, DUAL_PWM_SERVO_9_OC, oc_value);
-#endif
-
-#if DUAL_PWM_USE_TIM12
-  timer_set_oc_value(DUAL_PWM_SERVO_12_TIMER, DUAL_PWM_SERVO_12_OC, oc_value);
 #endif
 }
 
