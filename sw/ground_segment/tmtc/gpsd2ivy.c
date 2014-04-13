048f67ca5fc80b1741d18db022461057fce00630
Felix Ruess <felix.ruess@gmail.com>
2014-04-13 17:31:44 +0200

    [tmtc] fix FLIGHT_PARAM in gpsd2ivy

--- sw/ground_segment/tmtc/gpsd2ivy.c
+++ sw/ground_segment/tmtc/gpsd2ivy.c
@@ -107,7 +107,7 @@ static void update_gps(struct gps_data_t *gpsdata,
             printf("sending gps info viy Ivy: lat %g, lon %g, speed %g, course %g, alt %g, climb %g\n",
                    gpsdata->fix.latitude, gpsdata->fix.longitude, fix_speed, fix_track, fix_altitude, fix_climb);
 
-        IvySendMsg("%s %s %s %f %f %f %f %f %f %f %f %f %f %f %f %d",
+        IvySendMsg("%s %s %s %f %f %f %f %f %f %f %f %f %f %f %d %f",
                 MSG_DEST,
                 MSG_NAME,
                 MSG_ID, // ac_id
