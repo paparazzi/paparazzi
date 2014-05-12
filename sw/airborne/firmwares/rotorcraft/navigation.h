cf0f42d6a5e405895e05778fa1e31ecb5aa6c03e
kadir4172 <kcimenci@hotmail.com>
2014-05-12 16:19:11 +0300

    approaching_time for rotorcrafts is implemented

--- sw/airborne/firmwares/rotorcraft/navigation.h
+++ sw/airborne/firmwares/rotorcraft/navigation.h
@@ -88,6 +88,10 @@ void nav_move_waypoint(uint8_t wp_id, struct EnuCoor_i * new_pos);
 bool_t nav_detect_ground(void);
 bool_t nav_is_in_flight(void);
 
+/** default approaching_time for a wp */
+#ifndef CARROT
+#define CARROT 0
+#endif
 
 #define NavKillThrottle() ({ if (autopilot_mode == AP_MODE_NAV) { autopilot_set_motors_on(FALSE); } FALSE; })
 #define NavResurrect() ({ if (autopilot_mode == AP_MODE_NAV) { autopilot_set_motors_on(TRUE); } FALSE; })
@@ -146,9 +150,9 @@ extern void nav_route(uint8_t wp_start, uint8_t wp_end);
 }
 
 /** Proximity tests on approaching a wp */
-bool_t nav_approaching_from(uint8_t wp_idx, uint8_t from_idx);
-#define NavApproaching(wp, time) nav_approaching_from(wp, 0)
-#define NavApproachingFrom(wp, from, time) nav_approaching_from(wp, from)
+bool_t nav_approaching_from(uint8_t wp_idx, uint8_t from_idx, int16_t approaching_time);
+#define NavApproaching(wp, time) nav_approaching_from(wp, 0, time)
+#define NavApproachingFrom(wp, from, time) nav_approaching_from(wp, from, time)
 
 /** Check the time spent in a radius of 'ARRIVED_AT_WAYPOINT' around a wp  */
 bool_t nav_check_wp_time(uint8_t wp_idx, uint16_t stay_time);
