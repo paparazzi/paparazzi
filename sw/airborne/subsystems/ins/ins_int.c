c4356eb540ca0f0d0254a4eae6d517a978e18cb8
Alonso Acuña <acuna007@gmail.com>
2014-06-27 18:56:52 -0600

    [ins] allow define of VFF_R_SONAR_OF_M
    closes #764

--- sw/airborne/subsystems/ins/ins_int.c
+++ sw/airborne/subsystems/ins/ins_int.c
@@ -80,7 +80,9 @@ static void sonar_cb(uint8_t sender_id, const float *distance);
 #define INS_SONAR_MIN_RANGE 0.001
 #endif
 #define VFF_R_SONAR_0 0.1
+#ifndef VFF_R_SONAR_OF_M
 #define VFF_R_SONAR_OF_M 0.2
+#endif
 
 #ifndef INS_SONAR_UPDATE_ON_AGL
 #define INS_SONAR_UPDATE_ON_AGL FALSE
