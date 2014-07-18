42579b1dbbe02436a2570d8fbd24bbfe22fe3b27
Felix Ruess <felix.ruess@gmail.com>
2014-07-17 17:23:48 -0700

    [rotorcraft] guidance_v: only limit throttle if RC ok
    This fixes failsafe if no RC or joystick present.
    Thanks to evandersman

--- sw/airborne/firmwares/rotorcraft/guidance/guidance_v.c
+++ sw/airborne/firmwares/rotorcraft/guidance/guidance_v.c
@@ -277,24 +277,26 @@ void guidance_v_run(bool_t in_flight) {
   case GUIDANCE_V_MODE_CLIMB:
     gv_update_ref_from_zd_sp(guidance_v_zd_sp, stateGetPositionNed_i()->z);
     run_hover_loop(in_flight);
-#if NO_RC_THRUST_LIMIT
-    stabilization_cmd[COMMAND_THRUST] = guidance_v_delta_t;
-#else
-    // saturate max authority with RC stick
-    stabilization_cmd[COMMAND_THRUST] = Min(guidance_v_rc_delta_t, guidance_v_delta_t);
+#if !NO_RC_THRUST_LIMIT
+    /* use rc limitation if available */
+    if (radio_control.status == RC_OK)
+      stabilization_cmd[COMMAND_THRUST] = Min(guidance_v_rc_delta_t, guidance_v_delta_t);
+    else
 #endif
+      stabilization_cmd[COMMAND_THRUST] = guidance_v_delta_t;
     break;
 
   case GUIDANCE_V_MODE_HOVER:
     guidance_v_zd_sp = 0;
     gv_update_ref_from_z_sp(guidance_v_z_sp);
     run_hover_loop(in_flight);
-#if NO_RC_THRUST_LIMIT
-    stabilization_cmd[COMMAND_THRUST] = guidance_v_delta_t;
-#else
-    // saturate max authority with RC stick
-    stabilization_cmd[COMMAND_THRUST] = Min(guidance_v_rc_delta_t, guidance_v_delta_t);
+#if !NO_RC_THRUST_LIMIT
+    /* use rc limitation if available */
+    if (radio_control.status == RC_OK)
+      stabilization_cmd[COMMAND_THRUST] = Min(guidance_v_rc_delta_t, guidance_v_delta_t);
+    else
 #endif
+      stabilization_cmd[COMMAND_THRUST] = guidance_v_delta_t;
     break;
 
   case GUIDANCE_V_MODE_NAV:
@@ -318,15 +320,13 @@ void guidance_v_run(bool_t in_flight) {
         guidance_v_z_sum_err = 0;
         guidance_v_delta_t = nav_throttle;
       }
-#if NO_RC_THRUST_LIMIT
-      stabilization_cmd[COMMAND_THRUST] = guidance_v_delta_t;
-#else
+#if !NO_RC_THRUST_LIMIT
       /* use rc limitation if available */
       if (radio_control.status == RC_OK)
         stabilization_cmd[COMMAND_THRUST] = Min(guidance_v_rc_delta_t, guidance_v_delta_t);
       else
-        stabilization_cmd[COMMAND_THRUST] = guidance_v_delta_t;
 #endif
+        stabilization_cmd[COMMAND_THRUST] = guidance_v_delta_t;
       break;
     }
   default:
