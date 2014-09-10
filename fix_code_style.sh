04bd833e762f803f493f424f99760a1b8c2380b6
Felix Ruess <felix.ruess@gmail.com>
2014-09-10 15:23:55 +0200

    add initial fix_code_syle.sh script
    You need to have astyle installed.
    Run with ./fix_code_style.sh <FILENAME>

---
+++ fix_code_style.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+astyle \
+    --style=kr   \
+    --indent=spaces=2  \
+    --convert-tabs \
+    --indent-switches    \
+    --indent-preprocessor \
+    --pad-oper      \
+    --pad-header    \
+    --unpad-paren   \
+    --keep-one-line-blocks  \
+    --keep-one-line-statements  \
+    --align-pointer=name  \
+    --suffix=none   \
+    --lineend=linux   \
+    --add-brackets    \
+    $*
+
+# options that don't seem to be supported by astyle 2.01
+# --attach-extern-c --align-reference=name --ignore-exclude-errors-x --max-code-length=120
