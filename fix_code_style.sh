a3c3ea5cc356b74d6b40fb9a990c598f720ec34e
Felix Ruess <felix.ruess@gmail.com>
2014-11-19 13:20:12 +0100

    code style formaters
    fix_code_style.sh script uses astyle which works very well.
    Eclipse is not very good at this, but better than nothing...

---
+++ fix_code_style.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if ! type "astyle" > /dev/null; then
+    echo "You need to install astyle to use this script!"
+    exit 1
+fi
+
+if [ $# -eq 0 ]; then
+    echo "Please provide one or multiple files as arguments!"
+    exit 1
+fi
+
+ASTYLE_VERSION=`astyle --version 2>&1| awk '{print $4}'`
+echo "Using astyle version $ASTYLE_VERSION"
+
+if [ $(bc <<< "$ASTYLE_VERSION >= 2.03") -eq 1 ]; then
+    astyle --style=kr   \
+        --indent=spaces=2  \
+        --convert-tabs \
+        --indent-switches    \
+        --indent-preprocessor \
+        --pad-oper      \
+        --pad-header    \
+        --unpad-paren   \
+        --keep-one-line-blocks  \
+        --keep-one-line-statements  \
+        --align-pointer=name  \
+        --suffix=none   \
+        --lineend=linux   \
+        --add-brackets \
+        --ignore-exclude-errors-x \
+        --max-code-length=100 \
+        $*
+else
+    astyle --style=kr   \
+        --indent=spaces=2  \
+        --convert-tabs \
+        --indent-switches    \
+        --indent-preprocessor \
+        --pad-oper      \
+        --pad-header    \
+        --unpad-paren   \
+        --keep-one-line-blocks  \
+        --keep-one-line-statements  \
+        --align-pointer=name  \
+        --suffix=none   \
+        --lineend=linux   \
+        --add-brackets \
+        $*
+fi
