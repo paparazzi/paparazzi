31720db41431b5c86622dd73e6191c68d8988eb8
Freek van Tienen <freek.v.tienen@gmail.com>
2014-11-19 14:17:07 +0100

    [omap] Added linux i2c driver

--- sw/airborne/boards/ardrone/electrical_raw.c
+++ sw/airborne/boards/ardrone/electrical_raw.c
@@ -35,7 +35,7 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <math.h>
-#include "i2c-dev.h"
+#include "mcu_periph/i2c_smbus.h"
 #include "subsystems/commands.h"
 #include "generated/airframe.h"
 
@@ -67,45 +67,48 @@ static struct {
 
 int fd;
 
-void electrical_init(void) {
+void electrical_init(void)
+{
   // First we try to kill the program.elf and its respawner if it is running (done here because initializes first)
   int ret = system("killall -9 program.elf.respawner.sh; killall -9 program.elf");
   (void) ret;
 
   // Initialize 12c device for power
-  fd = open( "/dev/i2c-1", O_RDWR );
-  if ( ioctl( fd, I2C_SLAVE_FORCE, 0x4a) < 0 ) {
-    fprintf( stderr, "Failed to set slave address: %m\n" );
+  fd = open("/dev/i2c-1", O_RDWR);
+  if (ioctl(fd, I2C_SLAVE_FORCE, 0x4a) < 0) {
+    fprintf(stderr, "Failed to set slave address: %m\n");
   }
 
   electrical_setup();
   electrical_priv.nonlin_factor = CURRENT_ESTIMATION_NONLINEARITY;
 }
 
-void electrical_setup(void) {
+void electrical_setup(void)
+{
   // Turn on MADC in CTRL1
-  if( i2c_smbus_write_byte_data( fd, 0x00, 0x01))   {
-    fprintf( stderr, "Failed to write to I2C device. 1\n" );
+  if (i2c_smbus_write_byte_data(fd, 0x00, 0x01))   {
+    fprintf(stderr, "Failed to write to I2C device. 1\n");
   }
   // Select ADCIN0 for conversion in SW1SELECT_LSB
-  if( i2c_smbus_write_byte_data( fd, 0x06, 0xff)){
-    fprintf( stderr, "Failed to write to I2C device. 2\n" );
+  if (i2c_smbus_write_byte_data(fd, 0x06, 0xff)) {
+    fprintf(stderr, "Failed to write to I2C device. 2\n");
   }
   // Select ADCIN12 for conversion in SW1SELECT_MSB
-  if( i2c_smbus_write_byte_data( fd, 0x07, 0xff))  {
-    fprintf( stderr, "Failed to write to I2C device. 3\n" );
+  if (i2c_smbus_write_byte_data(fd, 0x07, 0xff))  {
+    fprintf(stderr, "Failed to write to I2C device. 3\n");
   }
   // Setup register for averaging
-  if( i2c_smbus_write_byte_data( fd, 0x08, 0xff))  {
-    fprintf( stderr, "Failed to write to I2C device. 4\n" );
+  if (i2c_smbus_write_byte_data(fd, 0x08, 0xff))  {
+    fprintf(stderr, "Failed to write to I2C device. 4\n");
   }
   // Start all channel conversion by setting bit 5 to one in CTRL_SW1
-  if( i2c_smbus_write_byte_data( fd, 0x12, 0x20))  {
-    fprintf( stderr, "Failed to write to I2C device. 5\n" );
+  if (i2c_smbus_write_byte_data(fd, 0x12, 0x20))  {
+    fprintf(stderr, "Failed to write to I2C device. 5\n");
   }
 }
 
-void electrical_periodic(void) {
+void electrical_periodic(void)
+{
 
   electrical_setup();
 
@@ -122,7 +125,7 @@ void electrical_periodic(void) {
   //from raw measurement we got quite a lineair response
   //9.0V=662, 9.5V=698, 10.0V=737,10.5V=774, 11.0V=811, 11.5V=848, 12.0V=886, 12.5V=923
   //leading to our 0.13595166 magic number for decivolts conversion
-  electrical.vsupply = raw_voltage*0.13595166;
+  electrical.vsupply = raw_voltage * 0.13595166;
 
   /*
    * Superellipse: abs(x/a)^n + abs(y/b)^n = 1
@@ -139,5 +142,5 @@ void electrical_periodic(void) {
   /* electrical.current y = ( b^n - (b* x/a)^n )^1/n
    * a=1, n = electrical_priv.nonlin_factor
    */
-  electrical.current = b - pow((pow(b,electrical_priv.nonlin_factor)-pow((b*x),electrical_priv.nonlin_factor)), (1./electrical_priv.nonlin_factor));
+  electrical.current = b - pow((pow(b, electrical_priv.nonlin_factor) - pow((b * x), electrical_priv.nonlin_factor)), (1. / electrical_priv.nonlin_factor));
 }
