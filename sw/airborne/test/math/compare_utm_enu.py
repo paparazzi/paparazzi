51058fdfd1e019051f8e5af3d9a8c13dad1bebb8
Felix Ruess <felix.ruess@gmail.com>
2015-01-10 19:51:28 +0100

    [tests] compare ENU to UTM with north fix

--- sw/airborne/test/math/compare_utm_enu.py
+++ sw/airborne/test/math/compare_utm_enu.py
@@ -8,7 +8,8 @@ PPRZ_SRC = os.getenv("PAPARAZZI_SRC", "../../../..")
 sys.path.append(PPRZ_SRC + "/sw/lib/python")
 
 from pprz_math.geodetic import *
-from pprz_math.algebra import DoubleRMat
+from pprz_math.algebra import DoubleRMat, DoubleEulers, DoubleVect3
+from math import radians, degrees, tan
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -26,10 +27,23 @@ ecef_origin = lla_origin.to_ecef()
 ltp_origin = ecef_origin.to_ltp_def()
 print(ltp_origin)
 
+# convergence angle to "true north" is approx 1 deg here
+earth_radius = 6378137.0
+n = 0.9996 * earth_radius
+UTM_DELTA_EAST = 500000.
+dist_to_meridian = utm_origin.east - UTM_DELTA_EAST
+conv = dist_to_meridian / n * tan(lla_origin.lat)
+# or (middle meridian of UTM zone 31 is at 3deg)
+#conv = atan(tan(lla_origin.lon - radians(3))*sin(lla_origin.lat))
+print("approx. convergence angle (north error compared to meridian): %f deg" % degrees(conv))
+# Rotation matrix to correct for "true north"
+R = DoubleEulers(psi=-conv).to_rmat()
+
 # calculate ENU coordinates for 100 points in 100m distance
 nb_points = 100
 dist_points = 100
 enu_res = np.zeros((nb_points, 2))
+enu_res_c = np.zeros((nb_points, 2))
 utm_res = np.zeros((nb_points, 2))
 for i in range(0, nb_points):
     utm = UtmCoor_d()
@@ -46,23 +60,31 @@ for i in range(0, nb_points):
     enu = ecef.to_enu(ltp_origin)
     enu_res[i, 0] = enu.x
     enu_res[i, 1] = enu.y
+    enu_c = R * DoubleVect3(enu.x, enu.y, enu.z)
+    enu_res_c[i, 0] = enu_c.x
+    enu_res_c[i, 1] = enu_c.y
     #print(enu)
 
 
 dist = np.linalg.norm(utm_res, axis=1)
 error = np.linalg.norm(utm_res - enu_res, axis=1)
+error_c = np.linalg.norm(utm_res - enu_res_c, axis=1)
 
 plt.figure(1)
-plt.subplot(211)
+plt.subplot(311)
 plt.title("utm vs. enu")
 plt.plot(enu_res[:, 0], enu_res[:, 1], 'g', label="ENU")
 plt.plot(utm_res[:, 0], utm_res[:, 1], 'r', label="UTM")
 plt.ylabel("y/north [m]")
 plt.xlabel("x/east [m]")
 plt.legend(loc='upper left')
-plt.subplot(212)
+plt.subplot(312)
 plt.plot(dist, error, 'r')
 plt.xlabel("dist from origin [m]")
 plt.ylabel("error [m]")
+plt.subplot(313)
+plt.plot(dist, error_c, 'r')
+plt.xlabel("dist from origin [m]")
+plt.ylabel("error with north fix [m]")
 
 plt.show()
