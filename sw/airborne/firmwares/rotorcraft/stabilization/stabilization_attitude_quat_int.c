8634b728ae6fff28acd89e14394b3906180f74b5
Felix Ruess <felix.ruess@gmail.com>
2015-02-13 21:05:17 +0100

    [rotorcraft] fix attitude_set_rpy_setpoint
    use quat_of_eulers instead of quat_from_rpy_cmd to match what users expect from attitude_set_rpy_setpoint

--- sw/airborne/firmwares/rotorcraft/stabilization/stabilization_attitude_quat_int.c
+++ sw/airborne/firmwares/rotorcraft/stabilization/stabilization_attitude_quat_int.c
@@ -166,7 +166,7 @@ void stabilization_attitude_set_rpy_setpoint_i(struct Int32Eulers *rpy)
   // stab_att_sp_euler.psi still used in ref..
   memcpy(&stab_att_sp_euler, rpy, sizeof(struct Int32Eulers));
 
-  quat_from_rpy_cmd_i(&stab_att_sp_quat, &stab_att_sp_euler);
+  int32_quat_of_eulers(&stab_att_sp_quat, &stab_att_sp_euler);
 }
 
 void stabilization_attitude_set_earth_cmd_i(struct Int32Vect2 *cmd, int32_t heading)
