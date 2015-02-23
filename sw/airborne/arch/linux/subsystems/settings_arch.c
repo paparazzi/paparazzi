b77c6a3ec1f50fbb12258be5eeddbffdf0c09dd2
Felix Ruess <felix.ruess@gmail.com>
2015-02-23 14:25:33 +0100

    [arch/linux] settings: print if coulnd't be properly read

--- sw/airborne/arch/linux/subsystems/settings_arch.c
+++ sw/airborne/arch/linux/subsystems/settings_arch.c
@@ -56,13 +56,17 @@ int32_t persistent_read(void *ptr, uint32_t size)
   /* check if binary file size matches requested struct size */
   fseek(file, 0, SEEK_END);
   if (ftell(file) != size) {
-    printf("Settings file %s size does not match, deleting it!", PERSISTENT_SETTINGS_FILE);
+    printf("Settings file %s size does not match, deleting it!\n", PERSISTENT_SETTINGS_FILE);
     fclose(file);
     remove(PERSISTENT_SETTINGS_FILE);
     return -1;
   }
   fseek(file, 0, SEEK_SET);
-  fread(ptr, size, 1, file);
+  int bytes_read = fread(ptr, size, 1, file);
+  if (bytes_read != size) {
+    printf("Could only read %d of %d bytes from %s!\n", bytes_read, size,
+           PERSISTENT_SETTINGS_FILE);
+  }
   fclose(file);
   return 0;
 }
