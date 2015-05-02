ae23111f3d098e95e55cd40c02734b62d801f247
agressiva <agressiva@hotmail.com>
2015-05-02 19:38:42 -0300

    code style fixed

--- sw/airborne/modules/nav/nav_survey_rectangle_rotorcraft.c
+++ sw/airborne/modules/nav/nav_survey_rectangle_rotorcraft.c
@@ -93,9 +93,9 @@ static void send_survey(struct transport_tx *trans, struct link_device *dev)
 
 void nav_survey_rectangle_rotorcraft_init(void)
 {
-  #if PERIODIC_TELEMETRY
-    register_periodic_telemetry(DefaultPeriodic, "SURVEY", send_survey);
-  #endif
+#if PERIODIC_TELEMETRY
+  register_periodic_telemetry(DefaultPeriodic, "SURVEY", send_survey);
+#endif
 }
 
 bool_t nav_survey_rectangle_rotorcraft_setup(uint8_t wp1, uint8_t wp2, float grid, survey_orientation_t so)
@@ -107,16 +107,16 @@ bool_t nav_survey_rectangle_rotorcraft_setup(uint8_t wp1, uint8_t wp2, float gri
   nav_survey_south = Min(WaypointY(wp1), WaypointY(wp2));
   nav_survey_north = Max(WaypointY(wp1), WaypointY(wp2));
   survey_orientation = so;
-  
+
   if (survey_orientation == NS) {
-    if (abs(stateGetPositionEnu_f()->x - nav_survey_west) < abs(stateGetPositionEnu_f()->x - nav_survey_east)){
+    if (abs(stateGetPositionEnu_f()->x - nav_survey_west) < abs(stateGetPositionEnu_f()->x - nav_survey_east)) {
       survey_from.x = survey_to.x = nav_survey_west + grid / 4.;
-    }else{
+    } else {
       survey_from.x = survey_to.x = nav_survey_east - grid / 4.;
       grid = -grid;
-    }    
-    
-    if (abs(stateGetPositionEnu_f()->y - nav_survey_south) > abs(stateGetPositionEnu_f()->y - nav_survey_north)){
+    }
+
+    if (abs(stateGetPositionEnu_f()->y - nav_survey_south) > abs(stateGetPositionEnu_f()->y - nav_survey_north)) {
       survey_to.y = nav_survey_south;
       survey_from.y = nav_survey_north;
     } else {
@@ -124,14 +124,14 @@ bool_t nav_survey_rectangle_rotorcraft_setup(uint8_t wp1, uint8_t wp2, float gri
       survey_to.y = nav_survey_north;
     }
   } else { /* survey_orientation == WE */
-    if (abs(stateGetPositionEnu_f()->y - nav_survey_south) < abs(stateGetPositionEnu_f()->y - nav_survey_north)){
+    if (abs(stateGetPositionEnu_f()->y - nav_survey_south) < abs(stateGetPositionEnu_f()->y - nav_survey_north)) {
       survey_from.y = survey_to.y = nav_survey_south + grid / 4.;
-    }else{
+    } else {
       survey_from.y = survey_to.y = nav_survey_north - grid / 4.;
       grid = -grid;
-    }      
-    
-    if (abs(stateGetPositionEnu_f()->x - nav_survey_west) > abs(stateGetPositionEnu_f()->x - nav_survey_east)){
+    }
+
+    if (abs(stateGetPositionEnu_f()->x - nav_survey_west) > abs(stateGetPositionEnu_f()->x - nav_survey_east)) {
       survey_to.x = nav_survey_west;
       survey_from.x = nav_survey_east;
     } else {
@@ -142,17 +142,17 @@ bool_t nav_survey_rectangle_rotorcraft_setup(uint8_t wp1, uint8_t wp2, float gri
   nav_survey_shift = grid;
   survey_uturn = FALSE;
   nav_survey_retange_active = FALSE;
-  
-  //go to start position 
+
+  //go to start position
   ENU_BFP_OF_REAL(survey_from_i, survey_from);
-  horizontal_mode = HORIZONTAL_MODE_ROUTE; 
+  horizontal_mode = HORIZONTAL_MODE_ROUTE;
   VECT3_COPY(navigation_target, survey_from_i);
   LINE_STOP_FUNCTION;
   NavVerticalAltitudeMode(waypoints[wp1].enu_f.z, 0.);
-    //=======NavVerticalAutoThrottleMode(0.); /* No pitch */
+  //=======NavVerticalAutoThrottleMode(0.); /* No pitch */
   if (survey_orientation == NS) {
     nav_set_heading_deg(0);
-  }else{
+  } else {
     nav_set_heading_deg(90);
   }
   return FALSE;
@@ -163,183 +163,187 @@ bool_t nav_survey_rectangle_rotorcraft_run(uint8_t wp1, uint8_t wp2)
 {
   static bool_t is_last_half = FALSE;
   static float survey_radius;
-    nav_survey_active = TRUE;
-            
- /* entry scan */ // wait for start position and altitude be reached
-if ( !nav_survey_retange_active && ((!nav_approaching_from(&survey_from_i, NULL, 0)) || ( abs (stateGetPositionEnu_f()->z - waypoints[wp1].enu_f.z )) > 1) ) { 
+  nav_survey_active = TRUE;
+
+  /* entry scan */ // wait for start position and altitude be reached
+  if (!nav_survey_retange_active && ((!nav_approaching_from(&survey_from_i, NULL, 0))
+                                     || (abs(stateGetPositionEnu_f()->z - waypoints[wp1].enu_f.z)) > 1)) {
   } else {
     if (!nav_survey_retange_active) {
       nav_survey_retange_active = TRUE;
       LINE_START_FUNCTION;
     }
-  
-  nav_survey_west = Min(WaypointX(wp1), WaypointX(wp2));
-  nav_survey_east = Max(WaypointX(wp1), WaypointX(wp2));
-  nav_survey_south = Min(WaypointY(wp1), WaypointY(wp2));
-  nav_survey_north = Max(WaypointY(wp1), WaypointY(wp2));
 
-  /* Update the current segment from corners' coordinates*/
-  if (SurveyGoingNorth()) {
-    survey_to.y = nav_survey_north;
-    survey_from.y = nav_survey_south;
-  } else if (SurveyGoingSouth()) {
-    survey_to.y = nav_survey_south;
-    survey_from.y = nav_survey_north;
-  } else if (SurveyGoingEast()) {
-    survey_to.x = nav_survey_east;
-    survey_from.x = nav_survey_west;
-  } else if (SurveyGoingWest()) {
-    survey_to.x = nav_survey_west;
-    survey_from.x = nav_survey_east;
-  }
+    nav_survey_west = Min(WaypointX(wp1), WaypointX(wp2));
+    nav_survey_east = Max(WaypointX(wp1), WaypointX(wp2));
+    nav_survey_south = Min(WaypointY(wp1), WaypointY(wp2));
+    nav_survey_north = Max(WaypointY(wp1), WaypointY(wp2));
 
-  if (!survey_uturn) { /* S-N, N-S, W-E or E-W straight route */
-/*
-    if ((stateGetPositionEnu_f()->y < nav_survey_north && SurveyGoingNorth()) ||
-        (stateGetPositionEnu_f()->y > nav_survey_south && SurveyGoingSouth()) ||
-        (stateGetPositionEnu_f()->x < nav_survey_east && SurveyGoingEast()) ||
-        (stateGetPositionEnu_f()->x > nav_survey_west && SurveyGoingWest())) {
-        */
+    /* Update the current segment from corners' coordinates*/
+    if (SurveyGoingNorth()) {
+      survey_to.y = nav_survey_north;
+      survey_from.y = nav_survey_south;
+    } else if (SurveyGoingSouth()) {
+      survey_to.y = nav_survey_south;
+      survey_from.y = nav_survey_north;
+    } else if (SurveyGoingEast()) {
+      survey_to.x = nav_survey_east;
+      survey_from.x = nav_survey_west;
+    } else if (SurveyGoingWest()) {
+      survey_to.x = nav_survey_west;
+      survey_from.x = nav_survey_east;
+    }
+
+    if (!survey_uturn) { /* S-N, N-S, W-E or E-W straight route */
+      /*
+          if ((stateGetPositionEnu_f()->y < nav_survey_north && SurveyGoingNorth()) ||
+              (stateGetPositionEnu_f()->y > nav_survey_south && SurveyGoingSouth()) ||
+              (stateGetPositionEnu_f()->x < nav_survey_east && SurveyGoingEast()) ||
+              (stateGetPositionEnu_f()->x > nav_survey_west && SurveyGoingWest())) {
+              */
       /* Continue ... */
       ENU_BFP_OF_REAL(survey_to_i, survey_to);
-    
-  if (!nav_approaching_from(&survey_to_i, NULL, 0)){
-      ENU_BFP_OF_REAL(survey_from_i, survey_from);
 
-      horizontal_mode = HORIZONTAL_MODE_ROUTE; 
-      nav_route(&survey_from_i, &survey_to_i);
-    
-    } else {
-      if (survey_orientation == NS) {
-        /* North or South limit reached, prepare turn and next leg */
-        float x0 = survey_from.x; /* Current longitude */
-        if ( (x0 + nav_survey_shift < nav_survey_west) || (x0 + nav_survey_shift > nav_survey_east) ) { // not room for full sweep
-	  //fprintf(stderr,"nao cabe inteiro\n");
-          if ( ((x0 + (nav_survey_shift/2)) < nav_survey_west) || ((x0 + (nav_survey_shift/2)) > nav_survey_east)) {//not room for half sweep 
-            if (is_last_half) {// was last sweep half?
-              nav_survey_shift = -nav_survey_shift;
-              survey_radius = nav_survey_shift;
-              is_last_half = FALSE;
-            }else{// last sweep not half
-              nav_survey_shift = -nav_survey_shift;
-              survey_radius = nav_survey_shift/2.;
+      if (!nav_approaching_from(&survey_to_i, NULL, 0)) {
+        ENU_BFP_OF_REAL(survey_from_i, survey_from);
+
+        horizontal_mode = HORIZONTAL_MODE_ROUTE;
+        nav_route(&survey_from_i, &survey_to_i);
+
+      } else {
+        if (survey_orientation == NS) {
+          /* North or South limit reached, prepare turn and next leg */
+          float x0 = survey_from.x; /* Current longitude */
+          if ((x0 + nav_survey_shift < nav_survey_west)
+              || (x0 + nav_survey_shift > nav_survey_east)) {   // not room for full sweep
+            //fprintf(stderr,"nao cabe inteiro\n");
+            if (((x0 + (nav_survey_shift / 2)) < nav_survey_west)
+                || ((x0 + (nav_survey_shift / 2)) > nav_survey_east)) { //not room for half sweep
+              if (is_last_half) {// was last sweep half?
+                nav_survey_shift = -nav_survey_shift;
+                survey_radius = nav_survey_shift;
+                is_last_half = FALSE;
+              } else { // last sweep not half
+                nav_survey_shift = -nav_survey_shift;
+                survey_radius = nav_survey_shift / 2.;
+              }
+              rectangle_survey_sweep_num ++;
+            } else { //room for half sweep after
+              survey_radius = nav_survey_shift / 2.;
+              is_last_half = true;
             }
-            rectangle_survey_sweep_num ++;
-          }else{//room for half sweep after
-            survey_radius = nav_survey_shift/2.;
-            is_last_half = true;
+          } else {// room for full sweep after
+            survey_radius = nav_survey_shift;
           }
-        }else {// room for full sweep after
-          survey_radius = nav_survey_shift;
-        }
 
-        x0 = x0 + survey_radius; /* Longitude of next leg */
-        survey_from.x = survey_to.x = x0;
+          x0 = x0 + survey_radius; /* Longitude of next leg */
+          survey_from.x = survey_to.x = x0;
 
-        /* Swap South and North extremities */
-        float tmp = survey_from.y;
-        survey_from.y = survey_to.y;
-        survey_to.y = tmp;
+          /* Swap South and North extremities */
+          float tmp = survey_from.y;
+          survey_from.y = survey_to.y;
+          survey_to.y = tmp;
 
-        /** Do half a circle around WP 0 */
-        waypoints[0].enu_f.x = x0;
-        waypoints[0].enu_f.y = survey_from.y;
+          /** Do half a circle around WP 0 */
+          waypoints[0].enu_f.x = x0;
+          waypoints[0].enu_f.y = survey_from.y;
 
-        /* Computes the right direction */
-        if (SurveyGoingEast()) {
-          survey_radius = -survey_radius;
-        }
-      } else { /* (survey_orientation == WE) */
-        /* East or West limit reached, prepare turn and next leg */
-        /* There is a y0 declared in math.h (for ARM) !!! */
-        float my_y0 = survey_from.y; /* Current latitude */
-        if (my_y0 + nav_survey_shift < nav_survey_south || my_y0 + nav_survey_shift > nav_survey_north) { // not room for full sweep
-          if ( ((my_y0 + (nav_survey_shift/2)) < nav_survey_south) || ((my_y0 + (nav_survey_shift/2)) > nav_survey_north)) {//not room for half sweep  
-            if (is_last_half) {// was last sweep half?
-              nav_survey_shift = -nav_survey_shift;
-              survey_radius = nav_survey_shift;
-              is_last_half = FALSE;
-            }else{// last sweep not half
-              nav_survey_shift = -nav_survey_shift;
-              survey_radius = nav_survey_shift/2.;
+          /* Computes the right direction */
+          if (SurveyGoingEast()) {
+            survey_radius = -survey_radius;
+          }
+        } else { /* (survey_orientation == WE) */
+          /* East or West limit reached, prepare turn and next leg */
+          /* There is a y0 declared in math.h (for ARM) !!! */
+          float my_y0 = survey_from.y; /* Current latitude */
+          if (my_y0 + nav_survey_shift < nav_survey_south
+              || my_y0 + nav_survey_shift > nav_survey_north) { // not room for full sweep
+            if (((my_y0 + (nav_survey_shift / 2)) < nav_survey_south)
+                || ((my_y0 + (nav_survey_shift / 2)) > nav_survey_north)) { //not room for half sweep
+              if (is_last_half) {// was last sweep half?
+                nav_survey_shift = -nav_survey_shift;
+                survey_radius = nav_survey_shift;
+                is_last_half = FALSE;
+              } else { // last sweep not half
+                nav_survey_shift = -nav_survey_shift;
+                survey_radius = nav_survey_shift / 2.;
+              }
+              rectangle_survey_sweep_num ++;
+            } else { //room for half sweep after
+              survey_radius = nav_survey_shift / 2.;
+              is_last_half = true;
             }
-            rectangle_survey_sweep_num ++;
-          }else{//room for half sweep after
-            survey_radius = nav_survey_shift/2.;
-            is_last_half = true;
+          } else {// room for full sweep after
+            survey_radius = nav_survey_shift;
           }
-        }else {// room for full sweep after
-          survey_radius = nav_survey_shift;
+
+          my_y0 = my_y0 + survey_radius; /* latitude of next leg */
+          survey_from.y = survey_to.y = my_y0;
+
+          /* Swap West and East extremities */
+          float tmp = survey_from.x;
+          survey_from.x = survey_to.x;
+          survey_to.x = tmp;
+
+          /** Do half a circle around WP 0 */
+          waypoints[0].enu_f.x = survey_from.x;
+          waypoints[0].enu_f.y = my_y0;
+
+          /* Computes the right direction */
+          if (SurveyGoingNorth()) {
+            survey_radius = -survey_radius;
+          }
         }
 
-        my_y0 = my_y0 + survey_radius; /* latitude of next leg */
-        survey_from.y = survey_to.y = my_y0;
-
-        /* Swap West and East extremities */
-        float tmp = survey_from.x;
-        survey_from.x = survey_to.x;
-        survey_to.x = tmp;
-
-        /** Do half a circle around WP 0 */
-       waypoints[0].enu_f.x = survey_from.x;
-       waypoints[0].enu_f.y = my_y0;
-
-        /* Computes the right direction */
-        if (SurveyGoingNorth()) {
-          survey_radius = -survey_radius;
-        }
+        nav_in_segment = FALSE;
+        survey_uturn = TRUE;
+        LINE_STOP_FUNCTION;
+        dc_send_command(DC_SHOOT); //take a picture in the last position before go to the next sweep
       }
+    } else { /* START turn */
 
-      nav_in_segment = FALSE;
-      survey_uturn = TRUE;
-      LINE_STOP_FUNCTION;
-      dc_send_command(DC_SHOOT); //take a picture in the last position before go to the next sweep
-    }
-  } else { /* START turn */
-    
-     static struct EnuCoor_f temp_f;
-      if (survey_orientation == WE) {     
+      static struct EnuCoor_f temp_f;
+      if (survey_orientation == WE) {
         temp_f.x = waypoints[0].enu_f.x;
         temp_f.y = waypoints[0].enu_f.y - survey_radius;
-      } else
-      {
+      } else {
         temp_f.y = waypoints[0].enu_f.y;
-        temp_f.x = waypoints[0].enu_f.x - survey_radius;     
+        temp_f.x = waypoints[0].enu_f.x - survey_radius;
       }
 
-    //detect when segment has done
-/*
-    if ( (stateGetPositionEnu_f()->y > waypoints[0].enu_f.y && ((survey_orientation == WE) && (temp_f.y < waypoints[0].enu_f.y)) )||
-         (stateGetPositionEnu_f()->y < waypoints[0].enu_f.y && ((survey_orientation == WE) && (temp_f.y > waypoints[0].enu_f.y)) )||
-         (stateGetPositionEnu_f()->x < waypoints[0].enu_f.x && ((survey_orientation == NS) && (temp_f.x > waypoints[0].enu_f.x)) )||
-         (stateGetPositionEnu_f()->x > waypoints[0].enu_f.x && ((survey_orientation == NS) && (temp_f.x < waypoints[0].enu_f.x)) ) ) {
-*/      
+      //detect when segment has done
+      /*
+          if ( (stateGetPositionEnu_f()->y > waypoints[0].enu_f.y && ((survey_orientation == WE) && (temp_f.y < waypoints[0].enu_f.y)) )||
+               (stateGetPositionEnu_f()->y < waypoints[0].enu_f.y && ((survey_orientation == WE) && (temp_f.y > waypoints[0].enu_f.y)) )||
+               (stateGetPositionEnu_f()->x < waypoints[0].enu_f.x && ((survey_orientation == NS) && (temp_f.x > waypoints[0].enu_f.x)) )||
+               (stateGetPositionEnu_f()->x > waypoints[0].enu_f.x && ((survey_orientation == NS) && (temp_f.x < waypoints[0].enu_f.x)) ) ) {
+      */
 
       if (survey_orientation == WE) {
-        ENU_BFP_OF_REAL(survey_from_i,temp_f);
+        ENU_BFP_OF_REAL(survey_from_i, temp_f);
         ENU_BFP_OF_REAL(survey_to_i, waypoints[0].enu_f);
       } else {
-        ENU_BFP_OF_REAL(survey_from_i,temp_f);
-        ENU_BFP_OF_REAL(survey_to_i, waypoints[0].enu_f);	
-      }
-      if (nav_approaching_from(&survey_to_i, NULL, 0)){
-      survey_uturn = FALSE;
-      nav_in_circle = FALSE;
-      LINE_START_FUNCTION;
-    } else {
-   
-      if (survey_orientation == WE) {
-        ENU_BFP_OF_REAL(survey_from_i,temp_f);
+        ENU_BFP_OF_REAL(survey_from_i, temp_f);
         ENU_BFP_OF_REAL(survey_to_i, waypoints[0].enu_f);
-      } else {
-        ENU_BFP_OF_REAL(survey_from_i,temp_f);
-        ENU_BFP_OF_REAL(survey_to_i, waypoints[0].enu_f);	
       }
-     
-     horizontal_mode = HORIZONTAL_MODE_ROUTE; 
-     nav_route(&survey_from_i, &survey_to_i);
-    }
-  } /* END turn */
+      if (nav_approaching_from(&survey_to_i, NULL, 0)) {
+        survey_uturn = FALSE;
+        nav_in_circle = FALSE;
+        LINE_START_FUNCTION;
+      } else {
+
+        if (survey_orientation == WE) {
+          ENU_BFP_OF_REAL(survey_from_i, temp_f);
+          ENU_BFP_OF_REAL(survey_to_i, waypoints[0].enu_f);
+        } else {
+          ENU_BFP_OF_REAL(survey_from_i, temp_f);
+          ENU_BFP_OF_REAL(survey_to_i, waypoints[0].enu_f);
+        }
+
+        horizontal_mode = HORIZONTAL_MODE_ROUTE;
+        nav_route(&survey_from_i, &survey_to_i);
+      }
+    } /* END turn */
 
   } /* END entry scan  */
   return TRUE;
