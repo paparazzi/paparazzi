ba8821d41c5d043a404511cef746b7a54570caac
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2015-06-10 01:02:02 +0200

    [GCS] update display for dynamic sectors

--- sw/lib/ocaml/mapFP.mli
+++ sw/lib/ocaml/mapFP.mli
@@ -45,6 +45,7 @@ class flight_plan :
     method insert_path : (MapWaypoints.waypoint * float) list -> unit
     method highlight_stage : int -> int -> unit
     method connect_activated : (XmlEdit.node->unit) -> unit
+    method update_sectors : string -> unit
   end
 
 (** Extracts [lat0] and [Lon0] attributes *)
