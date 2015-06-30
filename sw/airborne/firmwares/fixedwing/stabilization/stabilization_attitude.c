f052e69b91e4c62367a2290d3a1a3342c45047e8
Felix Ruess <felix.ruess@gmail.com>
2015-06-30 23:28:51 +0200

    [fixedwing] stabilization_attitude: fix bug in AGR_CLIMB
    Not sure exactly of the impact as this as lingering in the code for a long time,
    but it was clearly not meant this way...

--- sw/airborne/firmwares/fixedwing/stabilization/stabilization_attitude.c
+++ sw/airborne/firmwares/fixedwing/stabilization/stabilization_attitude.c
@@ -292,7 +292,8 @@ void h_ctl_course_loop(void)
 #if defined(AGR_CLIMB) && !USE_AIRSPEED
   /** limit navigation during extreme altitude changes */
   if (AGR_BLEND_START > AGR_BLEND_END && AGR_BLEND_END > 0) { /* prevent divide by zero, reversed or negative values */
-    if (v_ctl_auto_throttle_submode == V_CTL_AUTO_THROTTLE_AGRESSIVE || V_CTL_AUTO_THROTTLE_BLENDED) {
+    if (v_ctl_auto_throttle_submode == V_CTL_AUTO_THROTTLE_AGRESSIVE ||
+        v_ctl_auto_throttle_submode == V_CTL_AUTO_THROTTLE_BLENDED) {
       BoundAbs(cmd, h_ctl_roll_max_setpoint); /* bound cmd before NAV_RATIO and again after */
       /* altitude: z-up is positive -> positive error -> too low */
       if (v_ctl_altitude_error > 0) {
