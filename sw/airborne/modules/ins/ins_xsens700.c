3d41040278f724cffe9122bf37eebbc8bc6f3c71
Felix Ruess <felix.ruess@gmail.com>
2015-07-16 00:25:37 +0200

    [ins] try to fix ins xsens700

--- sw/airborne/modules/ins/ins_xsens700.c
+++ sw/airborne/modules/ins/ins_xsens700.c
@@ -162,7 +162,6 @@ struct UtmCoor_f utm_f;
 volatile int xsens_configured = 0;
 
 void xsens_init(void);
-void xsens_periodic(void);
 
 void xsens_init(void)
 {
@@ -176,7 +175,9 @@ void xsens_init(void)
 }
 
 #if USE_INS_MODULE
-void ins_init(void)
+void ins_xsens_update_gps(struct GpsState *gps_s);
+
+void ins_xsens_init(void)
 {
   struct UtmCoor_f utm0 = { nav_utm_north0, nav_utm_east0, 0., nav_utm_zone0 };
   stateSetLocalUtmOrigin_f(&utm0);
@@ -188,21 +189,36 @@ void ins_init(void)
   xsens_init();
 }
 
-void ins_update_gps(void)
+#include "subsystems/abi.h"
+static abi_event gps_ev;
+static void gps_cb(uint8_t sender_id __attribute__((unused)),
+                   uint32_t stamp __attribute__((unused)),
+                   struct GpsState *gps_s)
+{
+  ins_xsens_update_gps(gps_s);
+}
+
+void ins_xsens_register(void)
+{
+  ins_register_impl(ins_xsens_init);
+  AbiBindMsgGPS(ABI_BROADCAST, &gps_ev, gps_cb);
+}
+
+void ins_xsens_update_gps(struct GpsState *gps_s)
 {
   struct UtmCoor_f utm;
-  utm.east = gps.utm_pos.east / 100.;
-  utm.north = gps.utm_pos.north / 100.;
+  utm.east = gps_s->utm_pos.east / 100.;
+  utm.north = gps_s->utm_pos.north / 100.;
   utm.zone = nav_utm_zone0;
-  utm.alt = gps.hmsl / 1000.;
+  utm.alt = gps_s->hmsl / 1000.;
 
   // set position
   stateSetPositionUtm_f(&utm);
 
   struct NedCoor_f ned_vel = {
-    gps.ned_vel.x / 100.,
-    gps.ned_vel.y / 100.,
-    gps.ned_vel.z / 100.
+    gps_s->ned_vel.x / 100.,
+    gps_s->ned_vel.y / 100.,
+    gps_s->ned_vel.z / 100.
   };
   // set velocity
   stateSetSpeedNed_f(&ned_vel);
