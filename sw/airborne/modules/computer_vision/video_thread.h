e6ef98366f4f187c3f4d9486ae016bf4cc24dc38
Christophe De Wagter <dewagter@gmail.com>
2015-09-03 10:13:27 +0200

    [computervision] Setup a re-usable framework where computer vision methods can be added to the computer vision thread

---
+++ sw/airborne/modules/computer_vision/video_thread.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2015
+ *
+ * This file is part of Paparazzi.
+ *
+ * Paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * Paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * @file modules/computer_vision/video_thread.h
+ *
+ * Start a Video thread and grab images
+ *
+ * Works on Linux platforms
+ */
+
+#ifndef VIDEO_THREAD_H
+#define VIDEO_THREAD_H
+
+#include "std.h"
+
+// Main video_thread structure
+struct video_thread_t {
+  volatile bool_t is_running;   ///< When the device is running
+  struct v4l2_device *dev;        ///< The V4L2 device that is used for the video stream
+  uint8_t fps;                    ///< The amount of frames per second
+
+  volatile bool_t take_shot;      ///< Wether to take an image
+  uint16_t shot_number;           ///< The last shot number
+};
+extern struct video_thread_t video_thread;
+
+// Module functions
+extern void video_thread_init(void);
+extern void video_thread_periodic(void); ///< A dummy for now
+extern void video_thread_start(void);
+extern void video_thread_stop(void);
+extern void video_thread_take_shot(bool_t take);
+
+#endif /* VIDEO_THREAD_H */
+
