3bbce540dfa1d1d1bb8305cdf6b76f005c4131d7
Christophe De Wagter <dewagter@gmail.com>
2015-09-06 20:54:20 +0200

    [computer_vision] Sample cv module

---
+++ sw/airborne/modules/computer_vision/colorfilter.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2015
+ *
+ * This file is part of Paparazzi.
+ *
+ * Paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * Paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/**
+ * @file modules/computer_vision/colorfilter.h
+ */
+
+#ifndef COLORFILTER_CV_PLUGIN_H
+#define COLORFILTER_CV_PLUGIN_H
+
+#include <stdint.h>
+
+// Module functions
+extern void colorfilter_init(void);
+
+extern uint8_t color_lum_min;
+extern uint8_t color_lum_max;
+
+extern uint8_t color_cb_min;
+extern uint8_t color_cb_max;
+
+extern uint8_t color_cr_min;
+extern uint8_t color_cr_max;
+
+extern int color_count;
+
+#endif /* COLORFILTER_CV_PLUGIN_H */
