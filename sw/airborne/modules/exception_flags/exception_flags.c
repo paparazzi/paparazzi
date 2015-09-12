0d3e7a98f19a5f82d44e15a806147d81b0ed4d9a
Ewoud Smeur <ewoud_smeur@msn.com>
2015-09-12 23:25:16 +0200

    exception flags module to use in the flight plan

---
+++ sw/airborne/modules/exception_flags/exception_flags.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2015 Ewoud Smeur <ewoud.smeur@gmail.com>
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+/**
+ * @file modules/exception_flags/exception_flags.c
+ * @brief Flags to check if flight plan exceptions already triggered
+ */
+
+#include "exception_flags.h"
+#include <string.h>
+
+bool_t exception_flag[10];
+
+void exception_flags_init(void) {
+  memset(exception_flag,0,sizeof(exception_flag));
+}
