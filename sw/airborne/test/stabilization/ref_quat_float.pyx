b3572dae535913aa78e643ffdea9d5a09dabb7e0
Felix Ruess <felix.ruess@gmail.com>
2015-10-10 14:05:15 +0200

    [tests] update ref model python wrappers/test

--- sw/airborne/test/stabilization/ref_quat_float.pyx
+++ sw/airborne/test/stabilization/ref_quat_float.pyx
@@ -1,60 +1,29 @@
-cdef extern from "stdint.h":
-    ctypedef unsigned long int uintptr_t
-
-
+cimport ref_quat_float
+from pprz_algebra_float_c cimport FloatQuat
 import numpy as np
-cimport numpy as np
 
-cimport pprz_algebra_float_c
-from algebra_float import FloatQuat, FloatEulers, FloatRates
+cdef class RefQuatFloat:
+    cdef ref_quat_float.AttRefQuatFloat ref
+    cdef FloatQuat sp_quat
 
+    def __cinit__(self):
+        ref_quat_float.attitude_ref_quat_float_init(&self.ref)
+        self.setpoint = [1.0, 0, 0, 0]
 
-cdef extern from "stabilization/stabilization_attitude_ref_quat_float.h":
-    void stabilization_attitude_ref_init()
-    void stabilization_attitude_ref_update()
-    void stabilization_attitude_ref_enter()
-    void stabilization_attitude_ref_schedule(int idx)
+    cpdef update(self, dt, setpoint=None):
+        if setpoint is not None:
+            self.setpoint = setpoint
+        ref_quat_float.attitude_ref_quat_float_update(&self.ref, &self.sp_quat, dt)
 
-    pprz_algebra_float_c.FloatEulers stab_att_sp_euler
-    pprz_algebra_float_c.FloatQuat   stab_att_sp_quat
-    pprz_algebra_float_c.FloatEulers stab_att_ref_euler
-    pprz_algebra_float_c.FloatQuat   stab_att_ref_quat
-    pprz_algebra_float_c.FloatRates  stab_att_ref_rate
-    pprz_algebra_float_c.FloatRates  stab_att_ref_accel
+    property setpoint:
+        def __get__(self):
+            return np.array([self.sp_quat.qi, self.sp_quat.qx, self.sp_quat.qy, self.sp_quat.qz])
+        def __set__(self, sp):
+            self.sp_quat.qi = sp[0]
+            self.sp_quat.qx = sp[1]
+            self.sp_quat.qy = sp[2]
+            self.sp_quat.qz = sp[3]
 
-
-sp_euler = FloatEulers(<uintptr_t> &stab_att_sp_euler)
-sp_quat = FloatQuat(<uintptr_t> &stab_att_sp_quat)
-ref_euler = FloatEulers(<uintptr_t> &stab_att_ref_euler)
-ref_quat = FloatQuat(<uintptr_t> &stab_att_ref_quat)
-ref_rate = FloatRates(<uintptr_t> &stab_att_ref_rate)
-ref_accel = FloatRates(<uintptr_t> &stab_att_ref_accel)
-
-
-def init():
-    stabilization_attitude_ref_init()
-
-def enter():
-    stabilization_attitude_ref_enter()
-
-def update():
-    stabilization_attitude_ref_update()
-
-"""
-# test some different set methods
-def set_sp_euler(**kwds):
-    global stab_att_sp_euler
-    cdef pprz_algebra_float_c.FloatEulers c_sp = kwds
-    stab_att_sp_euler = c_sp
-
-def set_sp_euler(phi=0.0, theta=0.0, psi=0.0):
-    global sp_euler
-    sp_euler.phi = phi
-    sp_euler.theta = theta
-    sp_euler.psi = psi
-
-def set_sp_quat(**kwds):
-    global stab_att_sp_quat
-    cdef pprz_algebra_float_c.FloatQuat c_sp = kwds
-    stab_att_sp_quat = c_sp
-"""
+    property eulers:
+        def __get__(self):
+            return np.array([self.ref.euler.phi, self.ref.euler.theta, self.ref.euler.psi])
