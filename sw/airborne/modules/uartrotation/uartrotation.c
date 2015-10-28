850c365b679c197a36827d532a291bd5801bca86
Roland Meertens <rolandmeertens@gmail.com>
2015-10-28 10:47:28 +0100

    cleaned up

--- sw/airborne/modules/uartrotation/uartrotation.c
+++ sw/airborne/modules/uartrotation/uartrotation.c
@@ -23,96 +23,23 @@
  * reads from the odroid and sends information of the drone to the odroid using JSON messages
  */
 
-#include "modules/readlocationfromodroid/readlocationfromodroid.h"
+#include "modules/uartrotation/uartrotation.h"
 #include "subsystems/abi.h"
-#include "modules/computer_vision/opticflow_module.h"
-#include <fcntl.h>
-#include <stropts.h>
-#include <termios.h>
 #include <serial_port.h>
 #include <stdio.h>
 #include <inttypes.h>
 #include <errno.h>
-#include <string.h>
 #include "state.h"
 #include "subsystems/gps.h"
-//#include "subsystems/navigation/common_nav.h"
-#include "/home/roland/paparazzi/sw/ext/cJSON/cJSON.h"
-//#include "cJSON.h"
-#include "navdata.h"
 #include "subsystems/ins/ins_int.h"
-#include "subsystems/datalink/telemetry.h"
-#include "modules/computer_vision/opticflow/stabilization_opticflow.h"
-
-
 #include "firmwares/rotorcraft/autopilot.h"
-
 #include "mcu_periph/uart.h"
-#include "subsystems/radio_control.h"
-#include "subsystems/commands.h"
-#include "subsystems/actuators.h"
-#include "subsystems/electrical.h"
-#include "subsystems/settings.h"
-#include "subsystems/datalink/telemetry.h"
-#include "firmwares/rotorcraft/navigation.h"
-#include "firmwares/rotorcraft/guidance.h"
-
-#include "firmwares/rotorcraft/stabilization.h"
-#include "firmwares/rotorcraft/stabilization/stabilization_none.h"
-#include "firmwares/rotorcraft/stabilization/stabilization_rate.h"
-#include "firmwares/rotorcraft/stabilization/stabilization_attitude.h"
-
-#include "generated/settings.h"
-
 #include "subsystems/gps.h"
-#include "subsystems/abi.h"
-#include "navdata.h"
-#include "subsystems/ins/ins_int.h"
-#include "state.h"
-#include "generated/airframe.h"
 #include "subsystems/datalink/telemetry.h"
-#include "subsystems/gps/stereoprotocol.h"
-
-
-
-#include "subsystems/gps.h"
-#include "subsystems/abi.h"
-#include "navdata.h"
-#include "subsystems/ins/ins_int.h"
-#include "state.h"
-#include "generated/airframe.h"
-#include "subsystems/datalink/telemetry.h"
-#include "subsystems/gps/stereoprotocol.h"
-
+#include "subsystems/stereoprotocol.h"
 
 int frameNumberSending=0;
-static void write_serial_rot(struct transport_tx *trans, struct link_device *devasdf) {
-	 	struct Int32RMat *ltp_to_body_mat = stateGetNedToBodyRMat_i();
-	int32_t lengthArrayInformation = 11*sizeof(int32_t);
-	uint8_t ar[lengthArrayInformation];
-	int32_t *pointer = (int32_t*)ar;
-	for(int indexRot = 0; indexRot < 9; indexRot++){
-		pointer[indexRot]=ltp_to_body_mat->m[indexRot];
-	}
-	pointer[9]=(int32_t)(sonar_meas*100);
-	pointer[10]=frameNumberSending++;
-	stereoprot_sendArray( &((GPS_LINK).device),ar, lengthArrayInformation, 1);
-}
-
-struct SerialPort *READING_port;
-speed_t usbInputSpeed = B1000000;
-char *serialResponse;
-int writeLocationInput=0;
-
-void odroid_loc_init() {
-	// Open the serial port
-	READING_port = serial_port_new();
-	int lengthBytesImage=50000;
-	serialResponse=malloc(lengthBytesImage*sizeof(char));
-	memset(serialResponse, '0', lengthBytesImage);
-	register_periodic_telemetry(DefaultPeriodic, "SERIALRMAT", write_serial_rot);
- }
-
+float sonar_meas=0.0;
  static abi_event odroid_agl_ev;
  float lastKnownHeight = 0.0;
  int pleaseResetOdroid = 0;
@@ -145,11 +72,46 @@ void odroid_loc_init() {
  uint8_t ser_read_buf[STEREO_BUF_SIZE];           // circular buffer for incoming data
  uint16_t insert_loc, extract_loc, msg_start;   // place holders for buffer read and write
 
-static void gps_odroid_height(uint8_t sender_id __attribute__((unused)), float distance) {
-// Update the distance if we got a valid measurement
-if (distance > 0) {
-	lastKnownHeight = distance;
+static void write_serial_rot(struct transport_tx *trans, struct link_device *devasdf) {
+
+	 	struct Int32RMat *ltp_to_body_mat = stateGetNedToBodyRMat_i();
+	int32_t lengthArrayInformation = 11*sizeof(int32_t);
+	uint8_t ar[lengthArrayInformation];
+	int32_t *pointer = (int32_t*)ar;
+	for(int indexRot = 0; indexRot < 9; indexRot++){
+		pointer[indexRot]=ltp_to_body_mat->m[indexRot];
+	}
+	pointer[9]=(int32_t)(sonar_meas*100);
+	pointer[10]=frameNumberSending++;
+	printf("Whoo sending serial\n");
+	stereoprot_sendArray( &((GPS_LINK).device),ar, lengthArrayInformation, 1);
+	printf("Whoo sending serial2\n");
 }
+
+struct SerialPort *READING_port;
+speed_t usbInputSpeed = B1000000;
+char *serialResponse;
+int writeLocationInput=0;
+
+void odroid_loc_init() {
+	printf("Init odroid loc");
+	// Open the serial port
+	READING_port = serial_port_new();
+	int lengthBytesImage=50000;
+	serialResponse=malloc(lengthBytesImage*sizeof(char));
+	memset(serialResponse, '0', lengthBytesImage);
+	register_periodic_telemetry(DefaultPeriodic, "SERIALRMAT", write_serial_rot);
+ }
+
+void odroid_loc_periodic() {
+
+}
+
+static void gps_odroid_height(uint8_t sender_id __attribute__((unused)), float distance) {
+	// Update the distance if we got a valid measurement
+	if (distance > 0) {
+		lastKnownHeight = distance;
+	}
 }
 
 
