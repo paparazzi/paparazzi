ab088da41cb5cbe8608145edcb3a3fb2621f34ab
Christophe De Wagter <dewagter@gmail.com>
2015-11-12 16:24:39 +0100

    fake gps

---
+++ sw/airborne/subsystems/gps/gps_vision.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2014 Freek van Tienen
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/**
+ * @file gps_datalink.h
+ * @brief GPS system based on datalink
+ *
+ * This GPS parses the datalink REMOTE_GPS packet and sets the
+ * GPS structure to the values received.
+ */
+
+#ifndef GPS_VISION_H
+#define GPS_VISION_H
+
+#include "std.h"
+#include "generated/airframe.h"
+#define GPS_NB_CHANNELS 0
+
+extern bool_t gps_available;
+
+#define GpsEvent() {}
+
+#endif /* GPS_DATALINK_H */
