d608f1a58862137f8dd4b6f99a33003f154cf04d
Ewoud Smeur <ewoud_smeur@msn.com>
2015-12-08 18:48:27 +0100

    bebop correct motor directions i2c
    closes #1468

--- sw/airborne/boards/bebop/actuators.c
+++ sw/airborne/boards/bebop/actuators.c
@@ -99,12 +99,12 @@ void actuators_bebop_commit(void)
     // Start the motors
     actuators_bebop.i2c_trans.buf[0] = ACTUATORS_BEBOP_START_PROP;
 #if BEBOP_VERSION2
-    actuators_bebop.i2c_trans.buf[1] = 0b00000110; // For Bebop version 2 some motors are reversed (FIXME: test final version)
-    i2c_transmit(&i2c1, &actuators_bebop.i2c_trans, actuators_bebop.i2c_trans.slave_addr, 2);
+    // For Bebop version 2 some motors are reversed (FIXME: test final version)
+    actuators_bebop.i2c_trans.buf[1] = 0b00000110;
 #else
-    actuators_bebop.i2c_trans.buf[1] = 0b00000000;
-    i2c_transmit(&i2c1, &actuators_bebop.i2c_trans, actuators_bebop.i2c_trans.slave_addr, 1);
+    actuators_bebop.i2c_trans.buf[1] = 0b00000101;
 #endif
+    i2c_transmit(&i2c1, &actuators_bebop.i2c_trans, actuators_bebop.i2c_trans.slave_addr, 2);
   }
   // Stop the motors
   else if (actuators_bebop.i2c_trans.buf[10] == 4 && !autopilot_motors_on) {
