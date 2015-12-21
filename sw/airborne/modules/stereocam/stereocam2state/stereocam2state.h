8b719508111eb3467b8719ce7163a6f0f52b85ba
k.n.mcguire@tudelft.nl <k.n.mcguire@tudelft.nl>
2015-12-21 11:08:07 +0100

    [modules] stereocam2state improvements/fixes
    - changed description stereocam2state and replaced extern gps variable with abi message
    - stereocam2state fixed syntax and added downlink message
    
    closes #1490

--- sw/airborne/modules/stereocam/stereocam2state/stereocam2state.h
+++ sw/airborne/modules/stereocam/stereocam2state/stereocam2state.h
@@ -13,10 +13,23 @@
 #ifndef STEREOCAM2STATE_H
 #define STEREOCAM2STATE_H
 
+#include "state.h"
+
+#include "math/pprz_algebra_int.h"
+#include "math/pprz_algebra_float.h"
+#include "math/pprz_orientation_conversion.h"
+#include <std.h>
+#include "modules/stereocam/stereocam.h"
+
+struct GpsStereoCam {
+  struct EcefCoor_i ecef_vel;
+};
+
+extern struct GpsStereoCam gps_stereocam;
+
 extern void stereo_to_state_init(void);
 extern void stereo_to_state_periodic(void);
-extern void stereo_to_state_start(void);
-extern void stereo_to_state_stop(void);
+
 
 #endif
 
