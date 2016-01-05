47409e0b881dce4cfd157fe353f45bcd1f18fada
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2016-01-05 12:50:43 +0100

    fix typo

--- sw/airborne/peripherals/mpu60x0_spi.c
+++ sw/airborne/peripherals/mpu60x0_spi.c
@@ -176,7 +176,7 @@ bool_t mpu60x0_configure_i2c_slaves(Mpu60x0ConfigSet mpu_set, void *mpu)
       break;
     case MPU60X0_SPI_CONF_SLAVES_CONFIGURE:
       /* configure each slave until all nb_slaves are done */
-      if (mpu_spi->config.nb_slave_init < mpu_spi->config.nb_slaves && mpu_i2c->config.nb_slave_init < MPU60X0_I2C_NB_SLAVES) {
+      if (mpu_spi->config.nb_slave_init < mpu_spi->config.nb_slaves && mpu_spi->config.nb_slave_init < MPU60X0_I2C_NB_SLAVES) {
          // proceed to next slave if configure for current one returns true
         if (mpu_spi->config.slaves[mpu_spi->config.nb_slave_init].configure(mpu_set, mpu)) {
           mpu_spi->config.nb_slave_init++;
