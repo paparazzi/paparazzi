f849db6a018e67f626c3caf295534e57424a63ea
mavlabcourse <mavlabcourse@mavlabcourse.com>
2016-02-07 20:01:40 +0100

    fixed bug, added callback example

--- sw/ground_segment/python/flaskserver/flaskserver.py
+++ sw/ground_segment/python/flaskserver/flaskserver.py
@@ -9,9 +9,10 @@ from flask import Flask
 # file is a reasonable substitute
 PPRZ_SRC = getenv("PAPARAZZI_SRC", path.normpath(path.join(path.dirname(path.abspath(__file__)), '../../../../')))
 sys.path.append(PPRZ_SRC + "/sw/lib/python")
+sys.path.append(PPRZ_SRC + "/sw/ext/pprzlink/lib/v1.0/python")
 PPRZ_HOME = getenv("PAPARAZZI_HOME", PPRZ_SRC)
 from ivy_msg_interface import IvyMessagesInterface
-from pprz_msg.message import PprzMessage
+from pprzlink.message import PprzMessage
 from flask import Flask,Response
 import json
 app = Flask(__name__)
