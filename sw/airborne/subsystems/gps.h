bf47ef1364e36a724d91a5389253b0bd549dd2f8
Felix Ruess <felix.ruess@gmail.com>
2016-02-20 20:01:11 +0100

    [gps] minor dox fixes

--- sw/airborne/subsystems/gps.h
+++ sw/airborne/subsystems/gps.h
@@ -187,16 +187,16 @@ extern uint32_t gps_tow_from_sys_ticks(uint32_t sys_ticks);
 /**
  * Convenience function to get utm position in float from GPS structure.
  * Beware that altitude is initialized to zero but not set to the correct value
- * @param[in] gps pointer to the gps structure
+ * @param[in] gps_s pointer to the gps structure
  * @param[in] zone set the utm zone in which the position should be computed, 0 to try to get it automatically from lla position
  * @return utm position in float
  */
-extern struct UtmCoor_f utm_float_from_gps(struct GpsState *gps, uint8_t zone);
+extern struct UtmCoor_f utm_float_from_gps(struct GpsState *gps_s, uint8_t zone);
 
 /**
  * Convenience function to get utm position in int from GPS structure.
  * Beware that altitude is initialized to zero but not set to the correct value
- * @param[in] gps pointer to the gps structure
+ * @param[in] gps_s pointer to the gps structure
  * @param[in] zone set the utm zone in which the position should be computed, 0 to try to get it automatically from lla position
  * @return utm position in fixed point (cm)
  */
