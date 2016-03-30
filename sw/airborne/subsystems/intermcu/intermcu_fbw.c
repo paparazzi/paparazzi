6727ab54b9bb0e1621ea8a64ac3ff0a367345862
kevindehecker <kevindehecker@hotmail.com>
2016-03-30 09:19:34 +0200

    Fix compile error caused by mistakenly putting autopilot_motor_on in px4 ifdef

--- sw/airborne/subsystems/intermcu/intermcu_fbw.c
+++ sw/airborne/subsystems/intermcu/intermcu_fbw.c
@@ -38,9 +38,10 @@
 static uint8_t px4RebootSequence[] = {0x41, 0xd7, 0x32, 0x0a, 0x46, 0x39};
 static uint8_t px4RebootSequenceCount = 0;
 static bool_t px4RebootTimeout = FALSE;
-uint8_t autopilot_motors_on = FALSE;
 tid_t px4bl_tid; ///< id for time out of the px4 bootloader reset
 #endif
+
+uint8_t autopilot_motors_on = FALSE;
 
 #if RADIO_CONTROL_NB_CHANNEL > 8
 #undef RADIO_CONTROL_NB_CHANNEL
