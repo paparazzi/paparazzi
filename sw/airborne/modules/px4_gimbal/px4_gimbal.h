c3cf920c18ad2646bcf6c20879ccfa6757e1d8af
kevindehecker <kevindehecker@hotmail.com>
2016-03-31 22:17:50 +0200

    [modules] Added gimbal control for PX4
    closes #1592

---
+++ sw/airborne/modules/px4_gimbal/px4_gimbal.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) Kevin van Hecke
+ *
+ * This file is part of paparazzi
+ *
+ */
+/**
+ * @file "modules/px4_gimbal/px4_gimbal.h"
+ * @author Kevin van Hecke
+ * Control gimbal camera axis through px4 from rc
+ */
+
+#ifndef PX4_GIMBAL_H
+#define PX4_GIMBAL_H
+
+ extern void px4_gimbal_init(void);
+ extern void px4_set_gimbal_angle_periodic(void);
+
+#endif
+
