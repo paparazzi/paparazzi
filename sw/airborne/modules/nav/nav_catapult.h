2e3e97d8745cea6ecaa67c11bf3526ae3b416da7
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2016-05-23 11:59:11 +0200

    [nav] rewrite nav_catapult module (#1682)
    * [nav] rewrite nav_catapult module
    
    - state machine more readable
    - don't use imu struct directly
    - more configuration options
    - initial module was not working well anyway (was setting full throttle
      all the time unless high_freq function manually stopped)

--- sw/airborne/modules/nav/nav_catapult.h
+++ sw/airborne/modules/nav/nav_catapult.h
@@ -1,6 +1,6 @@
 /*
- *
  * Copyright (C) 2012, Christophe De Wagter
+ * Copyright (C) 2016, Gautier Hattenberger <gautier.hattenberger@enac.fr>
  *
  * This file is part of paparazzi.
  *
@@ -15,39 +15,62 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with paparazzi; see the file COPYING.  If not, write to
- * the Free Software Foundation, 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
  *
  */
 
 /**
  * @file modules/nav/nav_catapult.h
  * @brief catapult launch timing system
+ *
+ *
+ * - Phase 1: Zero Roll, Climb Pitch, Zero Throttle
+ * - Phase 2: After detecting the Start Acceleration\n
+ *            Zero Roll, Climb Pitch, Full Throttle
+ * - Phase 3: After getting the GPS heading (time based)\n
+ *            Place climb in front of us\n
+ *            GoTo(climb)
  */
 
 #ifndef NAV_CATAPULT_H
 #define NAV_CATAPULT_H
 
 #include "std.h"
-#include "paparazzi.h"
+#include "math/pprz_algebra_float.h"
 
-extern float nav_catapult_motor_delay;
-extern float nav_catapult_acceleration_threshold;
-extern float nav_catapult_heading_delay;
-extern float nav_catapult_initial_pitch;
-extern float nav_catapult_initial_throttle;
+// Take off state machine
+enum nav_catapult_state {
+  NAV_CATAPULT_UNINIT,
+  NAV_CATAPULT_ARMED,
+  NAV_CATAPULT_WAIT_ACCEL,
+  NAV_CATAPULT_MOTOR_ON,
+  NAV_CATAPULT_MOTOR_CLIMB,
+  NAV_CATAPULT_DISARM,
+};
+
+// Internal structure
+struct nav_catapult_struct {
+  enum nav_catapult_state status; ///< current procedure state
+  uint32_t timer;                 ///< internal timer
+  struct FloatVect3 pos;          ///< catapult position
+  float accel_threshold;          ///< acceleration threshold for launch detection (in g)
+  float motor_delay;              ///< delay to start motor after launch detection (in seconds)
+  float heading_delay;            ///< delay to estimate initial heading after launch (in seconds)
+  float initial_pitch;            ///< pitch angle during first take-off phase (in radian)
+  float initial_throttle;         ///< throttle during first take-off phase (in radian)
+};
+
+extern struct nav_catapult_struct nav_catapult;
+
+// Init
+extern void nav_catapult_init(void);
 
 // Module Code
-void nav_catapult_highrate_module(void);
+extern void nav_catapult_highrate_module(void);
 
 // Flightplan Code
-extern bool nav_catapult_setup(void);
-
-extern bool nav_catapult_arm(void);
-extern bool nav_catapult_run(uint8_t _to, uint8_t _climb);
-extern bool nav_catapult_disarm(void);
-
-extern bool nav_select_touch_down(uint8_t _td);
+extern bool nav_catapult_run(uint8_t _climb);
 
 #endif
+
