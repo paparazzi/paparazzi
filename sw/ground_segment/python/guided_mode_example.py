286b5f3d738d29f6642ad16853ec3bbdbcf14e5f
kirkscheper <kirkscheper@gmail.com>
2016-06-10 12:01:33 +0200

    update guided mode python

--- sw/ground_segment/python/guided_mode_example.py
+++ sw/ground_segment/python/guided_mode_example.py
@@ -91,7 +91,7 @@ class Guidance(object):
         """
         msg = PprzMessage("datalink", "GUIDED_SETPOINT_NED")
         msg['ac_id'] = self.ac_id
-        msg['flags'] = 0x0C
+        msg['flags'] = 0x0D
         msg['x'] = north
         msg['y'] = east
         msg['z'] = down
@@ -143,20 +143,22 @@ class Guidance(object):
 
 
 if __name__ == '__main__':
-    ac_id = 33
+    ac_id = 11
     try:
         g = Guidance(ac_id)
         sleep(0.1)
         g.set_guided_mode()
         sleep(0.2)
-        g.goto_ned(north=10.0, east=5.0, down=-5.0, heading=radians(90))
+        g.goto_ned(north=2.0, east=2.0, down=-3.0, heading=radians(90))
         sleep(10)
-        g.goto_ned_relative(north=-5.0, east=-5.0, down=-2.0, yaw=-radians(45))
+        g.goto_ned_relative(north=-2.0, east=-2.0, down=1.0, yaw=-radians(45))
         sleep(10)
-        g.goto_body_relative(forward=1.0, right=0.0, down=0.0)
+        g.goto_body_relative(forward=0.0, right=1.0, down=0.0)
         sleep(10)
-        g.move_at_body_vel(forward=3.0)
-        sleep(10)
+        g.move_at_ned_vel(north=0.5)
+        sleep(3)
+        g.move_at_body_vel(forward=-0.5)
+        sleep(3)
         g.set_nav_mode()
         sleep(0.2)
     except KeyboardInterrupt:
