9a5f4656033e67f8422287ce4a9bc1399e87186a
Felix Ruess <felix.ruess@gmail.com>
2016-07-07 12:59:42 +0200

    [plotter] remove average and stddev from curve menu
    See #1445
    updates were disabled already due to #1446 and not displayed on recent systems #1445

--- sw/logalizer/plotter.ml
+++ sw/logalizer/plotter.ml
@@ -467,23 +467,27 @@ let rec plot_window = fun window ->
     ignore (discrete_item#connect#toggled ~callback);
 
     (* Average *)
-    let average_value = GMisc.label ~text:"N/A" () in
-    let _avg_item = submenu_fact#add_image_item ~image:average_value#coerce ~label:"Average" () in
     (* on Ubuntu 14.04 with Unity: updating the menu often results in high CPU and memory usage of `hud-service`,
        even to the point where the PC becomes unusable, so we disable these updates:
        https://github.com/paparazzi/paparazzi/issues/1446
        Also the images/labels are currently not displayed anymore anyway:
        https://github.com/paparazzi/paparazzi/issues/1445 *)
-    (*let update_avg_item = fun () ->
+    (*
+    (* Average *)
+    let average_value = GMisc.label ~text:"N/A" () in
+    let _avg_item = submenu_fact#add_image_item ~image:average_value#coerce ~label:"Average" () in
+    let update_avg_item = fun () ->
       average_value#set_text (sprintf "%.6f" curve.average#value) in
-    ignore (curve.average#connect#value_changed update_avg_item);*)
+    ignore (curve.average#connect#value_changed update_avg_item);
 
     (* Standard deviation *)
     let stdev_value = GMisc.label ~text:"N/A" () in
     let _item = submenu_fact#add_image_item ~image:stdev_value#coerce ~label:"Stdev" () in
-    (*let update_stdev_value = fun () ->
+    let update_stdev_value = fun () ->
       stdev_value#set_text (sprintf "%.6f" curve.stdev#value) in
-    ignore (curve.stdev#connect#value_changed update_stdev_value)*)
+    ignore (curve.stdev#connect#value_changed update_stdev_value)
+    *)
+
     ()
   in
 
