daa5aef9d41286eb05e2b33191cc77c6fef85429
lplp8899 <P.Lu-1@tudelft.nl>
2016-07-07 15:28:58 +0200

    added detect contour

---
+++ sw/airborne/modules/computer_vision/detect_contour.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) Peng Lu
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file "modules/computer_vision/cv_opencvdemo.h"
+ * @author Peng Lu
+ * opencv
+ */
+
+#ifndef DETECT_CONTOUR_H
+#define DETECT_CONTOUR_H
+
+extern void opencvdemo_init(void);
+
+#endif
+
+
+
