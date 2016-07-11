b1ea8b595dd8447d0c6bcf2ff14a589e2713f4dc
podhrmic <michal.podhradsky@aggiemail.usu.edu>
2016-07-11 15:30:55 -0700

    Wind direction finally OK

--- sw/airborne/modules/nav/nav_smooth.c
+++ sw/airborne/modules/nav/nav_smooth.c
@@ -160,15 +160,15 @@ static inline float ground_speed_of_course(float x)
 /* Compute the ground speed for courses 0, 360/NB_ANGLES, ...
    (NB_ANGLES-1)360/NB_ANGLES */
 static void compute_ground_speed(float airspeed,
-                                 float wind_north,
-                                 float wind_east)
+                                 float wind_east,
+                                 float wind_north)
 {
   uint8_t i;
   float alpha = 0;
-  float c = wind_east * wind_east + wind_north * wind_north - airspeed * airspeed;
+  float c = wind_north * wind_north + wind_east * wind_east - airspeed * airspeed;
   for (i = 0; i < NB_ANGLES; i++, alpha += ANGLE_STEP) {
     /* g^2 -2 scal g + c = 0 */
-    float scal = wind_east * cos(alpha) + wind_north * sin(alpha);
+    float scal = wind_north * cos(alpha) + wind_east * sin(alpha);
     float delta = 4 * (scal * scal - c);
     ground_speeds[i] = scal + sqrt(delta) / 2.;
     Bound(ground_speeds[i], NOMINAL_AIRSPEED / 4, 2 * NOMINAL_AIRSPEED);
@@ -194,8 +194,8 @@ bool snav_on_time(float nominal_radius)
   /* Recompute ground speeds every 10 s */
   if (ground_speed_timer == 0) {
     ground_speed_timer = 40; /* every 10s, called at 40Hz */
-    compute_ground_speed(airspeed, stateGetHorizontalWindspeed_f()->x,
-                         stateGetHorizontalWindspeed_f()->y); // Wind in NED frame
+    compute_ground_speed(airspeed, stateGetHorizontalWindspeed_f()->y,
+                         stateGetHorizontalWindspeed_f()->x); // Wind in NED frame
   }
   ground_speed_timer--;
 
