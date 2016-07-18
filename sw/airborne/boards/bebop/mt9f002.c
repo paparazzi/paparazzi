fab6f2010ff10823a9f0a4142ae38ae56bd00ac4
Roland Meertens <rolandmeertens@gmail.com>
2016-07-18 17:17:26 +0200

    changed name of parameters

--- sw/airborne/boards/bebop/mt9f002.c
+++ sw/airborne/boards/bebop/mt9f002.c
@@ -43,18 +43,18 @@
 /* Camera structure */
 struct video_config_t front_camera = {
   .output_size = {
-    .w = MT9V117_OUTPUT_WIDTH,
-    .h = MT9V117_OUTPUT_HEIGHT
+    .w = MT9F002_OUTPUT_WIDTH,
+    .h = MT9F002_OUTPUT_HEIGHT
   },
   .sensor_size = {
-    .w = MT9V117_OUTPUT_WIDTH,
-    .h = MT9V117_OUTPUT_HEIGHT,
+    .w = MT9F002_OUTPUT_WIDTH,
+    .h = MT9F002_OUTPUT_HEIGHT,
   },
   .crop = {
     .x = 0,
     .y = 0,
-    .w = MT9V117_OUTPUT_WIDTH,
-    .h = MT9V117_OUTPUT_HEIGHT
+    .w = MT9F002_OUTPUT_WIDTH,
+    .h = MT9F002_OUTPUT_HEIGHT
   },
   .dev_name = "/dev/video1",
   .subdev_name = "/dev/v4l-subdev1",
