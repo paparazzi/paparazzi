8d5404c853402b258b503774e747344eead8f1e8
Wilco Schoneveld <schoneveld.wj@gmail.com>
2016-09-02 10:50:48 +0200

    [detector] changed minimum area needed to 250 pixels

--- sw/airborne/modules/computer_vision/opencv_imav_landingpad.cpp
+++ sw/airborne/modules/computer_vision/opencv_imav_landingpad.cpp
@@ -82,7 +82,7 @@ struct results opencv_imav_landing(char *img, int width, int height, int v_squar
     for( int i = 0; (unsigned)i < contours.size(); i++ )
     {
         double Area = contourArea(contours[i]);
-        if (Area > 1500)
+        if (Area > 250)
         {
             convexHull(Mat(contours[i]), hull[i], false );
             approxPolyDP(Mat(hull[i]), approx[i], arcLength(Mat(hull[i]), true)*0.1, true);
