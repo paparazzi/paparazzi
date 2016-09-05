f0cdd46b365a27267bf7f5a661ea09696ca4ca6d
Wilco Schoneveld <schoneveld.wj@gmail.com>
2016-09-05 12:49:17 +0200

    [detector] added feature tracking module to opencv

--- sw/airborne/modules/computer_vision/marker/marker_checkers.cpp
+++ sw/airborne/modules/computer_vision/marker/marker_checkers.cpp
@@ -1,18 +1,27 @@
 
 using namespace std;
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/imgcodecs/imgcodecs.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/features2d.hpp>
 using namespace cv;
 #include "modules/computer_vision/opencv_image_functions.h"
 
 #include "marker_checkers.h"
 
 Mat img_marker;
+Ptr<ORB> detector;
 
 void init_detect_checkers(void) {
 
     img_marker = imread("/data/ftp/internal_000/imav/marker_checkers.png", IMREAD_GRAYSCALE);
+
+    detector = ORB::create();
+    detector->setMaxFeatures(20);
+
+    std::vector<KeyPoint> keypoints;
+
+    detector->detect(img_marker, keypoints);
 }
 
 
@@ -25,7 +34,13 @@ struct resultsc opencv_detect_checkers(char *img, int width, int height) {
 
     cvtColor(M, image, CV_YUV2GRAY_Y422);
 
-    circle(image, Point(120, 120), 20, 200, 10);
+    std::vector<KeyPoint> keypoints;
+
+    detector->detect(image, keypoints);
+
+    drawKeypoints(image, keypoints, image, Scalar::all(-1), DrawMatchesFlags::DEFAULT);
+
+    cvtColor(image, image, CV_BGR2GRAY);
 
     grayscale_opencv_to_yuv422(image, img, width, height);
 
