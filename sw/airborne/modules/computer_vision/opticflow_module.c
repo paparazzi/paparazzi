5792ad86911ad652947a78ba327a2949a2d89ef9
k.n.mcguire@tudelft.nl <k.n.mcguire@tudelft.nl>
2016-09-14 09:56:13 +0200

    [pprzlink] removed extra items of OPTIC_FLOW_EST message

--- sw/airborne/modules/computer_vision/opticflow_module.c
+++ sw/airborne/modules/computer_vision/opticflow_module.c
@@ -78,7 +78,7 @@ static void opticflow_telem_send(struct transport_tx *trans, struct link_device 
                                  &opticflow_result.tracked_cnt, &opticflow_result.flow_x,
                                  &opticflow_result.flow_y, &opticflow_result.flow_der_x,
                                  &opticflow_result.flow_der_y, &opticflow_result.vel_x,
-                                 &opticflow_result.vel_y, &opticflow_result.vel_body_x, & opticflow_result.vel_body_y, &opticflow_result.div_size,
+                                 &opticflow_result.vel_y, &opticflow_result.div_size,
                                  &opticflow_result.surface_roughness, &opticflow_result.divergence); // TODO: no noise measurement here...
   }
   pthread_mutex_unlock(&opticflow_mutex);
