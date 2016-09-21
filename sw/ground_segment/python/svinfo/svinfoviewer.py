b6f9ad1c1266c4270b465a44bf5a2f42fcffa4fc
Christophe De Wagter <dewagter@gmail.com>
2016-09-21 05:44:32 +0200

    SVNINFO resize + colors + used

--- sw/ground_segment/python/svinfo/svinfoviewer.py
+++ sw/ground_segment/python/svinfo/svinfoviewer.py
@@ -6,20 +6,27 @@ import time
 import threading
 import math
 import pynotify
-#import pygame.mixer
 
 sys.path.append(os.getenv("PAPARAZZI_HOME") + "/sw/ext/pprzlink/lib/v1.0/python")
 
 from pprzlink.ivy import IvyMessagesInterface
 
 WIDTH = 300
-HEIGHT = 300
-
 BARH = 140
 CHANNEL = 16
 
-#UPDATE_INTERVAL = 250
 
+def QIColour(qi):
+    return {
+        0: wx.Colour(64,64,64),  # This channel is idle
+        1: wx.Colour(128,128,128),  # Searching
+        2: wx.Colour(0,128,128),  # Signal aquired
+        3: wx.Colour(255,0,0),  # Signal detected but unusable
+        4: wx.Colour(0,0,255),  # Code Lock on Signal
+        5: wx.Colour(0,255,0),  # Code and Carrier locked
+        6: wx.Colour(0,255,0),  # Code and Carrier locked
+        7: wx.Colour(0,255,0),  # Code and Carrier locked
+    }[qi]
 
 class SvChannel(object):
     def __init__(self, chn, msg):
@@ -37,24 +44,27 @@ class SVInfoFrame(wx.Frame):
         if msg.name == "SVINFO":
             chn = int(msg['chn'])
             self.sv[chn] = SvChannel(chn, msg)
-                
-            #self.gui_update();
-            #print('----------------------------')
-            #for s in self.sv:
-            #    e = self.sv[s]
-            #    print(s,e.QI, e.CNO, e.SVID)
             wx.CallAfter(self.update)
 
     def update(self):
         self.Refresh()
 
-    def OnPaint(self, e):
-        w = WIDTH
-        h = HEIGHT
+    def OnSize(self, event):
+        self.w = event.GetSize()[0]
+        self.h = event.GetSize()[1]
+        self.Refresh()
 
+    def OnPaint(self, e):
         tdx = -5
         tdy = -7
         th = 15
+
+        w = self.w
+        h = self.w
+        if h < self.w + 50:
+            h = self.w + 50
+        bar = self.h-w-th-th
+
 
         dc = wx.PaintDC(self) 
         brush = wx.Brush("white")  
@@ -63,26 +73,24 @@ class SVInfoFrame(wx.Frame):
         
         # Background
         dc.SetBrush(wx.Brush(wx.Colour(0,0,0), wx.TRANSPARENT)) 
-        dc.DrawCircle(w/2,h/2,w/2-1) 
-        dc.DrawCircle(w/2,h/2,w/4-1) 
-        dc.DrawCircle(w/2,h/2,1) 
+        dc.DrawCircle(w/2,w/2,w/2-1) 
+        dc.DrawCircle(w/2,w/2,w/4-1) 
+        dc.DrawCircle(w/2,w/2,1) 
         font = wx.Font(11, wx.ROMAN, wx.BOLD, wx.NORMAL) 
         dc.SetFont(font)
 
         dc.DrawText("N",w/2+tdx,2) 
-        dc.DrawText("S",w/2+tdx,h-17) 
-        dc.DrawText("E",w-15,h/2+tdy) 
-        dc.DrawText("W",2,h/2+tdy) 
+        dc.DrawText("S",w/2+tdx,w-17) 
+        dc.DrawText("E",w-15,w/2+tdy) 
+        dc.DrawText("W",2,w/2+tdy) 
 
         # SV
         for chn in self.sv:
             sv = self.sv[chn]
 
-            c = wx.Colour(255,0,0)
-            if (sv.QI > 0):
-                c = wx.Colour(0,128,128)
-                if (sv.QI >= 7):
-                    c = wx.Colour(0,255,0)
+            c = QIColour(sv.QI)
+            used = sv.Flags & 1
+            s = 7 + used * 5
 
             el = float(sv.Elev) / 90.0 * float(w) / 2.0
             az = float(sv.Azim) * math.pi / 180.0
@@ -91,22 +99,27 @@ class SVInfoFrame(wx.Frame):
             x = float(w)/2.0 + math.sin(az) * el
 
             dc.SetBrush(wx.Brush(c, wx.SOLID)) 
-            dc.DrawCircle(int(x),int(y),10) 
+            dc.DrawCircle(int(x),int(y),s) 
 		
             font = wx.Font(8, wx.ROMAN, wx.NORMAL, wx.NORMAL) 
             dc.SetFont(font) 
             dc.DrawText(str(sv.SVID),x+tdx,y+tdy)
 
-            h = float(BARH-th-th) * float(sv.CNO) / 55.0
-            dc.DrawRectangle(w/CHANNEL*chn,HEIGHT+BARH-th-th-h,w/CHANNEL-2,h)
-            dc.DrawText(str(chn),w/CHANNEL*chn,HEIGHT+BARH-th-th)
-            dc.DrawText(str(sv.CNO),w/CHANNEL*chn,HEIGHT+BARH-th)
+            bh = float(bar-th-th) * float(sv.CNO) / 55.0
+            dc.DrawRectangle(w/CHANNEL*chn+5 * (1-used),self.h-th-bh,w/CHANNEL-2 - 10 * (1-used),bh)
+            dc.DrawText(str(chn),w/CHANNEL*chn,self.h-th)
+            dc.DrawText(str(sv.CNO),w/CHANNEL*chn,self.h-bar)
 		
 
     def __init__(self):
+
+        self.w = WIDTH
+        self.h = WIDTH + BARH
+
         wx.Frame.__init__(self, id=-1, parent=None, name=u'SVInfoFrame',
-                          size=wx.Size(WIDTH, HEIGHT+BARH), title=u'SV Info')
+                          size=wx.Size(self.w, self.h), title=u'SV Info')
         self.Bind(wx.EVT_PAINT, self.OnPaint) 
+        self.Bind(wx.EVT_SIZE, self.OnSize)
         self.Bind(wx.EVT_CLOSE, self.OnClose)
 
         self.sv = {}
