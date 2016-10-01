cf09c5a98f51870649b3d52166aac092cdc45b73
Christophe De Wagter <dewagter@gmail.com>
2016-10-01 16:08:30 +0200

    [sideslip] measure sideslip from lateral force

---
+++ sw/airborne/modules/sideslip/sideslip.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) C. De Wagter
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file "modules/sideslip/sideslip.h"
+ * @author C. De Wagter
+ * sideslip-measurements
+ */
+
+#ifndef SIDESLIP_H
+#define SIDESLIP_H
+
+extern void sideslip_init(void);
+extern void sideslip_periodic(void);
+
+#endif
+
