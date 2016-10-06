7267c2e4de8ca70b3ccd541c4a136fb645359630
Christophe De Wagter <dewagter@gmail.com>
2016-10-06 12:58:55 +0200

    [svinfo] pep8 and header

--- sw/ground_segment/python/svinfo/svinfo.py
+++ sw/ground_segment/python/svinfo/svinfo.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+#
+# Copyright (C) 2016 TUDelft
+#
+# This file is part of paparazzi.
+#
+# paparazzi is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# paparazzi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with paparazzi.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 import wx
 import svinfoviewer
