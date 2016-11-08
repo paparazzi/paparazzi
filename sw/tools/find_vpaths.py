9f83d4c4947ac83a9a2c7a0d82272bff2b07ab3e
kevindehecker <kevindehecker@hotmail.com>
2016-11-08 08:11:04 +0100

    Fixed indent

--- sw/tools/find_vpaths.py
+++ sw/tools/find_vpaths.py
@@ -17,40 +17,40 @@ srcs = list()
 cflags = list()
 mode = 0
 for i in range(2,len(sys.argv)):
-  vpath = sys.argv[i]
-  if vpath == '+':
-    mode = mode + 1
-  else:
-    if mode == 0:
-      vpaths.append(vpath)
-    elif mode == 1:
-      srcs.append(sys.argv[i])
-    elif mode == 2:
-      cflags.append(sys.argv[i])
+    vpath = sys.argv[i]
+    if vpath == '+':
+        mode = mode + 1
+    else:
+        if mode == 0:
+            vpaths.append(vpath)
+        elif mode == 1:
+            srcs.append(sys.argv[i])
+        elif mode == 2:
+            cflags.append(sys.argv[i])
 
 #create the command for gcc -MM cflags srcs
 cmd = cc + ' -MM '
 for flg in cflags:
-  qflg = "" # append all quotes with escape char
-  for i in range(0,len(flg)):
-    if flg[i] == "\"":
-      qflg = qflg +  "\\\""
-    else:
-      qflg = qflg + flg[i]
-  cmd = cmd + qflg + ' '
+    qflg = "" # append all quotes with escape char
+    for i in range(0,len(flg)):
+        if flg[i] == "\"":
+            qflg = qflg +  "\\\""
+        else:
+            qflg = qflg + flg[i]
+    cmd = cmd + qflg + ' '
 
 for i in range(0,len(srcs)):
-  f = srcs[i]
-  found = 0
-  for vpath in vpaths:
-    if os.path.isfile(path.join(vpath, f)) :
-      cmd = cmd + path.join(vpath, f) + ' '
-      found = found +1
-      break
-  if found == 0:
-    print("ERROR, could not find: " + f)
-  if found > 1:
-    print("ERROR, found more than once: " + f)
+    f = srcs[i]
+    found = 0
+    for vpath in vpaths:
+        if os.path.isfile(path.join(vpath, f)) :
+            cmd = cmd + path.join(vpath, f) + ' '
+            found = found +1
+            break
+    if found == 0:
+        print("ERROR, could not find: " + f)
+    if found > 1:
+        print("ERROR, found more than once: " + f)
 
 #call gcc
 os.system(cmd)
