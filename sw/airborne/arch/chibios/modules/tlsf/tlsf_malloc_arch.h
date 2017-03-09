5bdcbe05d2360cc7f84b77add2da8b8075b86d38
Alexandre Bustico <alexandre.bustico@enac.fr>
2017-03-09 08:51:20 +0100

    raise dynamic ram pool size and sdLog ram cache size on STM32F7 (chimera)

--- sw/airborne/arch/chibios/modules/tlsf/tlsf_malloc_arch.h
+++ sw/airborne/arch/chibios/modules/tlsf/tlsf_malloc_arch.h
@@ -29,20 +29,17 @@
 #define TLSF_MALLOC_ARCH_H
 #include <ch.h>
 #include <hal.h>
+#include "mcu_periph/ram_arch.h"
 
-#if defined STM32F4XX
-#define NODMA_SECTION ".ram4"
-#define DMA_SECTION ".ram0"
-#elif  defined STM32F7XX
-#define NODMA_SECTION ".ram0"
-#define DMA_SECTION ".ram3"
-#else
-#error "section defined only for STM32F4 and STM32F7"
-#endif
 
 #define HEAP_CCM          ccmHeap
+// F7 has more than twice RAM than others families
+#if defined STM32F7XX
+#define HEAP_CCM_SIZE     65536
+#else 
 #define HEAP_CCM_SIZE     16384
-#define HEAP_CCM_SECTION  NODMA_SECTION
+#endif
+#define HEAP_CCM_SECTION  FAST_SECTION
 
 #define HEAP_DEFAULT      HEAP_CCM
 
