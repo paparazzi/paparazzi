79c73d07288523d4750bf7bc619da97c32fe9a8d
Hector Garcia de Marina <hgdemarina@gmail.com>
2017-10-18 17:06:05 +0200

    Init tables from GCS

---
+++ sw/ground_segment/python/collective_tracking_control/ctcInitTables.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import time
+import sys
+import wx
+import numpy as np
+import sys
+from os import path, getenv
+PPRZ_HOME = getenv("PAPARAZZI_HOME", path.normpath(path.join(path.dirname(path.abspath(__file__)), '../../../../')))
+PPRZ_SRC = getenv("PAPARAZZI_SRC", path.normpath(path.join(path.dirname(path.abspath(__file__)), '../../../../')))
+sys.path.append(PPRZ_SRC + "/sw/lib/python")
+sys.path.append(PPRZ_HOME + "/var/lib/python")
+from pprzlink.ivy import IvyMessagesInterface
+from pprzlink.message import PprzMessage 
+from settings_xml_parse import PaparazziACSettings
+
+list_ids = []
+interface = IvyMessagesInterface("CTC")
+
+if len(sys.argv) != 4:
+    print("Usage: dcfInitTables ids.txt speed_ref heading_ref")
+    interface.shutdown()
+    exit()
+
+ids = np.loadtxt(sys.argv[1])
+list_ids = np.ndarray.tolist(ids)
+speed_ref = sys.argv[2]
+heading_ref = sys.argv[3]
+
+for i in list_ids:
+    msg_clean = PprzMessage("datalink", "CTC_CLEAN_TABLE")
+    msg_clean['ac_id'] = int(i)
+
+    print(msg_clean)
+    interface.send(msg_clean)
+
+for i in list_ids:
+    msg = PprzMessage("datalink", "CTC_REG_TABLE")
+    msg['ac_id'] = int(i)
+    for ii in list_ids:
+        if(i != ii):
+            msg['nei_id'] = int(ii)
+            msg['desired_speed'] = int(speed_ref)*100
+            msg['desired_theta'] = int(heading_ref)*10
+
+    print(msg)
+    interface.send(msg)
+
+time.sleep(2)
+interface.shutdown()
