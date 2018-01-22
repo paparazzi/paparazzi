b18335e53c227e4677e97604c1d221ee1ac74c75
Titus <titusbraber@gmail.com>
2018-01-22 14:29:53 +0100

    restored size div

--- sw/airborne/modules/computer_vision/opticflow/size_divergence.c
+++ sw/airborne/modules/computer_vision/opticflow/size_divergence.c
@@ -114,9 +114,8 @@ float get_size_divergence(struct flow_t *vectors, int count, int n_samples)
     }
   }
 
-  float mean_divergence;
-  // calculate the mean divergence or use 0 when used_samples is still 0
-  mean_divergence = mean_f(divs, used_samples);
+  // calculate the mean divergence:
+  float mean_divergence = mean_f(divs, used_samples);
 
   // free the memory of divs:
   free(divs);
