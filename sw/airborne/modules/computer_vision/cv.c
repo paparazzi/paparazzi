38666cf297d8410fb5f2ed503371e4928b3a20c1
Tom van Dijk <tomvand@users.noreply.github.com>
2018-05-30 18:10:10 +0200

    Fix #2187

--- sw/airborne/modules/computer_vision/cv.c
+++ sw/airborne/modules/computer_vision/cv.c
@@ -113,13 +113,13 @@ int8_t cv_async_function(struct cv_async *async, struct image_t *img)
   if (async->img_copy.buf_size == 0) {
     image_create(&async->img_copy, img->w, img->h, img->type);
   }
-  if (async->img_copy.buf_size != img->buf_size) {
-    printf("[cv_async_function] BUFFER SIZE CHANGED!\n");
-    printf("Old:\t%d x %d\n", async->img_copy.w, async->img_copy.h);
-    printf("New:\t%d x %d\n", img->w, img->h);
+#if CV_ALLOW_VIDEO_TO_CHANGE_SIZE
+  // Note: must be enabled explicitly as not all modules may support this. (See issue #2187)
+  if (img->buf_size > async->img_copy.buf_size) {
     image_free(&async->img_copy);
     image_create(&async->img_copy, img->w, img->h, img->type);
   }
+#endif
 
   // Copy image
 // TODO:this takes time causing some thread lag, should be replaced with gpu operation
