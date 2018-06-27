ee87f82e93eaf8f3b92dbb3f4af5618c367e8fd2
kirkscheper <kirkscheper@gmail.com>
2018-06-27 14:22:27 +0200

    [mcu_periph] add full duplex named pipe

---
+++ sw/airborne/mcu_periph/pipe.c
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2018 Kirk Scheper <kirkscheper@gmail.com>
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+/** \file mcu_periph/pipe.c
+ *  \brief arch independent PIPE API
+ *
+ */
+
+#include "mcu_periph/pipe.h"
+
+#include <string.h>
+
+/* Print the configurations */
+#if USE_PIPE0_WRITER || USE_PIPE0_READER
+struct pipe_periph pipe0;
+#endif
+
+#if USE_PIPE0_WRITER
+PRINT_CONFIG_VAR(USE_PIPE0_WRITER)
+#endif
+
+#if USE_PIPE0_READER
+PRINT_CONFIG_VAR(USE_PIPE0_READER)
+#endif
+
+#if USE_PIPE1_WRITER || USE_PIPE1_READER
+struct pipe_periph pipe1;
+#endif
+
+#if USE_PIPE1_WRITER
+PRINT_CONFIG_VAR(USE_PIPE1_WRITER)
+#endif
+
+#if USE_PIPE1_READER
+PRINT_CONFIG_VAR(USE_PIPE1_READER)
+#endif
+
+#if USE_PIPE2_WRITER || USE_PIPE2_READER
+struct pipe_periph pipe2;
+#endif
+
+#if USE_PIPE2_WRITER
+PRINT_CONFIG_VAR(USE_PIPE2_WRITER)
+#endif
+
+#if USE_PIPE2_READER
+PRINT_CONFIG_VAR(USE_PIPE2_READER)
+#endif
+
+/**
+ * Initialize the PIPE peripheral
+ */
+void pipe_periph_init(struct pipe_periph *p, char *read_name, char *write_name)
+{
+  p->rx_insert_idx = 0;
+  p->rx_extract_idx = 0;
+  p->tx_insert_idx = 0;
+  p->device.periph = (void *)p;
+  p->device.check_free_space = (check_free_space_t) pipe_check_free_space;
+  p->device.put_byte = (put_byte_t) pipe_put_byte;
+  p->device.put_buffer = (put_buffer_t) pipe_put_buffer;
+  p->device.send_message = (send_message_t) pipe_send_message;
+  p->device.char_available = (char_available_t) pipe_char_available;
+  p->device.get_byte = (get_byte_t) pipe_getch;
+
+  pipe_arch_periph_init(p, read_name, write_name);
+}
+
+/**
+ * Check if there is enough free space in the transmit buffer.
+ * @param p   pointer to PIPE peripheral
+ * @param len how many bytes of free space to check for
+ * @return TRUE if enough space for len bytes
+ */
+bool WEAK pipe_check_free_space(struct pipe_periph *p, long *fd __attribute__((unused)), uint16_t len)
+{
+  return (PIPE_TX_BUFFER_SIZE - p->tx_insert_idx) >= len;
+}
+
+/**
+ * Add one data byte to the tx buffer.
+ * @param p    pointer to PIPE peripheral
+ * @param data byte to add to tx buffer
+ */
+void WEAK pipe_put_byte(struct pipe_periph *p, long fd __attribute__((unused)), uint8_t data)
+{
+  if (p->tx_insert_idx >= PIPE_TX_BUFFER_SIZE) {
+    return;  // no room
+  }
+
+  p->tx_buf[p->tx_insert_idx] = data;
+  p->tx_insert_idx++;
+}
+
+void WEAK pipe_put_buffer(struct pipe_periph *p, long fd __attribute__((unused)), const uint8_t *data, uint16_t len)
+{
+  if (p->tx_insert_idx + len >= PIPE_TX_BUFFER_SIZE) {
+    return;  // no room
+  }
+
+  memcpy(&(p->tx_buf[p->tx_insert_idx]), data, len);
+  p->tx_insert_idx += len;
+}
---
+++ sw/airborne/mcu_periph/pipe.h
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2018 Kirk Scheper <kirkscheper@gmail.com>
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+/** \file mcu_periph/pipe.h
+ *  \brief arch independent PIPE API
+ *
+ */
+
+#ifndef MCU_PERIPH_PIPE_H
+#define MCU_PERIPH_PIPE_H
+
+#include "std.h"
+#include "mcu_periph/pipe_arch.h"
+#include "pprzlink/pprzlink_device.h"
+
+struct pipe_periph {
+  /** Receive buffer */
+  uint8_t rx_buf[PIPE_RX_BUFFER_SIZE];
+  uint16_t rx_insert_idx;
+  uint16_t rx_extract_idx;
+  /** Transmit buffer */
+  uint8_t tx_buf[PIPE_TX_BUFFER_SIZE];
+  uint16_t tx_insert_idx;
+  /** PIPE file descriptor */
+  int fd_read;
+  int fd_write;
+  /** Generic device interface */
+  struct link_device device;
+};
+
+extern void     pipe_arch_init(void);
+extern void     pipe_arch_periph_init(struct pipe_periph *p, char *read_name, char* write_name);
+extern void     pipe_periph_init(struct pipe_periph *p, char *name, char* write_name);
+extern uint16_t pipe_char_available(struct pipe_periph *p);
+extern uint8_t  pipe_getch(struct pipe_periph *p);
+extern void     pipe_receive(struct pipe_periph *p);
+extern void     pipe_send_message(struct pipe_periph *p, long fd);
+extern void     pipe_send_raw(struct pipe_periph *p, long fd, uint8_t *buffer, uint16_t size);
+extern bool     pipe_check_free_space(struct pipe_periph *p, long *fd, uint16_t len);
+extern void     pipe_put_byte(struct pipe_periph *p, long fd, uint8_t data);
+extern void     pipe_put_buffer(struct pipe_periph *p, long fd, const uint8_t *data,uint16_t len);
+
+#if USE_PIPE0_WRITER || USE_PIPE0_READER
+extern struct pipe_periph pipe0;
+
+#ifndef USE_PIPE0_WRITER
+#define USE_PIPE0_WRITER NULL
+#endif
+
+#ifndef USE_PIPE0_READER
+#define USE_PIPE0_READER NULL
+#endif
+
+#define PIPE0Init() pipe_periph_init(&pipe0, STRINGIFY(USE_PIPE0_READER), STRINGIFY(USE_PIPE0_WRITER))
+#endif // USE_PIPE0
+
+#if USE_PIPE1_WRITER || USE_PIPE1_READER
+extern struct pipe_periph pipe1;
+
+#ifndef USE_PIPE1_WRITER
+#define USE_PIPE1_WRITER NULL
+#endif
+
+#ifndef USE_PIPE1_READER
+#define USE_PIPE1_READER NULL
+#endif
+
+#define PIPE1Init() pipe_periph_init(&pipe1, STRINGIFY(USE_PIPE1_READER), STRINGIFY(USE_PIPE1_WRITER))
+#endif // USE_PIPE1
+
+#if USE_PIPE2_WRITER || USE_PIPE2_READER
+extern struct pipe_periph pipe2;
+
+#ifndef USE_PIPE2_WRITER
+#define USE_PIPE2_WRITER NULL
+#endif
+
+#ifndef USE_PIPE2_READER
+#define USE_PIPE2_READER NULL
+#endif
+
+#define PIPE2Init() pipe_periph_init(&pipe2, STRINGIFY(USE_PIPE2_READER), STRINGIFY(USE_PIPE2_WRITER))
+#endif // USE_PIPE2
+
+#endif /* MCU_PERIPH_PIPE_H */
