b2dc87a22eb140270385974a4af16b9befa9e4d7
guidoAI <guido.de.croon@gmail.com>
2018-09-12 13:41:02 +0200

    changed makefile to compile with gnu99 and modified the documentation

--- sw/airborne/math/RANSAC.c
+++ sw/airborne/math/RANSAC.c
@@ -25,6 +25,11 @@
  * The concept is to select (minimal) subsets of the samples for learning
  * a model, then determining the error on the entire data set, and selecting the best fit. In determining the error, the individual sample errors
  * are capped, so that outliers can be identified and have only a modest influence on the results.
+ *
+ * Read: Fischler, M. A., & Bolles, R. C. (1981). Random sample consensus: a paradigm for model fitting with applications to image analysis and automated cartography.
+ * Communications of the ACM, 24(6), 381-395.
+ *
+ * This file depends on the function fit_linear_model in math/pprz_matrix_decomp_float.h/c
  */
 
 
@@ -119,6 +124,7 @@ void RANSAC_linear_model(int n_samples, int n_iterations, float error_threshold,
  * @param[in] sample The sample vector of size D
  * @param[in] weights The weight vector of size D+1
  * @param[in] D The dimension of the sample.
+ * @return The predicted value
  */
 float predict_value(float *sample, float *weights, int D, bool use_bias)
 {
@@ -139,7 +145,7 @@ float predict_value(float *sample, float *weights, int D, bool use_bias)
 
 /** Get indices without replacement.
  *
- * @param[in] indices_subset This will be filled with the sampled indices
+ * @param[out] indices_subset This will be filled with the sampled indices
  * @param[in] n_samples The number of samples / indices.
  * @param[in] count The function will sample n_sample numbers from the range 1, 2, 3,..., count
  */
