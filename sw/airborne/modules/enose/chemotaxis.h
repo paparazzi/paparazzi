f0974eebe9c73a2312b150516d38185e1fa84cca
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2018-09-13 15:38:27 +0200

    [cleaning] remove ENOSE module

--- sw/airborne/modules/enose/chemotaxis.h
+++
@@ -1,9 +0,0 @@
-#ifndef CHEMOTAXIS_H
-#define CHEMOTAXIS_H
-
-#include "std.h"
-
-extern bool nav_chemotaxis_init(uint8_t c, uint8_t plume);
-extern bool nav_chemotaxis(uint8_t c, uint8_t plume);
-
-#endif /** CHEMOTAXIS_H */
