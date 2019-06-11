637ab382df1a2c7373b96bb27e71c4ea670151ef
Tom van Dijk <tomvand@users.noreply.github.com>
2019-06-11 14:22:53 +0200

    WIP ignore GCC errors that do not appear in compilation for ARM
    Might be better to actually fix these errors, but I am not familiar
    with that part of the code.

--- sw/airborne/boards/bebop/mt9f002_nps.c
+++ sw/airborne/boards/bebop/mt9f002_nps.c
@@ -2,4 +2,7 @@
 
 #define front_camera mt9f002_front_camera // Do not overwrite NPS camera struct
 
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 #include "mt9f002.c"
+#pragma GCC diagnostic pop
--- sw/airborne/boards/bebop/mt9v117_nps.c
+++ sw/airborne/boards/bebop/mt9v117_nps.c
@@ -2,4 +2,7 @@
 
 #define bottom_camera mt9v117_bottom_camera // Do not overwrite NPS camera struct
 
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 #include "mt9v117.c"
+#pragma GCC diagnostic pop
