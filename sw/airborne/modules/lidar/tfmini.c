58deebc744348a40994a789d25f472fa398efad0
OpenUAS <OpenUAS@users.noreply.github.com>
2019-10-08 13:31:54 +0200

    Update tfmini.c (#2460)
    Tested with TFMini, now works correctly. Gain between 0..1 thus multiply not divide

--- sw/airborne/modules/lidar/tfmini.c
+++ sw/airborne/modules/lidar/tfmini.c
@@ -157,7 +157,7 @@ static void tfmini_parse(uint8_t byte)
             float phi = stateGetNedToBodyEulers_f()->phi;
             float theta = stateGetNedToBodyEulers_f()->theta;
             float gain = (float)fabs((double)(cosf(phi) * cosf(theta)));
-            tfmini.distance = tfmini.distance / gain;
+            tfmini.distance = tfmini.distance * gain;
           }
 
           // send message (if requested)
