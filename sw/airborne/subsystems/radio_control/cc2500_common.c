b64989a24a4f8e1610df48ea78eee440fe3b20c4
Tom van Dijk <tomvand@users.noreply.github.com>
2019-11-18 13:39:04 +0100

    Remove compatibility code from betaflight files
    Leave only the modified includes. All other changes removed.

--- sw/airborne/subsystems/radio_control/cc2500_common.c
+++ sw/airborne/subsystems/radio_control/cc2500_common.c
@@ -1,48 +1,10 @@
-/*
- * Copyright (C) 2019 Tom van Dijk <tomvand@users.noreply.github.com>
- *
- * This code is based on the betaflight cc2500 and FrskyX implementation.
- * https://github.com/betaflight/betaflight
- *
- * This file is part of paparazzi.
- *
- * paparazzi is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
- *
- * paparazzi is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with paparazzi; see the file COPYING.  If not, write to
- * the Free Software Foundation, 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- */
+#include "cc2500_compat.h"
 
+#include "peripherals/cc2500.h"
+#include "cc2500_settings.h"
 #include "cc2500_common.h"
 
-#include "cc2500_compat.h"
-#include "cc2500_settings.h"
-
-#include "subsystems/radio_control.h"
-#include "mcu_periph/gpio.h"
-#include "peripherals/cc2500.h"
-#include <assert.h>
-
-#include <string.h>
-#include "subsystems/datalink/downlink.h"
-
-
-// Paparazzi code
-
-
-
-
 // betaflight/src/main/rx/cc2500_common.c  @ 4a79046
-
 /*
  * This file is part of Cleanflight and Betaflight.
  *
@@ -62,8 +24,6 @@
  *
  * If not, see <http://www.gnu.org/licenses/>.
  */
-
-#include <stdbool.h>
 
 //#include "platform.h"
 
--- sw/airborne/subsystems/radio_control/cc2500_frsky_shared.c
+++ sw/airborne/subsystems/radio_control/cc2500_frsky_shared.c
@@ -1,39 +1,6 @@
-/*
- * Copyright (C) 2019 Tom van Dijk <tomvand@users.noreply.github.com>
- *
- * This code is based on the betaflight cc2500 and FrskyX implementation.
- * https://github.com/betaflight/betaflight
- *
- * This file is part of paparazzi.
- *
- * paparazzi is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
- *
- * paparazzi is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with paparazzi; see the file COPYING.  If not, write to
- * the Free Software Foundation, 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- */
-
-#define USE_RX_FRSKY_SPI
-
-#include "peripherals/cc2500.h"
-#include "subsystems/radio_control/cc2500_common.h"
-#include "subsystems/radio_control/cc2500_frsky_common.h"
-//#include "subsystems/radio_control/cc2500_frsky_d.h"
-//#include "subsystems/radio_control/cc2500_frsky_x.h"
-#include "subsystems/radio_control/cc2500_frsky_shared.h"
-
+#include "cc2500_compat.h"
 
 // betaflight/src/main/rx/cc2500_frsky_shared.c  @ 4a79046
-
 /*
  * This file is part of Cleanflight and Betaflight.
  *
