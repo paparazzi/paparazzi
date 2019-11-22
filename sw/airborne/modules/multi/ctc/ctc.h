ec7fee643672ccfc1c0ae7123b2b379a44d90ca4
Hector G. de Marina <noeth3r@gmail.com>
2019-11-22 10:57:29 +0100

    source files moved to multi dir

---
+++ sw/airborne/modules/multi/ctc/ctc.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2017  Hector Garcia de Marina
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+/** @file ctc.h
+ *
+ *  Collective Tracking Control
+ */
+
+#ifndef CTC_H
+#define CTC_H
+
+#include "std.h"
+
+typedef struct {
+  float k1;
+  float k2;
+  float alpha;
+  uint16_t timeout;
+  float p_centroid_x;
+  float p_centroid_y;
+  float v_centroid_x;
+  float v_centroid_y;
+  float target_px;
+  float target_py;
+  float target_vx;
+  float target_vy;
+  float vx;
+  float vy;
+  float px;
+  float py;
+  float ref_px;
+  float ref_py;
+  float omega;
+  uint16_t time_broad;
+} ctc_con;
+
+extern ctc_con ctc_control;
+extern int16_t tableNei[][6];
+extern float ctc_error_to_target;
+
+extern void ctc_init(void);
+extern bool collective_tracking_vehicle(void);
+extern bool collective_tracking_waypoint(uint8_t wp);
+extern bool collective_tracking_point(float x, float y);
+extern void collective_tracking_control(void);
+extern void ctc_send_info_to_nei(void);
+
+extern void parse_ctc_RegTable(void);
+extern void parse_ctc_CleanTable(void);
+extern void parse_ctc_NeiInfoTable(void);
+extern void parse_ctc_TargetInfo(void);
+
+#endif // CTC_H
---
+++ sw/airborne/modules/multi/ctc/ctc_target.c
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2017 Hector Garcia de Marina
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#include <math.h>
+#include <std.h>
+#include <stdio.h>
+
+#include "modules/ctc/ctc_target.h"
+#include "subsystems/datalink/datalink.h" // dl_buffer
+#include "subsystems/datalink/telemetry.h"
+#include "firmwares/rotorcraft/navigation.h"
+#include "autopilot.h"
+
+/*! Max expected number of aircraft */
+#ifndef CTC_MAX_AC
+#define CTC_MAX_AC 4
+#endif
+
+int16_t tableNei[CTC_MAX_AC][6];
+
+void ctc_target_init(void)
+{
+  for (int i = 0; i < CTC_MAX_AC; i++) {
+    tableNei[i][0] = -1;
+  }
+}
+
+void ctc_target_send_info_to_nei(void)
+{
+  struct EnuCoor_f *v = stateGetSpeedEnu_f();
+  struct EnuCoor_f *p = stateGetPositionEnu_f();
+
+  float vx = v->x;
+  float vy = v->y;
+  float px = p->x;
+  float py = p->y;
+
+  struct pprzlink_msg msg;
+
+  for (int i = 0; i < CTC_MAX_AC; i++)
+    if (tableNei[i][0] != -1) {
+      msg.trans = &(DefaultChannel).trans_tx;
+      msg.dev = &(DefaultDevice).device;
+      msg.sender_id = AC_ID;
+      msg.receiver_id = tableNei[i][0];
+      msg.component_id = 0;
+      pprzlink_msg_send_CTC_INFO_FROM_TARGET(&msg, &px, &py, &vx, &vy);
+    }
+}
+
+void parse_ctc_target_RegTable(void)
+{
+  uint8_t ac_id = DL_CTC_REG_TABLE_ac_id(dl_buffer);
+  if (ac_id == AC_ID) {
+    uint8_t nei_id = DL_CTC_REG_TABLE_nei_id(dl_buffer);
+    for (int i = 0; i < CTC_MAX_AC; i++)
+      if (tableNei[i][0] == -1) {
+        tableNei[i][0] = (int16_t)nei_id;
+        return;
+      }
+  }
+}
+
+void parse_ctc_target_CleanTable(void)
+{
+  uint8_t ac_id = DL_CTC_REG_TABLE_ac_id(dl_buffer);
+  if (ac_id == AC_ID)
+    for (int i = 0; i < CTC_MAX_AC; i++) {
+      tableNei[i][0] = -1;
+    }
+}
+
