efcba61d79fd3540e45514f2e37be61405ed0526
Open UAS <noreply@openuas.org>
2020-01-24 08:27:17 +0100

    Refresh WMM data for 2020 to 2025

--- sw/airborne/modules/geo_mag/geo_mag.c
+++ sw/airborne/modules/geo_mag/geo_mag.c
@@ -27,7 +27,7 @@
  */
 
 #include "modules/geo_mag/geo_mag.h"
-#include "math/pprz_geodetic_wmm2015.h"
+#include "math/pprz_geodetic_wmm2020.h"
 #include "math/pprz_algebra_double.h"
 #include "subsystems/gps.h"
 #include "subsystems/abi.h"
