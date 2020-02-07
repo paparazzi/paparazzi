1601135b38d828540f15bb74849e1240dae3eb3c
jburns11 <36611496+jburns11@users.noreply.github.com>
2020-02-07 02:01:14 -0500

    Update nav_survey_rectangle.h
    fixed small bug

--- sw/airborne/subsystems/navigation/nav_survey_rectangle.h
+++ sw/airborne/subsystems/navigation/nav_survey_rectangle.h
@@ -36,7 +36,7 @@ typedef enum {NS, WE} survey_orientation_t;
 
 extern void nav_survey_rectangle_init(uint8_t wp1, uint8_t wp2, float grid, survey_orientation_t so);
 extern void nav_survey_rectangle(uint8_t wp1, uint8_t wp2);
-extern void nav_survey_rectangle_dynamic(uint8_t wp1, uint8_t wp2, float grid)
+extern void nav_survey_rectangle_dynamic(uint8_t wp1, uint8_t wp2, float grid);
 
 #define NavSurveyRectangleInit(_wp1, _wp2, _grid, _orientation) nav_survey_rectangle_init(_wp1, _wp2, _grid, _orientation)
 #define NavSurveyRectangle(_wp1, _wp2) nav_survey_rectangle(_wp1, _wp2);
