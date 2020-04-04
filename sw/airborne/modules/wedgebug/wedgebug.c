fe9e7eb395ea978d2af7ff5e75a0f8e2ec9f221a
ralphrschmidt <ralph.r.schmidt@outlook.com>
2020-04-04 16:30:10 +0200

    Created new module "wedgebu" and added it to the airframe "bebop_ralphthesis2020_stereo.xml" Also, used "make" to create tool for creating modules On branch wedgeBug Your branch is up to date with 'origin/wedgeBug'. Changes to be committed: 	modified:   conf/airframes/tudelft/bebop_ralphthesis2020_stereo.xml 	new file:   conf/modules/wedgebug.xml 	modified:   conf/userconf/tudelft/ralphthesis2020_conf.xml 	new file:   sw/airborne/modules/wedgebug/wedgebug.c 	new file:   sw/airborne/modules/wedgebug/wedgebug.h 	new file:   sw/tools/create_module/create_module_ui.py 	new file:   sw/tools/create_module/datalink_ui.py 	new file:   sw/tools/create_module/event_ui.py 	modified:   sw/tools/create_module/files_create.py 	new file:   sw/tools/create_module/init_ui.py 	new file:   sw/tools/create_module/periodic_ui.py

---
+++ sw/airborne/modules/wedgebug/wedgebug.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) Ralph Rudi schmidt <ralph.r.schmidt@outlook.com>
+
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+
+ */
+/** @file "modules/wedgebug/wedgebug.h"
+ * @author Ralph Rudi schmidt <ralph.r.schmidt@outlook.com>
+ * An integration of the WegdeBug algorithm (Laubach 1999) for path finding, for drones with stereo vision.
+ */
+#include "modules/wedgebug/wedgebug.h"
+void wedgebug_init(){
+  // your init code here
+}
+
+void wedgebug_periodic(){
+  // your periodic code here.
+  // freq = 4.0 Hz
+}
+
+
---
+++ sw/airborne/modules/wedgebug/wedgebug.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) Ralph Rudi schmidt <ralph.r.schmidt@outlook.com>
+
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+
+ */
+/** @file "modules/wedgebug/wedgebug.h"
+ * @author Ralph Rudi schmidt <ralph.r.schmidt@outlook.com>
+ * An integration of the WegdeBug algorithm (Laubach 1999) for path finding, for drones with stereo vision.
+ */
+#ifndef WEDGEBUG_H
+#define WEDGEBUG_H
+extern void wedgebug_init(void);
+extern void wedgebug_periodic(void);
+
+#endif  // WEDGEBUG_H
