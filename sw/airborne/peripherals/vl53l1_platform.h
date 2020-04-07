56db193148beb5ab5f4cc523e2b3f05b10611666
Tom van Dijk <tomvand@users.noreply.github.com>
2020-04-07 11:49:07 +0200

    Add header license to vl53l1x_platform.h, format code style

--- sw/airborne/peripherals/vl53l1_platform.h
+++ sw/airborne/peripherals/vl53l1_platform.h
@@ -1,3 +1,25 @@
+/*
+ * Copyright (C) 2020 Tom van Dijk <tomvand@users.noreply.github.com>
+ * Adapted from STSW-IMG009 vl53l1_platform.h
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
 /**
  * @file  vl53l1_platform.h
  * @brief Those platform functions are platform dependent and have to be implemented by the user
