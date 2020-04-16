e8037af5209e8d3590814ee18197491fa6c00fa0
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2020-04-16 13:52:48 +0200

    [jsbsim] support of more recent jsbsim versions (#2520)
    The development of jsbsim is now switch to git with an new active team.
    The stable release v1.0.0rc1 has several fix and improvements, but
    requires some modification from us to properly handle the type of file
    path passed as arguments to load models.
    
    Also, the default path when pkg-config is not found was /opt, which was
    used in very early versions of NPS. The default is now /usr so that it
    is compatible with a standard install of the new lib version.

--- sw/simulator/nps/nps_fdm_jsbsim.cpp
+++ sw/simulator/nps/nps_fdm_jsbsim.cpp
@@ -73,6 +73,13 @@
 #define PascalOfPsf(_p) ((_p) * 47.8802588889)
 #define CelsiusOfRankine(_r) (((_r) - 491.67) / 1.8)
 
+/// Macro to build file path according to the lib version
+#if NPS_JSBSIM_USE_SGPATH
+#define JSBSIM_PATH(_x) SGPath(_x)
+#else
+#define JSBSIM_PATH(_x) _x
+#endif
+
 /** Name of the JSBSim model.
  *  Defaults to the AIRFRAME_NAME
  */
@@ -558,9 +565,9 @@ static void init_jsbsim(double dt)
   FDMExec->DisableOutput();
   FDMExec->SetDebugLevel(0); // No DEBUG messages
 
-  if (! FDMExec->LoadModel(rootdir + "aircraft",
-                           rootdir + "engine",
-                           rootdir + "systems",
+  if (! FDMExec->LoadModel(JSBSIM_PATH(rootdir + "aircraft"),
+                           JSBSIM_PATH(rootdir + "engine"),
+                           JSBSIM_PATH(rootdir + "systems"),
                            NPS_JSBSIM_MODEL,
                            false)) {
 #ifdef DEBUG
@@ -580,7 +587,7 @@ static void init_jsbsim(double dt)
 
   FGInitialCondition *IC = FDMExec->GetIC();
   if (!jsbsim_ic_name.empty()) {
-    if (! IC->Load(jsbsim_ic_name)) {
+    if (! IC->Load(JSBSIM_PATH(jsbsim_ic_name))) {
 #ifdef DEBUG
       cerr << "Initialization unsuccessful" << endl;
 #endif
