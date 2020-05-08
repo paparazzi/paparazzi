b24a306e916d75adc6b305c83c7205fde92db752
Tom van Dijk <tomvand@users.noreply.github.com>
2020-05-08 11:12:42 +0200

    Refactoring

--- sw/airborne/modules/sensors/opticflow_pmw3901.c
+++ sw/airborne/modules/sensors/opticflow_pmw3901.c
@@ -59,8 +59,7 @@ PRINT_CONFIG_VAR(OPTICFLOW_PMW3901_AGL_ID)
 #define RAD(_x) ((_x) / 180.0 * M_PI)
 
 
-struct pmw3901_t of_pmw;
-struct opticflow_pmw3901_settings_t of_pmw_set;
+struct opticflow_pmw3901_settings_t of_pmw;
 
 abi_event agl_ev;
 static float agl_dist;
@@ -75,7 +74,7 @@ static void agl_cb(uint8_t sender_id, uint32_t stamp, float distance) {
 
 static bool agl_valid(uint32_t at_ts) {
   return \
-      ((at_ts - agl_ts) < of_pmw_set.agl_timeout) &&
+      ((at_ts - agl_ts) < of_pmw.agl_timeout) &&
       (agl_dist > 0.080);
 }
 
@@ -83,8 +82,8 @@ static bool agl_valid(uint32_t at_ts) {
 static void opticflow_pmw3901_publish(int16_t delta_x, int16_t delta_y, uint32_t ts_usec) {
   /* Prepare message variables */
   // Sensor orientation
-  float c = cosf(RAD(of_pmw_set.sensor_angle));
-  float s = sinf(RAD(of_pmw_set.sensor_angle));
+  float c = cosf(RAD(of_pmw.sensor_angle));
+  float s = sinf(RAD(of_pmw.sensor_angle));
   // Time
   static uint32_t prev_ts_usec = 0;
   float dt = (ts_usec - prev_ts_usec) / 1.0e6;
@@ -95,24 +94,24 @@ static void opticflow_pmw3901_publish(int16_t delta_x, int16_t delta_y, uint32_t
   // Flow [px/s] (body-frame)
   float flow_x = (c * delta_x - s * delta_y) / dt;
   float flow_y = (s * delta_x + c * delta_y) / dt;
-  int16_t flow_x_subpix = (int16_t)(of_pmw_set.subpixel_factor * flow_x);
-  int16_t flow_y_subpix = (int16_t)(of_pmw_set.subpixel_factor * flow_y);
+  int16_t flow_x_subpix = (int16_t)(of_pmw.subpixel_factor * flow_x);
+  int16_t flow_y_subpix = (int16_t)(of_pmw.subpixel_factor * flow_y);
   // Derotated flow [px/s] (body-frame)
   struct FloatRates *rates = stateGetBodyRates_f();
-  float flow_dy_p =  rates->p / of_pmw.rad_per_px;
-  float flow_dx_q = -rates->q / of_pmw.rad_per_px;
+  float flow_dy_p =  rates->p / of_pmw.pmw.rad_per_px;
+  float flow_dx_q = -rates->q / of_pmw.pmw.rad_per_px;
   float flow_der_x = flow_x - flow_dx_q;
   float flow_der_y = flow_y - flow_dy_p;
-  int16_t flow_der_x_subpix = (int16_t)(of_pmw_set.subpixel_factor * flow_der_x);
-  int16_t flow_der_y_subpix = (int16_t)(of_pmw_set.subpixel_factor * flow_der_y);
+  int16_t flow_der_x_subpix = (int16_t)(of_pmw.subpixel_factor * flow_der_x);
+  int16_t flow_der_y_subpix = (int16_t)(of_pmw.subpixel_factor * flow_der_y);
   // Velocity
   static float vel_x = 0;  // static: keep last measurement for telemetry if agl not valid
   static float vel_y = 0;
   static float noise = 0;
   if (agl_valid(ts_usec)) {
-    vel_x = flow_der_x * of_pmw.rad_per_px * agl_dist;
-    vel_y = flow_der_y * of_pmw.rad_per_px * agl_dist;
-    noise = of_pmw_set.std_px * of_pmw.rad_per_px * agl_dist;
+    vel_x = flow_der_x * of_pmw.pmw.rad_per_px * agl_dist;
+    vel_y = flow_der_y * of_pmw.pmw.rad_per_px * agl_dist;
+    noise = of_pmw.std_px * of_pmw.pmw.rad_per_px * agl_dist;
   }
 
   /* Send ABI messages */
@@ -166,25 +165,28 @@ static void opticflow_pmw3901_publish(int16_t delta_x, int16_t delta_y, uint32_t
 
 
 void opticflow_pmw3901_init(void) {
-  pmw3901_init(&of_pmw, &OPTICFLOW_PMW3901_SPI_DEV, OPTICFLOW_PMW3901_SPI_SLAVE_IDX);
+  pmw3901_init(&of_pmw.pmw, &OPTICFLOW_PMW3901_SPI_DEV, OPTICFLOW_PMW3901_SPI_SLAVE_IDX);
   AbiBindMsgAGL(OPTICFLOW_PMW3901_AGL_ID, &agl_ev, agl_cb);
-  of_pmw_set.sensor_angle = OPTICFLOW_PMW3901_SENSOR_ANGLE;
-  of_pmw_set.subpixel_factor = OPTICFLOW_PMW3901_SUBPIXEL_FACTOR;
-  of_pmw_set.std_px = OPTICFLOW_PMW3901_STD_PX;
-  of_pmw_set.agl_timeout = OPTICFLOW_PMW3901_AGL_TIMEOUT_US;
+  of_pmw.sensor_angle = OPTICFLOW_PMW3901_SENSOR_ANGLE;
+  of_pmw.subpixel_factor = OPTICFLOW_PMW3901_SUBPIXEL_FACTOR;
+  of_pmw.std_px = OPTICFLOW_PMW3901_STD_PX;
+  of_pmw.agl_timeout = OPTICFLOW_PMW3901_AGL_TIMEOUT_US;
+#ifdef OPTICFLOW_PMW3901_RAD_PER_PX
+  of_pmw.pmw.rad_per_px = OPTICFLOW_PMW3901_RAD_PER_PX;
+#endif
 }
 
 void opticflow_pmw3901_periodic(void) {
-  if (pmw3901_is_idle(&of_pmw)) {
-    pmw3901_start_read(&of_pmw);
+  if (pmw3901_is_idle(&of_pmw.pmw)) {
+    pmw3901_start_read(&of_pmw.pmw);
   }
 }
 
 void opticflow_pmw3901_event(void) {
-  pmw3901_event(&of_pmw);
-  if (pmw3901_data_available(&of_pmw)) {
+  pmw3901_event(&of_pmw.pmw);
+  if (pmw3901_data_available(&of_pmw.pmw)) {
     int16_t delta_x, delta_y;
-    pmw3901_get_data(&of_pmw, &delta_x, &delta_y);
+    pmw3901_get_data(&of_pmw.pmw, &delta_x, &delta_y);
     opticflow_pmw3901_publish(delta_x, delta_y, get_sys_time_usec());
   }
 }
