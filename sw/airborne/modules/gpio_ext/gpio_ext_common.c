837779538d9640dac9f875125b4cbd382fddce3f
Tom van Dijk <tomvand@users.noreply.github.com>
2020-07-15 09:19:46 +0200

    GCC wrap example

---
+++ sw/airborne/modules/gpio_ext/gpio_ext_common.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) Tom van Dijk <tomvand@users.noreply.github.com>
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+/** @file "modules/gpio_ext/gpio_ext_common.c"
+ * @author Tom van Dijk <tomvand@users.noreply.github.com>
+ * Common external GPIO functions.
+ */
+
+#include "modules/gpio_ext/gpio_ext_common.h"
+
+void gpio_ext_common_init(void)
+{
+  // your init code here
+}
+
+void gpio_ext_common_event(void)
+{
+  // your event code here
+}
+
+
