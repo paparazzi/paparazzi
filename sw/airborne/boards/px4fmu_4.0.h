ad8fe87026f1d04da78d0c33082ba4f12ac197fd
Open UAS <noreply@openuas.org>
2020-09-02 20:45:39 +0200

    Add sensor for ranging with IO is PWM

--- sw/airborne/boards/px4fmu_4.0.h
+++ sw/airborne/boards/px4fmu_4.0.h
@@ -3,6 +3,11 @@
 
 #define BOARD_PX4FMU_V4
 
+/* differences between board not implemented ATM consider them all the same for time being */
+//#define BOARD_PX4FMU_V4_R12
+//#define BOARD_PX4FMU_V4_R14
+//#define BOARD_PX4FMU_V4_R15
+
 /* PX4FMU_V4 a.k.a. Pixracer board has a 24MHz external clock and 168MHz internal. */
 
 /* STM32F4 STM32F427VIT6 */
@@ -47,7 +52,7 @@
 /* On PCB other LEDs */
 /* The Green Pixracer Power LED in not controllable, not on a MCU pin */
 
-/* UART SCHTUFFFF*/
+/* UART */
 
 /* -WiFi ESP Connector, it is just a serial port*/
 //TODO: Test
@@ -56,6 +61,18 @@
 #define UART1_GPIO_RX GPIO7
 #define UART1_GPIO_PORT_TX GPIOB
 #define UART1_GPIO_TX GPIO6
+
+//Not used yet unil find some time to test
+/*
+#define ESP8266_TX
+#define ESP8266_RX
+#define ESP8266_PD ( power down )
+#define ESP8266_GPIO2
+#define ESP8266_RESET
+#define ESP8266_GPIO0
+#define ESP8266_RTS
+#define ESP8266_CTS
+*/
 
 /* -TELEM1 Connector */
 #define UART2_GPIO_AF GPIO_AF7
@@ -91,7 +108,7 @@
 #define UART6_GPIO_PORT_RX GPIOC
 #define UART6_GPIO_RX GPIO7
 
-/* Serial Debugging Connector, not used with PPRZ as of now, use JTAG, can be put to other good use */
+/* Serial Debugging Info Connector, not used with PPRZ as of now, use JTAG for debugging, so this uart can be put to other use if really needed */
 #define UART7_GPIO_AF GPIO_AF8
 #define UART7_GPIO_PORT_RX GPIOE
 #define UART7_GPIO_RX GPIO7
@@ -99,7 +116,6 @@
 #define UART7_GPIO_TX GPIO8
 
 /* Connector -FRS FrSky */
-//TODO: Test
 #define UART8_GPIO_AF GPIO_AF8
 #define UART8_GPIO_PORT_RX GPIOE
 #define UART8_GPIO_RX GPIO0
@@ -197,9 +213,7 @@ When a read-operation of an RTD resistance data register occurs, DRDY returns hi
 //#define SPI_SELECT_SLAVE5_PORT GPIOA
 //#define SPI_SELECT_SLAVE5_PIN GPIO4
 
-
 /* SDIO to microSD card connector */
-//FIXME: Fix and test
 #define SDIO_AF GPIO_AF12
 #define SDIO_D0_PORT GPIOC
 #define SDIO_D0_PIN GPIO8
@@ -220,12 +234,10 @@ When a read-operation of an RTD resistance data register occurs, DRDY returns hi
 #endif
 #define USE_AD_TIM3 1
 
-// Internal ADC for voltage level measurement
-////FIXME: test and fix if still not working
+// Internal ADC used for board voltage level measurement
 #ifndef USE_ADC_1
 #define USE_ADC_1 1
 #endif
-
 #if USE_ADC_1
 #define AD1_1_CHANNEL 4 //ADC12_IN4
 #define ADC_1 AD1_1
@@ -233,8 +245,6 @@ When a read-operation of an RTD resistance data register occurs, DRDY returns hi
 #define ADC_1_GPIO_PIN GPIO4
 #endif
 
-// Per default for the board to sense voltage level via external sensor on ADC
-//FIXME: test and fix if still not working
 #ifndef USE_ADC_2
 #define USE_ADC_2 1
 #endif
@@ -245,7 +255,6 @@ When a read-operation of an RTD resistance data register occurs, DRDY returns hi
 #define ADC_2_GPIO_PIN GPIO2
 #endif
 
-// Per default for the board to sense current via external sensor on ADC
 #ifndef USE_ADC_3
 #define USE_ADC_3 1
 #endif
@@ -255,24 +264,43 @@ When a read-operation of an RTD resistance data register occurs, DRDY returns hi
 #define ADC_3_GPIO_PORT GPIOA
 #define ADC_3_GPIO_PIN GPIO3
 #endif
-#define MilliAmpereOfAdc(adc)((float)adc) * (3.3f / 4096.0f) * (90.0f / 5.0f) //TODO: test if valid
 
-/* Allow to define ADC_CHANNEL_VSUPPLY in the airframe file */
+//ADC_pin_RSSI_IN
+#ifndef USE_ADC_4
+#define USE_ADC_4 1
+#endif
+#if USE_ADC_4
+#define AD1_4_CHANNEL 11 // ADC123_IN11
+#define ADC_4 AD1_4
+#define ADC_4_GPIO_PORT GPIOC
+#define ADC_4_GPIO_PIN GPIO1
+#endif
+
+/* Allow to define another ADC_CHANNEL_VSUPPLY in the airframe file */
 #ifndef ADC_CHANNEL_VSUPPLY
-#define ADC_CHANNEL_VSUPPLY ADC_2
+  #define ADC_CHANNEL_VSUPPLY ADC_1 // Per default for the board to sense voltage (V) level via external sensor is via ADC_2
+  #define DefaultVoltageOfAdc(adc) (10.5 * (float)adc) // FIXME: More precise value scale internal vdd to 5V
+#else
+  #if USE_ADC_2
+    #define DefaultVoltageOfAdc(adc) ((3.3f/4096.0f) * 10.245f * (float)adc) // About the value scale for a common 3DR clone Power Brick 
+  #else
+    #define DefaultVoltageOfAdc(adc) ((3.3f/4096.0f) * 6.0f * (float)adc) // About the value scale for a common other sensor 
+  #endif
 #endif
 
 /* Allow to define another ADC for Current measurement in the airframe file */
 #ifndef ADC_CHANNEL_CURRENT
-#define ADC_CHANNEL_CURRENT ADC_3
+#define ADC_CHANNEL_CURRENT ADC_3 // Per default for the board to sense current (I) via external sensor is via ADC_3
 #endif
 
-//??FT = Five-volt tolerant. In order to sustain a voltage higher than VDD+0.3 the internal pull-up/pull-down resistors must be disabled.
-//FIXME: Determine best stock values
-#if USE_ADC_2
-#define DefaultVoltageOfAdc(adc) (0.004f * (float)adc) // FIXME: Value scale for a common PowerBrick
+#if USE_ADC_3
+#define DefaultMilliAmpereOfAdc(adc) (9.55 * ((float)adc))// Quite close to the value scale for a common 3DR clone Power Brick 
 #else
-#define DefaultVoltageOfAdc(adc) (0.004f * (float)adc) // FIXME: Value scale internal vdd to 5V
+#define DefaultMilliAmpereOfAdc(adc) (0.1 * ((float)adc)) // FIXME: Value scale internal current use, for whatever it is useful
+#endif
+
+#ifndef ADC_CHANNEL_RSSI
+#define ADC_CHANNEL_RSSI ADC_4
 #endif
 
 /* I2C mapping */
@@ -280,12 +308,12 @@ When a read-operation of an RTD resistance data register occurs, DRDY returns hi
 #define I2C1_GPIO_SCL GPIO8
 #define I2C1_GPIO_SDA GPIO9
 
-//Onboard Barometer
+//Enable Onboard Barometer per default
 #ifndef USE_BARO_BOARD
 #define USE_BARO_BOARD 1
 #endif
 
-/* Another Magnetometer on board a HMC5983 not the one in the IMU 9250*/
+/* Another Magnetometer on board on R14 a HMC5983 on R15 ST ,so not the one in the IMU 9250*/
 //FIXME: better default option for use of maybe fuse data
 #ifndef USE_MAGNETOMETER_B
 #define USE_MAGNETOMETER_B 0
@@ -301,9 +329,8 @@ When a read-operation of an RTD resistance data register occurs, DRDY returns hi
 #define PWM_USE_TIM1 1
 #define PWM_USE_TIM4 1
 
-
 //TODO: ifdef USE_SERVO6 for PPM out to e.g. servo extender board ...
-// Basically a inter mcu Extra device;)
+// Basically a inter mcu Extra device ;)
 
 #define USE_PWM1 1
 #define USE_PWM2 1
@@ -393,8 +420,63 @@ When a read-operation of an RTD resistance data register occurs, DRDY returns hi
 #define PWM_TIM1_CHAN_MASK (PWM_SERVO_1_OC_BIT|PWM_SERVO_2_OC_BIT|PWM_SERVO_3_OC_BIT|PWM_SERVO_4_OC_BIT)
 #define PWM_TIM4_CHAN_MASK (PWM_SERVO_5_OC_BIT|PWM_SERVO_6_OC_BIT)
 
+/*
+ * PWM input, also could be used for 
+ */
+// PWM_INPUT1 on TIM4
+#ifdef USE_PWM_INPUT1
+#define PWM_INPUT1_GPIO_PORT      GPIOD
+#define PWM_INPUT1_GPIO_PIN       GPIO12
+#define PWM_INPUT1_GPIO_AF        GPIO_AF2
+
+#define PWM_INPUT1_TIMER          TIM4
+#define PWM_INPUT1_CHANNEL_PERIOD TIM_IC1
+#define PWM_INPUT1_CHANNEL_DUTY   TIM_IC2
+#define PWM_INPUT1_TIMER_INPUT    TIM_IC_IN_TI1
+#define PWM_INPUT1_SLAVE_TRIG     TIM_SMCR_TS_TI1FP1
+#define PWM_INPUT1_IRQ            NVIC_TIM4_IRQ
+#define PWM_INPUT1_CC_IE          (TIM_DIER_CC1IE | TIM_DIER_CC2IE)
+#define USE_PWM_INPUT_TIM4        TRUE
+
+#ifdef PWM_INPUT1_TICKS_PER_USEC
+#define TIM4_TICKS_PER_USEC PWM_INPUT1_TICKS_PER_USEC
+#endif
+#define TIM4_PWM_INPUT_IDX        0
+#define TIM4_CC_IF_PERIOD         TIM_SR_CC1IF
+#define TIM4_CC_IF_DUTY           TIM_SR_CC2IF
+#define TIM4_CCR_PERIOD           TIM4_CCR1
+#define TIM4_CCR_DUTY             TIM4_CCR2
+#endif
+
+// PWM_INPUT2 on TIM?
+//FIXME: Find some useful not often used pin to outside world...
+
+/* Ofboard LED Safety LED */
+#ifndef USE_LED_4
+#define USE_LED_4 1
+#endif
+#define LED_4_GPIO GPIOC
+#define LED_4_GPIO_PIN GPIO3
+#define LED_4_GPIO_ON gpio_clear
+#define LED_4_GPIO_OFF gpio_set
+#define LED_4_AFIO_REMAP ((void)0)
+
+//#if USE_LED_4
+//#ifndef SAFETY_WARNING_LED
+#define SAFETY_WARNING_LED 4
+//#endif
+//#endif
+// #define LED_SAFETY_GPIO_PORT LED_4_GPIO
+// #define LED_SAFETY_GPIO_PIN LED_4_GPIO_PIN
+// #define LED_SAFETY_GPIO_ON LED_4_GPIO_ON
+// #define LED_SAFETY_GPIO_OFF LED_4_GPIO_OFF
+// #define LED_SAFETY_AFIO_REMAP LED_4_AFIO_REMAP
+
+/* Safety Button */
+#define BUTTON_SAFETY_GPIO_PORT      GPIOC
+#define BUTTON_SAFETY_GPIO_PIN       GPIO4
+
 /* Buzzer (A.k.a. Alarm) */
-//TODO: Test
 #if USE_BUZZER
 #define PWM_BUZZER
 #define PWM_BUZZER_TIMER TIM2
---
+++ sw/airborne/modules/sonar/sonar_pwm.c
@@ -0,0 +1,156 @@
+/*
+* Copyright (C) 2020 OpenuAS
+*
+* Thanks to Jean-François Erdelyi & Gautier Hattenberger for ADC one
+*
+* This file is part of paparazzi
+*
+* paparazzi is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
+*
+* paparazzi is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with paparazzi; see the file COPYING.  If not, see
+* <http://www.gnu.org/licenses/>.
+*/
+
+/**
+* @file "modules/sensors/sonar_pwm.h"
+* @author OpenUAS
+* @brief Range sensor input via PWM
+*
+* Driver for a PWM based sonar range sensor
+* Reads sensor using PWM input and outputs sonar distance to object
+*
+* Sensor example: Maxbotix EZ1
+* https://www.maxbotix.com/033-using-pulse-width-pin-2.htm
+*
+*
+*/
+
+#include "modules/sonar/sonar_pwm.h"
+#include "mcu_periph/pwm_input.h"
+#include "subsystems/abi.h"
+
+#include "filters/median_filter.h"
+
+#if PERIODIC_TELEMETRY
+#include "subsystems/datalink/telemetry.h"
+#include "pprzlink/messages.h"
+#endif
+#ifdef SENSOR_SYNC_SEND_SONAR
+#include "subsystems/datalink/downlink.h"
+#endif
+#include "generated/airframe.h"
+#ifdef SITL
+#include "state.h"
+#endif
+
+#ifdef SONAR_USE_PWM_FILTER
+struct MedianFilterFloat sonar_filt;
+#endif
+
+/* Config parameters for sensor */
+
+/// The input channel of the PWM based sensor
+#ifndef SONAR_PWM_CHANNEL
+#error "No SONAR_PWM_CHANNEL defined to use sonar_pwm module, add a line like define=SONAR_PWM_CHANNEL value=PWM_INPUT1"
+#endif
+
+/// Ranger offset value for what considered the distance should be zero
+#ifndef SONAR_OFFSET
+#define SONAR_OFFSET 0
+#endif
+
+/// Default to a 12 bit PWM sensor
+#ifndef SONAR_PWM_PERIOD
+#define SONAR_PWM_PERIOD 4096
+#endif
+
+/// Ranger scale or sensitivity as you wish
+#ifndef SONAR_SCALE
+#define SONAR_SCALE SONAR_PWM_PERIOD
+#endif
+
+/// The Median Filter strength
+#ifndef SONAR_MEDIAN_SIZE
+#define SONAR_MEDIAN_SIZE 7 //Good for a noisy sonar
+#endif
+
+/// The minimum range for the device to be able to measure
+#ifndef SONAR_MIN_RANGE
+#define SONAR_MIN_RANGE 0.15f //Default is a common value for regular sonars. Cannot measure closer than that
+#endif
+
+/// The maximum range for the device to be able to measure
+#ifndef SONAR_MAX_RANGE
+#define SONAR_MAX_RANGE 7.0f //Reasonable maximum value for regular sonars.
+#endif
+
+/// Some sensor may need an initial PWM offset (823 usec in the case of an EZ1 sensor)
+#ifndef SONAR_PWM_OFFSET
+#define SONAR_PWM_OFFSET 820 //822
+#endif
+
+// Enable telemetry report
+#ifndef SENSOR_SYNC_SEND_SONAR
+#define SENSOR_SYNC_SEND_SONAR TRUE
+#endif
+
+struct SonarPwm sonar_pwm;
+
+/* Set the default values at initialization */
+void sonar_pwm_init(void)
+{
+  sonar_pwm.offset = SONAR_OFFSET;
+  sonar_pwm.scale = SONAR_SCALE;
+  sonar_pwm.raw = 0.0f;
+  #ifdef SONAR_USE_PWM_FILTER
+    init_median_filter_f(&sonar_filt, SONAR_MEDIAN_SIZE);
+  #endif
+}
+
+/* Read the sensor current measured value */
+void sonar_pwm_read(void) {
+
+#ifndef SITL
+  // raw duty cycle in usec
+  uint16_t sonar_duty_raw = get_pwm_input_duty_in_usec(SONAR_PWM_CHANNEL);
+
+  // remove PWM offset where needed
+  sonar_pwm.raw = sonar_duty_raw - SONAR_PWM_OFFSET;
+
+#ifdef SONAR_USE_PWM_FILTER
+  sonar_pwm.distance = update_median_filter_f(&sonar_filt, ((float)sonar_pwm.raw * sonar_pwm.scale) - sonar_pwm.offset);
+#else
+  sonar_pwm.distance = (((float)sonar_pwm.raw * sonar_pwm.scale) - sonar_pwm.offset);
+#endif
+
+  Bound(sonar_pwm.distance, (float)SONAR_MIN_RANGE, (float)SONAR_MAX_RANGE);
+
+#if SONAR_COMPENSATE_ROTATION
+  float phi = stateGetNedToBodyEulers_f()->phi;
+  float theta = stateGetNedToBodyEulers_f()->theta;
+  float gain = (float)fabs( (double) (cosf(phi) * cosf(theta)));
+  sonar_adc.distance =  sonar_adc.distance * gain;
+#endif
+
+#else // SITL
+  sonar_adc.distance = stateGetPositionEnu_f()->z;
+#endif // SITL
+
+#if USE_SONAR
+  uint32_t now_ts = get_sys_time_usec();
+  AbiSendMsgAGL(AGL_SONAR_PWM_ID, now_ts, sonar_pwm.distance);
+#endif
+
+#ifdef SENSOR_SYNC_SEND_SONAR
+  DOWNLINK_SEND_SONAR(DefaultChannel, DefaultDevice, &sonar_pwm.raw, &sonar_pwm.distance);
+#endif
+}
