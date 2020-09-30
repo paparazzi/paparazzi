f1a9086409edfe02929995c2f18254d5f5d23ce8
Fabien-B <fabien.bonneval@gmail.com>
2020-09-30 19:36:54 +0200

    [Python libs] Adds license headers.

--- sw/lib/python/flight_plan.py
+++ sw/lib/python/flight_plan.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
+#
+# Copyright (C) 2020 Fabien Bonneval <fabien.bonneval@enac.fr>
+#
+# This file is part of paparazzi.
+#
+# paparazzi is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# paparazzi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with paparazzi; see the file COPYING.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+
 
 from lxml import etree
 from typing import List, Union
