38549041ac47018159af7ae409f5e2b5282ac8a9
hendrixgr <hendrixgr@gmail.com>
2020-11-05 11:52:38 +0200

    Improved OSD display
    The OSD is improved drastically with new string format capabilities, magnetic course calculation and smoother display.
    Fixed the heading back home calculation, i don't know what i was thinking....
    I also added some safety checks in the magnetic course calculation, removed any code related to simulation since this module is not included in simulation 
    and corrected some mistakes.
    I also added two styles for seeing the heading back home, relative style and ILS style, In Relative style you need to turn
    the aircraft until the home heading DIFFERENCE is 0 and in ILS style you need to much the aircraft heading with the home direction heading indicated.

--- sw/airborne/modules/display/max7456.c
+++ sw/airborne/modules/display/max7456.c
@@ -688,19 +688,14 @@ void draw_osd(void)
     case (10):
 #if defined(OSD_HOME_DIR_RELATIVE_STYLE) && OSD_HOME_DIR_RELATIVE_STYLE == 1
   PRINT_CONFIG_MSG("OSD HOME DIRECTION USES THE RELATIVE STYLE, 0 IS GOING BACK HOME")
+      // The reading is actually showing the difference between your heading and home heading.
+      // When the home_dir goes to 0 the aircraft is headed straight back home.
       home_direction_degrees = home_direction();
 #else
   PRINT_CONFIG_MSG("OSD HOME DIRECTION USES THE ILS STYLE, MATCH THE HEADING TO HOME DIR TO GET BACK HOME")
-      home_direction_degrees = (float)(atan2(pos->y, pos->x));
-      home_direction_degrees = RadOfDeg(90) - home_direction_degrees; // convert to 0 rad = North
-      if (home_direction_degrees > M_PI) { // Angle normalization (-180 deg to 180 deg still in radians)
-        home_direction_degrees -= (2.0 * M_PI);
-      } else if (home_direction_degrees < -M_PI) { gps_course_deg += (2.0 * M_PI); }
-      home_direction_degrees = DegOfRad(home_direction_degrees);  // Now convert to degrees.
-      if (home_direction_degrees < 0) { home_direction_degrees += 360; } // translate the +180, -180 to 0-360.
+      home_direction_degrees = gps_course_deg + home_direction(); //home_direction returns degrees -180 to +180
+      if (home_direction_degrees < 0) { home_direction_degrees += 360; } // translate the -180, +180 to 0-360.
 #endif
-      // The reading is actually showing the difference between your heading and home heading.
-      // When the home_dir goes to 0 the aircraft is headed straight back home.
 #if defined(USE_MATEK_TYPE_OSD_CHIP) && USE_MATEK_TYPE_OSD_CHIP == 1
       // All special character codes must be in 3 digit format!
       osd_sprintf(osd_string, "%191c%.0f", home_direction_degrees); // 0 when heading straight back home.
