90bd77f07e95c536fad4ed7e98027b6e2248b6d6
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2021-02-02 21:12:10 +0100

    [chibios] fix ICU config

--- sw/airborne/boards/apogee/chibios/v1.0/mcuconf.h
+++ sw/airborne/boards/apogee/chibios/v1.0/mcuconf.h
@@ -216,9 +216,9 @@
 #define STM32_ICU_USE_TIM2                  FALSE
 #define STM32_ICU_USE_TIM3                  FALSE
 #define STM32_ICU_USE_TIM4                  FALSE
-#define STM32_ICU_USE_TIM5                  FALSE
+#define STM32_ICU_USE_TIM5                  TRUE
 #define STM32_ICU_USE_TIM8                  FALSE
-#define STM32_ICU_USE_TIM9                  TRUE
+#define STM32_ICU_USE_TIM9                  FALSE
 #define STM32_ICU_TIM1_IRQ_PRIORITY         7
 #define STM32_ICU_TIM2_IRQ_PRIORITY         7
 #define STM32_ICU_TIM3_IRQ_PRIORITY         7
