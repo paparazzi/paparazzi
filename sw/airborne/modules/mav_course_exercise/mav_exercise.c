dbb1ea851758334a7161563655201f6deec82cbf
matteobarbera <matteo.barbera97@gmail.com>
2021-02-15 16:22:15 +0100

    Reformatted indents to 2 spaces

--- sw/airborne/modules/mav_course_exercise/mav_exercise.c
+++ sw/airborne/modules/mav_course_exercise/mav_exercise.c
@@ -28,17 +28,17 @@
 #define NAV_C // needed to get the nav functions like Inside...
 #include "generated/flight_plan.h"
 
-#define PRINT(string,...) fprintf(stderr, "[mav_exercise->%s()] " string,__FUNCTION__ , ##__VA_ARGS__)
+#define PRINT(string, ...) fprintf(stderr, "[mav_exercise->%s()] " string,__FUNCTION__ , ##__VA_ARGS__)
 
 uint8_t increase_nav_heading(float incrementDegrees);
 uint8_t moveWaypointForward(uint8_t waypoint, float distanceMeters);
 uint8_t moveWaypoint(uint8_t waypoint, struct EnuCoor_i *new_coor);
 
 enum navigation_state_t {
-    SAFE,
-    OBSTACLE_FOUND,
-    OUT_OF_BOUNDS,
-    HOLD
+  SAFE,
+  OBSTACLE_FOUND,
+  OUT_OF_BOUNDS,
+  HOLD
 };
 
 // define and initialise global variables
@@ -58,125 +58,119 @@ const int16_t max_trajectory_confidence = 5; // number of consecutive negative o
 static abi_event color_detection_ev;
 static void color_detection_cb(uint8_t __attribute__((unused)) sender_id,
                                int16_t __attribute__((unused)) pixel_x, int16_t __attribute__((unused)) pixel_y,
-                               int16_t __attribute__((unused)) pixel_width, int16_t __attribute__((unused)) pixel_height,
-                               int32_t quality, int16_t __attribute__((unused)) extra)
-{
-    color_count = quality;
+                               int16_t __attribute__((unused)) pixel_width,
+                               int16_t __attribute__((unused)) pixel_height,
+                               int32_t quality, int16_t __attribute__((unused)) extra) {
+  color_count = quality;
 }
 
-void mav_exercise_init(void)
-{
-    // bind our colorfilter callbacks to receive the color filter outputs
-    AbiBindMsgVISUAL_DETECTION(ORANGE_AVOIDER_VISUAL_DETECTION_ID, &color_detection_ev, color_detection_cb);
+void mav_exercise_init(void) {
+  // bind our colorfilter callbacks to receive the color filter outputs
+  AbiBindMsgVISUAL_DETECTION(ORANGE_AVOIDER_VISUAL_DETECTION_ID, &color_detection_ev, color_detection_cb);
 }
 
-void mav_exercise_periodic(void)
-{
-    // only evaluate our state machine if we are flying
-    if(!autopilot_in_flight()){
-        return;
-    }
+void mav_exercise_periodic(void) {
+  // only evaluate our state machine if we are flying
+  if (!autopilot_in_flight()) {
+    return;
+  }
 
-    // compute current color thresholds
-    // front_camera defined in airframe xml, with the video_capture module
-    int32_t color_count_threshold = oa_color_count_frac * front_camera.output_size.w * front_camera.output_size.h;
+  // compute current color thresholds
+  // front_camera defined in airframe xml, with the video_capture module
+  int32_t color_count_threshold = oa_color_count_frac * front_camera.output_size.w * front_camera.output_size.h;
 
-    PRINT("Color_count: %d  threshold: %d state: %d \n", color_count, color_count_threshold, navigation_state);
+  PRINT("Color_count: %d  threshold: %d state: %d \n", color_count, color_count_threshold, navigation_state);
 
-    // update our safe confidence using color threshold
-    if(color_count < color_count_threshold){
-        obstacle_free_confidence++;
-    } else {
-        obstacle_free_confidence -= 2;  // be more cautious with positive obstacle detections
-    }
+  // update our safe confidence using color threshold
+  if (color_count < color_count_threshold) {
+    obstacle_free_confidence++;
+  } else {
+    obstacle_free_confidence -= 2;  // be more cautious with positive obstacle detections
+  }
 
-    // bound obstacle_free_confidence
-    Bound(obstacle_free_confidence, 0, max_trajectory_confidence);
+  // bound obstacle_free_confidence
+  Bound(obstacle_free_confidence, 0, max_trajectory_confidence);
 
-    switch (navigation_state) {
-        case SAFE:
-            moveWaypointForward(WP_TRAJECTORY, 1.5f * moveDistance);
-            if (!InsideObstacleZone(WaypointX(WP_TRAJECTORY),WaypointY(WP_TRAJECTORY))) {
-                navigation_state = OUT_OF_BOUNDS;
-            } else if (obstacle_free_confidence == 0) {
-                navigation_state = OBSTACLE_FOUND;
-            } else {
-                moveWaypointForward(WP_GOAL, moveDistance);
-            }
-            break;
-        case OBSTACLE_FOUND:
-            // TODO Change behavior
-            // stop as soon as obstacle is found
-            waypoint_move_here_2d(WP_GOAL);
-            waypoint_move_here_2d(WP_TRAJECTORY);
+  switch (navigation_state) {
+    case SAFE:
+      moveWaypointForward(WP_TRAJECTORY, 1.5f * moveDistance);
+      if (!InsideObstacleZone(WaypointX(WP_TRAJECTORY), WaypointY(WP_TRAJECTORY))) {
+        navigation_state = OUT_OF_BOUNDS;
+      } else if (obstacle_free_confidence == 0) {
+        navigation_state = OBSTACLE_FOUND;
+      } else {
+        moveWaypointForward(WP_GOAL, moveDistance);
+      }
+      break;
+    case OBSTACLE_FOUND:
+      // TODO Change behavior
+      // stop as soon as obstacle is found
+      waypoint_move_here_2d(WP_GOAL);
+      waypoint_move_here_2d(WP_TRAJECTORY);
 
-            navigation_state = HOLD;
-            break;
-        case OUT_OF_BOUNDS:
-            // stop
-            waypoint_move_here_2d(WP_GOAL);
-            waypoint_move_here_2d(WP_TRAJECTORY);
+      navigation_state = HOLD;
+      break;
+    case OUT_OF_BOUNDS:
+      // stop
+      waypoint_move_here_2d(WP_GOAL);
+      waypoint_move_here_2d(WP_TRAJECTORY);
 
-            increase_nav_heading(oob_haeding_increment);
-            moveWaypointForward(WP_TRAJECTORY, 1.5f);
+      increase_nav_heading(oob_haeding_increment);
+      moveWaypointForward(WP_TRAJECTORY, 1.5f);
 
-            if (InsideObstacleZone(WaypointX(WP_TRAJECTORY),WaypointY(WP_TRAJECTORY))) {
-                // add offset to head back into arena
-                increase_nav_heading(oob_haeding_increment);
-                navigation_state = SAFE;
-            }
-            break;
-        case HOLD:
-        default:
-            break;
-    }
+      if (InsideObstacleZone(WaypointX(WP_TRAJECTORY), WaypointY(WP_TRAJECTORY))) {
+        // add offset to head back into arena
+        increase_nav_heading(oob_haeding_increment);
+        navigation_state = SAFE;
+      }
+      break;
+    case HOLD:
+    default:
+      break;
+  }
 }
 
 /*
  * Increases the NAV heading. Assumes heading is an INT32_ANGLE. It is bound in this function.
  */
-uint8_t increase_nav_heading(float incrementDegrees)
-{
-    float new_heading = stateGetNedToBodyEulers_f()->psi + RadOfDeg(incrementDegrees);
+uint8_t increase_nav_heading(float incrementDegrees) {
+  float new_heading = stateGetNedToBodyEulers_f()->psi + RadOfDeg(incrementDegrees);
 
-    // normalize heading to [-pi, pi]
-    FLOAT_ANGLE_NORMALIZE(new_heading);
+  // normalize heading to [-pi, pi]
+  FLOAT_ANGLE_NORMALIZE(new_heading);
 
-    // set heading
-    nav_heading = ANGLE_BFP_OF_REAL(new_heading);
+  // set heading
+  nav_heading = ANGLE_BFP_OF_REAL(new_heading);
 
-    return false;
+  return false;
 }
 
 /*
  * Calculates coordinates of a distance of 'distanceMeters' forward w.r.t. current position and heading
  */
-static uint8_t calculateForwards(struct EnuCoor_i *new_coor, float distanceMeters)
-{
-    float heading  = stateGetNedToBodyEulers_f()->psi;
+static uint8_t calculateForwards(struct EnuCoor_i *new_coor, float distanceMeters) {
+  float heading = stateGetNedToBodyEulers_f()->psi;
 
-    // Now determine where to place the waypoint you want to go to
-    new_coor->x = stateGetPositionEnu_i()->x + POS_BFP_OF_REAL(sinf(heading) * (distanceMeters));
-    new_coor->y = stateGetPositionEnu_i()->y + POS_BFP_OF_REAL(cosf(heading) * (distanceMeters));
-    return false;
+  // Now determine where to place the waypoint you want to go to
+  new_coor->x = stateGetPositionEnu_i()->x + POS_BFP_OF_REAL(sinf(heading) * (distanceMeters));
+  new_coor->y = stateGetPositionEnu_i()->y + POS_BFP_OF_REAL(cosf(heading) * (distanceMeters));
+  return false;
 }
 
 /*
  * Sets waypoint 'waypoint' to the coordinates of 'new_coor'
  */
-uint8_t moveWaypoint(uint8_t waypoint, struct EnuCoor_i *new_coor)
-{
-    waypoint_move_xy_i(waypoint, new_coor->x, new_coor->y);
-    return false;
+uint8_t moveWaypoint(uint8_t waypoint, struct EnuCoor_i *new_coor) {
+  waypoint_move_xy_i(waypoint, new_coor->x, new_coor->y);
+  return false;
 }
 
 /*
  * Calculates coordinates of distance forward and sets waypoint 'waypoint' to those coordinates
  */
-uint8_t moveWaypointForward(uint8_t waypoint, float distanceMeters)
-{
-    struct EnuCoor_i new_coor;
-    calculateForwards(&new_coor, distanceMeters);
-    moveWaypoint(waypoint, &new_coor);
-    return false;
+uint8_t moveWaypointForward(uint8_t waypoint, float distanceMeters) {
+  struct EnuCoor_i new_coor;
+  calculateForwards(&new_coor, distanceMeters);
+  moveWaypoint(waypoint, &new_coor);
+  return false;
 }
