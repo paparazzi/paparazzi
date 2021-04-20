627ecce31b543c506c94de2cdd7c43098265a920
Freek van Tienen <freek.v.tienen@gmail.com>
2021-04-20 14:10:25 +0200

    [actuators] Add UAVCAN actuators for chibios (#2511)
    * [actuators] Add UAVCAN actuators for chibios
    
    * [conf] Fixes for Nederdrone
    
    * [conf] Cleanup nederdrone
    
    * [boards] Fix FMUv5 i2c DMA
    
    * [conf] Fixes for Nederdrone4
    
    * [actuators] Add uavcan simulator
    
    * [uavcan] Rewrite to seperate module for actuators and sensors

---
+++ sw/airborne/subsystems/actuators/actuators_uavcan2.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2021 Freek van Tienen <freek.v.tienen@gmail.com>
+ *
+ * This file is part of Paparazzi.
+ *
+ * Paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * Paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef ACTUATORS_UAVCAN2_H
+#define ACTUATORS_UAVCAN2_H
+
+#include "actuators_uavcan.h"
+
+/** Stub file needed per interface because of generator */
+extern int16_t actuators_uavcan2_values[SERVOS_UAVCAN2_NB];
+
+#define ActuatorsUavcan2Init() actuators_uavcan_init(&uavcan2)
+#define ActuatorUavcan2Set(_i, _v) { actuators_uavcan2_values[_i] = _v; }
+#define ActuatorsUavcan2Commit()  actuators_uavcan_commit(&uavcan2, actuators_uavcan2_values, SERVOS_UAVCAN2_NB)
+
+#endif /* ACTUATORS_UAVCAN2_H */
