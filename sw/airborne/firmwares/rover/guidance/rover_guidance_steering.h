c5cecd843f3be20d92252b0ce0176ab68ae91753
jesusBV20 <jesbauti@ucm.es>
2021-10-10 12:40:18 +0200

    Main features for rover_steering_wheel
    feat:
    	- Configuración para el airframe del rover_steering.
    	- Configuración para el autopiloto del rover_steering.
    	- Configuración para el modulo de la placa del rover_steering.
    	- Configuración para la placa matek_f765 adaptada a rovers.
            - Configucación para el modulo de guiado del rover_steering.
    	- Configucación para el radio_control del rover_steering.
    	- Firmware para el sistema de guiado del rover_steering.

---
+++ sw/airborne/firmwares/rover/guidance/rover_guidance_steering.h
@@ -0,0 +1,13 @@
+/*  rover_guidance_steering.h */
+
+#ifndef ROVER_GUIDANCE_STEERING_H
+#define ROVER_GUIDANCE_STEERING_H
+
+#include "math/pprz_algebra_float.h"
+#include "generated/airframe.h"
+#include "std.h"
+
+extern void rover_guidance_steering_periodic(void);
+extern void rover_guidance_steering_init(void);
+
+#endif // ROVER_GUIDANCE_STEERING_H
