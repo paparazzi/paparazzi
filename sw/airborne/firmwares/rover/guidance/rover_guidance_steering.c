ae6bbdfd51670891c8f7a70022da60f92444781d
jesusBV20 <jesbauti@ucm.es>
2021-10-22 20:46:43 +0200

    Adapting rover's autopilot environment and (MANUAL mode is now functional)

--- sw/airborne/firmwares/rover/guidance/rover_guidance_steering.c
+++ sw/airborne/firmwares/rover/guidance/rover_guidance_steering.c
@@ -2,20 +2,57 @@
 
 #define AUTOPILOT_CORE_GUIDANCE_C
 
+// Mandatory headers
 #include "firmwares/rover/guidance/rover_guidance_steering.h"
+
 #include "generated/airframe.h"
 #include "generated/autopilot_core_guidance.h"
+
+#include "subsystems/datalink/telemetry.h"
+#include "subsystems/actuators/actuators_default.h"
+#include "subsystems/radio_control.h"
+#include "autopilot.h"
+#include "navigation.h"
 #include "state.h"
+
+#include <stdio.h>
+
+
+/** Send DEBUG Telemetry messages **/
+static void send_msg(struct transport_tx *trans, struct link_device *dev)
+{
+  uint8_t ap_mode  = autopilot_get_mode();
+  uint8_t nav_mode = nav.mode;
+  int16_t rc_t = radio_control.values[RADIO_THROTTLE];
+  int16_t rc_r = radio_control.values[RADIO_ROLL]; 
+  int16_t cmd_speed    = commands[COMMAND_SPEED];
+  int16_t cmd_steering = commands[COMMAND_STEERING];
+  int16_t ac_speed     = actuators[SERVO_MOTOR_THROTTLE_IDX];
+  int16_t ac_steering  = actuators[SERVO_MOTOR_STEERING_IDX];
+
+  pprz_msg_send_STEERING_ROVER_DATA(trans, dev, AC_ID, &ap_mode, &nav_mode, &rc_t, &rc_r, &cmd_speed, &cmd_steering, &ac_speed, &ac_steering);
+}
 
 void rover_guidance_steering_init(void)
 {
-  // from code generation
+  // guidance state machine from code generation
   autopilot_core_guidance_init();
+
+  // init debugging telemetry
+  register_periodic_telemetry(DefaultPeriodic, PPRZ_MSG_ID_STEERING_ROVER_DATA, send_msg);
 }
 
 void rover_guidance_steering_periodic(void)
 {
-  // from code generation
-  autopilot_core_guidance_periodic_task();
+  //guidance state machine from code generation 
+  #if EXTERN_AP == AP_MODE_NAV 
+    autopilot_core_guidance_periodic_task();
+  #endif
+
+  // periodic debugging telemetry
+  register_periodic_telemetry(DefaultPeriodic, PPRZ_MSG_ID_STEERING_ROVER_DATA, send_msg);
 }
+
+
+
 
--- sw/airborne/firmwares/rover/guidance/rover_guidance_steering.h
+++ sw/airborne/firmwares/rover/guidance/rover_guidance_steering.h
@@ -3,11 +3,24 @@
 #ifndef ROVER_GUIDANCE_STEERING_H
 #define ROVER_GUIDANCE_STEERING_H
 
-#include "math/pprz_algebra_float.h"
+/** Generated airframe.h from airframe.xml
+ * - fun: SetActuatorsFromCommands
+ * - var: commands
+ * - var: 
+ **/
+
 #include "generated/airframe.h"
+
+#include "math/pprz_algebra_float.h"
 #include "std.h"
 
-extern void rover_guidance_steering_periodic(void);
 extern void rover_guidance_steering_init(void);
+extern void rover_guidance_steering_periodic(void);
+
+
+// helper macro to set AP throttle value
+#define SetAPThrottleFromCommands(_cmd_x, _cmd_y) { \
+    autopilot.throttle = sqrtf(((_cmd_x * _cmd_x) + (_cmd_y * _cmd_y)) / 2.f); \
+  }
 
 #endif // ROVER_GUIDANCE_STEERING_H
