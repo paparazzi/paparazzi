389754d1f860852da78813f5a1a37fc4d528874a
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2021-11-17 17:15:10 +0100

    [cv] test optical flow module and fix warnings

--- tests/modules/dummy.h
+++ tests/modules/dummy.h
@@ -4,4 +4,9 @@
 //#define BOARD_DUMY
 //
 
+/* Cameras */
+#include "peripherals/video_device.h"
+extern struct video_config_t bottom_camera;
+extern struct video_config_t front_camera;
+
 #endif // CONFIG_DUMMY_H
