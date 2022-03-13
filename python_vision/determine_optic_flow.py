068bd273eca2d8dc48152b8e33e8baeee26ce190
NoahWe <74419424+NoahWe@users.noreply.github.com>
2022-03-13 12:04:26 +0100

    start performance analysis optical flow

--- python_vision/determine_optic_flow.py
+++ python_vision/determine_optic_flow.py
@@ -9,47 +9,59 @@ Determine optic flow given two images
 
 import cv2
 import numpy as np
+import time
 from matplotlib import pyplot as plt
 from Edge_detect import *
 
 
+def determine_optic_flow(filename_1, filename_2, window_x, window_y, corners, method="ShiTomasi", max_points=100, graphics=True):
+    if type(filename_1) is str:
+        BGR_1 = cv2.imread(filename_1)
+    else:
+        BGR_1 = filename_1  # load the BGR color image:
 
-def determine_optic_flow(filename_1, filename_2, method="ShiTomasi", max_points=100, graphics=True):
-
-    BGR_1 = cv2.imread(filename_1);
-    # BGR_1 = filename_1 # load the BGR color image:
     gray_1 = cv2.cvtColor(BGR_1, cv2.COLOR_BGR2GRAY)  # convert the image to gray scale
-    BGR_2 = cv2.imread(filename_2);
-    # BGR_2 = filename_2     # load the BGR color image:
-    gray_2 = cv2.cvtColor(BGR_2, cv2.COLOR_BGR2GRAY) # convert the image to gray scale:
+
+    if type(filename_2) is str:
+        BGR_2 = cv2.imread(filename_2)
+    else:
+        BGR_2 = filename_2  # load the BGR color image:
+
+    gray_2 = cv2.cvtColor(BGR_2, cv2.COLOR_BGR2GRAY)  # convert the image to gray scale:
 
     # (1) Detect features:
-    if method == 'Harris':
-        corners, inds = edge_detect_Harris(image=BGR_1, image_gray=gray_1,blockSize=2,ksize=3,k=0.04,threshold_factor=0.01)
 
-    elif method == 'FAST':
+    if corners is None:
 
-        corners = edge_detect_FAST(image=BGR_1, image_gray=gray_1, threshold=70, max_points=max_points)
+        if method == 'Harris':
+            corners, inds = edge_detect_Harris(image=BGR_1, image_gray=gray_1, blockSize=2, ksize=3, k=0.04,
+                                               threshold_factor=0.01)
 
-    elif method == 'ShiTomasi':
+        elif method == 'FAST':
 
-        corners = edge_detect_ShiTomasi(image=BGR_1, image_gray=gray_1, maxCorners=100, qualityLevel=0.01, minDistance=10)
+            corners = edge_detect_FAST(image=BGR_1, image_gray=gray_1, threshold=70, max_points=max_points)
 
-    elif method == "Tomasso":
-        edges = find_edge_OF(BGR_1)
-        inds = np.where(edges == 255)
-        corners = np.stack((inds[1].T, inds[0].T), axis=1)
-        corners = corners.astype("float32")
+        elif method == 'ShiTomasi':
 
+            corners = edge_detect_ShiTomasi(image=BGR_1, image_gray=gray_1, maxCorners=100, qualityLevel=0.01,
+                                            minDistance=10)
+
+        elif method == "Tomasso":
+            edges = find_edge_OF(BGR_1)
+            inds = np.where(edges == 255)
+            corners = np.stack((inds[1].T, inds[0].T), axis=1)
+            corners = corners.astype("float32")
 
     # (2) Track the features to the next frame:
 
     # Parameters for lucas kanade optical flow
-    lk_params = dict(winSize=(15, 15),
+    lk_params = dict(winSize=(window_x, window_y),
                      maxLevel=2,
-                     criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+                     criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 100, 0.03))
 
     # calculate optical flow
+    start = time.time()
+
     corners_new, status, error_match = cv2.calcOpticalFlowPyrLK(gray_1, gray_2, corners, None, **lk_params)
 
     # filter the points by their status:
@@ -57,6 +69,10 @@ def determine_optic_flow(filename_1, filename_2, method="ShiTomasi", max_points=
     # corners_new = corners_new[status == 1, :];
 
     flow_vectors = corners_new - corners
+
+    end = time.time()
+
+    # print("Optic Flow took: {} s".format(round(end - start, 5)))
 
     if (graphics):
         im = (0.5 * BGR_1.copy().astype(float) + 0.5 * BGR_2.copy().astype(float)) / 255.0
@@ -70,8 +86,13 @@ def determine_optic_flow(filename_1, filename_2, method="ShiTomasi", max_points=
         plt.figure();
         plt.imshow(im);
         plt.title('Optical flow');
+        cv2.waitKey(200)
+        cv2.destroyAllWindows()
+        plt.close()
+
+    return end - start, flow_vectors, corners
 
 
 if __name__ == "__main__":
     # Determine optic flow on the images provided on the repository:
-    determine_optic_flow('test_image_1.jpg', 'test_image_2.jpg')
+    determine_optic_flow('bebop_flowers_1.jpg', 'bebop_flowers_2.jpg')
