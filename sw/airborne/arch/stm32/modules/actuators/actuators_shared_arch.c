37c065da2962eadee06f9409aa0152753d98a641
Fabien-B <fabien.bonneval@gmail.com>
2023-01-27 10:06:30 +0100

    [libopencm3] Replace timer_reset by rcc_periph_reset_pulse.

--- sw/airborne/arch/stm32/modules/actuators/actuators_shared_arch.c
+++ sw/airborne/arch/stm32/modules/actuators/actuators_shared_arch.c
@@ -58,10 +58,6 @@ void actuators_pwm_arch_channel_init(uint32_t timer_peripheral,
  */
 void set_servo_timer(uint32_t timer, uint32_t freq, uint8_t channels_mask)
 {
-  // WARNING, this reset is only implemented for TIM1-8 in libopencm3!!
-  
-  //FIXME removed deprecated timer_reset(timer), should it be replaced by rcc_periph_reset_pulse(RST_TIMx) ?
-
   /* Timer global mode:
    * - No divider.
    * - Alignement edge.
