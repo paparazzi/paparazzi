b0e0170ae2c0e5002eb034dd46d901bf96a5915c
Christophe De Wagter <dewagter@gmail.com>
2023-02-13 08:05:00 -0800

    [installer] Use Qt5 instead of Tkinter
    Final clean
    
    Clean up the root
    
    typo
    
    Disable buttons while installing.

--- install.sh
+++ install.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install -f -y python3
-sudo apt-get install -f -y python3-tk
-python3 ./install.py
+sudo apt-get install -f -y python3 python3-pyqt5
+python3 ./sw/tools/install.py
 
-#make clean
-#make
-
-#@echo "Make BEBOP OpenCV"
-#make -C ./sw/ext/opencv_bebop/
 
