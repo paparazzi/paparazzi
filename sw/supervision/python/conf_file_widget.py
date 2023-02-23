e6ffbd3fcbe44e16080d0cbaaddec6cc59724113
Fabien-B <fabien.bonneval@gmail.com>
2023-02-23 16:51:06 +0100

    [supervision] Fix telemetry select base path.

--- sw/supervision/python/conf_file_widget.py
+++ sw/supervision/python/conf_file_widget.py
@@ -11,9 +11,9 @@ class ConfFileWidget(QWidget, Ui_FileConf):
     file_changed = QtCore.pyqtSignal()
     edit_alt = QtCore.pyqtSignal(str)
 
-    def __init__(self, title: str, parent=None):
+    def __init__(self, title: str, conf_subdir: str, parent=None):
         QWidget.__init__(self, parent=parent)
-        self.nature = title.lower().replace(" ", "_") + "s"
+        self.conf_subdir = conf_subdir
         self.path = None
         self.setupUi(self)
         self.edit_alt_button.hide()
@@ -31,9 +31,8 @@ class ConfFileWidget(QWidget, Ui_FileConf):
             utils.edit_file(self.path)
 
     def select_file(self):
-        base_path = os.path.join(utils.CONF_DIR, self.nature)
-        print(base_path)
-        (path, _) = QFileDialog().getOpenFileName(self, "Select {} file".format(self.nature),
+        base_path = os.path.join(utils.CONF_DIR, self.conf_subdir)
+        (path, _) = QFileDialog().getOpenFileName(self, "Select {} file".format(self.title_label.text()),
                                                   base_path, "Xml (*.xml)")
         if path != "":
             self.path = os.path.relpath(path, utils.CONF_DIR)
