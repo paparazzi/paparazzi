0b82505bbd700ab75429e2290d1e829a2494915a
Christophe De Wagter <dewagter@gmail.com>
2023-05-02 07:17:17 -0700

    Camera ID was already added.

--- sw/airborne/modules/computer_vision/opticflow_module.c
+++ sw/airborne/modules/computer_vision/opticflow_module.c
@@ -142,8 +142,7 @@ void opticflow_module_run(void)
                              opticflow_result[idx_camera].flow_der_x,
                              opticflow_result[idx_camera].flow_der_y,
                              opticflow_result[idx_camera].noise_measurement,
-                             opticflow_result[idx_camera].div_size,
-                             (uint8_t) idx_camera);
+                             opticflow_result[idx_camera].div_size);
       //TODO Find an appropriate quality measure for the noise model in the state filter, for now it is tracked_cnt
       if (opticflow_result[idx_camera].noise_measurement < 0.8) {
         AbiSendMsgVELOCITY_ESTIMATE(VEL_OPTICFLOW_ID + idx_camera, now_ts,
