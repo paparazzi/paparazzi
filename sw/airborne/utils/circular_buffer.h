5caff5ae5d51100a12ee6e65c62dad42ce10ea73
Fabien-B <fabien.bonneval@gmail.com>
2023-07-11 13:42:25 +0200

    Adds licence header a documentation.

--- sw/airborne/utils/circular_buffer.h
+++ sw/airborne/utils/circular_buffer.h
@@ -1,14 +1,30 @@
+/*
+ * General purpose circular buffer
+ *
+ * Copyright (C) 2021 Fabien-B <fabien-b@github.com> 
+ *
+ * This file is part of paparazzi. See LICENCE file.
+ */
+
 #pragma once
 
 #include <stdint.h>
 #include <stddef.h>
 
-struct circular_buffer
-{
+/**
+ * This is a general purpose circular buffer for storing buffers in a FIFO order.
+ * A current limitation: the size of the buffers is limited to 255, is size beeing stored on a uint8_t.
+ * 
+ * Declare a \ref circular_buffer and allocate a buffer that will outlive it.
+ * Initialize the \ref circular_buffer using \ref circular_buffer_init.
+ * 
+*/
+
+struct circular_buffer {
   size_t read_offset;
   size_t write_offset;
   size_t _buf_len;
-  uint8_t* _buf;
+  uint8_t *_buf;
 };
 
 enum cir_error {
@@ -23,7 +39,7 @@ enum cir_error {
  * @param buffer buffer used internally by the circular buffer
  * @param len size of \p buffer
 */
-void circular_buffer_init(struct circular_buffer* cb, uint8_t* buffer, size_t len);
+void circular_buffer_init(struct circular_buffer *cb, uint8_t *buffer, size_t len);
 
 
 /**
@@ -33,7 +49,7 @@ void circular_buffer_init(struct circular_buffer* cb, uint8_t* buffer, size_t le
  * @param len size of \p buf
  * @return Size of the data copied to \p buf, or an error code if negative.
 */
-int circular_buffer_get(struct circular_buffer* cb, uint8_t* buf, size_t len);
+int circular_buffer_get(struct circular_buffer *cb, uint8_t *buf, size_t len);
 
 /**
  * @brief Copy \p buf in the circular buffer
@@ -42,4 +58,4 @@ int circular_buffer_get(struct circular_buffer* cb, uint8_t* buf, size_t len);
  * @param len Size of \p buf
  * @return 0 on success, Error code if negative
 */
-int circular_buffer_put(struct circular_buffer* cb, uint8_t* buf, size_t len);
+int circular_buffer_put(struct circular_buffer *cb, uint8_t *buf, size_t len);
