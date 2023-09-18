e7fb123a80af7fcf91e012aec47161c65a633cd0
Christophe De Wagter <dewagter@gmail.com>
2023-09-18 02:14:42 -0700

    [modules] airspeed UAVCAN
    ABI

---
+++ sw/airborne/modules/sensors/airspeed_uavcan.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2021 Freek van Tienen <freek.v.tienen@gmail.com>
+ *
+ * This file is part of Paparazzi.
+ *
+ * Paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * Paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/** @file modules/sensors/airspeed_uavcan.h
+ * Airspeed sensor on the uavcan bus
+ */
+
+#ifndef AIRSPEED_UAVCAN_H
+#define AIRSPEED_UAVCAN_H
+
+/* External variables */
+extern struct airspeed_uavcan_s {
+  float diff_p;
+  float temperature;
+} airspeed_uavcan;
+
+/* External functions */
+extern void airspeed_uavcan_init(void);
+
+
+
+#endif /* AIRSPEED_UAVCAN_H */
