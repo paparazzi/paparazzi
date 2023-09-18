014cffcbbcaddd935929498ce53a6b6ad846d6b4
Christophe De Wagter <dewagter@gmail.com>
2023-09-18 02:23:26 -0700

    [modules] system identification updates: auto-doublet-sequence, new doublets, RC-triggered chirps, testing
    extra sysid
    
    rename generic
    
    test compile

--- sw/airborne/modules/system_identification/sys_id_doublet.c
+++ sw/airborne/modules/system_identification/sys_id_doublet.c
@@ -48,17 +48,21 @@
 #define DOUBLET_ENABLED TRUE
 #endif
 
+#ifdef DOUBLET_RADIO_CHANNEL
+#include "modules/radio_control/radio_control.h"
+pprz_t previous_radio_value_doublet = 0;
+#endif
 
 
 static struct doublet_t doublet;
 
 uint8_t doublet_active = false;
-uint8_t doublet_mode_3211 = false;
+uint8_t doublet_mode = 0;
 
 uint8_t doublet_axis = 0;
 
-pprz_t doublet_amplitude = 4500;
-float doublet_length_s = 20.0f;
+pprz_t doublet_amplitude = 0;
+float doublet_length_s = 0.5f;
 float doublet_extra_waiting_time_s = 0.0f;
 
 
@@ -83,7 +87,7 @@ static void set_current_doublet_values(void)
 static void send_doublet(struct transport_tx *trans, struct link_device *dev){
     pprz_msg_send_DOUBLET(trans, dev, AC_ID, &doublet_active,
                         &doublet_axis, &doublet_amplitude,
-                        &current_doublet_values[doublet_axis], &doublet_mode_3211);
+                        &current_doublet_values[doublet_axis], &doublet_mode);
 }
 
 static void start_doublet(void)
@@ -107,8 +111,15 @@ uint8_t sys_id_doublet_running(void){
 void sys_id_doublet_activate_handler(uint8_t activate)
 {
     doublet_active = activate;
+    #ifdef DOUBLET_RADIO_CHANNEL
+    // Don't activate doublet when radio signal is low
+        if (radio_control.values[DOUBLET_RADIO_CHANNEL] < 1750)
+        {
+            doublet_active = 0;
+        }
+    #endif
     if (doublet_active) {
-        doublet_init(&doublet, doublet_length_s, doublet_extra_waiting_time_s, get_sys_time_float(), doublet_mode_3211);
+        doublet_init(&doublet, doublet_length_s, doublet_extra_waiting_time_s, get_sys_time_float(), doublet_mode);
         start_doublet();
     } else {
         stop_doublet();
@@ -122,13 +133,13 @@ void sys_id_doublet_axis_handler(uint8_t axis)
     }
 }
 
-void sys_id_doublet_mod3211_handler(uint8_t mode){
-    doublet_mode_3211 = mode;
+void sys_id_doublet_mod_handler(uint8_t mode){
+    doublet_mode = mode;
 }
 
 void sys_id_doublet_init(void)
 {
-    doublet_init(&doublet, doublet_length_s, doublet_extra_waiting_time_s, get_sys_time_float(), doublet_mode_3211);
+    doublet_init(&doublet, doublet_length_s, doublet_extra_waiting_time_s, get_sys_time_float(), doublet_mode);
 
     set_current_doublet_values();
     register_periodic_telemetry(DefaultPeriodic, PPRZ_MSG_ID_DOUBLET, send_doublet);
@@ -139,7 +150,28 @@ void sys_id_doublet_init(void)
 }
 
 void sys_id_doublet_run(void)
-{
+{   
+    #ifdef DOUBLET_RADIO_CHANNEL
+        // Check if doublet switched on when off before
+        if (previous_radio_value_doublet < 1750)
+        {
+            if (radio_control.values[DOUBLET_RADIO_CHANNEL] > 1750)
+            {
+                // Activate doublet
+                sys_id_doublet_activate_handler(1);
+            }
+        }
+        // Check if doublet switched off when on before
+        if (previous_radio_value_doublet > 1750)
+        {
+            if (radio_control.values[DOUBLET_RADIO_CHANNEL] < 1750)
+            {
+                // Deactivate doublet
+                sys_id_doublet_activate_handler(0);
+            }
+        }
+        previous_radio_value_doublet = radio_control.values[DOUBLET_RADIO_CHANNEL];
+    #endif
     if (doublet_active) {
         if (!doublet_is_running(&doublet, get_sys_time_float())) {
             stop_doublet();
