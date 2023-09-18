4dc20f82caabb3cb26405e9492287db6f6fd49c0
Christophe De Wagter <dewagter@gmail.com>
2023-09-18 02:42:48 -0700

    [boards] orange cube changed outputs for inputs

--- sw/airborne/boards/cube/orange/mcuconf.h
+++ sw/airborne/boards/cube/orange/mcuconf.h
@@ -339,7 +339,7 @@
 #define STM32_ICU_USE_TIM1                  FALSE
 #define STM32_ICU_USE_TIM2                  FALSE
 #define STM32_ICU_USE_TIM3                  FALSE
-#define STM32_ICU_USE_TIM4                  FALSE
+#define STM32_ICU_USE_TIM4                  TRUE
 #define STM32_ICU_USE_TIM5                  FALSE
 #define STM32_ICU_USE_TIM8                  FALSE
 #define STM32_ICU_USE_TIM12                 FALSE
@@ -367,7 +367,7 @@
 #define STM32_PWM_USE_TIM1                  TRUE
 #define STM32_PWM_USE_TIM2                  FALSE
 #define STM32_PWM_USE_TIM3                  FALSE
-#define STM32_PWM_USE_TIM4                  TRUE
+#define STM32_PWM_USE_TIM4                  FALSE
 #define STM32_PWM_USE_TIM5                  FALSE
 #define STM32_PWM_USE_TIM8                  FALSE
 #define STM32_PWM_USE_TIM9                  FALSE
