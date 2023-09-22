640d94102ddcc63998434872ad5a8600b75198c4
Christophe De Wagter <dewagter@gmail.com>
2023-09-22 13:12:32 +0200

    [geofence] When altitude of the geofence is not changed, this is not a warning but Info. When the WP are too low it remains a warning. (#3097)

--- sw/tools/generators/gen_flight_plan.ml
+++ sw/tools/generators/gen_flight_plan.ml
@@ -1065,7 +1065,7 @@ let print_flight_plan_h = fun xml ref0 xml_file out_file ->
       else if geofence_max_alt < (float_of_string alt) then
         fprintf stderr "\nWarning: Geofence max altitude below default waypoint alt (%.0f < %.0f)\n" geofence_max_alt (float_of_string alt);
       Xml2h.define_out out "GEOFENCE_MAX_ALTITUDE" (sof geofence_max_alt);
-      fprintf stderr "\nWarning: Geofence max altitude set to %.0f\n" geofence_max_alt;
+      fprintf stderr "\nInfo: Geofence max altitude set to %.0f\n" geofence_max_alt;
     with
       _ -> ()
   end;
