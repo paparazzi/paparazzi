593ba63b016e9f8bc20169a23bce9b9bf7503fb7
Christophe De Wagter <dewagter@gmail.com>
2023-09-23 14:16:18 +0200

    When no if-statement was triggered, this is information and not a warning.

--- sw/tools/generators/gen_flight_plan.ml
+++ sw/tools/generators/gen_flight_plan.ml
@@ -1086,7 +1086,7 @@ let print_flight_plan_h = fun xml ref0 xml_file out_file ->
       else if (geofence_max_height +. !ground_alt) < (float_of_string alt) then
         fprintf stderr "\nWarning: Geofence max AGL below default waypoint AGL (%.0f < %.0f)\n" (geofence_max_height +. !ground_alt) (float_of_string alt);
       Xml2h.define_out out "GEOFENCE_MAX_HEIGHT" (sof geofence_max_height);
-      fprintf stderr "\nWarning: Geofence max AGL set to %.0f\n" geofence_max_height;
+      fprintf stderr "\nInfo: Geofence max AGL set to %.0f\n" geofence_max_height;
     with
       _ -> ()
   end;
