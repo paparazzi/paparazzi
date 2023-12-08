e7db345a58cfa4520b48be1a641403d8e0856ede
Freek van Tienen <freek.v.tienen@gmail.com>
2023-12-08 14:16:41 +0100

    [modules] Add UAVCAN range sensor

---
+++ sw/airborne/modules/sensors/range_sensor_uavcan.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2023 Freek van Tienen <freek.v.tienen@gmail.com>
+ *
+ * This file is part of Paparazzi.
+ *
+ * Paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * Paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/** @file modules/sensors/range_sensor_uavcan.h
+ * Range sensor sensor on the uavcan bus
+ */
+
+#ifndef RANGE_SENSOR_UAVCAN_H
+#define RANGE_SENSOR_UAVCAN_H
+
+/* External functions */
+extern void range_sensor_uavcan_init(void);
+
+#endif /* RANGE_SENSOR_UAVCAN_H */
