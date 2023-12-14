7c8dccf268d4bb436bceb6e225404817c375ae41
Christophe De Wagter <dewagter@gmail.com>
2023-12-14 20:31:10 +0100

    Remove command_pitch and command_roll and command_yaw

--- sw/airborne/firmwares/rotorcraft/autopilot_utils.c
+++ sw/airborne/firmwares/rotorcraft/autopilot_utils.c
@@ -121,7 +121,7 @@ uint8_t ap_mode_of_two_switches(void)
 void WEAK set_rotorcraft_commands(pprz_t *cmd_out, int32_t *cmd_in, bool in_flight __attribute__((unused)), bool motors_on __attribute__((unused)))
 {
 #if !ROTORCRAFT_IS_HELI
-#if !ROTORCRAFT_COMMANDS_YAW_ALWAYS_ENABLED
+#if !ROTORCRAFT_COMMANDS_YAW_ALWAYS_ENABLED && defined(COMMAND_YAW)
   if (!in_flight) {
     cmd_in[COMMAND_YAW] = 0;
   }
@@ -130,9 +130,8 @@ void WEAK set_rotorcraft_commands(pprz_t *cmd_out, int32_t *cmd_in, bool in_flig
     cmd_in[COMMAND_THRUST] = 0;
   }
 #endif
-  cmd_out[COMMAND_ROLL] = cmd_in[COMMAND_ROLL];
-  cmd_out[COMMAND_PITCH] = cmd_in[COMMAND_PITCH];
-  cmd_out[COMMAND_YAW] = cmd_in[COMMAND_YAW];
-  cmd_out[COMMAND_THRUST] = cmd_in[COMMAND_THRUST];
+  for (int i=0; i < COMMANDS_NB; i++) {
+    cmd_out[i] = cmd_in[i];
+  }
 }
 
