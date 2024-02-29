ae5f47a5187fb972c12b2dd3e3642bd1f90ae90e
Fabien-B <fabien.bonneval@gmail.com>
2024-02-29 10:46:01 +0100

    Add vl53l5cx driver  (#3240)
    Based on St ULD

---
+++ sw/airborne/modules/lidar/vl53l5cx_platform.h
@@ -0,0 +1,186 @@
+/*
+ * Copyright (C) 2024 Fabien-B <name.surname@gmail.com>
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ */
+
+
+#ifndef _PLATFORM_H_
+#define _PLATFORM_H_
+#pragma once
+
+#include <stdint.h>
+#include <string.h>
+#include "mcu_periph/i2c.h"
+
+#define VL53L5CX_I2C_BUF_SIZE 512
+
+
+enum VL53L5CX_ERRORS {
+  VL53L5CX_NO_ERROR = 0,
+  VL53L5CX_NOT_DETECTED = 1,
+  VL53L5CX_ULD_LOADING_FAILED = 2,
+  VL53L5CX_SET_RESOLUTION_FAILED = 3,
+  VL53L5CX_RUNTIME_FAILURE = 4,
+};
+
+/**
+ * @brief Structure VL53L5CX_Platform needs to be filled by the customer,
+ * depending on his platform. At least, it contains the VL53L5CX I2C address.
+ * Some additional fields can be added, as descriptors, or platform
+ * dependencies. Anything added into this structure is visible into the platform
+ * layer.
+ */
+
+typedef struct
+{
+	/* To be filled with customer's platform. At least an I2C address/descriptor
+	 * needs to be added */
+	/* Example for most standard platform : I2C address of sensor */
+  uint16_t  			address;
+	//I2CDriver*      i2cp;
+	struct i2c_periph *i2cdev;
+	bool data_available;
+  int16_t distances_mm[64];
+
+	enum VL53L5CX_ERRORS error_code;
+	uint8_t buf[VL53L5CX_I2C_BUF_SIZE+2];
+	void* user_data;
+
+} VL53L5CX_Platform;
+
+/*
+ * @brief The macro below is used to define the number of target per zone sent
+ * through I2C. This value can be changed by user, in order to tune I2C
+ * transaction, and also the total memory size (a lower number of target per
+ * zone means a lower RAM). The value must be between 1 and 4.
+ */
+
+#define 	VL53L5CX_NB_TARGET_PER_ZONE		1U
+
+/*
+ * @brief The macro below can be used to avoid data conversion into the driver.
+ * By default there is a conversion between firmware and user data. Using this macro
+ * allows to use the firmware format instead of user format. The firmware format allows
+ * an increased precision.
+ */
+
+// #define 	VL53L5CX_USE_RAW_FORMAT
+
+/*
+ * @brief All macro below are used to configure the sensor output. User can
+ * define some macros if he wants to disable selected output, in order to reduce
+ * I2C access.
+ */
+
+// #define VL53L5CX_DISABLE_AMBIENT_PER_SPAD
+// #define VL53L5CX_DISABLE_NB_SPADS_ENABLED
+// #define VL53L5CX_DISABLE_NB_TARGET_DETECTED
+// #define VL53L5CX_DISABLE_SIGNAL_PER_SPAD
+// #define VL53L5CX_DISABLE_RANGE_SIGMA_MM
+// #define VL53L5CX_DISABLE_DISTANCE_MM
+// #define VL53L5CX_DISABLE_REFLECTANCE_PERCENT
+// #define VL53L5CX_DISABLE_TARGET_STATUS
+// #define VL53L5CX_DISABLE_MOTION_INDICATOR
+
+/**
+ * @param (VL53L5CX_Platform*) p_platform : Pointer of VL53L5CX platform
+ * structure.
+ * @param (uint16_t) Address : I2C location of value to read.
+ * @param (uint8_t) *p_values : Pointer of value to read.
+ * @return (uint8_t) status : 0 if OK
+ */
+
+uint8_t RdByte(
+		VL53L5CX_Platform *p_platform,
+		uint16_t RegisterAdress,
+		uint8_t *p_value);
+
+/**
+ * @brief Mandatory function used to write one single byte.
+ * @param (VL53L5CX_Platform*) p_platform : Pointer of VL53L5CX platform
+ * structure.
+ * @param (uint16_t) Address : I2C location of value to read.
+ * @param (uint8_t) value : Pointer of value to write.
+ * @return (uint8_t) status : 0 if OK
+ */
+
+uint8_t WrByte(
+		VL53L5CX_Platform *p_platform,
+		uint16_t RegisterAdress,
+		uint8_t value);
+
+/**
+ * @brief Mandatory function used to read multiples bytes.
+ * @param (VL53L5CX_Platform*) p_platform : Pointer of VL53L5CX platform
+ * structure.
+ * @param (uint16_t) Address : I2C location of values to read.
+ * @param (uint8_t) *p_values : Buffer of bytes to read.
+ * @param (uint32_t) size : Size of *p_values buffer.
+ * @return (uint8_t) status : 0 if OK
+ */
+
+uint8_t RdMulti(
+		VL53L5CX_Platform *p_platform,
+		uint16_t RegisterAdress,
+		uint8_t *p_values,
+		uint32_t size);
+
+/**
+ * @brief Mandatory function used to write multiples bytes.
+ * @param (VL53L5CX_Platform*) p_platform : Pointer of VL53L5CX platform
+ * structure.
+ * @param (uint16_t) Address : I2C location of values to write.
+ * @param (uint8_t) *p_values : Buffer of bytes to write.
+ * @param (uint32_t) size : Size of *p_values buffer.
+ * @return (uint8_t) status : 0 if OK
+ */
+
+uint8_t WrMulti(
+		VL53L5CX_Platform *p_platform,
+		uint16_t RegisterAdress,
+		uint8_t *p_values,
+		uint32_t size);
+
+/**
+ * @brief Optional function, only used to perform an hardware reset of the
+ * sensor. This function is not used in the API, but it can be used by the host.
+ * This function is not mandatory to fill if user don't want to reset the
+ * sensor.
+ * @param (VL53L5CX_Platform*) p_platform : Pointer of VL53L5CX platform
+ * structure.
+ * @return (uint8_t) status : 0 if OK
+ */
+
+// uint8_t Reset_Sensor(
+// 		VL53L5CX_Platform *p_platform);
+
+/**
+ * @brief Mandatory function, used to swap a buffer. The buffer size is always a
+ * multiple of 4 (4, 8, 12, 16, ...).
+ * @param (uint8_t*) buffer : Buffer to swap, generally uint32_t
+ * @param (uint16_t) size : Buffer size to swap
+ */
+
+void SwapBuffer(
+		uint8_t 		*buffer,
+		uint16_t 	 	 size);
+/**
+ * @brief Mandatory function, used to wait during an amount of time. It must be
+ * filled as it's used into the API.
+ * @param (VL53L5CX_Platform*) p_platform : Pointer of VL53L5CX platform
+ * structure.
+ * @param (uint32_t) TimeMs : Time to wait in ms.
+ * @return (uint8_t) status : 0 if wait is finished.
+ */
+
+uint8_t WaitMs(
+		VL53L5CX_Platform *p_platform,
+		uint32_t TimeMs);
+
+#endif	// _PLATFORM_H_
