329c2540a161743606af612b525709ba6ec36ee2
Ewoud Smeur <e.j.j.smeur@tudelft.nl>
2024-04-23 10:41:34 +0200

    max bank in deg

--- sw/airborne/firmwares/rotorcraft/guidance/guidance_indi_hybrid_tailsitter.c
+++ sw/airborne/firmwares/rotorcraft/guidance/guidance_indi_hybrid_tailsitter.c
@@ -88,13 +88,14 @@ void guidance_indi_calcg_wing(float Gmat[GUIDANCE_INDI_HYBRID_V][GUIDANCE_INDI_H
 
 void guidance_indi_hybrid_set_wls_settings(float body_v[3] UNUSED, float roll_angle, float pitch_angle)
 {
+  float roll_limit_rad = RadOfDeg(guidance_indi_max_bank);
   // Set lower limits
-  du_min_gih[0] = -guidance_indi_max_bank - roll_angle; // roll
+  du_min_gih[0] = -roll_limit_rad - roll_angle; // roll
   du_min_gih[1] = RadOfDeg(guidance_indi_min_pitch) - pitch_angle; // pitch
   du_min_gih[2] = (MAX_PPRZ - actuator_state_filt_vect[0]) * g1g2[3][0] + (MAX_PPRZ - actuator_state_filt_vect[1]) * g1g2[3][1] + (MAX_PPRZ - actuator_state_filt_vect[2]) * g1g2[3][2] + (MAX_PPRZ - actuator_state_filt_vect[3]) * g1g2[3][3];
 
   // Set upper limits limits
-  du_max_gih[0] = guidance_indi_max_bank - roll_angle; //roll
+  du_max_gih[0] = roll_limit_rad - roll_angle; //roll
   du_max_gih[1] = RadOfDeg(GUIDANCE_INDI_MAX_PITCH) - pitch_angle; // pitch
   du_max_gih[2] = -(actuator_state_filt_vect[0]*g1g2[3][0] + actuator_state_filt_vect[1]*g1g2[3][1] + actuator_state_filt_vect[2]*g1g2[3][2] + actuator_state_filt_vect[3]*g1g2[3][3]);
 
