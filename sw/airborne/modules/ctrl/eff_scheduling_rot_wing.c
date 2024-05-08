a07c7b884afc73645eaba46d15a760b8e962246f
Ewoud Smeur <e.j.j.smeur@tudelft.nl>
2024-05-08 10:06:05 +0200

    takeoff procedure update

--- sw/airborne/modules/ctrl/eff_scheduling_rot_wing.c
+++ sw/airborne/modules/ctrl/eff_scheduling_rot_wing.c
@@ -30,6 +30,8 @@
 
 #include "firmwares/rotorcraft/stabilization/stabilization_indi.h"
 
+#include "autopilot.h"
+
 #include "modules/actuators/actuators.h"
 #include "modules/core/abi.h"
 
@@ -294,8 +296,10 @@ void eff_scheduling_rot_wing_update_hover_motor_effectiveness(void)
   Bound(roll_motor_p_eff_right, -1, -0.00001);
 
   float roll_motor_p_eff_left = (dM_dpprz_left * eff_sched_var.cosr + eff_sched_p.hover_roll_roll_coef[0] * eff_sched_var.wing_rotation_rad * eff_sched_var.wing_rotation_rad * eff_sched_var.airspeed * eff_sched_var.cosr) / eff_sched_var.Ixx;
-  float roll_motor_airspeed_compensation = eff_sched_p.hover_roll_roll_coef[1] * eff_sched_var.airspeed * eff_sched_var.cosr / eff_sched_var.Ixx;
-  roll_motor_p_eff_left += roll_motor_airspeed_compensation;
+  if(autopilot.in_flight) {
+    float roll_motor_airspeed_compensation = eff_sched_p.hover_roll_roll_coef[1] * eff_sched_var.airspeed * eff_sched_var.cosr / eff_sched_var.Ixx;
+    roll_motor_p_eff_left += roll_motor_airspeed_compensation;
+  }
   Bound(roll_motor_p_eff_left, 0.00001, 1);
 
   float roll_motor_q_eff = (eff_sched_p.hover_roll_pitch_coef[0] * eff_sched_var.wing_rotation_rad + eff_sched_p.hover_roll_pitch_coef[1] * eff_sched_var.wing_rotation_rad * eff_sched_var.wing_rotation_rad * eff_sched_var.sinr) / eff_sched_var.Iyy;
