33ce07a31079c6233812424e47bdcc476d93ca22
Christophe De Wagter <dewagter@gmail.com>
2024-05-22 15:04:01 +0200

    use unit and alt_unit for conversions instead

--- sw/airborne/firmwares/rotorcraft/guidance/guidance_indi_hybrid.c
+++ sw/airborne/firmwares/rotorcraft/guidance/guidance_indi_hybrid.c
@@ -172,7 +172,7 @@ float descend_vspeed_fwd = GUIDANCE_INDI_DESCEND_SPEED_FWD;
 float inv_eff[4];
 
 // Max bank angle in radians
-float guidance_indi_max_bank = DegOfRad(GUIDANCE_H_MAX_BANK);
+float guidance_indi_max_bank = GUIDANCE_H_MAX_BANK;
 float guidance_indi_min_pitch = GUIDANCE_INDI_MIN_PITCH;
 
 /** state eulers in zxy order */
@@ -472,7 +472,7 @@ struct StabilizationSetpoint guidance_indi_run(struct FloatVect3 *accel_sp, floa
   guidance_euler_cmd.theta = pitch_filt.o[0] + euler_cmd.y;
 
   //Bound euler angles to prevent flipping
-  Bound(guidance_euler_cmd.phi, -RadOfDeg(guidance_indi_max_bank), RadOfDeg(guidance_indi_max_bank));
+  Bound(guidance_euler_cmd.phi, -guidance_indi_max_bank, guidance_indi_max_bank);
   Bound(guidance_euler_cmd.theta, RadOfDeg(guidance_indi_min_pitch), RadOfDeg(GUIDANCE_INDI_MAX_PITCH));
 
   // Use the current roll angle to determine the corresponding heading rate of change.
