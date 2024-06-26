722b56166afd74bed7324abcffd47c1078ed2e0c
Christophe De Wagter <dewagter@gmail.com>
2024-06-26 10:51:20 +0200

    cleanup PR

--- sw/airborne/modules/ctrl/eff_scheduling_rot_wing.c
+++ sw/airborne/modules/ctrl/eff_scheduling_rot_wing.c
@@ -154,8 +154,6 @@ float eff_sched_pusher_time = 0.002;
 
 float roll_eff_scaling = 1.f;
 float scale_roll_roll_coeff = 1.f;
-float fake_airspeed = 0.f;
-bool bool_fake_airspeed = false;
 
 struct rot_wing_eff_sched_var_t eff_sched_var;
 
@@ -281,11 +279,6 @@ void eff_scheduling_rot_wing_update_cmd(void)
 
 void eff_scheduling_rot_wing_update_airspeed(void)
 {
-
-  if (bool_fake_airspeed) {
-    stateSetAirspeed_f(fake_airspeed);
-  }
-
   eff_sched_var.airspeed = stateGetAirspeed_f();
   Bound(eff_sched_var.airspeed, 0. , 30.);
   eff_sched_var.airspeed2 = eff_sched_var.airspeed * eff_sched_var.airspeed;
@@ -315,7 +308,6 @@ void eff_scheduling_rot_wing_update_hover_motor_effectiveness(void)
   Bound(dM_dpprz_left,  eff_sched_var.roll_motor_dMdpprz * 0.5, eff_sched_var.roll_motor_dMdpprz * 2.0);
 
   float roll_motor_p_eff_right = -(dM_dpprz_right * eff_sched_var.cosr + scale_roll_roll_coeff * eff_sched_p.hover_roll_roll_coef[0] * eff_sched_var.wing_rotation_rad * eff_sched_var.wing_rotation_rad * eff_sched_var.airspeed * eff_sched_var.cosr) / eff_sched_var.Ixx;
-  // Bound(roll_motor_p_eff_right, -1, -0.00001);
 
   // Set the roll effectiveness to zero when below a certain threshold
   if (roll_motor_p_eff_right >= -ROLL_EFFECTIVENESS_CUTOFF) {
@@ -323,13 +315,11 @@ void eff_scheduling_rot_wing_update_hover_motor_effectiveness(void)
   } 
 
   float roll_motor_p_eff_left = (dM_dpprz_left * eff_sched_var.cosr + scale_roll_roll_coeff * eff_sched_p.hover_roll_roll_coef[0] * eff_sched_var.wing_rotation_rad * eff_sched_var.wing_rotation_rad * eff_sched_var.airspeed * eff_sched_var.cosr) / eff_sched_var.Ixx;
-  
+
   if(autopilot.in_flight) {
     float roll_motor_airspeed_compensation = eff_sched_p.hover_roll_roll_coef[1] * eff_sched_var.airspeed * eff_sched_var.cosr / eff_sched_var.Ixx;
     roll_motor_p_eff_left += roll_motor_airspeed_compensation;
   }
-  // Bound(roll_motor_p_eff_left, 0.00001, 1);
-
   if (roll_motor_p_eff_left <= ROLL_EFFECTIVENESS_CUTOFF) {
     roll_motor_p_eff_left = 0;
   }
