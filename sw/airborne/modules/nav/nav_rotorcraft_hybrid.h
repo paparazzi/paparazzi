92bcdcba20218abd6ad5f15d3c1f7c6f75fa5f07
tomaso_de_ponti <tomasodp@gmail.com>
2024-07-12 16:01:24 +0200

    first push

--- sw/airborne/modules/nav/nav_rotorcraft_hybrid.h
+++ sw/airborne/modules/nav/nav_rotorcraft_hybrid.h
@@ -35,13 +35,14 @@
 extern float nav_max_speed;   // max speed in route mode
 extern float nav_goto_max_speed; // max speed in goto/stay mode
 extern float nav_max_deceleration_sp;
+extern float nav_max_acceleration_sp;
 extern float nav_hybrid_line_gain; 
 extern float nav_hybrid_pos_gain; 
 extern float nav_hybrid_max_bank;
+extern float nav_hybrid_max_expected_wind;
 #ifndef GUIDANCE_INDI_HYBRID
 extern bool force_forward;
 #endif
-
 
 extern void nav_rotorcraft_hybrid_init(void);
 
