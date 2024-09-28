4601b2010e6da41e40e3d48ecbb4acadf2f9ac6e
Christophe De Wagter <dewagter@gmail.com>
2024-09-28 13:27:10 +0200

    RunOnceEvery instead of counter

--- sw/airborne/modules/actuators/actuators_uavcan.c
+++ sw/airborne/modules/actuators/actuators_uavcan.c
@@ -454,14 +454,9 @@ void actuators_uavcan_commit(struct uavcan_iface_t *iface, int16_t *values, uint
     offset += 14;
   }
 
-  iface->cnt_act_raw++;
-  if(iface->cnt_act_raw >= ACTUATORS_UAVCAN_RAW_DIV) {
-    iface->cnt_act_raw = 0;
-
-    // Broadcast the raw command message on the interface
-    uavcan_broadcast(iface, UAVCAN_EQUIPMENT_ESC_RAWCOMMAND_SIGNATURE, UAVCAN_EQUIPMENT_ESC_RAWCOMMAND_ID,
-                    CANARD_TRANSFER_PRIORITY_HIGH, buffer, (offset + 7) / 8);
-  }
+  // Broadcast the raw command message on the interface
+  RunOnceEvery(ACTUATORS_UAVCAN_RAW_DIV, uavcan_broadcast(iface, UAVCAN_EQUIPMENT_ESC_RAWCOMMAND_SIGNATURE, UAVCAN_EQUIPMENT_ESC_RAWCOMMAND_ID,
+                    CANARD_TRANSFER_PRIORITY_HIGH, buffer, (offset + 7) / 8));
 }
 
 /**
@@ -493,12 +488,7 @@ void actuators_uavcan_cmd_commit(struct uavcan_iface_t *iface, int16_t *values, 
     offset += 16;
   }
 
-  iface->cnt_act_cmd++;
-  if(iface->cnt_act_cmd >= ACTUATORS_UAVCAN_CMD_DIV) {
-    iface->cnt_act_cmd = 0;
-
-    // Broadcast the raw command message on the interface
-    uavcan_broadcast(iface, UAVCAN_EQUIPMENT_ACTUATOR_ARRAYCOMMAND_SIGNATURE, UAVCAN_EQUIPMENT_ACTUATOR_ARRAYCOMMAND_ID,
-                    CANARD_TRANSFER_PRIORITY_HIGH, buffer, (offset + 7) / 8);
-  }
+  // Broadcast the raw command message on the interface
+  RunOnceEvery(ACTUATORS_UAVCAN_CMD_DIV, uavcan_broadcast(iface, UAVCAN_EQUIPMENT_ACTUATOR_ARRAYCOMMAND_SIGNATURE, UAVCAN_EQUIPMENT_ACTUATOR_ARRAYCOMMAND_ID,
+                    CANARD_TRANSFER_PRIORITY_HIGH, buffer, (offset + 7) / 8));
 }
