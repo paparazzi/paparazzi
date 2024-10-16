d5d845f490a2cbe663f99e1e2b8dab008b062177
Florian Sansou <florian.sansou@enac.fr>
2024-10-16 09:59:36 +0200

    Command laws

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -47,6 +47,7 @@ extensions = [
 myst_enable_extensions = [
   "colon_fence",
 ]
+myst_heading_anchors = 3
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
