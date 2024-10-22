88e8c2de1a92ae6f772c419ed22119c91a51746d
Mael FEURGARD <mael.feurgard@laas.fr>
2024-10-22 09:26:09 +0200

    Gaia fix: Wind dial has North up

--- sw/simulator/gaia_python/AngleSelector.py
+++ sw/simulator/gaia_python/AngleSelector.py
@@ -3,29 +3,45 @@
 import typing
 
 from PyQt5 import QtCore, QtGui
-from PyQt5.QtWidgets import QWidget,QMainWindow,QApplication,\
-                            QLabel
+from PyQt5.QtWidgets import QWidget,QMainWindow,QApplication
 
 from gaia_python.generated.AngleSelector import Ui_Form
-from gaia_python.betterAbstractSlider import BetterAbstractSlider
 
-
-class AngleSelector(QWidget,Ui_Form):
+class AngleSelector(QWidget):
     def __init__(self, parent: QWidget | None = ..., flags: QtCore.Qt.WindowFlags | QtCore.Qt.WindowType = QtCore.Qt.WindowType.Widget) -> None:
         super().__init__(parent, flags)
-        self.setupUi(self)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
         
-        self.valueChanged = self.dial.valueChanged
+        self.ui.dial.valueChanged.connect(lambda v : self.ui.spinBox.setValue((v-180)%360))
+        self.ui.spinBox.valueChanged.connect(lambda v : self.ui.dial.setValue((v-180)%360))
+        
+        self.valueChanged = self.ui.dial.valueChanged
+        self.ui.dial.setStyleSheet(
+        """
+QSlider::add-page{
+    border: 1px solid $border;
+    background:$background;
+}
+QSlider::sub-page{                           
+    border: 1px solid $border;
+    background:$background;
+}
+        """
+        )
+        
+        
+        self.setValue(0)
         
     def setText(self,s:str):
-        self.label.setText(s)
+        self.ui.label.setText(s)
         
     def setValue(self,v:int):
-        self.dial.setValue(v)
-        self.spinBox.setValue(v)
+        self.ui.dial.setValue((180+v)%360)
+        self.ui.spinBox.setValue(v)
         
     def value(self) -> int:
-        return self.dial.value()
+        return self.ui.spinBox.value()
     
         
         
