7d03cfcb425ea19b0e6511cc4f5d2c8a9f68fee7
Fabien-B <Fabien-B@github.com>
2025-01-23 17:39:29 +0100

    [threads] Move linux arch files and create symbolic link for sim arch.

--- sw/airborne/arch/sim/threads_arch.h
+++ sw/airborne/arch/sim/threads_arch.h
@@ -1,18 +1,1 @@
-#pragma once
-
-#include <pthread.h>
-#include <semaphore.h>
-
-#define PPRZ_NORMAL_PRIO 128
-
-struct pprzMutex {
-  pthread_mutex_t mtx;
-};
-
-struct pprzBSem {
-  pthread_mutex_t mtx;
-  sem_t sem;
-};
-
-
-typedef pthread_t pprz_thread_t;
+../linux/threads_arch.h
