fda93554d6b28078f31894b2b0121ead60ba5759
Fabien-B <fabien.bonneval@gmail.com>
2025-02-14 09:09:11 +0100

    CAN abstraction (#3430)
    ---------
    
    Co-authored-by: Fabien-B <Fabien-B@github.com>

--- sw/airborne/mcu_periph/can.c
+++ sw/airborne/mcu_periph/can.c
@@ -25,24 +25,68 @@
 #include "mcu_periph/can.h"
 #include "mcu_periph/can_arch.h"
 
-can_rx_callback_t can_rx_callback;
+#if USE_CAN1
 
-void _can_run_rx_callback(uint32_t id, uint8_t *buf, uint8_t len);
+struct can_periph can1 = {
+  .fd = 1,
+  .callbacks = {0},
+  .callback_user_data = {0}
+};
+#endif
 
-void ppz_can_init(can_rx_callback_t callback)
+static const uint8_t dlc_to_len[] = {0,1,2,3,4,5,6,7,8,12,16,20,24,32,48,64};
+
+uint8_t can_dlc_to_len(uint8_t dlc) {
+  if(dlc < 16) {
+    return dlc_to_len[dlc];
+  }
+  return 0;
+}
+
+uint8_t can_len_to_dlc(uint8_t len) {
+  if(len <= 8) {
+    return len;
+  }
+  for(int i=9; i<16; i++) {
+    if(dlc_to_len[i] >= len) {
+      return i;
+    }
+  }
+  return 15;
+}
+
+
+void can_init()
 {
-  can_rx_callback = callback;
   can_hw_init();
 }
 
-int ppz_can_transmit(uint32_t id, const uint8_t *buf, uint8_t len)
-{
-  return can_hw_transmit(id, buf, len);
+
+static int add_can_callback(struct can_periph* canp, can_rx_frame_callback_t callback, void* user_data) {
+  for(int i =0; i<CAN_NB_CALLBACKS_MAX; i++) {
+    // use the first available slot
+    if(canp->callbacks[i] == NULL) {
+      canp->callbacks[i] = callback;
+      canp->callback_user_data[i] = user_data;
+      return 0;
+    }
+  }
+  // no available slot
+  return -1;
 }
 
-void _can_run_rx_callback(uint32_t id, uint8_t *buf, uint8_t len)
-{
-  if (can_rx_callback) {
-    can_rx_callback(id, buf, len);
+int can_register_callback(can_rx_frame_callback_t callback, struct pprzaddr_can* src_addr, void* user_data) {
+  #if USE_CAN1
+  if(src_addr->can_ifindex == 1 || src_addr->can_ifindex == 0) {
+    int ret = add_can_callback(&can1, callback, user_data);
+    if(ret) { return ret; }
   }
+  #endif
+  #if USE_CAN2
+  if(src_addr->can_ifindex == 2 || src_addr->can_ifindex == 0) {
+    int ret = add_can_callback(&can2, callback, user_data);
+    if(ret) { return ret; }
+  }
+  #endif
+  return 0;
 }
--- sw/airborne/mcu_periph/can.h
+++ sw/airborne/mcu_periph/can.h
@@ -23,9 +23,97 @@
 #ifndef CAN_H
 #define CAN_H
 
-typedef void(* can_rx_callback_t)(uint32_t id, uint8_t *buf, uint8_t len);
+#include "std.h"
+#include "mcu_periph/can_arch.h"
 
-void ppz_can_init(can_rx_callback_t callback);
-int ppz_can_transmit(uint32_t id, const uint8_t *buf, uint8_t len);
+#ifndef CAN_FD_MODE
+#define CAN_FD_MODE TRUE
+#endif
 
+#ifdef CAN_FD_MODE
+#define SOCKETCAN_MAX_DLEN 64U
+#else
+#define SOCKETCAN_MAX_DLEN 8U
+#endif
+
+#ifndef CAN_NB_CALLBACKS_MAX
+#define CAN_NB_CALLBACKS_MAX 10
+#endif
+
+// CAN identifier
+// +------+--------------------------------------------------------------+
+// | Bits | Description                                                  |
+// +======+==============================================================+
+// | 0-28 | CAN identifier (11/29 bit)                                   |
+// +------+--------------------------------------------------------------+
+// |  29  | Error message frame flag (0 = data frame, 1 = error message) |
+// +------+--------------------------------------------------------------+
+// |  30  | Remote transmission request flag (1 = RTR frame)             |
+// +------+--------------------------------------------------------------+
+// |  31  | Frame format flag (0 = standard 11 bit, 1 = extended 29 bit) |
+// +------+--------------------------------------------------------------+
+typedef uint32_t socketcan_id_t;
+
+// error flag
+#define 	CAN_FRAME_ERR   (1<<29)
+// remote transmition request
+#define 	CAN_FRAME_RTR   (1<<30)
+// extended identifier
+#define 	CAN_FRAME_EFF   (1<<31)
+
+#define CAN_EID_MASK 0x1FFFFFFF
+#define CAN_SID_MASK 0x7FF
+
+// /* CAN FD specific flags from Linux Kernel (include/uapi/linux/can.h) */
+#define CANFD_BRS 0x01 
+#define CANFD_ESI 0x02 
+#define CANFD_FDF 0x04 
+
+struct pprzcan_frame {
+  socketcan_id_t can_id;
+  uint8_t len;
+  uint8_t flags;  // CAN FD specific flags
+  uint32_t timestamp;   // timestamp in ms.
+  uint8_t data[SOCKETCAN_MAX_DLEN];
+};
+
+// socketaddr_can paparazzi abstraction
+struct pprzaddr_can {
+        //sa_family_t can_family;   
+        int         can_ifindex;  // network interface index
+};
+
+typedef void(* can_rx_frame_callback_t)(struct pprzcan_frame* rxframe, struct pprzaddr_can* src_addr, void* user_data);
+
+
+struct can_periph {
+  void* arch_struct;
+  int fd;
+  can_rx_frame_callback_t callbacks[CAN_NB_CALLBACKS_MAX];
+  void* callback_user_data[CAN_NB_CALLBACKS_MAX];
+};
+
+#if USE_CAN1
+extern struct can_periph can1;
+#endif
+#if USE_CAN2
+extern struct can_periph can2;
+#endif
+
+void can_init(void);
+
+/**
+ * Add a callback on received frames from an interface
+ * @param callback The callback called on received frames
+ * @param src_addr Interface from which frames are received. 0 means all interfaces.
+ * @param user_data Pointer that will be passed in callback parameters
+ * @return 0 if the callback was successfully added.
+ */
+int can_register_callback(can_rx_frame_callback_t callback, struct pprzaddr_can* src_addr, void* user_data);
+
+int can_transmit_frame(struct pprzcan_frame* txframe, struct pprzaddr_can* dst_addr);
+
+
+uint8_t can_dlc_to_len(uint8_t dlc);
+uint8_t can_len_to_dlc(uint8_t len);
 #endif /* CAN_H */
