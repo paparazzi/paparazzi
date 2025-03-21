e0a130d4e7a8ad7718d9ff06bfa92518dceaa661
Fabien-B <Fabien-B@github.com>
2025-03-21 15:25:33 +0100

    [Settings] Allows to set setting by name.

--- sw/ground_segment/python/guided_mode_example.py
+++ sw/ground_segment/python/guided_mode_example.py
@@ -32,21 +32,26 @@ def main():
         settings = PprzSettingsManager(conf.settings, conf.id, connect.ivy)
         guided = GuidedMode(connect.ivy)
         
-        settings['auto2'] = 19 # assuming GUIDED mode in static autopilot
+        settings['auto2'] = 'Guided'
 
         sleep(0.1)
+        print("Goto NED 2, 2, -3, 90°")
         guided.goto_ned(conf.id, north=2.0, east=2.0, down=-3.0, heading=radians(90))
         sleep(5)
+        print("Goto NED relative  -2, -2, 1, -45°")
         guided.goto_ned_relative(conf.id, north=-2.0, east=-2.0, down=1.0, yaw=-radians(45))
         sleep(5)
+        print("Goto body 0, 1, 0")
         guided.goto_body_relative(conf.id, forward=0.0, right=1.0, down=0.0)
         sleep(5)
+        print("Move NED 0.5m/s north")
         guided.move_at_ned_vel(conf.id, north=0.5)
         sleep(5)
+        print("Move body -0.5m/s forward")
         guided.move_at_body_vel(conf.id, forward=-0.5)
         sleep(2)
 
-        settings['auto2'] = 13 # assuming NAV mode in static autopilot
+        settings['auto2'] = 'Nav'
 
     except KeyboardInterrupt:
         print("Stopping on request")
