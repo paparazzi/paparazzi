7896808099652d9b3aebac49c44e92377b3f6625
OpenUAS <OpenUAS@users.noreply.github.com>
2026-07-16 20:18:24 +0200

    Driver added for Barometer DPS310

--- sw/airborne/modules/core/abi_sender_ids.h
+++ sw/airborne/modules/core/abi_sender_ids.h
@@ -76,6 +76,10 @@
 #define BARO_BMP3_SENDER_ID 20
 #endif
 
+#ifndef BARO_DPS310_SENDER_ID
+#define BARO_DPS310_SENDER_ID 21
+#endif
+
 #ifndef METEO_STICK_SENDER_ID
 #define METEO_STICK_SENDER_ID 30
 #endif
